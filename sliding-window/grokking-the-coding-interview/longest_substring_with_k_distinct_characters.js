17e60cf7dcbbc81bfbde2dd9c1f88c327722e34f
Red <crabmarket56@gmail.com>
2022-10-16 23:23:07 +0900

    Initialize file

---
+++ sliding-window/grokking-the-coding-interview/longest_substring_with_k_distinct_characters.js
@@ -0,0 +1,5 @@
+// Given a string, find the length of the longest substring in it with no more than K distinct characters
+
+function find_largest_substring_with_K_distinct_characters(K, string) {
+
+}
--- sliding-window/grokking-the-coding-interview/smallest_subarray_with_a_given_sum.js
+++ sliding-window/grokking-the-coding-interview/smallest_subarray_with_a_given_sum.js
@@ -5,7 +5,7 @@ function find_smallest_subarray_with_given_sum(S, arr) {
   let windowStart = 0
   let minLength = Math.pow(10, 1000)
   for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-    windowSum += arr[windowEnd];
+    windowSum += arr[windowEnd]
     while (windowSum >= S) {
       minLength = math.Min(windowEnd - windowStart + 1, minLength)
       windowSum -= arr[windowStart]
