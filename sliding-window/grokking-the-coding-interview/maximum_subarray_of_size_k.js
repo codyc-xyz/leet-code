10d4cb84d32c2a80a6a655cbc928b25b74e81fde
Red <crabmarket56@gmail.com>
2022-10-16 18:53:41 +0900

    Maximum subarray of size k solution

--- sliding-window/grokking-the-coding-interview/maximum_subarray_of_size_k.js
+++ sliding-window/grokking-the-coding-interview/maximum_subarray_of_size_k.js
@@ -1,5 +1,19 @@
 // Given an array of positive numbers and a positive number 'k', find the maximum sum of any contiguous subarray of size 'k'
-function find_max_sum_subarray(K, arr) {
 
-  
+function find_max_sum_subarray(K, arr) {
+  let maxSum = 0
+  let windowSum = 0
+  let windowStart = 0
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+
+    if (windowEnd >= K - 1) {
+      maxSum = math.Max(windowSum, maxSum)
+      windowSum -= arr[windowStart]
+      windowStart += 1
+    }
+  }
+  return maxSum
 }
+
+find_max_sum_subarray(3, [2, 1, 5, 1, 3, 2])
