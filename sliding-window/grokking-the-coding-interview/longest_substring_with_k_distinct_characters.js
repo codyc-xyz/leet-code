9f024865711de470f65d7a7067fa542fcd85ba5e
Red <crabmarket56@gmail.com>
2022-10-17 12:01:14 +0900

    Longest substring with k distinct characters solution

--- sliding-window/grokking-the-coding-interview/longest_substring_with_k_distinct_characters.js
+++ sliding-window/grokking-the-coding-interview/longest_substring_with_k_distinct_characters.js
@@ -1,5 +1,17 @@
 // Given a string, find the length of the longest substring in it with no more than K distinct characters
 
 function find_largest_substring_with_K_distinct_characters(K, string) {
-
+  let arr = string.split('')
+  let substring = []
+  let largestSubstring = []
+  let windowStart = 0
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    while (substring.uniq <= K) {
+      substring += arr[windowEnd]
+    }
+      largestLength = math.Max(largestSubstring.length, substring.length)
+      substring -= arr[windowStart]
+      windowStart += 1
+  }
+  return largestLength
 }
