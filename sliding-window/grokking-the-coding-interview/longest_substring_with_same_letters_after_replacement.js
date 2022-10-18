e25d1d009e24fe8478a29cd36dc0c2e4d59e2bdc
Red <crabmarket56@gmail.com>
2022-10-18 16:21:06 +0900

    Initialize file

---
+++ sliding-window/grokking-the-coding-interview/longest_substring_with_same_letters_after_replacement.js
@@ -0,0 +1,5 @@
+// Given a string with lowercase letters only, if you are allowed to replace no more than 'k' letters with any letter, find the length of the longest substring having the same letters after replacement
+
+function longest_substring_with_same_letters_after_replacement(string) {
+  
+}
