ad034f38f7a8a0f71469cc5f6040c55ad70e9cd3
Red <crabmarket56@gmail.com>
2022-10-18 16:10:19 +0900

    No repeat substring solution

--- sliding-window/grokking-the-coding-interview/no_repeat_substring.js
+++ sliding-window/grokking-the-coding-interview/no_repeat_substring.js
@@ -1,5 +1,18 @@
 // Given a string, find the length of the longest substring which has no repeating characters.
 
 function no_repeat_substring(string) {
-  
+  let arr = string.split('')
+  let windowStart = 0
+  let substring = []
+  let longestSubstring = 0
+  for(let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    while (substring[windowEnd] !== substring[windowEnd - 1]) {
+      substring += arr[windowEnd]
+      windowEnd += 1
+    }
+    longestSubstring = math.Max(longestSubstring, substring.length)
+    substring -= arr[windowStart]
+    windowStart += 1
+  }
+  return longestSubstring
 }
