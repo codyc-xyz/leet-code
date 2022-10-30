5828092779eb05e905e99d16f8f619f5e8834956
Red <crabmarket56@gmail.com>
2022-10-30 20:48:23 +0900

    Submit solution

--- sliding-window/problems/1763_longest_nice_substring.py
+++ sliding-window/problems/1763_longest_nice_substring.py
@@ -5,6 +5,36 @@
 # If there are multiple, return the substring of the earliest occurrence. If there are none, return an empty string.
 
 class Solution:
+     def isNice(self, sub):
+        uniq = set(sub)
+        for c in uniq:
+            if c.lower() not in uniq or c.upper() not in uniq:
+                return False
+        return True
+        
     def longestNiceSubstring(self, s: str) -> str:
+        longestSubstring = ""
+        windowStart, windowEnd = 0, 1
+        for n in range(len(s)):
+            while windowEnd <= len(s):
+                sub = s[windowStart:windowEnd]
+                if self.isNice(sub) and len(sub) > len(longestSubstring):
+                    longestSubstring = sub
+                windowEnd += 1
+            windowStart += 1
+            windowEnd = windowStart + 1
+        return longestSubstring
+                    
+   
+          
 
+
+     
+      
+
+        
+
+     
+
+        
 
