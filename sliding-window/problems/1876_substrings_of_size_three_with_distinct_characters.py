659b94a9fa1b0cc0e04f1b1976360a06643031c9
Red <crabmarket56@gmail.com>
2022-10-30 10:41:32 +0900

    Submit solution

--- sliding-window/problems/1876_substrings_of_size_three_with_distinct_characters.py
+++ sliding-window/problems/1876_substrings_of_size_three_with_distinct_characters.py
@@ -3,3 +3,17 @@
 # Given a string 's', return the number of good substrings of length three in s
 # Note that if there are multiple occurrences of the same substring, every occurrence should be counted
 # A substring is a contiguous sequence of characters in a string
+def countGoodSubstrings(self, s: str) -> int:
+        hm = {}
+        count = 0
+        windowStart = 0
+        for windowEnd, c in enumerate(s):
+            while c in hm.values():
+                hm.pop(windowStart)
+                windowStart += 1
+            if len(hm) + 1 == 3:
+                count += 1
+                hm.pop(windowStart)
+                windowStart += 1
+            hm[windowEnd] = c
+        return count
