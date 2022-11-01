7d87da5ac344c68cdac090a180ddd5309c8c9552
Red <crabmarket56@gmail.com>
2022-11-01 17:48:18 +0900

    Add initial solution

--- sliding-window/problems/1004_max_consecutive_ones_III.py
+++ sliding-window/problems/1004_max_consecutive_ones_III.py
@@ -1,5 +1,17 @@
 # Given a binary array nums and an integer k, 
 # return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
-
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        windowStart = 0
+        for windowEnd in range(len(nums)):
+            if nums[windowEnd] == 0:
+                k -= 1
+            if k < 0:
+                if nums[windowStart] == 0:
+                    k += 1
+                windowStart += 1
+        return windowEnd - windowStart + 1
+        
+            
         
