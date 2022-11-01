09676da94bea557ac33c09b6b1e90594a1219907
Red <crabmarket56@gmail.com>
2022-11-01 16:05:44 +0900

    Add initial solution

--- sliding-window/problems/1343_number_of_subarrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py
+++ sliding-window/problems/1343_number_of_subarrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py
@@ -1,3 +1,14 @@
 # Given an array of integers arr and two integers k and threshold, 
 # return the number of sub-arrays of size k and average greater than or equal to threshold.
 
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        count = 0
+        for n in range(len(arr)):
+            for i in range(len(arr) - k + 1):
+                sub = arr[n:i+k]
+                if len(sub) != k:
+                    continue
+                if (sum(sub) / k) >= threshold:
+                    count += 1
+        return count
