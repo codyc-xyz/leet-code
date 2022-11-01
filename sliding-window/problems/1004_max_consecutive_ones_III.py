f6d8de90d033bfc37f197895def0d91faaeb3485
Red <crabmarket56@gmail.com>
2022-11-01 21:33:46 +0900

    Refactor for clarity

--- sliding-window/problems/1004_max_consecutive_ones_III.py
+++ sliding-window/problems/1004_max_consecutive_ones_III.py
@@ -4,6 +4,7 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         windowStart = 0
+        maxLen = 0
         for windowEnd in range(len(nums)):
             if nums[windowEnd] == 0:
                 k -= 1
@@ -11,7 +12,7 @@ class Solution:
                 if nums[windowStart] == 0:
                     k += 1
                 windowStart += 1
-        return windowEnd - windowStart + 1
-        
-            
+            maxLen = max(maxLen, windowEnd - windowStart + 1)
+                
+        return maxLen
         
