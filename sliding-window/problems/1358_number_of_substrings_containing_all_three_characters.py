83fa09370426a88a08e82b992c8c666bb32eb305
Red <crabmarket56@gmail.com>
2022-11-02 11:52:44 +0900

    Initial solution

--- sliding-window/problems/1358_number_of_substrings_containing_all_three_characters.py
+++ sliding-window/problems/1358_number_of_substrings_containing_all_three_characters.py
@@ -1,4 +1,20 @@
 # Given a string s consisting only of characters a, b and c.
 # Return the number of substrings containing at least one occurrence of all these characters a, b and c.
 
+class Solution:
+    def unique(self, sub):
+        if 'a' not in sub or 'b' not in sub or 'c' not in sub:
+            return False
+        return True
 
+    def numberOfSubstrings(self, s: str) -> int:
+        windowStart = count = 0
+        for c in range(len(s)):
+            windowEnd = windowStart + 2
+            while windowEnd <= len(s):
+                sub = s[windowStart:windowEnd]
+                if self.unique(sub):
+                    count += 1
+                windowEnd += 1
+            windowStart += 1
+        return count
