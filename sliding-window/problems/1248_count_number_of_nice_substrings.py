7f6d9e61a68f6a01dd48eef2a9c69eef8269f63d
Red <crabmarket56@gmail.com>
2022-11-02 21:16:02 +0900

    Fix solution by counting from front of list

--- sliding-window/problems/1248_count_number_of_nice_substrings.py
+++ sliding-window/problems/1248_count_number_of_nice_substrings.py
@@ -4,18 +4,19 @@
 
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        count = 0
-        for i in range(len(nums)):
-            windowStart = 0
-            k1 = k
-            for windowEnd in range(len(nums) - i):
-                if nums[windowEnd + i] % 2 != 0:
-                    k1 -= 1
-                if k1 == 0:
-                    count += 1
-                if 0 > k1:
-                    if nums[windowStart + i] % 2 != 0:
-                        k1 += 1
-                    windowStart += 1
-        return count
+        count = res = 0
+        windowStart = 0
+        
+        for windowEnd in range(len(nums)):
+            if nums[windowEnd] % 2 != 0:
+                k -= 1
+                count = 0
+            while k == 0:
+                count += 1
+                if nums[windowStart] % 2 != 0:
+                    k += 1
+                windowStart += 1
+            res += count
+        
+        return res
 
