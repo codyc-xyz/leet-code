9e2264d6158bdf8d5d2c5c08b0a21617777d171d
Red <crabmarket56@gmail.com>
2022-11-02 13:02:52 +0900

    Initial solution

--- sliding-window/problems/1493_longest_subarray_of_1s_after_deleting_one_element.py
+++ sliding-window/problems/1493_longest_subarray_of_1s_after_deleting_one_element.py
@@ -2,3 +2,17 @@
 # Return the size of the longest non-empty subarray containing only 1's in the resulting array. 
 # Return 0 if there is no such subarray.
 
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        res, k = 0, 1
+        maxLen = windowStart = 0
+        for windowEnd in range(len(nums)):
+            if nums[windowEnd] == 0:
+                k -= 1
+            if k < 0:
+                if nums[windowStart] == 0:
+                    k += 1
+                windowStart += 1
+            maxLen = max(maxLen, windowEnd - windowStart)
+        return maxLen
