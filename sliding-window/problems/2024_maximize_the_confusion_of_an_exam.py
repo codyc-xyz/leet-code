14dbe0c506f92e5f61e2695f690bdb21c0689e46
Red <crabmarket56@gmail.com>
2022-11-02 14:37:00 +0900

    Initial solution

--- sliding-window/problems/2024_maximize_the_confusion_of_an_exam.py
+++ sliding-window/problems/2024_maximize_the_confusion_of_an_exam.py
@@ -5,5 +5,27 @@
 # Change the answer key for any question to 'T' or 'F' (i.e., set answerKey[i] to 'T' or 'F').
 # Return the maximum number of consecutive 'T's or 'F's in the answer key after performing the operation at most k times.
 
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        tWindowStart = fWindowStart = 0
+        tK = fK = k
+        tMax = fMax = 0
+        for tWindowEnd in range(len(answerKey)):
+            if answerKey[tWindowEnd] == 'F':
+                tK -= 1
+            if tK < 0:
+                if answerKey[tWindowStart] == 'F':
+                    tK += 1
+                tWindowStart += 1
+            tMax = max(tMax, tWindowEnd - tWindowStart + 1)
+        for fWindowEnd in range(len(answerKey)):
+            if answerKey[fWindowEnd] == 'T':
+                fK -= 1
+            if fK < 0:
+                if answerKey[fWindowStart] == 'T':
+                    fK += 1
+                fWindowStart += 1
+            fMax = max(fMax, fWindowEnd - fWindowStart + 1)
+        return max(tMax, fMax)
 
 
