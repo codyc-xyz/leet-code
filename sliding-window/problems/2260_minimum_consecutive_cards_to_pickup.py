0b8c27e30182535b1d4e0a1929bdda64beb61581
Red <crabmarket56@gmail.com>
2022-11-04 12:58:07 +0900

    Add initial solution

--- sliding-window/problems/2260_minimum_consecutive_cards_to_pickup.py
+++ sliding-window/problems/2260_minimum_consecutive_cards_to_pickup.py
@@ -4,5 +4,16 @@
 # Return the minimum number of consecutive cards you have to pick up to have a pair of matching cards among the picked cards. 
 # If it is impossible to have matching cards, return -1.
 
-
-
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        hm = {}
+        diff, minDiff = 0, float("inf")
+        for i, c in enumerate(cards):
+            if c in hm:
+                diff = i - hm[c] + 1
+                minDiff = min(minDiff, diff)
+            hm[c] = i
+        if diff != 0:
+             return minDiff 
+        else:
+            return -1
