02d7aef397a66cd5b08c2c346e7cf9b631841335
Red <crabmarket56@gmail.com>
2022-11-04 15:49:17 +0900

    Refactor solution

--- sliding-window/problems/1984_minimum_difference_between_highest_and_lowest_k_scores.py
+++ sliding-window/problems/1984_minimum_difference_between_highest_and_lowest_k_scores.py
@@ -13,7 +13,15 @@ class Solution:
             windowEnd += 1
             windowStart += 1
         return minDiff
-                
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        ordered = sorted(nums)
+        minRes = float("inf")
+        k -= 1
+        for i in range(len(nums) - k):
+            minRes = min(minRes, ordered[i+k] - ordered[i])
+        return minRes
 
 
 
