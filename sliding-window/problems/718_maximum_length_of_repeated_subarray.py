a66fcba9c193a1abafa1a40d15f80964f937198c
Red <crabmarket56@gmail.com>
2022-11-04 15:24:57 +0900

    Add initial solution

--- sliding-window/problems/718_maximum_length_of_repeated_subarray.py
+++ sliding-window/problems/718_maximum_length_of_repeated_subarray.py
@@ -1,2 +1,14 @@
 # Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
 
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        maxStr = ""
+        res = 0
+        nums2Str = "".join([chr(num) for num in nums2])
+        for n in nums1:
+          maxStr += chr(n)
+          if maxStr in nums2Str:
+            res = max(res, len(maxStr))
+          else:
+            maxStr = maxStr[1:]
+        return res
