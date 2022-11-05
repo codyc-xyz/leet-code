c7e3194cc3cfe019d78db837a1860b7003a38535
Red <crabmarket56@gmail.com>
2022-11-05 17:37:00 +0900

    Add initial solution

--- sliding-window/problems/930_binary_subarrays_with_sum.py
+++ sliding-window/problems/930_binary_subarrays_with_sum.py
@@ -2,3 +2,21 @@
 
 # A subarray is a contiguous part of the array.
 
+class Solution:
+     
+    def atmost(self, nums, goal):
+        res = count = windowStart = 0
+        for windowEnd in range(len(nums)):
+            if goal < 0:
+                return 0
+            goal -= nums[windowEnd]
+            while goal < 0:
+                goal += nums[windowStart]
+                windowStart += 1
+            res += windowEnd - windowStart + 1
+        return res
+    
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+         
+        return self.atmost(nums, goal) - self.atmost(nums, goal - 1)
+     
