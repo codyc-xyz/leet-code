218a7e7919da5d353577c2f7146180081ebefb98
Red <crabmarket56@gmail.com>
2022-11-06 10:06:42 +0900

    Add initial solution

--- sliding-window/problems/2134_minimum_swaps_to_group_all_1s_together_II.py
+++ sliding-window/problems/2134_minimum_swaps_to_group_all_1s_together_II.py
@@ -4,3 +4,25 @@
 
 # Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
 
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        total = sum(nums)
+        length = len(nums[:total])
+        window = sum(nums[:total])
+        res = total - window
+        minSwaps = float("inf")
+        windowStart, windowEnd = 0, len(nums) - total
+        if res == 0:
+                return 0
+        for i in range(length, len(nums) + length):
+            if i >= len(nums) and windowEnd <= len(nums):
+                window += nums[windowStart] - nums[windowEnd]
+                windowStart += 1
+                windowEnd += 1
+            else:
+                window += nums[i] - nums[i - length]
+            minSwaps = min(minSwaps, total - window)
+        return minSwaps
+            
+            
+            
