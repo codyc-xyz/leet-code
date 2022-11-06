2fde04d456cde9450452764eb305276727240929
Red <crabmarket56@gmail.com>
2022-11-07 08:36:46 +0900

    Refactor solution with dequeus

--- sliding-window/problems/1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
+++ sliding-window/problems/1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
@@ -4,13 +4,24 @@
 
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        longestSub = 0
-        sub = []
+        longestSub = windowStart = 0
+        minQueue = deque()
+        maxQueue = deque()
         for windowEnd in range(len(nums)):
-            sub.append(nums[windowEnd])
-            if max(sub) - min(sub) <= limit:
-                longestSub = max(longestSub, len(sub))
-            while max(sub) - min(sub) > limit:
-                sub.pop(0)
+            while minQueue and nums[windowEnd] < minQueue[-1]:
+                minQueue.pop()
+            while maxQueue and nums[windowEnd] > maxQueue[-1]:
+                maxQueue.pop()
+            maxQueue.append(nums[windowEnd])
+            minQueue.append(nums[windowEnd])                
+            while maxQueue[0] - minQueue[0] > limit:
+                if maxQueue[0] == nums[windowStart]:
+                    maxQueue.popleft()
+                if minQueue[0] == nums[windowStart]:
+                    minQueue.popleft()
+                windowStart += 1
+            longestSub = max(longestSub, windowEnd - windowStart + 1)
         return longestSub
+    
+
     
