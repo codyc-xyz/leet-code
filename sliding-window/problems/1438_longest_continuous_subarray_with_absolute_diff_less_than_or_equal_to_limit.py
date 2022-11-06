91995d0551f3717fac39500637a26794b9b96555
Red <crabmarket56@gmail.com>
2022-11-07 08:20:18 +0900

    Add initial solution

--- sliding-window/problems/1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
+++ sliding-window/problems/1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
@@ -2,3 +2,15 @@
 # return the size of the longest non-empty subarray such that the absolute difference 
 # between any two elements of this subarray is less than or equal to limit.
 
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        longestSub = 0
+        sub = []
+        for windowEnd in range(len(nums)):
+            sub.append(nums[windowEnd])
+            if max(sub) - min(sub) <= limit:
+                longestSub = max(longestSub, len(sub))
+            while max(sub) - min(sub) > limit:
+                sub.pop(0)
+        return longestSub
+    
