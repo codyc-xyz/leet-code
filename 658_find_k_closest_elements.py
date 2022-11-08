ceaea2b136bba224a502192897935c3d6e186df8
Red <crabmarket56@gmail.com>
2022-11-08 16:03:03 +0900

    Add initial solution

---
+++ 658_find_k_closest_elements.py
@@ -0,0 +1,23 @@
+# Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. 
+# The result should also be sorted in ascending order.
+
+# An integer a is closer to x than an integer b if:
+# |a - x| < |b - x|, or
+# |a - x| == |b - x| and a < b
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        closest = deque()
+        for windowEnd in range(len(arr)):
+            if len(closest) < k:
+                closest.append(arr[windowEnd])
+            elif len(closest) >= k and abs(arr[windowEnd] - x) < abs(closest[0] - x):
+                closest.popleft()
+                closest.append(arr[windowEnd])
+            elif len(closest) >= k and (abs(arr[windowEnd] - x) == abs(closest[0] - x) and arr[windowEnd] < closest[0]):
+                closest.popleft()
+                closest.appen(arr[windowEnd])
+        return closest
+        
+
+
