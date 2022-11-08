b466c8ab0255f4439935ea3a1e363b9fb67e26cb
Red <crabmarket56@gmail.com>
2022-11-08 10:38:54 +0900

    Add initial solution

--- sliding-window/problems/1208_get_equal_substrings_within_budget.py
+++ sliding-window/problems/1208_get_equal_substrings_within_budget.py
@@ -6,5 +6,20 @@
 # Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. 
 # If there is no substring from s that can be changed to its corresponding substring from t, return 0.
 
-
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        longest = windowStart = 0
+        hm = {}
+        for iS, cS in enumerate(s):
+            hm[iS] = cS
+        
+        for iT, cT in enumerate(t):
+            maxCost -= abs(ord(t[iT]) - ord(hm[iT]))
+            
+            while maxCost < 0:
+                maxCost += abs(ord(t[windowStart]) - ord(hm[windowStart]))
+                windowStart += 1
+            longest = max(longest, iT - windowStart + 1)
+        return longest
+        
 
