56f9d3a32a24d24a07b0473b365ade9bbe2b9c2e
Red <crabmarket56@gmail.com>
2022-11-13 09:26:59 +0900

    Add initial solution

--- sliding-window/problems/209_minimum_size_subarray_sum.py
+++ sliding-window/problems/209_minimum_size_subarray_sum.py
@@ -2,4 +2,17 @@
 # return the minimal length of a subarray whose sum is greater than or equal to target. 
 # If there is no such subarray, return 0 instead.
 
-
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        minLength, windowStart = float("inf"), 0
+        for windowEnd in range(len(nums)):
+            target -= nums[windowEnd]
+            
+            while target <= 0:
+                minLength = min(minLength, windowEnd - windowStart + 1)
+                target += nums[windowStart]
+                windowStart += 1
+        if minLength != float("inf"):
+            return minLength
+        else:
+            return 0
