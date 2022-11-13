11f494b0619c513893b0be673c2c6a6778c5f334
Red <crabmarket56@gmail.com>
2022-11-13 16:51:33 +0900

    Add initial solution

--- sliding-window/problems/567_permutation_in_a_string.py
+++ sliding-window/problems/567_permutation_in_a_string.py
@@ -1,3 +1,21 @@
 # Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 # In other words, return true if one of s1's permutations is the substring of s2.
 
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        sort1 = sorted(s1)     
+        windowEnd = len(s1) 
+        sort2 = list(s2[:windowEnd])
+        
+        if sorted(sort2) == sort1:
+            return True
+        
+        while windowEnd < len(s2):
+            sort2.append(s2[windowEnd])
+            sort2.pop(0)
+            if sorted(sort2) == sort1:
+                return True
+            windowEnd += 1
+        
+
+            
