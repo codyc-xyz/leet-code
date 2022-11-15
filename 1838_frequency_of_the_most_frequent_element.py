8998ceddb796e00f879f74b8e32e2d82e724a9ca
Red <crabmarket56@gmail.com>
2022-11-15 21:55:55 +0900

    Add initial solution

--- 1838_frequency_of_the_most_frequent_element.py
+++ 1838_frequency_of_the_most_frequent_element.py
@@ -5,3 +5,20 @@
 
 # Return the maximum possible frequency of an element after performing at most k operations.
 
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        sortedNums = sorted(nums)
+        maxCount = 0
+        for i, n in enumerate(sortedNums):
+            diff = []
+            diffK = k
+            windowStart = 0
+            for c in range(len(sortedNums)):
+                diff.append(abs(sortedNums[i] - sortedNums[c]))
+            sortedDiff = sorted(diff)
+            while diffK >= 0 and windowStart < len(sortedDiff):
+                diffK -= sortedDiff[windowStart]
+                if diffK >= 0:
+                    windowStart += 1
+            maxCount = max(maxCount, windowStart)
+        return maxCount
