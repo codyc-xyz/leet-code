d72559dff95757bdbc9838ebc092c2af59bc3719
Red <crabmarket56@gmail.com>
2022-11-17 06:28:57 +0900

    Remove incorrect answer

--- sliding-window/problems/1477_find_two_non_overlapping_sub_arrays_each_with_target_sum.py
+++ sliding-window/problems/1477_find_two_non_overlapping_sub_arrays_each_with_target_sum.py
@@ -5,29 +5,4 @@
 
 # Return the minimum sum of the lengths of the two required sub-arrays, or return -1 if you cannot find such two sub-arrays.
 
-class Solution:
-    def minSumOfLengths(self, arr: List[int], target: int) -> int:
-        minLen = float("inf")
-        testTarget = target
-        diffs = []
-        windowStart = windowEnd = 0
-        repeat = False
-        while windowEnd < len(arr):
-            testTarget -= arr[windowEnd]
-            while windowStart <= windowEnd and testTarget < 0:
-                testTarget += arr[windowStart]
-                windowStart += 1
-            if testTarget == 0 and repeat == False:
-                diffs.append(windowEnd - windowStart + 1)
-                windowStart = windowEnd
-                testTarget = target
-                repeat = True
-            else:
-                repeat = False
-                windowEnd += 1
-        sortedDiffs = sorted(diffs)
-        if len(diffs) > 1:
-            return sortedDiffs[1] + sortedDiffs[0]
-        else:
-            return -1
 
