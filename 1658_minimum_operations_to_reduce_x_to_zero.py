a963156d73b299e1ada6d2a0a5db53cf06476342
Red <crabmarket56@gmail.com>
2022-11-16 15:48:31 +0900

    Add initial solution

--- 1658_minimum_operations_to_reduce_x_to_zero.py
+++ 1658_minimum_operations_to_reduce_x_to_zero.py
@@ -4,5 +4,24 @@
 
 # Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1.
 
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        windowStart = 0
+        target = sum(nums) - x
+        maxLen = 0
+        for windowEnd in range(len(nums)):
+            target -= nums[windowEnd]
+            while windowStart <= windowEnd and target < 0:
+                target += nums[windowStart]
+                windowStart += 1
+            if target == 0:
+                maxLen = max(maxLen, windowEnd - windowStart + 1)
+        if maxLen != 0:
+            return len(nums) - maxLen 
+        else:
+            return -1
+                
+            
+        
 
 
