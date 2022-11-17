3a66f04d721d274fae1140e3eddec75ff8763065
Red <crabmarket56@gmail.com>
2022-11-17 13:46:49 +0900

    Add initial solution

--- sliding-window/problems/1234_replace_the_substring_for_balanced_string.py
+++ sliding-window/problems/1234_replace_the_substring_for_balanced_string.py
@@ -5,3 +5,17 @@
 # Return the minimum length of the substring that can be replaced with any other string of the same length to make s balanced. 
 # If s is already balanced, return 0.
 
+class Solution:
+    def balancedString(self, s: str) -> int:
+        count = collections.Counter(s)
+        windowStart = 0
+        minLen = len(s)
+        for windowEnd in range(len(s)):
+            count[s[windowEnd]] -= 1
+            while windowStart < len(s) and all(len(s) / 4 >= count[c] for c in 'QWER'):
+                minLen = min(minLen, windowEnd - windowStart + 1)
+                count[s[windowStart]] += 1
+                windowStart += 1
+                
+        return minLen
+            
