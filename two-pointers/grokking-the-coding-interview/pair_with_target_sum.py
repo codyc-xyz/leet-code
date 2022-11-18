052549e1b846ed8d7736055d9b8981628c348f77
Red <crabmarket56@gmail.com>
2022-11-18 10:24:50 +0900

    Add initial solution

--- two-pointers/grokking-the-coding-interview/pair_with_target_sum.py
+++ two-pointers/grokking-the-coding-interview/pair_with_target_sum.py
@@ -2,3 +2,18 @@
 
 # Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 
+def pair_with_target_sum(arr, target_sum):
+  right, left = len(arr) - 1, 0
+  currSum = 0
+
+  while right > left:
+    currSum = arr[right] + arr[left]
+
+    if currSum = target_sum:
+      return [left, right]    
+    elif currSum > target_sum:
+      right -= 1
+    else:
+      left += 1
+
+  return [-1, -1] 
