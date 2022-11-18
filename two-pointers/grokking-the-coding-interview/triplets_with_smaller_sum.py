1b8008168fde7f4bb37beb4ddc88b7900deeba39
Red <crabmarket56@gmail.com>
2022-11-18 13:00:04 +0900

    Intiialize file

---
+++ two-pointers/grokking-the-coding-interview/triplets_with_smaller_sum.py
@@ -0,0 +1,4 @@
+# Given an array arr of unsorted numbers and a target sum, 
+# count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. 
+# Write a function to return the count of such triplets.
+
