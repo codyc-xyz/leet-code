83b39c012b2f4dff96725a9e05f2dd59b4c4ab03
Red <crabmarket56@gmail.com>
2022-11-18 14:52:56 +0900

    Add initial solution

--- two-pointers/grokking-the-coding-interview/dutch_national_flag_problem.py
+++ two-pointers/grokking-the-coding-interview/dutch_national_flag_problem.py
@@ -4,7 +4,21 @@
 # The flag of the Netherlands consists of three colors: red, white and blue; 
 # and since our input array also consists of three different numbers that is why it is called Dutch National Flag problem.
 
+def dutch_flag_sort(arr):
+  left, right = 0, len(arr) - 1
+  i = 0
 
+  while i <= right:
+    if arr[i] == 0:
+      arr[i], arr[low] = arr[low], arr[i]
+      i += 1
+      low += 1
+    elif arr[i] == 2:
+      arr[i], arr[high] = arr[high], arr[i]
+      high -= 1
+    else:
+      i += 1
+  return arr
 
 
 
