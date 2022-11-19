cc1328e8aa413e890db509ba9ffdee9ef85fa4dc
Red <crabmarket56@gmail.com>
2022-11-19 17:03:33 +0900

    Add initial solution

--- two-pointers/942_di_string_match.py
+++ two-pointers/942_di_string_match.py
@@ -5,3 +5,19 @@
 # Given a string s, reconstruct the permutation perm and return it. 
 # If there are multiple valid permutations perm, return any of them.
 
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        low, high = 0, len(s)
+        ans = []
+        for i in range(len(s)):
+            if s[i] == 'I':
+                ans.append(low)
+                low += 1
+                if i == len(s) - 1:
+                    ans.append(high)
+            else:
+                ans.append(high)
+                high -= 1
+                if i == len(s) - 1:
+                    ans.append(low)
+        return ans
