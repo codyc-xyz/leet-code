33f9b458b927fd2072fb7e402ec17a31bfc51da2
Red <crabmarket56@gmail.com>
2022-11-19 17:37:18 +0900

    Add initial solution

--- two-pointers/problems/1768_merge_strings_alternately.py
+++ two-pointers/problems/1768_merge_strings_alternately.py
@@ -3,4 +3,14 @@
 
 # Return the merged string.
 
-
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        length = max(len(word1), len(word2))
+        merged = ""
+        for i in range(length):
+            if i < len(word1):
+                merged += word1[i]
+            if i < len(word2):
+                merged+= word2[i]
+        return merged
+        
