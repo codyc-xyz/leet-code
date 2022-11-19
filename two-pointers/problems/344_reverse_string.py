9c5f88eeb022c7a6eccf18a473cce15560052abc
Red <crabmarket56@gmail.com>
2022-11-19 17:10:14 +0900

    Add initial solution

--- two-pointers/problems/344_reverse_string.py
+++ two-pointers/problems/344_reverse_string.py
@@ -2,3 +2,14 @@
 
 # You must do this by modifying the input array in-place with O(1) extra memory.
 
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        right, left = len(s) - 1, 0
+        for i in range((len(s) + 1) // 2):
+            tmp = s[left]
+            s[left] = s[right]
+            s[right] = tmp
+            right -= 1
+            left += 1
+        return s
+        
