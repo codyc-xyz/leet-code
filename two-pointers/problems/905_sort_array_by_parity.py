8db56de8ccf2a5dc7fb1aaf6389e1b760e82cb0a
Red <crabmarket56@gmail.com>
2022-11-19 17:48:03 +0900

    Add initial solution

--- two-pointers/problems/905_sort_array_by_parity.py
+++ two-pointers/problems/905_sort_array_by_parity.py
@@ -2,3 +2,16 @@
 
 # Return any array that satisfies this condition.
 
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left, right = 0, len(nums) - 1
+        arr = [None] * len(nums)
+        
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                arr[left] = nums[i]
+                left += 1
+            else:
+                arr[right] = nums[i]
+                right -= 1
+        return arr
