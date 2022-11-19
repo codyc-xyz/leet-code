3bdc91193d24825d3c5caa067ea2f071996f0c44
Red <crabmarket56@gmail.com>
2022-11-19 16:24:23 +0900

    Add initial solution

--- two-pointers/problems/2108_find_first_palindromic_string_in_the_array.py
+++ two-pointers/problems/2108_find_first_palindromic_string_in_the_array.py
@@ -1,3 +1,24 @@
 # Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
 
 # A string is palindromic if it reads the same forward and backward.
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        palindromic = ""
+        
+        for word in words:
+            right = len(word) - 1
+            left = 0
+            count = 0
+            while right > left:
+                if word[right] == word[left]:
+                    right -= 1
+                    left += 1
+                    count += 1
+                    continue
+                else:
+                    break
+            if count == len(word) // 2:
+                palindromic += word
+                break
+        return palindromic
