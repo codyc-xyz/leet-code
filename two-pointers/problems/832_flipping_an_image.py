ce4642012fecdf3c478888d875ecaa3b638581e2
Red <crabmarket56@gmail.com>
2022-11-19 16:13:18 +0900

    Add initial solution

--- two-pointers/problems/832_flipping_an_image.py
+++ two-pointers/problems/832_flipping_an_image.py
@@ -6,5 +6,12 @@
 # To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
 # For example, inverting [0,1,1] results in [1,0,0].
 
-
-
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        c = len(image) 
+        for row in image:
+            for i in range((len(image) + 1) // 2):
+                tmp = row[i]
+                row[i] = 1 - row[c - 1 - i]
+                row[c - 1 - i] = 1 - tmp
+        return image
