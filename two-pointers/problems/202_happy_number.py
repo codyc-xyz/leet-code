3836a46d2a0ceb0099e6d05263dbca93e04b1ab4
Red <crabmarket56@gmail.com>
2022-11-20 19:09:38 +0900

    Add initial solution

--- two-pointers/problems/202_happy_number.py
+++ two-pointers/problems/202_happy_number.py
@@ -6,3 +6,22 @@
 # Those numbers for which this process ends in 1 are happy.
 
 # Return true if n is a happy number, and false if not.
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        
+        while n not in seen:
+            if n == 1:
+                return True
+            seen.add(n)
+            n = self.helper(n)
+        return False
+    def helper(self, n):
+        res = 0
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            res += digit
+            n = n // 10
+        return res
