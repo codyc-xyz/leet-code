fff915a03078d0597854197a163439dba0ae6e4a
Red <crabmarket56@gmail.com>
2022-11-20 15:24:19 +0900

    Add initial solution

--- two-pointers/problems/283_move_zeroes.py
+++ two-pointers/problems/283_move_zeroes.py
@@ -2,3 +2,20 @@
 
 # Note that you must do this in-place without making a copy of the array.
 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """   
+        count = pops = 0
+        arr = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                arr.append(i)
+                count += 1
+        for n in arr:
+            nums.pop(n - pops)
+            pops += 1
+        for i in range(count):
+            nums.append(0)
+        return nums
