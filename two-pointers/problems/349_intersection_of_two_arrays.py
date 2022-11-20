e8bd60f43be74598cd15c4b2000becdba38ae72e
Red <crabmarket56@gmail.com>
2022-11-20 13:19:12 +0900

    Refactor solution

--- two-pointers/problems/349_intersection_of_two_arrays.py
+++ two-pointers/problems/349_intersection_of_two_arrays.py
@@ -3,19 +3,13 @@
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hm = {}
         res = []
         if len(nums1) > len(nums2):
-            for i in range(len(nums2)):
-                hm[nums2[i]] = i
             for i in range(len(nums1)):
-                if nums1[i] in hm:
+                if nums1[i] in nums2:
                     res.append(nums1[i])
         else:
-            for i in range(len(nums1)):
-                hm[nums1[i]] = i
             for i in range(len(nums2)):
-                if nums2[i] in hm:
+                if nums2[i] in nums1:
                     res.append(nums2[i])
-            
         return set(res)
