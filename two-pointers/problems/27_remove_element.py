a66416e1ed5fa9783606f09c1c1d8c1c18ee7cb6
Red <crabmarket56@gmail.com>
2022-11-20 20:16:44 +0900

    Add initial solution

--- two-pointers/problems/27_remove_element.py
+++ two-pointers/problems/27_remove_element.py
@@ -2,3 +2,13 @@
 
 # Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            if nums[left] == val:
+                nums.pop(left)
+                right -= 1
+                left -= 1
+            left += 1
