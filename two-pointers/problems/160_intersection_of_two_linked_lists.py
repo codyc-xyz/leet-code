2c21b773c9b93edd96c2562af79b74a32b2d0c0c
Red <crabmarket56@gmail.com>
2022-11-20 19:19:58 +0900

    Add initial solution

--- two-pointers/problems/160_intersection_of_two_linked_lists.py
+++ two-pointers/problems/160_intersection_of_two_linked_lists.py
@@ -5,3 +5,15 @@
 
 # Note that the linked lists must retain their original structure after the function returns.
 
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        seen = set()
+        while headA:
+            seen.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in seen:
+                return headB
+            headB = headB.next
+        return None
+            
