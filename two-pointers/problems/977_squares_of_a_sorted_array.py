78bd4d6a0b3ccc9978f428d5451335aa3437d7d8
Red <crabmarket56@gmail.com>
2022-11-20 11:42:43 +0900

    Add initial solution

--- two-pointers/problems/977_squares_of_a_sorted_array.py
+++ two-pointers/problems/977_squares_of_a_sorted_array.py
@@ -1,2 +1,20 @@
 # Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        left, right = 0, len(nums) - 1
+        n = len(nums) - 1
+        arr = [None] * len(nums)
+        while right >= left:
+            l = nums[left] * nums[left]
+            r = nums[right] * nums[right]
+            if l > r:
+                arr[n] = l
+                left += 1
+            else:
+                arr[n] = r
+                right -= 1
+            n -= 1
+        return arr
+                
