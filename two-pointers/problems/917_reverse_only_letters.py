ea8d0f518adec8bc3a8a389807b4c9cf205df60e
Red <crabmarket56@gmail.com>
2022-11-20 15:11:14 +0900

    Add initial solution

--- two-pointers/problems/917_reverse_only_letters.py
+++ two-pointers/problems/917_reverse_only_letters.py
@@ -4,3 +4,20 @@
 # All the English letters (lowercase or uppercase) should be reversed.
 # Return s after reversing it.
 
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        left, right = 0, len(s) - 1
+        c = list(s)
+        while right > left:
+            while left < len(s) - 1 and s[left].isalpha() == False:
+                left += 1
+            while right > 0 and s[right].isalpha() == False:
+                right -= 1
+            if left < right and s[left].isalpha() and s[right].isalpha():
+                tmp = s[left]
+                c[left] = c[right]
+                c[right] = tmp
+                left += 1
+                right -= 1
+        return "".join(str(x) for x in c)
+            
