e7cac2667627165275f674d7c28ab240ba9e1b72
Red <crabmarket56@gmail.com>
2022-11-20 13:46:22 +0900

    Add initial solution

--- two-pointers/1385_find_the_distance_value_between_two_arrays.py
+++ two-pointers/1385_find_the_distance_value_between_two_arrays.py
@@ -2,3 +2,15 @@
 
 # The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
 
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        ans = 0
+        for i in range(len(arr1)):
+            count = 0
+            for n in range(len(arr2)):
+                if abs(arr1[i] - arr2[n]) <= d:
+                    count += 1
+                    break
+            if count == 0:
+                ans += 1
+        return ans
