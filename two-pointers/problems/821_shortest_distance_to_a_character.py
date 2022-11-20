c84f600bead211ee3eb30d123667b9f10704d7c5
Red <crabmarket56@gmail.com>
2022-11-20 11:56:56 +0900

    Add initial solution

--- two-pointers/problems/821_shortest_distance_to_a_character.py
+++ two-pointers/problems/821_shortest_distance_to_a_character.py
@@ -2,3 +2,18 @@
 
 # The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
 
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        arr = []
+        ret = []
+        n = 0
+        for i in range(len(s)):
+            if s[i] == c:
+                arr.append(i)
+        
+        for i in range(len(s)):
+            if n < len(arr) - 1:
+                if abs(arr[n] - i) > abs(arr[n + 1] - i):
+                    n += 1
+            ret.append(abs(arr[n] - i))
+        return ret
