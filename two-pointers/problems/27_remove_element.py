9217182f6b04af055ea46a8c8a58aa8f1b40bbf2
Red <crabmarket56@gmail.com>
2022-11-20 20:16:24 +0900

    Initialize file

---
+++ two-pointers/problems/27_remove_element.py
@@ -0,0 +1,4 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
