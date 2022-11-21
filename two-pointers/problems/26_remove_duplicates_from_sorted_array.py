684900ae76d3da111c9f9ed5c0925728505ae316
Red <crabmarket56@gmail.com>
2022-11-21 09:38:25 +0900

    Initialize file

---
+++ two-pointers/problems/26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,6 @@
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+# Return k after placing the final result in the first k slots of nums.
+
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
