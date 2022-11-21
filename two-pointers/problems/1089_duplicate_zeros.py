d39e26e13a8db4c47805337f8fe986deaf6addfd
Red <crabmarket56@gmail.com>
2022-11-21 09:15:40 +0900

    Add initial solution

--- two-pointers/problems/1089_duplicate_zeros.py
+++ two-pointers/problems/1089_duplicate_zeros.py
@@ -3,3 +3,14 @@
 # Note that elements beyond the length of the original array are not written. 
 # Do the above modifications to the input array in place and do not return anything.
 
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        queue = deque()
+        for i in range(len(arr)):
+            queue.append(arr[i])
+            if arr[i] == 0:
+                queue.append(arr[i])
+            arr[i] = queue.popleft()
