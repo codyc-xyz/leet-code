0fbfc6365766af4eec2af1e45ce82f77eb056b3f
Red <crabmarket56@gmail.com>
2022-11-22 09:43:07 +0900

    Refactor solution

--- 345_reverse_vowels_of_a_string.py
+++ 345_reverse_vowels_of_a_string.py
@@ -6,14 +6,17 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
         dq = deque()
-        k = 0
         a = list(s)
-        for i in range(len(s)):
-            if s[i] in vowels:
-                dq.appendleft(s[i])
-                k += 1
-        for i in range(len(s)):
-            if s[i] in vowels:
-                a[i] = dq.popleft()
+        left, right = 0, len(s) - 1
+        
+        while right > left:
+            while left < right and s[left] not in vowels:
+                left += 1
+            while right > left and s[right] not in vowels:
+                right -= 1
+            a[left] = s[right]
+            a[right] = s[left]
+            left += 1
+            right -= 1
         return "".join(a)
             
