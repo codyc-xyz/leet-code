d2473b2fc35e52c9f445affee4faac1d308aea90
Red <crabmarket56@gmail.com>
2022-11-22 14:24:01 +0900

    Add initial solution

--- two-pointers/problems/125_valid_palindrome.py
+++ two-pointers/problems/125_valid_palindrome.py
@@ -2,3 +2,19 @@
 # Alphanumeric characters include letters and numbers.
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        left, right = 0, len(s) - 1
+        
+        while right > left:
+            while left < right and s[left].isalnum() == False:
+                left += 1
+            while right > left and s[right].isalnum() == False:
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
