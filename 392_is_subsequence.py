4b772ebc6c499c5151069db10ed59f96705b39fe
Red <crabmarket56@gmail.com>
2022-11-22 11:11:10 +0900

    Add initial solution

--- 392_is_subsequence.py
+++ 392_is_subsequence.py
@@ -3,3 +3,17 @@
 # A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
 # (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+            
+        a = 0
+        for i in range(len(t)):
+            if s[a] == t[i]:
+                a += 1
+            if a == len(s):
+                return True
+            if len(s) - a > (len(t) - i - 1):
+                break
+        return False
