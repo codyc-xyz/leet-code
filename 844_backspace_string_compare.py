cc73a88f33b5ca98a44a3962f9e55d1a473b1215
Red <crabmarket56@gmail.com>
2022-11-22 13:19:02 +0900

    Add initial solution

--- 844_backspace_string_compare.py
+++ 844_backspace_string_compare.py
@@ -3,3 +3,22 @@
 
 # Note that after backspacing an empty text, the text will continue empty.
 
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        resS = []
+        resT = []
+        
+        for i in range(len(s)):
+            if s[i] != '#':
+                resS.append(s[i])
+            else:
+                if resS:
+                    resS.pop()
+
+        for i in range(len(t)):
+            if t[i] != '#':
+                resT.append(t[i])
+            else:
+                if resT:
+                    resT.pop()
+        return resS == resT
