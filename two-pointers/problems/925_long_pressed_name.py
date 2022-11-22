a37161c15c5f0ad4943236fb1b1e543ab17bb277
Red <crabmarket56@gmail.com>
2022-11-22 15:44:57 +0900

    Add initial solution

--- two-pointers/problems/925_long_pressed_name.py
+++ two-pointers/problems/925_long_pressed_name.py
@@ -3,3 +3,13 @@
 
 # You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
 
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        
+        a = 0
+        for i in range(len(typed)):
+            if a < len(name) and typed[i] == name[a]:
+                a += 1
+            elif i == 0 or typed[i] != typed[i - 1]:
+                return False
+        return a == len(name)
