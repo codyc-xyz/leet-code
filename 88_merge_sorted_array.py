85307f33eba9d269daca3682bea8e4abc5e5e285
Red <crabmarket56@gmail.com>
2022-11-22 14:12:19 +0900

    Add initial solution

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -6,3 +6,26 @@
 # To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. 
 # nums2 has a length of n.
 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+      
+        a, b = m - 1, n - 1
+        
+        while a >= 0 and b >= 0:
+            if nums1[a] >= nums2[b]:
+                nums1[a + b + 1] = nums1[a]
+                a -= 1
+            else:
+                nums1[a + b + 1] = nums2[b]
+                b -= 1
+        if a < 0:
+            while b >= 0:
+                nums1[a + b + 1] = nums2[b]
+                b -= 1
+        else:
+            while a >= 0:
+                nums1[a + b + 1] = nums1[a]
+                a -= 1
