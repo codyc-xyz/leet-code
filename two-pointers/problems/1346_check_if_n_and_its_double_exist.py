0b3bb6cb282a1686f401fd80de203e423040ac46
Red <crabmarket56@gmail.com>
2022-11-22 15:09:14 +0900

    Add initial solution

--- two-pointers/problems/1346_check_if_n_and_its_double_exist.py
+++ two-pointers/problems/1346_check_if_n_and_its_double_exist.py
@@ -3,3 +3,12 @@
 # 0 <= i, j < arr.length
 # arr[i] == 2 * arr[j]
 
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        hm = {}
+        
+        for i in range(len(arr)):
+            if arr[i] * 2 in hm or arr[i] / 2 in hm:
+                return True
+            hm[arr[i]] = i
+        return False
