f72506baf6f22051566fb1893b2034dcc85d683f
Red <crabmarket56@gmail.com>
2022-11-22 09:37:19 +0900

    Add initial solution

--- 345_reverse_vowels_of_a_string.py
+++ 345_reverse_vowels_of_a_string.py
@@ -2,3 +2,18 @@
 
 # The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
+        dq = deque()
+        k = 0
+        a = list(s)
+        for i in range(len(s)):
+            if s[i] in vowels:
+                dq.appendleft(s[i])
+                k += 1
+        for i in range(len(s)):
+            if s[i] in vowels:
+                a[i] = dq.popleft()
+        return "".join(a)
+            
