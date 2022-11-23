9b21ab6072399eb112d03a5c912ad04b62708520
Red <crabmarket56@gmail.com>
2022-11-23 15:40:59 +0900

    Add initial solution

--- 2149_rearrange_array_elements_by_sign.py
+++ 2149_rearrange_array_elements_by_sign.py
@@ -6,3 +6,16 @@
 # The rearranged array begins with a positive integer.
 
 # Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        positive, negative = 0, 1
+        arr = [None] * len(nums) 
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                arr[positive] = nums[i]
+                positive += 2
+            else:
+                arr[negative] = nums[i]
+                negative += 2
+        return arr
