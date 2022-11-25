e0ef7873d4e175b66e7aeacd05ddb5d13b760aa5
Red <crabmarket56@gmail.com>
2022-11-25 16:16:08 +0900

    Add initial solution

--- two-pointers/problems/2095_delete_the_middle_node_of_a_linked_list.py
+++ two-pointers/problems/2095_delete_the_middle_node_of_a_linked_list.py
@@ -3,3 +3,20 @@
 # The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
 
 # For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if head.next == None:
+            return None
+        
+        fast, slow = head.next.next, head
+        
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+            
+        return head
