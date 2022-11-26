4f84edff3870dd993300eb26abef152c99213de0
Red <crabmarket56@gmail.com>
2022-11-26 17:41:51 +0900

    Add initial solution

--- two-pointers/problems/287_find_the_duplicate_number.py
+++ two-pointers/problems/287_find_the_duplicate_number.py
@@ -4,3 +4,10 @@
 
 # You must solve the problem without modifying the array nums and uses only constant extra space.
 
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        hm = {}
+        for i in range(len(nums)):
+            if nums[i] in hm:
+                return nums[i]
+            hm[nums[i]] = i
