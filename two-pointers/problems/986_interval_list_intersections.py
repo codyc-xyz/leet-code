a52d7bf47ba26b311df6faec98a7bdffdbf092e3
Red <crabmarket56@gmail.com>
2022-11-26 15:23:52 +0900

    Initialize file

--- two-pointers/problems/986_interval_list_intersections.py
+++ two-pointers/problems/986_interval_list_intersections.py
@@ -7,3 +7,18 @@
 
 # The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
 
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        
+        first = second = 0
+        output = []
+        length = max(len(firstList), len(secondList))
+        while first < len(firstList) and second < len(secondList):
+            if firstList[first][0] <= secondList[second][1] and firstList[first][1] >= secondList[second][0]:
+                output.append([max(firstList[first][0], secondList[second][0]), min(firstList[first][1], secondList[second][1])])
+            
+            if firstList[first][1] > secondList[second][1]:
+                second += 1
+            else:
+                first += 1
+        return output
