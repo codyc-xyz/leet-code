ff0c169ffd8f3de0ea76cfca17b77930d42f7d2f
Red <crabmarket56@gmail.com>
2022-11-26 16:54:46 +0900

    Add initial solution

--- two-pointers/problems/167_two_sum_II_input_array_is_sorted.py
+++ two-pointers/problems/167_two_sum_II_input_array_is_sorted.py
@@ -7,3 +7,15 @@
 
 # Your solution must use only constant extra space.
 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        left, right = 0, len(numbers) - 1
+        
+        while left < right:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
