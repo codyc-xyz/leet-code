9a7c01fb9dc941e7f4a2d024ccf17cd647cb8746
Red <crabmarket56@gmail.com>
2022-11-26 17:45:49 +0900

    Add alternate solution

--- two-pointers/problems/287_find_the_duplicate_number.py
+++ two-pointers/problems/287_find_the_duplicate_number.py
@@ -11,3 +11,17 @@ class Solution:
             if nums[i] in hm:
                 return nums[i]
             hm[nums[i]] = i
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        hm = {}
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left] in hm:
+                return nums[left]
+            hm[nums[left]] = left
+            if nums[right] in hm:
+                return nums[right]
+            hm[nums[right]] = right
+            left += 1
+            right -= 1
