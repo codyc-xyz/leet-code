c9eb830b697a6898c75bba483f85f45a445a5a0e
Red <crabmarket56@gmail.com>
2022-11-26 15:24:00 +0900

    Add initial solution

--- two-pointers/problems/1861_rotating_the_box.py
+++ two-pointers/problems/1861_rotating_the_box.py
@@ -12,3 +12,29 @@
 
 # Return an n x m matrix representing the box after the rotation described above.
 
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+
+        rows, columns = len(box), len(box[0])
+        
+        for i in range(rows):
+            for j in range(columns -1, -1, -1):
+                if box[i][j] == '#':
+                    empty = j + 1
+                    while empty < columns and box[i][empty] == '.':
+                        empty += 1
+                    if empty < columns and box[i][empty] == '.':
+                        box[i][empty] = box[i][j]
+                        box[i][j] = '.'
+                    elif empty - 1 < columns and box[i][empty - 1] == '.':
+                        box[i][empty - 1] = box[i][j]
+                        box[i][j] = '.'
+                        
+        k = rows - 1
+        res = [[None for a in range(rows)] for b in range(columns)]
+        
+        for i in range(rows):
+            for j in range(columns):
+                res[j][k] = box[i][j]
+            k -= 1
+        return res
