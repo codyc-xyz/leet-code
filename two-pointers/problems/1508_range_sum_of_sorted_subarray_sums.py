fedb9b9ed0a6363d16869b3cb161f15b04a27184
Red <crabmarket56@gmail.com>
2022-11-26 17:29:15 +0900

    Add initial solution

--- two-pointers/problems/1508_range_sum_of_sorted_subarray_sums.py
+++ two-pointers/problems/1508_range_sum_of_sorted_subarray_sums.py
@@ -4,3 +4,20 @@
 # Return the sum of the numbers from index left to index right (indexed from 1), inclusive, in the new array. 
 # Since the answer can be a huge number return it modulo 109 + 7.
 
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        sums = []
+        ans = 0
+        for i in range(len(nums)):
+            sums.append(nums[i])
+            for j in range(i + 1, len(nums)):
+                sums.append(sums[-1] + nums[j])
+        sums.sort()
+        while left < right:
+            ans += sums[left - 1] + sums[right - 1]
+            left += 1
+            right -= 1
+        if left == right:
+            ans += sums[right - 1]
+        return ans 
+            
