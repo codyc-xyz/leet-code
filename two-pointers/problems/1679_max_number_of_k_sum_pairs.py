3f0beb68422d7787f3d433a9307287790e09d815
Red <crabmarket56@gmail.com>
2022-11-27 14:55:42 +0900

    Add initial solution

--- two-pointers/problems/1679_max_number_of_k_sum_pairs.py
+++ two-pointers/problems/1679_max_number_of_k_sum_pairs.py
@@ -4,3 +4,19 @@
 
 # Return the maximum number of operations you can perform on the array.
 
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        ops = 0
+        while left < right:
+            if nums[left] + nums[right] > k:
+                right -= 1
+            elif nums[left] + nums[right] < k:
+                left += 1
+            else:
+                ops += 1
+                right -= 1
+                left += 1
+        return ops
+        
