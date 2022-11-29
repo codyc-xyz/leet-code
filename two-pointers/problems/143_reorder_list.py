a24d457762dd0b85395a2816613b31d940779651
Red <crabmarket56@gmail.com>
2022-11-29 13:38:08 +0900

    Add initial solution

--- two-pointers/problems/143_reorder_list.py
+++ two-pointers/problems/143_reorder_list.py
@@ -6,3 +6,28 @@
 
 # You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
