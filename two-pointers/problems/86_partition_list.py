8695d16bda5efe50318a3ffb513fd4e34075c2b6
Red <crabmarket56@gmail.com>
2022-11-29 11:34:32 +0900

    Add intiial solution

--- two-pointers/problems/86_partition_list.py
+++ two-pointers/problems/86_partition_list.py
@@ -2,3 +2,20 @@
 
 # You should preserve the original relative order of the nodes in each of the two partitions.
 
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+        
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+        ltail.next = right.next
+        rtail.next = None
+        return left.next
