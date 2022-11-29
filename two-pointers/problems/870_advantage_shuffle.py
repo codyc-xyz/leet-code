9fbeb6c1ad28ce55e595e90bb879a942ad8b31e6
Red <crabmarket56@gmail.com>
2022-11-29 11:09:27 +0900

    Add initial solution

--- two-pointers/problems/870_advantage_shuffle.py
+++ two-pointers/problems/870_advantage_shuffle.py
@@ -2,3 +2,20 @@
 
 # Return any permutation of nums1 that maximizes its advantage with respect to nums2.
 
+class Solution:
+    def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        advantage = deque()
+        for i, n in enumerate(nums2):
+            k = 0
+            while k < len(nums1) and n >= nums1[k]:
+                k += 1
+
+            if k < len(nums1) and nums1[k] > n:
+                advantage.append(nums1[k])
+                nums1.pop(k)
+            else:
+                advantage.append(nums1[0])
+                nums1.pop(0)
+        return advantage
+            
