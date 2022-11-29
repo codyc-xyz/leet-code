acb50665dc57acb8540e5f81d6b4b677cce1026e
Red <crabmarket56@gmail.com>
2022-11-29 10:05:35 +0900

    Add initial solution

--- 881_boats_to_save_people.py
+++ 881_boats_to_save_people.py
@@ -3,3 +3,17 @@
 
 # Return the minimum number of boats to carry every given person.
 
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
+        boats = 0
+        while right >= left:
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1
+                boats += 1
+            else:
+                right -= 1
+                boats += 1
+        return boats
