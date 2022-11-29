511697ea0c074a3b343273d061a173d45784ac3f
Red <crabmarket56@gmail.com>
2022-11-29 09:11:18 +0900

    Refactor solution

--- two-pointers/problems/795_number_of_subarrays_with_bounded_maximum.py
+++ two-pointers/problems/795_number_of_subarrays_with_bounded_maximum.py
@@ -6,19 +6,11 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
         res = 0
-        needed = {}
-        reset = {}
+        r = l = -1
         for i, n in enumerate(nums):
             if n <= right and n >= left:
-                needed[n] = i
-            if n > right:
-                reset[n] = i
-    
-        r = l = -1
-        for i in range(len(nums)):
-            if nums[i] in needed:
                 r = i
-            if nums[i] in reset:
+            if n > right:
                 l = r = i
                 continue
             res += r - l
