27d6954e79129942ebdce220f3d99a07878b529f
Red <crabmarket56@gmail.com>
2022-11-29 10:20:06 +0900

    Add initial solution

--- two-pointers/problems/1855_maximum_distance_between_a_pair_of_values.py
+++ two-pointers/problems/1855_maximum_distance_between_a_pair_of_values.py
@@ -6,4 +6,17 @@
 
 # An array arr is non-increasing if arr[i-1] >= arr[i] for every 1 <= i < arr.length.
 
-
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        p1 = p2 = ans = 0
+        
+        while p1 < len(nums1) and p2 < len(nums2):
+            
+            if p1 <= p2 and nums1[p1] <= nums2[p2]:
+                ans = max(ans, p2 - p1)
+                p2 += 1
+            elif p1 > p2:
+                p2 += 1
+            elif nums1[p1] > nums2[p2]:
+                p1 += 1
+        return ans
