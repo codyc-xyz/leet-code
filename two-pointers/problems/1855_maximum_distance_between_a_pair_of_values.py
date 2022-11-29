deede65766c19c9c1d0901b85429053967d493af
Red <crabmarket56@gmail.com>
2022-11-29 10:23:38 +0900

    Refactor solution

--- two-pointers/problems/1855_maximum_distance_between_a_pair_of_values.py
+++ two-pointers/problems/1855_maximum_distance_between_a_pair_of_values.py
@@ -12,11 +12,10 @@ class Solution:
         
         while p1 < len(nums1) and p2 < len(nums2):
             
-            if p1 <= p2 and nums1[p1] <= nums2[p2]:
+            if nums1[p1] > nums2[p2]:
+                p1 += 1
+            else:
                 ans = max(ans, p2 - p1)
                 p2 += 1
-            elif p1 > p2:
-                p2 += 1
-            elif nums1[p1] > nums2[p2]:
-                p1 += 1
         return ans
+        
