d6e4f5643dda98b40e4b64022b23ef98fda56b9e
Red <crabmarket56@gmail.com>
2022-11-30 20:24:38 +0900

    Add initial solution

--- two-pointers/problems/611_valid_triangle_number.py
+++ two-pointers/problems/611_valid_triangle_number.py
@@ -1,2 +1,16 @@
 # Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
 
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        for i in range(len(nums) - 1, 1, -1):
+            left, right = 0, i - 1
+            
+            while right > left:
+                if nums[left] + nums[right] > nums[i]:
+                    res += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return res
