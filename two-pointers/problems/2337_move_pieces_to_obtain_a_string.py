3e7fa85be349a98d4a965a8a75faf350fe9f9fc8
Red <crabmarket56@gmail.com>
2022-12-01 15:42:20 +0900

    Add initial solution

--- two-pointers/problems/2337_move_pieces_to_obtain_a_string.py
+++ two-pointers/problems/2337_move_pieces_to_obtain_a_string.py
@@ -6,3 +6,24 @@
 
 # Return true if it is possible to obtain the string target by moving the pieces of the string start any number of times. Otherwise, return false.
 
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        start = list(start)
+        target = list(target)
+        filtered_start = [pos for pos in start if pos != '_']
+        filtered_target = [pos for pos in target if pos != '_']
+        left = []
+        right = []
+        if filtered_start != filtered_target:
+            return False
+        for i, n in enumerate(target):
+            if n == 'L':
+                left.append(i)
+            if n == 'R':
+                right.append(i)
+        for i, n in enumerate(start):
+            if n == 'L' and i < max(left):
+                return False
+            if n == 'R' and i > min(right):
+                return False
+        return True
