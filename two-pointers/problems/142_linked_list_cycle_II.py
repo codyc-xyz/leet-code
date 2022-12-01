c8f57cd51e8a9496dadd87b0b94a02e21a2781da
Red <crabmarket56@gmail.com>
2022-12-01 17:41:20 +0900

    Reinstate solution without using memory

--- two-pointers/problems/142_linked_list_cycle_II.py
+++ two-pointers/problems/142_linked_list_cycle_II.py
@@ -8,6 +8,23 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        else:
+            return None
+        
+        while head != slow:
+            head = head.next
+            slow = slow.next
+        return head
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
         curr = head
         seen = set()
         while curr:
