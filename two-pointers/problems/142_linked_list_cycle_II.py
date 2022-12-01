e4ac5d144b0067f32a06575d56198fd354ff8279
Red <crabmarket56@gmail.com>
2022-12-01 17:22:45 +0900

    Refactor solution

--- two-pointers/problems/142_linked_list_cycle_II.py
+++ two-pointers/problems/142_linked_list_cycle_II.py
@@ -8,16 +8,11 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        slow = fast = head
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            if fast == slow:
-                break
-        else:
-            return None
-        
-        while head != slow:
-            head = head.next
-            slow = slow.next
-        return head
+        curr = head
+        seen = set()
+        while curr:
+            if curr in seen:
+                return curr
+            seen.add(curr)
+            curr = curr.next
+        return None
