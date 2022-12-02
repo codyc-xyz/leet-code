64350c864a1302e58c8d38ec63f14f8767706311
Red <crabmarket56@gmail.com>
2022-12-02 16:44:02 +0900

    Add initial solution

--- two-pointers/problems/826_most_profit_assigning_work.py
+++ two-pointers/problems/826_most_profit_assigning_work.py
@@ -8,3 +8,17 @@
 # For example, if three workers attempt the same job that pays $1, then the total profit will be $3. If a worker cannot complete any job, their profit is $0.
 # Return the maximum profit we can achieve after assigning the workers to the jobs.
 
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        
+        jobs = zip(difficulty, profit)
+        jobs = sorted(jobs)
+        worker.sort()
+        ans = i = best = 0
+
+        for skill in worker:
+            while i < len(jobs) and skill >= jobs[i][0]:
+                best = max(best, jobs[i][1])
+                i += 1
+            ans += best
+        return ans
