252c8122039336b7924f6d4832d459c7f25ee585
Red <crabmarket56@gmail.com>
2022-12-02 15:08:51 +0900

    Add initial solution

--- two-pointers/problems/82_remove_duplicates_from_sorted_list_II.py
+++ two-pointers/problems/82_remove_duplicates_from_sorted_list_II.py
@@ -1,2 +1,17 @@
 # Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
 
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode(0, head)
+        fake = dummy
+        
+        while head:
+            if head.next and head.next.val == head.val:
+                while head.next and head.next.val == head.val:
+                    head = head.next
+                fake.next = head.next
+            else:
+                fake = fake.next
+            head = head.next
+        return dummy.next
