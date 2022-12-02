7897c1a72815e97fca9fcf93ed07e69d849c8510
Red <crabmarket56@gmail.com>
2022-12-02 11:43:42 +0900

    Add initial solution

--- two-pointers/problems/16_3sum_closest.py
+++ two-pointers/problems/16_3sum_closest.py
@@ -4,3 +4,18 @@
 
 # You may assume that each input would have exactly one solution.
 
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        hm = {}
+        for i in range(len(nums)):
+            left, right = i + 1, len(nums) - 1
+            curr = 0
+            while right > left:
+                curr = nums[i] + nums[left] + nums[right]
+                hm[curr] = abs(curr-target)
+                if curr > target:
+                    right -= 1
+                else:
+                    left += 1
+        return min(hm, key = hm.get)
