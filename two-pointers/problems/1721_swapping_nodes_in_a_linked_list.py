7768c5460464c61f4b968462a97e6b0b8e396f64
Red <crabmarket56@gmail.com>
2022-12-03 16:05:30 +0900

    Add initial solution

--- two-pointers/problems/1721_swapping_nodes_in_a_linked_list.py
+++ two-pointers/problems/1721_swapping_nodes_in_a_linked_list.py
@@ -2,3 +2,22 @@
 
 # Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
 
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        curr = head
+        
+        dq = deque()
+        while curr:
+            dq.append(curr.val)
+            curr = curr.next
+        
+        tmp = dq[k - 1]
+        dq[k - 1] = dq[-k]
+        dq[-k] = tmp
+        
+        alt = dummy = ListNode()
+        for i in dq:
+            alt.next = ListNode(i)
+            alt = alt.next
+        return dummy.next
