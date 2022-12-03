a18e33cdd45d63360bf9e8e0f7956dba358196ff
Red <crabmarket56@gmail.com>
2022-12-03 16:32:05 +0900

    Add alternative solution

--- two-pointers/problems/1721_swapping_nodes_in_a_linked_list.py
+++ two-pointers/problems/1721_swapping_nodes_in_a_linked_list.py
@@ -21,3 +21,29 @@ class Solution:
             alt.next = ListNode(i)
             alt = alt.next
         return dummy.next
+
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        count = head
+        n = 0
+        while count:
+            n += 1
+            count = count.next
+        
+        right = head
+        end_node = n - k
+        while end_node > 0:
+            right = right.next
+            end_node -= 1
+        
+        left = head
+        start_node = k - 1
+        while start_node > 0:
+            left = left.next
+            start_node -= 1
+            
+        tmp = right.val
+        right.val = left.val
+        left.val = tmp
+        return head
