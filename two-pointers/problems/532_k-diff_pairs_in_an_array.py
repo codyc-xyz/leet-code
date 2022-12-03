d57b8e19c1317c1e7b99ab86744724b2cdfad6a7
Red <crabmarket56@gmail.com>
2022-12-03 15:28:25 +0900

    Add initial solution

--- two-pointers/problems/532_k-diff_pairs_in_an_array.py
+++ two-pointers/problems/532_k-diff_pairs_in_an_array.py
@@ -8,3 +8,12 @@
 
 # Notice that |val| denotes the absolute value of val.
 
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        count = collections.Counter(nums)
+        ans = 0
+        
+        for i in count:
+            if (k != 0 and i + k in count) or (k == 0 and count[i] > 1):
+                ans += 1
+        return ans
