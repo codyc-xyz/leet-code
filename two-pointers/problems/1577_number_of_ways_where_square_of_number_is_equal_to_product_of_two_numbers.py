68ee6160e960e31c40bd71e75eb17e98e0d1d4dd
Red <crabmarket56@gmail.com>
2022-12-05 16:08:48 +0900

    Add initial solution

--- two-pointers/problems/1577_number_of_ways_where_square_of_number_is_equal_to_product_of_two_numbers.py
+++ two-pointers/problems/1577_number_of_ways_where_square_of_number_is_equal_to_product_of_two_numbers.py
@@ -2,3 +2,25 @@
 
 # Type 1: Triplet (i, j, k) if nums1[i]2 == nums2[j] * nums2[k] where 0 <= i < nums1.length and 0 <= j < k < nums2.length.
 # Type 2: Triplet (i, j, k) if nums2[i]2 == nums1[j] * nums1[k] where 0 <= i < nums2.length and 0 <= j < k < nums1.length.
+
+
+class Solution:
+    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        n1 = defaultdict(int)
+        n2 = defaultdict(int)
+        res = 0
+        for n in nums1:
+            n1[n ** 2] += 1
+        
+        for n in nums2:
+            n2[n**2] += 1
+            
+        for i in range(len(nums2) - 1):
+            for j in range(i + 1, len(nums2)):
+                res += n1[nums2[i] * nums2[j]]
+        
+        for i in range(len(nums1) - 1):
+            for j in range(i + 1, len(nums1)):
+                res += n2[nums1[i] * nums1[j]]
+                
+        return res
