80716282085f6bdbb0a1023baf47d0968f9d6197
Red <crabmarket56@gmail.com>
2022-12-05 20:09:44 +0900

    Add initial solution

--- two-pointers/problems/1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
+++ two-pointers/problems/1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
@@ -2,3 +2,16 @@
 
 # Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
 
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        
+        count = left = 0
+        while left < len(nums) and nums[left] * 2 <= target:
+            right = left
+            while right < len(nums) and nums[left] + nums[right] <= target:
+                right += 1
+            count += pow(2, right - left) // 2
+            left += 1
+            
+        return count%(10**9 + 7)
