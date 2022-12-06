534dc6282e78eb56f8fc32e4da6b54ee64f4dbb5
Red <crabmarket56@gmail.com>
2022-12-06 15:51:54 +0900

    Add initial solution

--- two-pointers/problems/777_swap_adjacent_in_LR_string.py
+++ two-pointers/problems/777_swap_adjacent_in_LR_string.py
@@ -1,3 +1,20 @@
 # In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR". 
 # Given the starting string start and the ending string end, return True if and only if there exists a sequence of moves to transform one string to the other.
 
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        if start.replace('X', '') != end.replace('X', ''):
+            return False
+        
+        s = t = 0
+        length = len(start) 
+        while s < length and t < length:
+            while s < length and start[s] == 'X':
+                s += 1
+            while t < length and end[t] == 'X':
+                t += 1
+            if (s < length and t < length) and (start[s] == 'L' and s < t or start[s] == 'R' and s > t):
+                return False
+            s += 1
+            t += 1
+        return True
