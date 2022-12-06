a344fad8e972dc02fe0e79a89390ccef4490efe8
Red <crabmarket56@gmail.com>
2022-12-06 13:49:57 +0900

    Add alternative solution

--- two-pointers/problems/1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
+++ two-pointers/problems/1498_number_of_subsequences_that_satisfy_the_given_sum_condition.py
@@ -15,3 +15,21 @@ class Solution:
             left += 1
             
         return count%(10**9 + 7)
+
+
+  class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        res = 0
+        mod = 10**9 + 7
+        r = n-1
+        
+        
+        for l in range(n):
+            while l <= r and nums[l] + nums[r] > target:
+                r -= 1
+            if l <= r:
+                res += pow(2, r- l, mod)
+                res %= mod
+        return res
