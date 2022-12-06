f7c458f33f3a307a5df63410c6ab19cc08976f52
Red <crabmarket56@gmail.com>
2022-12-06 14:40:32 +0900

    Add initial solution

--- two-pointers/problems/31_next_permutation.py
+++ two-pointers/problems/31_next_permutation.py
@@ -12,4 +12,32 @@
 # Given an array of integers nums, find the next permutation of nums.
 # The replacement must be in place and use only constant extra memory.
 
-
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        pivot = 0
+        
+        for i in range(n - 1, 0, -1):
+            if nums[i - 1] < nums[i]:
+                pivot = i
+                break
+        if pivot == 0:
+            return nums.sort()
+        
+        swap = n - 1
+        lPiv = pivot - 1
+        while nums[swap] <= nums[lPiv]:
+            swap -= 1
+        
+        nums[swap], nums[lPiv] = nums[lPiv], nums[swap]
+        
+        nums1 = nums[:pivot]
+        nums2 = nums[pivot:][::-1]
+        a = nums1 + nums2
+    
+        
+        for i in range(len(a)):
+            nums[i] = a[i]
