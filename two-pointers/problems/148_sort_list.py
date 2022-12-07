37bdec60ff181afb631bcf8616a197398a249f82
Red <crabmarket56@gmail.com>
2022-12-07 15:39:28 +0900

    Add initial solution

--- two-pointers/problems/148_sort_list.py
+++ two-pointers/problems/148_sort_list.py
@@ -1,2 +1,16 @@
 # Given the head of a linked list, return the list after sorting it in ascending order.
 
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dq = deque()
+        curr = head
+        while curr:
+            dq.append(curr.val)
+            curr = curr.next
+        sortDq = sorted(dq)
+        
+        sort = head
+        while sort:
+            sort.val = sortDq.pop(0)
+            sort = sort.next
+        return head
