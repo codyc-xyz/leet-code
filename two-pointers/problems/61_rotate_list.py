125aa7e8d2418f82bb81d9ae51e63a52cec13e7e
Red <crabmarket56@gmail.com>
2022-12-07 17:24:35 +0900

    Add alternate solution

--- two-pointers/problems/61_rotate_list.py
+++ two-pointers/problems/61_rotate_list.py
@@ -27,4 +27,32 @@ class Solution:
             curr = curr.next
             j += 1
         return head
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        if not head:
+            return head
+        
+        i, tail = 1, head
+        
+        while tail.next:
+            tail = tail.next
+            i += 1
+            
+        k = k % i
+        
+        if k == 0:
+            return head
+        
+        curr, j = head, 0
+        
+        while j < i - k - 1:
+            curr = curr.next
+            j += 1
+        
+        newHead = curr.next
+        curr.next = None
+        tail.next = head
+        return newHead
             
