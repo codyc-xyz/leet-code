4e6a3397499ca2266439b8cf2464bf854b8065d0
Red <crabmarket56@gmail.com>
2022-12-07 16:26:39 +0900

    Add initial solution

--- two-pointers/problems/475_heaters.py
+++ two-pointers/problems/475_heaters.py
@@ -6,3 +6,17 @@
 
 # Notice that all the heaters follow your radius standard, and the warm radius will the same.
 
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        a = 0
+        res = []
+        while a < len(houses):
+            b = 0
+            maxDist = float("inf")
+            while b < len(heaters):
+                dist = abs(heaters[b] - houses[a])
+                maxDist = (min(maxDist, dist))
+                b += 1
+            res.append(maxDist)
+            a += 1
+        return max(res)
