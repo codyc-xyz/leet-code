9e4f4034063c4af72b1d4c5f60faabfeaa7f87a4
Red <crabmarket56@gmail.com>
2022-12-10 10:00:35 +0900

    Add initial solution

--- two-pointers/problems/636_sum_of_square_numbers.py
+++ two-pointers/problems/636_sum_of_square_numbers.py
@@ -1,2 +1,13 @@
 # Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
 
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        i = 0
+        hm = {}
+        
+        while i * i <= c:
+            hm[i * i] = i
+            if c - i * i in hm:
+                return True
+            i += 1
+        return False
