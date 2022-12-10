c52356de9a8913d1324aec9a11c2637175ae3b7f
Red <crabmarket56@gmail.com>
2022-12-10 13:45:43 +0900

    Add initial solution

--- two-pointers/problems/1574_shortest_subarray_to_be_removed_to_make_array_sorted.py
+++ two-pointers/problems/1574_shortest_subarray_to_be_removed_to_make_array_sorted.py
@@ -3,3 +3,28 @@
 # Return the length of the shortest subarray to remove.
 
 # A subarray is a contiguous subsequence of the array.
+
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        
+        left, right = 0, len(arr) - 1
+        
+        while right - 1 >= 0 and arr[right - 1] <= arr[right]:
+            right -= 1
+            
+        while left + 1 < len(arr) and arr[left + 1] >= arr[left]:
+            left += 1
+        
+        if left == len(arr) - 1:
+            return 0
+        
+        remove = min(len(arr) - left - 1, right)
+        
+        for i in range(left + 1):
+            while right < len(arr) and arr[i] > arr[right]:
+                right += 1
+            if right == len(arr):
+                break
+            remove = min(remove, right - i - 1)
+            
+        return remove
