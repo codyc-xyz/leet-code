d6961033f7456857806c480765667ac21e9c81bb
Red <crabmarket56@gmail.com>
2022-12-10 14:26:28 +0900

    Add initial solution

--- two-pointers/problems/1813_sentence_similarity_III.py
+++ two-pointers/problems/1813_sentence_similarity_III.py
@@ -6,3 +6,17 @@
 
 # Given two sentences sentence1 and sentence2, return true if sentence1 and sentence2 are similar. Otherwise, return false.
 
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split()
+        s2 = sentence2.split()
+        
+        while s1 and s2 and s1[0] == s2[0]:
+            s1.pop(0)
+            s2.pop(0)
+            
+        while s1 and s2 and s1[-1] == s2[-1]:
+            s1.pop()
+            s2.pop()
+            
+        return not s1 or not s2
