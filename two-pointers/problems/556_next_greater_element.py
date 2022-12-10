aa82faccd8fc9356f7988b10a9c34e3b3551e3f1
Red <crabmarket56@gmail.com>
2022-12-10 09:51:50 +0900

    Add initial solution

--- two-pointers/problems/556_next_greater_element.py
+++ two-pointers/problems/556_next_greater_element.py
@@ -2,3 +2,30 @@
 
 # Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it does not fit in 32-bit integer, return -1.
 
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        nums = list(str(n))
+                    
+        i = len(nums) - 1
+                    
+        while i - 1 >= 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        
+        if i == 0:
+            return -1
+                    
+        j = i
+                    
+        while j + 1 < len(nums) and nums[j + 1] > nums[i - 1]:
+            j += 1
+                    
+        nums[i - 1], nums[j] = nums[j], nums[i - 1]
+                    
+        nums[i:] = nums[i:][::-1]
+                
+        res = int("".join(nums))
+                    
+        if res < 1<<31:
+            return res
+        else:
+            return -1
