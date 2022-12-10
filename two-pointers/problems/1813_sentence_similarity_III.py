04e550b805bb7deb361bfcefa6ac0291d5fc2fd8
Red <crabmarket56@gmail.com>
2022-12-10 14:27:35 +0900

    Refactor solution for effiiency

--- two-pointers/problems/1813_sentence_similarity_III.py
+++ two-pointers/problems/1813_sentence_similarity_III.py
@@ -11,9 +11,13 @@ class Solution:
         s1 = sentence1.split()
         s2 = sentence2.split()
         
-        while s1 and s2 and s1[0] == s2[0]:
-            s1.pop(0)
-            s2.pop(0)
+        while s1 and s2 and s1[-1] == s2[-1]:
+            s1.pop()
+            s2.pop()
+            
+        if s1 and s2:
+            s1.reverse()
+            s2.reverse()
             
         while s1 and s2 and s1[-1] == s2[-1]:
             s1.pop()
