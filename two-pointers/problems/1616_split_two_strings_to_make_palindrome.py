bd6414c7ade9539992f1a76a2405c467b7cfcc07
Red <crabmarket56@gmail.com>
2022-12-11 12:03:54 +0900

    Add initial solution

--- two-pointers/problems/1616_split_two_strings_to_make_palindrome.py
+++ two-pointers/problems/1616_split_two_strings_to_make_palindrome.py
@@ -8,3 +8,26 @@
 
 # Notice that x + y denotes the concatenation of strings x and y.
 
+class Solution:
+    def checkPalindromeFormation(self, a: str, b: str) -> bool:
+        return self.checkChars(a, b) or self.checkChars(b, a)
+    
+    def checkChars(self, a, b):
+        
+        l, r = 0, len(a) - 1
+        
+        while l < r and a[l] == b[r]:
+            l += 1
+            r -= 1
+            
+        return self.isPalindrome(a[:l] + b[l:]) or self.isPalindrome(a[:r + 1] + b[r+ 1:])
+    
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
