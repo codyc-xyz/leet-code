2a6f8bfac9191748cb5bbf02fda7bc2024eb2ed0
Red <crabmarket56@gmail.com>
2022-12-11 18:33:07 +0900

    Initialize file

---
+++ two-pointers/problems/1237_find_positive_integer_solution_for_a_given_equation.py
@@ -0,0 +1,9 @@
+# Given a callable function f(x, y) with a hidden formula and a value z, reverse engineer the formula and return all positive integer pairs x and y where f(x,y) == z. 
+# You may return the pairs in any order.
+
+# While the exact formula is hidden, the function is monotonically increasing, i.e.:
+
+# f(x, y) < f(x + 1, y)
+# f(x, y) < f(x, y + 1)
+
+
