9a447229ac81f5ee85898b01b7a848a1d0840bc5
Red <crabmarket56@gmail.com>
2022-12-12 14:25:23 +0900

    Clean code

--- two-pointers/problems/88_merge_sorted_array.py
+++ two-pointers/problems/88_merge_sorted_array.py
@@ -11,21 +11,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-      
-        a, b = m - 1, n - 1
         
+        a, b = m - 1, n - 1
         while a >= 0 and b >= 0:
-            if nums1[a] >= nums2[b]:
-                nums1[a + b + 1] = nums1[a]
-                a -= 1
+            if nums1[a] <= nums2[b]:
+                nums1[a + b + 1] = nums2[b]
+                b -= 1
             else:
-                nums1[a + b + 1] = nums2[b]
-                b -= 1
-        if a < 0:
-            while b >= 0:
-                nums1[a + b + 1] = nums2[b]
-                b -= 1
-        else:
-            while a >= 0:
                 nums1[a + b + 1] = nums1[a]
                 a -= 1
+        
+        while a >= 0:
+            nums1[a + b + 1] = nums1[a]
+            a -= 1
+            
+        while b >= 0:
+            nums1[a + b + 1] = nums2[b]
+            b -= 1
