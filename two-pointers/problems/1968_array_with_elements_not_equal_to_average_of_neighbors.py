c4d5696e5e242643a76585f51ce918aee71920f5
Red <crabmarket56@gmail.com>
2022-12-13 21:23:30 +0900

    Add initial solution

--- two-pointers/problems/1968_array_with_elements_not_equal_to_average_of_neighbors.py
+++ two-pointers/problems/1968_array_with_elements_not_equal_to_average_of_neighbors.py
@@ -5,3 +5,15 @@
 
 # Return any rearrangement of nums that meets the requirements.
 
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        i = 1
+        while i < len(nums) - 1:
+            l, r = i - 1, i + 1
+            if nums[i] == (nums[l] + nums[r]) / 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                i -= 1
+            else:
+                i += 1
+            
+        return nums
