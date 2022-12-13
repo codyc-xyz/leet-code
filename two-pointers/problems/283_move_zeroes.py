4068d6e06c29248e198011b09a707dc2c085bd15
Red <crabmarket56@gmail.com>
2022-12-13 15:46:31 +0900

    Add alternative solution

--- two-pointers/problems/283_move_zeroes.py
+++ two-pointers/problems/283_move_zeroes.py
@@ -18,3 +18,16 @@ class Solution:
         
         for i in range(pops):
             nums.append(0)
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """   
+        l = r = 0
+        while r < len(nums):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
