345eb5f2f185b505d74f5b19288f7b3c53951fe6
Red <crabmarket56@gmail.com>
2022-12-14 15:19:03 +0900

    Add alternative solution

--- two-pointers/problems/42_trapping_rain_water.py
+++ two-pointers/problems/42_trapping_rain_water.py
@@ -19,3 +19,19 @@ class Solution:
                     ans += rMax - height[r]
                 r -= 1
         return ans
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[l], height[r]
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                ans += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                ans += maxR - height[r]
+        return ans
