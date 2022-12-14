cb3e1d171ebd8f001871ea5783490602979ba05f
Red <crabmarket56@gmail.com>
2022-12-14 21:35:12 +0900

    Add more efficient solution

--- sliding-window/problems/567_permutation_in_a_string.py
+++ sliding-window/problems/567_permutation_in_a_string.py
@@ -10,3 +10,32 @@ class Solution:
             if sorted(s2[windowStart:windowStart + length]) == s1:
                 return True
         return False
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        counter1 = defaultdict(int)
+        counter2 = defaultdict(int)
+        if len(s1) > len(s2):
+            return False
+        for i in range(len(s1)):
+            counter1[s1[i]] += 1
+        
+        for i in range(len(s1)):
+            counter2[s2[i]] += 1
+            
+        if counter1 == counter2:
+            return True
+        windowStart, windowEnd = 0, len(s1)
+        
+        while windowEnd < len(s2):
+            counter2[s2[windowStart]] -= 1
+            if counter2[s2[windowStart]] == 0:
+                del counter2[s2[windowStart]]
+            counter2[s2[windowEnd]] += 1
+            if counter1 == counter2:
+                return True
+            windowStart += 1
+            windowEnd += 1
+        return False
+            
