454cd2988d9e9a0868a01610372b6029bb45ecbe
Red <crabmarket56@gmail.com>
2022-12-15 17:30:06 +0900

    Refactor solution

--- sliding-window/problems/76_minimum_window_substring.py
+++ sliding-window/problems/76_minimum_window_substring.py
@@ -7,25 +7,29 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         countT = collections.Counter(t)
         countS = defaultdict(int)
-        seen = deque()
+        seen = set()
         windowStart = 0
         minLen = float("inf")
-        res = ""
-        if s == t:
-            return t
+        chars = len(set(t))
+        r = 0
         for windowEnd, n in enumerate(s):
             countS[n] += 1
             
             if n not in seen and n in countT and countS[n] >= countT[n]:
-                seen.append(n)
+                seen.add(n)
+            else:
+                continue
                 
-            while len(seen) == len(set(t)):
+            while len(seen) == chars:
                 if windowEnd - windowStart + 1 < minLen:
-                    res = s[windowStart:windowEnd + 1]
+                    l, r = windowStart, windowEnd + 1
                     minLen = windowEnd - windowStart + 1
                 countS[s[windowStart]] -= 1
                 if s[windowStart] in countT and countS[s[windowStart]] < countT[s[windowStart]]:
                     seen.remove(s[windowStart])
                 windowStart += 1
                 
-        return res
+        if r != 0:
+            return s[l:r]
+        else:
+            return ""
