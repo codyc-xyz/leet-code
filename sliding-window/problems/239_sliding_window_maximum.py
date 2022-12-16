a338572250fc88b3ad0b16050498dcbd75019a8b
Red <crabmarket56@gmail.com>
2022-12-16 16:45:14 +0900

    Refactor solution

--- sliding-window/problems/239_sliding_window_maximum.py
+++ sliding-window/problems/239_sliding_window_maximum.py
@@ -4,8 +4,20 @@
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()
+        l = r = 0
         ans = []
-        for i in range(len(nums) - k + 1):
-            ans.append(max(nums[i:i+k]))
-        
+        while r < len(nums):
+            while dq and nums[dq[-1]] < nums[r]:
+                dq.pop()  
+                
+            dq.append(r)
+            
+            if l > dq[0]:
+                dq.popleft()
+                
+            if (r + 1) >= k:
+                ans.append(nums[dq[0]])
+                l += 1
+            r += 1
         return ans
