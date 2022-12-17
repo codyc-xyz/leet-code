01d566826f50dabedf1b6c3cc96652dc9505e67b
Red <crabmarket56@gmail.com>
2022-12-17 17:00:28 +0900

    Add initial solution

--- stack/problems/234_palindrome_linked_list.py
+++ stack/problems/234_palindrome_linked_list.py
@@ -1,2 +1,15 @@
 # Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        curr = head
+        dq = deque()
+        while curr:
+            dq.append(curr.val)
+            curr = curr.next
+        
+        while dq:
+            if head.val != dq.pop():
+                return False
+            head = head.next
+        return True
