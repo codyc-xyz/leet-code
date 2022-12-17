5cc9f31528977506b3141a9285f076b178e8fc16
Red <crabmarket56@gmail.com>
2022-12-17 16:48:11 +0900

    Add initial solution

--- stack/problems/225_implement_stack_using_queues.py
+++ stack/problems/225_implement_stack_using_queues.py
@@ -10,3 +10,30 @@
 # You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
 # Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
 
+class MyStack:
+
+    def __init__(self):
+        self.q1 = []
+        self.q2 = []
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+            
+    def pop(self) -> int:
+        return self.q1.pop()
+        
+
+    def top(self) -> int:
+        return self.q1[-1]
+
+    def empty(self) -> bool:
+        return not self.q1
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
