a2f61b6558506f5f52cbf086dc3c70f8cbaa1f7f
Red <crabmarket56@gmail.com>
2022-12-17 16:55:32 +0900

    Refactor solution

--- stack/problems/225_implement_stack_using_queues.py
+++ stack/problems/225_implement_stack_using_queues.py
@@ -13,14 +13,15 @@
 class MyStack:
 
     def __init__(self):
-        self.q1 = []
-        self.q2 = []
+        self.q1 = deque()
 
     def push(self, x: int) -> None:
         self.q1.append(x)
             
     def pop(self) -> int:
-        return self.q1.pop()
+        for i in range(len(self.q1) - 1):
+            self.q1.append(self.q1.popleft())
+        return self.q1.popleft()
         
 
     def top(self) -> int:
