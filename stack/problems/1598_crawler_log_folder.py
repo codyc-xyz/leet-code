d3d165d05282fb3bf57491c40caa10123553b93e
Red <crabmarket56@gmail.com>
2022-12-17 15:57:34 +0900

    Initialize file

---
+++ stack/problems/1598_crawler_log_folder.py
@@ -0,0 +1,12 @@
+# The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+# The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+# "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+# "./" : Remain in the same folder.
+# "./" : Remain in the same folder.
+
+# You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+# You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+# You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
