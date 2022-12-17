4e2b1bc7d5198f2c516752a4a05afcb504d92e1d
Red <crabmarket56@gmail.com>
2022-12-17 14:16:30 +0900

    Add initial solution

--- stack/problems/496_next_greater_element_I.py
+++ stack/problems/496_next_greater_element_I.py
@@ -6,3 +6,23 @@
 # If there is no next greater element, then the answer for this query is -1.
 
 # Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = []
+
+        for i, n in enumerate(nums1):
+            j = 0
+            res = -1
+            while j < len(nums2):
+                if nums2[j] == n:
+                    t = j + 1
+                    break
+                j += 1
+            while t < len(nums2):
+                if nums2[t] > nums2[j]:
+                    res = nums2[t]
+                    break
+                t += 1
+            ans.append(res)
+        return ans
