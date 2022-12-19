826c05a990613598c4557b198cb093ce428d7aec
Red <crabmarket56@gmail.com>
2022-12-19 14:03:06 +0900

    Add initial solution

--- stack/problems/20_valid_parentheses.py
+++ stack/problems/20_valid_parentheses.py
@@ -4,3 +4,28 @@
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
 # Every close bracket has a corresponding open bracket of the same type.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        dq = deque()
+    
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                dq.append(c)
+            elif c == '}':
+                if not dq or dq[-1] != '{':
+                    return False
+                else:
+                    dq.pop()
+            elif c == ')':
+                if not dq or dq[-1] != '(':
+                    return False
+                else:
+                    dq.pop()
+            elif c == ']':
+                if not dq or dq[-1] != '[':
+                    return False
+                else:
+                    dq.pop()
+        return len(dq) == 0
