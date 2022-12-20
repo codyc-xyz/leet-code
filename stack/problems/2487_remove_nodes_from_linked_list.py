43bbbf5c083cfd405ffff99749406ff9ff75f195
Red <crabmarket56@gmail.com>
2022-12-20 15:35:32 +0900

    Add initial solution

--- stack/problems/2487_remove_nodes_from_linked_list.py
+++ stack/problems/2487_remove_nodes_from_linked_list.py
@@ -4,3 +4,26 @@
 
 # Return the head of the modified linked list.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        stack = deque()
+        tmp = head
+        while tmp:
+            while stack and tmp.val > stack[-1].val:
+                stack.pop()
+            stack.append(tmp)
+            tmp = tmp.next
+        
+        dummy = ans = ListNode(0)
+        
+        while stack:
+            dummy.next = stack.popleft()
+            dummy = dummy.next
+        return ans.next
+        
