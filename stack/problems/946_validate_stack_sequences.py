a36c533aae8646c1602909462671a4d69291fa45
Red <crabmarket56@gmail.com>
2022-12-20 16:21:17 +0900

    Add initial solution

--- stack/problems/946_validate_stack_sequences.py
+++ stack/problems/946_validate_stack_sequences.py
@@ -1,2 +1,14 @@
 # Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
 
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        
+        j = 0
+        stack = []
+        
+        for n in pushed:
+            stack.append(n)
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return not stack
