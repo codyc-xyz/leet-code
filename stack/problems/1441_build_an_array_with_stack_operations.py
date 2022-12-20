8b8264c81cb5ed4ee45adb7945b0fde4c7d80f31
Red <crabmarket56@gmail.com>
2022-12-20 15:12:33 +0900

    Add initial solution

--- stack/problems/1441_build_an_array_with_stack_operations.py
+++ stack/problems/1441_build_an_array_with_stack_operations.py
@@ -13,3 +13,22 @@
 # If, at any moment, the elements in the stack (from the bottom to the top) are equal to target, do not read new integers from the stream and do not do more operations on the stack.
 
 # Return the stack operations needed to build target following the mentioned rules. If there are multiple valid answers, return any of them.
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        
+        i = 1
+        ans = []
+        res = []
+        j = 0
+        for i in range(1, n + 1):
+            res.append(i)
+            ans.append("Push")
+            if j < len(target) and res[j] != target[j]:
+                res.pop()
+                ans.append("Pop")
+            elif j < len(target) and res[j] == target[j]:
+                j += 1
+            if res == target:
+                break
+        return ans
