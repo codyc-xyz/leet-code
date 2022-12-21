5eabb77142051935cb57260443447176ee0a3d0f
Red <crabmarket56@gmail.com>
2022-12-21 14:50:59 +0900

    Add initial solution

--- stack/problems/1249_minimum_remove_to_make_valid_parentheses.py
+++ stack/problems/1249_minimum_remove_to_make_valid_parentheses.py
@@ -6,3 +6,25 @@
 # It is the empty string, contains only lowercase characters, or
 # It can be written as AB (A concatenated with B), where A and B are valid strings, or
 # It can be written as (A), where A is a valid string.
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        opens = 0
+        for c in s:
+            if c == ')' and opens < 1:
+                continue
+            elif c == ')':
+                opens -= 1
+            elif c == '(':
+                opens += 1
+            stack.append(c)
+            
+        r = len(stack) - 1
+        while opens > 0:
+            if stack[r] == '(':
+                stack.pop(r)
+                opens -= 1
+            r -= 1 
+        
+        return "".join(stack)
