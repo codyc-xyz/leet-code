f58973b72a8f3f791a5f0822ec126dfe4a1954c0
Red <crabmarket56@gmail.com>
2022-12-22 16:39:54 +0900

    Add initial solution

--- stack/problems/1019_next_greater_node_in_linked_list.py
+++ stack/problems/1019_next_greater_node_in_linked_list.py
@@ -6,3 +6,27 @@
 # Return an integer array answer where answer[i] is the value of the next greater node of the ith node (1-indexed). 
 # If the ith node does not have a next greater node, set answer[i] = 0.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        
+        curr = lenCheck = head
+        i = length = 0
+        while lenCheck:
+            lenCheck = lenCheck.next
+            length += 1
+            
+        ans = [0] * length
+        stack = []
+        while curr:
+            while stack and curr.val > stack[-1][0]:
+                ans[stack[-1][1]] = curr.val
+                stack.pop()
+            stack.append([curr.val, i])
+            i += 1
+            curr = curr.next
+        return ans
