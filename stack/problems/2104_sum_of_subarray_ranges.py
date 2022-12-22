7c9b5d7ad6a413975961eadc518486c783c367c2
Red <crabmarket56@gmail.com>
2022-12-22 16:25:42 +0900

    Add inital solution

--- stack/problems/2104_sum_of_subarray_ranges.py
+++ stack/problems/2104_sum_of_subarray_ranges.py
@@ -4,3 +4,18 @@
 
 # A subarray is a contiguous non-empty sequence of elements within an array.
 
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        stack = deque()
+        ans = 0
+        
+        for i, n in enumerate(nums):
+            smallest = n
+            largest = n
+            right = i
+            while right < len(nums):
+                smallest = min(smallest, nums[right])
+                largest = max(largest, nums[right])
+                ans += largest - smallest
+                right += 1
+        return ans
