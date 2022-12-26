6c4a5597ef079c303c4fa0032b434f2da6064d93
Red <crabmarket56@gmail.com>
2022-12-26 14:51:00 +0900

    Add initial solution

--- stack/problems/1081_smallest_subsequence_of_distinct_characters.py
+++ stack/problems/1081_smallest_subsequence_of_distinct_characters.py
@@ -1,2 +1,14 @@
 # Given a string s, return the lexicographically smallest subsequence of s that contains all the distinct characters of s exactly once.
 
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        stack = []
+        hm = {}
+        for i,c in enumerate(s):
+            hm[c] = i
+        for i, c in enumerate(s):
+            while stack and c not in stack and c < stack[-1] and hm[stack[-1]] > i:
+                stack.pop()
+            if c not in stack:
+                stack.append(c)
+        return "".join(stack)
