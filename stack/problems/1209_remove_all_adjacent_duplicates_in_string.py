b5fd4b8049e53cc2cc876b6b78074786b50055c0
Red <crabmarket56@gmail.com>
2022-12-26 15:10:00 +0900

    Add initial solution

--- stack/problems/1209_remove_all_adjacent_duplicates_in_string.py
+++ stack/problems/1209_remove_all_adjacent_duplicates_in_string.py
@@ -4,3 +4,19 @@
 
 # Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
 
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = deque()
+        ans = ""
+        for c in s:
+            if stack and c == stack[-1][0]:
+                stack.append([c, stack[-1][1] + 1])
+            else:
+                stack.append([c, 1])
+            if stack[-1][1] == k:
+                for i in range(k):
+                    stack.pop()
+            
+        while stack:
+            ans += stack.popleft()[0]
+        return ans
