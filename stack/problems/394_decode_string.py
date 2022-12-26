e38ddfb0fb4ea814c97ebdd5569c64a309ebe4db
Red <crabmarket56@gmail.com>
2022-12-26 10:56:08 +0900

    Add initial solution (incorrect)

--- stack/problems/394_decode_string.py
+++ stack/problems/394_decode_string.py
@@ -7,3 +7,33 @@
 
 # The test cases are generated so that the length of the output will never exceed 105.
 
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        ans = ""
+        opened = 0
+        for c in s:
+            if c == '[':
+                opened += 1
+            elif opened == 0 and c.isalpha() == True:
+                ans += c
+            if c != ']':
+                stack.append(c)
+            else:
+                num = ""
+                string = ""
+                opened -= 1
+                while stack[-1].isalpha() == True:
+                    string += stack.pop()
+                stack.pop()
+                while stack and stack[-1].isnumeric() == True:
+                    num += stack.pop()
+                num = num[::-1]
+                if opened > 0:
+                    for i in range(int(num)):
+                        for c in string:
+                            stack.append(c)
+                else:
+                    string = string[::-1]
+                    ans += int(num) * string
+        return ans
