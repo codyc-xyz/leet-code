7e1ca02bbb855343f3cad95d02e2e4e013354624
Red <crabmarket56@gmail.com>
2022-12-27 17:04:34 +0900

    Add initial solution

--- stack/problems/1541_minimum_insertions_to_balance_a_parentheses_string.py
+++ stack/problems/1541_minimum_insertions_to_balance_a_parentheses_string.py
@@ -8,3 +8,29 @@
 # You can insert the characters '(' and ')' at any position of the string to balance it if needed.
 
 # Return the minimum number of insertions needed to make s balanced.
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        
+        stack = []
+        i = ans = 0
+        
+        while i < len(s):
+            
+            c = s[i]
+            
+            if c == '(':
+                stack.append(c)
+            elif c == ')':
+                if not stack:
+                    stack.append('(')
+                    ans += 1
+                if i < len(s) - 1 and s[i+1] == ')':
+                    stack.pop()
+                    i += 1
+                else:
+                    ans += 1
+                    stack.pop()
+            i += 1
+            
+        return ans + len(stack) * 2
