c3addd384876297571d328fd61e3796ebe18e6fb
Red <crabmarket56@gmail.com>
2022-12-27 13:09:25 +0900

    Add initial solution

--- stack/problems/155_min_stack.py
+++ stack/problems/155_min_stack.py
@@ -9,3 +9,28 @@
 
 # You must implement a solution with O(1) time complexity for each function.
 
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return min(self.stack)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
