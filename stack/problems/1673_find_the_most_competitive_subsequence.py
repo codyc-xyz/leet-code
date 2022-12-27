d684ed918161cbb21c7a3cbe9e86bb46879005f1
Red <crabmarket56@gmail.com>
2022-12-27 17:46:57 +0900

    Add initial solution

--- stack/problems/1673_find_the_most_competitive_subsequence.py
+++ stack/problems/1673_find_the_most_competitive_subsequence.py
@@ -5,3 +5,16 @@
 # We define that a subsequence a is more competitive than a subsequence b (of the same length) if in the first position where a and b differ, 
 # subsequence a has a number less than the corresponding number in b. For example, [1,3,4] is more competitive than [1,3,5] because the first position they differ is at the final number, and 4 is less than 5.
 
+class Solution:
+    def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
+        stack = []
+        
+        for i in range(len(nums)):
+            t = len(nums) - i 
+            while stack and nums[i] < stack[-1] and len(stack) - 1  + t >= k:
+                stack.pop()
+    
+            if len(stack) < k:
+                stack.append(nums[i])
+        
+        return stack
