998915815fee9398f62cd9ac847e437917d4d1e9
Red <crabmarket56@gmail.com>
2022-12-28 13:20:36 +0900

    Add initial solution

--- stack/problems/962_maximum_width_ramp.py
+++ stack/problems/962_maximum_width_ramp.py
@@ -2,3 +2,26 @@
 
 # Given an integer array nums, return the maximum width of a ramp in nums. If there is no ramp in nums, return 0.
 
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        ramp = 0
+        decrease = []
+        increase = []
+        j = len(nums) - 1
+    
+        for i, n in enumerate(nums):
+            if not decrease or n < decrease[-1][0]:
+                decrease.append([n, i])
+                
+        for i, n in enumerate(nums[::-1]):
+            if not increase or n > increase[-1][0]:
+                increase.append([n, j - i])
+        
+        
+        for i in range(len(increase)):
+            dec = 0
+            while increase[i][0] < decrease[dec][0]:
+                dec += 1
+            ramp = max(ramp, increase[i][1] - decrease[dec][1])
+        
+        return ramp
