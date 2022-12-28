a61f7a951b1908d65b2dd6e0033477f255efaf03
Red <crabmarket56@gmail.com>
2022-12-28 14:24:08 +0900

    Add initial solution

--- stack/problems/2216_minimum_deletions_to_make_array_beautiful.py
+++ stack/problems/2216_minimum_deletions_to_make_array_beautiful.py
@@ -9,3 +9,18 @@
 
 # Return the minimum number of elements to delete from nums to make it beautiful.
 
+class Solution:
+    def minDeletion(self, nums: List[int]) -> int:
+        stack = []
+        deletions = 0
+        for i, n in enumerate(nums):
+            
+            if stack and n == stack[-1][0] and stack[-1][1] % 2 == 0:
+                deletions += 1
+                continue
+            stack.append([n, len(stack)])
+            
+        if len(stack) % 2 == 1:
+            deletions += 1
+        
+        return deletions
