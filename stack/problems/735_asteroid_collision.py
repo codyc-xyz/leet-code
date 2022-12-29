767bbea61ded6edf14701ff3dc84ef1f4d86852c
Red <crabmarket56@gmail.com>
2022-12-29 14:10:44 +0900

    Add initial solution

--- stack/problems/735_asteroid_collision.py
+++ stack/problems/735_asteroid_collision.py
@@ -4,3 +4,21 @@
 
 # Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
 
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stack = []
+        
+        for a in asteroids:
+            flag = False
+            while stack and stack[-1] >= 0 and a < 0 and flag == False:
+                if abs(a) > abs(stack[-1]):
+                    stack.pop()
+                elif abs(a) < abs(stack[-1]):
+                    flag = True
+                else:
+                    stack.pop()
+                    flag = True
+            if flag == False:
+                stack.append(a)
+        return stack
