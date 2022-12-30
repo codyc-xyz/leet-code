51b7ef4b5d9789305f06eea2c40acb486e582b30
Red <crabmarket56@gmail.com>
2022-12-30 15:57:15 +0900

    Add initial solution

--- stack/problems/581_shortest_continuous_subarray.py
+++ stack/problems/581_shortest_continuous_subarray.py
@@ -2,3 +2,21 @@
 
 # Return the shortest such subarray and output its length.
 
+class Solution:
+    def findUnsortedSubarray(self, nums):
+        stack = []
+        start = len(nums)
+        end = 0
+
+        for i, n in enumerate(nums):
+            while stack and stack[-1][1] > n:
+                start = min(start, stack.pop()[0])
+            stack.append([i, n])
+            
+        for i in range(len(nums) - 1, -1, -1):
+            while stack and stack[-1][1] < nums[i]:
+                end = max(end, stack.pop()[0])
+            stack.append([i, nums[i]])
+        if end == 0 and start == len(nums):
+            return 0
+        return end - start + 1
