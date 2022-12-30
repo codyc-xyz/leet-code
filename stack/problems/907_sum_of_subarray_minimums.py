718ce60ffa5b663c8c36963e1863a7a3db9eba26
Red <crabmarket56@gmail.com>
2022-12-30 16:16:28 +0900

    Add brute force solution

--- stack/problems/907_sum_of_subarray_minimums.py
+++ stack/problems/907_sum_of_subarray_minimums.py
@@ -1,2 +1,13 @@
 # Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
 
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        arrSums = 0
+        for i, n in enumerate(arr):
+            j = i
+            minN = float("inf")
+            while j < len(arr):
+                minN = min(minN, arr[j])
+                arrSums += minN
+                j += 1
+        return arrSums%(10**9+7)
