f06ee13bc3b968e6b18acefc98957233f0ba4f22
Red <crabmarket56@gmail.com>
2022-12-31 18:17:51 +0900

    Add initial solution

--- stack/problems/678_valid_parenthesis_string.py
+++ stack/problems/678_valid_parenthesis_string.py
@@ -5,3 +5,33 @@
 # Any right parenthesis ')' must have a corresponding left parenthesis '('.
 # Left parenthesis '(' must go before the corresponding right parenthesis ')'.
 # '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack = []
+        star = []
+  
+        for i, n in enumerate(s):
+            if n == '(':
+                stack.append(i)
+            elif n == ')':
+                if stack:
+                    stack.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+            else:
+                star.append(i)
+        
+        if not stack:
+            return True
+        elif star:
+            while star and stack and star[-1] > stack[-1]:
+                star.pop()
+                stack.pop()
+            if stack:
+                return False
+            return True
+        else:
+            return False
