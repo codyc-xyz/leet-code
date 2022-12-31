b112268ac9fb838bac7bbd1f46be25db3cb94948
Red <crabmarket56@gmail.com>
2022-12-31 16:09:23 +0900

    Add initial solution

--- stack/problems/1124_longest_well-performing_interval.py
+++ stack/problems/1124_longest_well-performing_interval.py
@@ -5,3 +5,26 @@
 # A well-performing interval is an interval of days for which the number of tiring days is strictly larger than the number of non-tiring days.
 
 # Return the length of the longest well-performing interval.
+
+class Solution:
+    def longestWPI(self, hours: List[int]) -> int:
+        interval = 0
+        psum = [0]
+        seen = {}
+        
+        for i, n in enumerate(hours):
+        
+            if n > 8:
+                psum.append(psum[-1] + 1)
+            else:
+                psum.append(psum[-1] - 1)
+            
+        for i, n in enumerate(psum):
+            if n > 0:
+                interval = max(interval, i)
+            if n not in seen:
+                seen[n] = i
+            if n - 1 in seen:
+                interval = max(interval, i - seen[n - 1])
+                
+        return interval
