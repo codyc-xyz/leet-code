c757937af1e4fc8fa9628d614a93b0b145fa495d
Red <crabmarket56@gmail.com>
2023-01-01 12:09:46 +0900

    Add initial brute force solution

--- stack/problems/880_decoded_string_at_index.py
+++ stack/problems/880_decoded_string_at_index.py
@@ -5,3 +5,20 @@
 
 # Given an integer k, return the kth letter (1-indexed) in the decoded string.
 
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        stack = []
+        for c in s:
+            if c.isdigit():
+                n = len(stack)
+                c = int(c)
+                while c - 1 > 0:
+                    for i in range(n):
+                        stack.append(stack[i])
+                        if len(stack) > k - 1:
+                            return stack[k-1]
+                    c -= 1                
+            else:
+                stack.append(c)
+                
+        return stack[k - 1]
