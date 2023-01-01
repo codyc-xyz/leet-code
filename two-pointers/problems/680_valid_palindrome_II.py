625b23e30e46c440e54427503c6139f479c6c2c0
Red <crabmarket56@gmail.com>
2023-01-01 19:34:36 +0900

    Refactor solution

--- two-pointers/problems/680_valid_palindrome_II.py
+++ two-pointers/problems/680_valid_palindrome_II.py
@@ -2,13 +2,10 @@
 
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        left, right = 0, len(s) - 1
-        
-        while right >= left:
-            if s[right] != s[left]:
-                string1 = s[:left] + s[left+1:]
-                string2 = s[:right] + s[right+1:]
-                return string1 == string1[::-1] or string2 == string2[::-1]
-            right -= 1
-            left += 1
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return s[l:r] == s[l:r][::-1] or s[l + 1:r + 1] == s[l+1:r+1][::-1]
+            l += 1
+            r -= 1
         return True
