e20a7cc2ee5f11c07068c5cd700b4530c5990863
Red <crabmarket56@gmail.com>
2023-01-01 19:36:47 +0900

    Add initial solution

--- two-pointers/problems/1984_minimum_difference_between_highest_and_lowest_k_scores.py
+++ two-pointers/problems/1984_minimum_difference_between_highest_and_lowest_k_scores.py
@@ -4,3 +4,14 @@
 
 # Return the minimum possible difference.
 
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        
+        l, r = 0, k -1
+        minDiff = float("inf")
+        while r < len(nums):
+            minDiff = min(minDiff, nums[r] - nums[l])
+            r += 1
+            l += 1
+        return minDiff
