7437d5817280843c7cfda01d8bcd60d61919f19b
Red <crabmarket56@gmail.com>
2023-01-01 14:23:25 +0900

    Add initial solution

--- stack/problems/2289_steps_to_make_array_non_decreasing.py
+++ stack/problems/2289_steps_to_make_array_non_decreasing.py
@@ -2,3 +2,15 @@
 
 # Return the number of steps performed until nums becomes a non-decreasing array.
 
+class Solution:
+    def totalSteps(self, nums: List[int]) -> int:
+        stack = [[nums[-1], 0]]
+        ans = 0
+        
+        for i in range(len(nums) -2, -1, -1):
+            count = 0
+            while stack and nums[i] > stack[-1][0]:
+                count = max(count + 1, stack.pop()[1])
+            ans = max(ans, count)
+            stack.append([nums[i], count])
+        return ans
