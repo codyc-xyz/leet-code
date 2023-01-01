814855507b3b3d9cfbb604e81d5e60790cd8cc13
Red <crabmarket56@gmail.com>
2023-01-01 11:57:37 +0900

    Add initial solution

--- stack/problems/402_remove_k_digits.py
+++ stack/problems/402_remove_k_digits.py
@@ -1,2 +1,21 @@
 # Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
 
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        
+        if len(num) <= k:
+            return '0'
+        
+        for n in num:
+            while stack and k and stack[-1] > n:
+                stack.pop()
+                k -= 1
+            if int(n) or stack:
+                stack.append(n)
+        if k:
+            stack = stack[:-k]
+        if stack:
+            return "".join(stack)
+        else:
+            return '0'
