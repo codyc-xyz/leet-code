705868c3cab8340f759a6b0e238d0efcf3082b29
Red <crabmarket56@gmail.com>
2023-01-02 10:29:17 +0900

    Add alternative solution

--- two-pointers/problems/15_3sum.py
+++ two-pointers/problems/15_3sum.py
@@ -24,3 +24,22 @@ class Solution:
                         right -= 1
             i += 1
         return res
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(len(nums) - 2):
+            l, r = i + 1, len(nums) - 1
+            
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                elif threeSum == 0:
+                    if [nums[i], nums[l], nums[r]] not in ans:
+                        ans.append([nums[i], nums[l], nums[r]])
+                    l += 1
+        return ans
