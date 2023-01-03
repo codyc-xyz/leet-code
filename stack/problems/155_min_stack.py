5199d0b6027c1960efd0a5a4169d45a769c49abd
Red <crabmarket56@gmail.com>
2023-01-03 16:13:13 +0900

    Add alternative solution

--- stack/problems/155_min_stack.py
+++ stack/problems/155_min_stack.py
@@ -33,3 +33,25 @@ class MinStack:
         
     def getMin(self) -> int:
         return self.minStack[-1]
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minVal = float("inf")
+
+    def push(self, val: int) -> None:
+        if self.stack:
+            self.minVal = min(self.stack[-1][1], val)
+        else:
+            self.minVal = val
+        self.stack.append([val, self.minVal])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+    def getMin(self) -> int:
+        return self.stack[-1][1]
