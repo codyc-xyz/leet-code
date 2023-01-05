b7115185fbb0e5785b7bff9c8400d6cc51bca029
Red <crabmarket56@gmail.com>
2023-01-05 20:59:34 +0900

    Add alternative solution

--- two-pointers/problems/27_remove_element.py
+++ two-pointers/problems/27_remove_element.py
@@ -12,3 +12,24 @@ class Solution:
                 right -= 1
                 left -= 1
             left += 1
+            
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        l, r = 0, len(nums) - 1
+  
+        if len(nums) < 1:
+            return len(nums)
+        count = 0
+        while l <=  r:
+            if nums[l] == val:
+                nums[l], nums[r] = nums[r], nums[l]
+                r -= 1
+                count += 1
+            elif nums[l] != val:
+                l += 1
+            elif nums[r] == val:
+                r -= 1
+                count += 1 
+            
+        return len(nums) - count
