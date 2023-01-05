62a2af890aac9743955bb8c1f1f84bf0088fc99e
Red <crabmarket56@gmail.com>
2023-01-05 16:48:54 +0900

    Add initial solution

--- arrays_and_hashing/problems/14_longest_common_prefix.py
+++ arrays_and_hashing/problems/14_longest_common_prefix.py
@@ -2,3 +2,17 @@
 
 # If there is no common prefix, return an empty string "".
 
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        minLen = float("inf")
+        
+        for s in strs:
+            minLen = min(minLen, len(s))
+        
+        for i in range(minLen):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += s[i]
+        return res
