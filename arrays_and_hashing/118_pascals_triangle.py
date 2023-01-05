d5c5e7e61e72c35da5834b9bb1e8b1fb7184bf6f
Red <crabmarket56@gmail.com>
2023-01-05 17:41:24 +0900

    Add initial solution

--- arrays_and_hashing/118_pascals_triangle.py
+++ arrays_and_hashing/118_pascals_triangle.py
@@ -2,3 +2,22 @@
 
 # In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return [[1]]
+        else:
+            triangle = [[1], [1, 1]]
+        
+        for i in range(2, numRows):
+            row = []
+            for i in range(len(triangle[-1]) + 1):
+                if i == 0 or i == len(triangle[-1]):
+                    row.append(1)
+                else:
+                    row.append(triangle[-1][i - 1] + triangle[-1][i])
+            triangle.append(row)
+        return triangle
