8151f91a03729ab6addbb27d569ffac6e40a9bc8
Red <crabmarket56@gmail.com>
2023-01-05 16:51:27 +0900

    Add alternative solution

--- arrays_and_hashing/problems/14_longest_common_prefix.py
+++ arrays_and_hashing/problems/14_longest_common_prefix.py
@@ -16,3 +16,16 @@ class Solution:
                     return res
             res += s[i]
         return res
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        short = min(strs, key=len)
+        if not strs:
+            return ""
+        
+        for i, c in enumerate(short):
+            for s in strs:
+                if s[i] != c:
+                    return short[:i]
+        return short
