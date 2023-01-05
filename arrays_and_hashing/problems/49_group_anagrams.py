2fdd1c8834fcdc3e54a86a6b2e2f9b397d54918e
Red <crabmarket56@gmail.com>
2023-01-05 17:14:15 +0900

    Add initial solution

--- arrays_and_hashing/problems/49_group_anagrams.py
+++ arrays_and_hashing/problems/49_group_anagrams.py
@@ -2,3 +2,12 @@
 
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        res = defaultdict(list)  
+        for s in strs:
+            ordered = sorted(s)
+            res[tuple(ordered)].append(s)
+        
+        return res.values()
