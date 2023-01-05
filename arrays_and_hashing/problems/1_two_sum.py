743e5dbdf8805342d82835f9714b324a57b22f54
Red <crabmarket56@gmail.com>
2023-01-05 15:56:14 +0900

    Add initial solution

--- arrays_and_hashing/problems/1_two_sum.py
+++ arrays_and_hashing/problems/1_two_sum.py
@@ -4,3 +4,13 @@
 
 # You can return the answer in any order.
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hm = {}
+        
+        for i, n in enumerate(nums):
+            if target - n in hm:
+                return [hm[target - n], i]
+            else:
+                hm[n] = i
