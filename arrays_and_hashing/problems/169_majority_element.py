e6db3f0b91a5852b750e570849526cfa46a389f8
Red <crabmarket56@gmail.com>
2023-01-06 15:47:50 +0900

    Add initial solution

--- arrays_and_hashing/problems/169_majority_element.py
+++ arrays_and_hashing/problems/169_majority_element.py
@@ -2,3 +2,14 @@
 
 # The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hm = {}
+        for n in nums:
+            if n in hm:
+                hm[n] += 1
+                if hm[n] > len(nums) / 2:
+                    return n
+            else:
+                hm[n] = 1
+        return max(hm, key=hm.get) 
