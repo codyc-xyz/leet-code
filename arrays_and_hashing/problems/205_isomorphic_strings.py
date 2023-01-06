443f6b586845c447693316fe41ecbddd9b18523f
Red <crabmarket56@gmail.com>
2023-01-06 13:39:11 +0900

    Add iniital solution

--- arrays_and_hashing/problems/205_isomorphic_strings.py
+++ arrays_and_hashing/problems/205_isomorphic_strings.py
@@ -4,3 +4,16 @@
 
 # All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hmS = {}
+        hmT = {}
+        if len(s) != len(t):
+            return False
+        
+        for i, c in enumerate(s):
+            if c in hmS and hmS[c] != t[i] or t[i] in hmT and hmT[t[i]] != c:
+                return False
+            hmS[c] = t[i]
+            hmT[t[i]] = c
+        return True
