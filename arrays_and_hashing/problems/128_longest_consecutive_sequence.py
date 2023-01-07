779e39a8ddaee1a6938437d59a658f1f5d5fda08
Red <crabmarket56@gmail.com>
2023-01-08 08:45:17 +0900

    Add initial solution

--- arrays_and_hashing/problems/128_longest_consecutive_sequence.py
+++ arrays_and_hashing/problems/128_longest_consecutive_sequence.py
@@ -2,3 +2,21 @@
 
 # You must write an algorithm that runs in O(n) time.
 
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        arr = []
+        maxConsec = consec = 0
+        for i, c in enumerate(nums):
+            if i == 0:
+                consec += 1
+            else:
+                if c == arr[-1] + 1:
+                    consec += 1
+                elif c == arr[-1]:
+                    continue
+                else:
+                    consec = 1
+            arr.append(c)
+            maxConsec = max(maxConsec, consec)
+        return maxConsec
