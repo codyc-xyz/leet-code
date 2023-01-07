7fcb74f47bfe699cdaed2fce08d71882d398a40c
Red <crabmarket56@gmail.com>
2023-01-07 14:48:57 +0900

    Add initial solution

--- arrays_and_hashing/problems/347_top_k_frequent_elements.py
+++ arrays_and_hashing/problems/347_top_k_frequent_elements.py
@@ -1,2 +1,11 @@
 # Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = collections.Counter(nums)
+        ans = []
+        while k > 0:
+            ans.append(max(count, key=count.get))
+            del count[max(count, key=count.get)]
+            k -= 1
+        return ans
