0eaa59f6673de6674cc8668d3d8777355d817908
Red <crabmarket56@gmail.com>
2023-01-07 13:18:04 +0900

    Add initial solution

--- arrays_and_hashing/problems/1189_maximum_number_of_balloons.py
+++ arrays_and_hashing/problems/1189_maximum_number_of_balloons.py
@@ -2,3 +2,17 @@
 
 # You can use each character in text at most once. Return the maximum number of instances that can be formed.
 
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = defaultdict(int)
+        singles = {'b', 'a', 'n'}
+        doubles = {'o', 'l'}
+        for c in text:
+            if c in doubles:
+                count[c] += 0.5
+            elif c in singles:
+                count[c] += 1
+        if len(count) == 5:
+            return int(min(count.values()))
+        else:
+            return 0
