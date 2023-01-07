53a74a9bc3efd02855f64b33bcd038d6457d8236
Red <crabmarket56@gmail.com>
2023-01-07 18:42:08 +0900

    Add alternative solution

--- arrays_and_hashing/problems/238_product_of_array_except_self.py
+++ arrays_and_hashing/problems/238_product_of_array_except_self.py
@@ -17,3 +17,17 @@ class Solution:
                 prodsL.append(prodsL[-1] * nums[i - 1])
             ans.append(prodsL[-1] * prodsR[i])
         return ans
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
+        prefix = postfix = 1
+        
+        for i in range(len(nums)):
+            ans[i] = prefix
+            prefix *= nums[i]
+            
+        for i in range(len(nums) -1, -1, -1):
+            ans[i] *= postfix
+            postfix *= nums[i]
+        return ans
