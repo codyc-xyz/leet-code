72e1d726f276379a86b0a9c55f7a60228a4ffa6f
Red <crabmarket56@gmail.com>
2023-01-07 14:35:55 +0900

    Add initial solution

--- arrays_and_hashing/problems/290_word_pattern.py
+++ arrays_and_hashing/problems/290_word_pattern.py
@@ -2,3 +2,17 @@
 
 # Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        string = s.split(" ")
+        if len(pattern) != len(string):
+            return False
+        hmP = {}
+        hmS = {}
+        for i,c in enumerate(string):
+            if pattern[i] in hmP and hmP[pattern[i]] != c or c in hmS and hmS[c] != pattern[i]:
+                return False
+            elif pattern[i] not in hmP or c not in hmS:
+                hmP[pattern[i]] = c
+                hmS[c] = pattern[i]
+        return True
