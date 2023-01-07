580c38e73c03b42b384079173a4d813f2910723c
Red <crabmarket56@gmail.com>
2023-01-07 11:21:38 +0900

    Add brute force solution

--- arrays_and_hashing/problems/448_find_all_numbers_disappeared_in_an_array.py
+++ arrays_and_hashing/problems/448_find_all_numbers_disappeared_in_an_array.py
@@ -1,2 +1,11 @@
 # Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(1, len(nums) + 1):
+            if i in nums:
+                continue
+            ans.append(i)
+        
+        return ans
