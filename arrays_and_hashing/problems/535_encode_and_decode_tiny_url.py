b464997a95ddefcd5523c9c7b5693da4a617ad05
Red <crabmarket56@gmail.com>
2023-01-08 11:27:01 +0900

    Add initial solution

--- arrays_and_hashing/problems/535_encode_and_decode_tiny_url.py
+++ arrays_and_hashing/problems/535_encode_and_decode_tiny_url.py
@@ -6,3 +6,33 @@
 # Solution() Initializes the object of the system.
 # String encode(String longUrl) Returns a tiny URL for the given longUrl.
 # String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        self.hm = {}
+        split = longUrl.split('/')
+        i = 0
+        for s in split:
+            if i == 0:
+                self.hm[i] = s
+            else:
+                self.hm[i] = '/' + s
+            i += 1
+        encoded = ""
+        for i in range(i):
+            encoded += str(i)
+        return encoded 
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        decoded = ""
+        i = 0
+        for c in shortUrl:
+            decoded += self.hm[i]
+            i += 1
+        return decoded
