4d97eb27c75bed99bf32d032ff2dcc87103354b9
Red <crabmarket56@gmail.com>
2023-01-08 22:15:19 +0900

    Add brute force solution

--- arrays_and_hashing/problems/1930_unique_length_3_palindromic_subsequences.py
+++ arrays_and_hashing/problems/1930_unique_length_3_palindromic_subsequences.py
@@ -7,3 +7,19 @@
 # A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
 
 # For example, "ace" is a subsequence of "abcde".
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        subsequences = set()
+        
+        for i, c in enumerate(s):
+            j = i + 1
+            chars = set()
+            while j < len(s):
+                if s[j] == s[i]:
+                    outer = s[j]
+                    for c in chars:
+                        subsequences.add(outer + c + outer)
+                chars.add(s[j])
+                j += 1
+        return len(subsequences)
