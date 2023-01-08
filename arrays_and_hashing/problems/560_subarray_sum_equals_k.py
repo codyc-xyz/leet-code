eface706f452a80c0b9f0481246a49da83f4b0c2
Red <crabmarket56@gmail.com>
2023-01-08 16:40:51 +0900

    Add brute force solution

--- arrays_and_hashing/problems/560_subarray_sum_equals_k.py
+++ arrays_and_hashing/problems/560_subarray_sum_equals_k.py
@@ -2,3 +2,16 @@
 
 # A subarray is a contiguous non-empty sequence of elements within an array.
 
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        for i, n in enumerate(nums):
+            j = i
+            res = 0
+            while j < len(nums):
+                res += nums[j]
+                if res == k:
+                    ans += 1
+                j += 1
+       
+        return ans
