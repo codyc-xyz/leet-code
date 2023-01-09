12c41ee4457828de45490530ea51ba4f1dbc6e8e
Red <crabmarket56@gmail.com>
2023-01-09 11:13:23 +0900

    Add initial solution

--- arrays_and_hashing/problems/2017_grid_game.py
+++ arrays_and_hashing/problems/2017_grid_game.py
@@ -8,3 +8,25 @@
 # The first robot wants to minimize the number of points collected by the second robot. In contrast, the second robot wants to maximize the number of points it collects. 
 # If both robots play optimally, return the number of points collected by the second robot.
 
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        
+        prefix1, prefix2 = [0], [0]
+        
+        for i in range(2):
+            j = 0
+            if i == 0:
+                while j < len(grid[i]):
+                    prefix1.append(prefix1[-1] + grid[i][j])
+                    j += 1
+            else:
+                while j < len(grid[i]):
+                    prefix2.append(prefix2[-1] + grid[i][j])
+                    j += 1
+       
+        minScore = float("inf")
+        for i in range(1, len(prefix1)):
+            p2 = max(prefix1[-1] - prefix1[i], prefix2[i - 1])
+            minScore = min(minScore, p2)
+            
+        return minScore
