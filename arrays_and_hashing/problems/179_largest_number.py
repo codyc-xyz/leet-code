44fa3661c1593a6ee1b1be9ff53af98d6b761205
Red <crabmarket56@gmail.com>
2023-01-09 13:43:55 +0900

    Add initial solution

--- arrays_and_hashing/problems/179_largest_number.py
+++ arrays_and_hashing/problems/179_largest_number.py
@@ -2,3 +2,22 @@
 
 # Since the result may be very large, so you need to return a string instead of an integer.
 
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        for i, n in enumerate(nums):
+            nums[i] = str(n)
+            
+        def compare(c1, c2):
+            if c1 + c2 > c2 + c1:
+                return -1
+            else:
+                return 1
+        nums = sorted(nums, key=cmp_to_key(compare))
+        
+        i = 0
+        while nums[i] == '0':
+            i += 1
+            if i == len(nums):
+                return '0'
+        return "".join(nums[i:])
