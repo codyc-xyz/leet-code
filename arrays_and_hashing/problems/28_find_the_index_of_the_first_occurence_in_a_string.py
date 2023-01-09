fb5b93f626e108657ac00ed821a7b8e546224ab0
Red <crabmarket56@gmail.com>
2023-01-09 11:37:50 +0900

    Add initial solution

--- arrays_and_hashing/problems/28_find_the_index_of_the_first_occurence_in_a_string.py
+++ arrays_and_hashing/problems/28_find_the_index_of_the_first_occurence_in_a_string.py
@@ -1,2 +1,14 @@
 # Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
+        
+        l = 0
+        for r in range(len(haystack)):
+            if r - l == len(needle) - 1:
+                if haystack[l:r + 1] == needle:
+                    return l
+                l += 1
+        return -1
