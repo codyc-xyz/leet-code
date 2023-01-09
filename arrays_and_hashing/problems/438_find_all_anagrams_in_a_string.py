8758140f2c426e82a85bd7d777f189622e28ee9b
Red <crabmarket56@gmail.com>
2023-01-09 11:31:53 +0900

    Add initial solution

--- arrays_and_hashing/problems/438_find_all_anagrams_in_a_string.py
+++ arrays_and_hashing/problems/438_find_all_anagrams_in_a_string.py
@@ -2,3 +2,28 @@
 
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        ans = []
+        if len(p) > len(s):
+            return ans
+        countS = defaultdict(int)
+        countP = defaultdict(int)
+        for i in range(len(p)):
+            countS[s[i]] += 1
+            countP[p[i]] += 1
+            
+        l = 0
+        if countS == countP:
+            ans.append(l)
+            
+        for r in range(len(p), len(s)):
+            if countS[s[l]] == 1:
+                del countS[s[l]]
+            else:
+                countS[s[l]] -= 1
+            countS[s[r]] += 1
+            l += 1
+            if countS == countP:
+                ans.append(l)
+        return ans
