2d53b0f5e2348054b3b203fa40829640c0a09888
Red <crabmarket56@gmail.com>
2023-01-10 15:19:24 +0900

    Add initial solution

--- arrays_and_hashing/problems/655_non_decreasing_array.py
+++ arrays_and_hashing/problems/655_non_decreasing_array.py
@@ -2,3 +2,17 @@
 
 # We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
 
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        changed = False
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1] and changed == False:
+                changed = True
+                if i == 1 or nums[i] >= nums[i-2]:
+                    nums[i-1] = nums[i]
+                else:
+                    nums[i] = nums[i-1]
+            elif nums[i] < nums[i - 1] and changed == True:
+                return False
+        return True
