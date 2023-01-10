e5024545c232be3375b50e1cc7bc0c1055369482
Red <crabmarket56@gmail.com>
2023-01-10 13:20:31 +0900

    Initialize file

--- arrays_and_hashing/problems/1461_check_if_a_string_contains_all_binary_codes_of_size_k.py
+++ arrays_and_hashing/problems/1461_check_if_a_string_contains_all_binary_codes_of_size_k.py
@@ -1,2 +1,13 @@
 # Given a binary string s and an integer k, return true if every binary code of length k is a substring of s. Otherwise, return false.
 
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        codes = set()
+        l = 0
+        for r in range(len(s) + 1):
+            if r - l == k:
+                code = s[l:r]
+                codes.add(code)
+                l += 1
+    
+        return len(codes) == 2**k
