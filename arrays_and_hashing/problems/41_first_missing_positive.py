aab803115fab03f25d35b214844c0a76653bbc8e
Red <crabmarket56@gmail.com>
2023-01-10 15:49:15 +0900

    Add initial solution

--- arrays_and_hashing/problems/41_first_missing_positive.py
+++ arrays_and_hashing/problems/41_first_missing_positive.py
@@ -2,3 +2,12 @@
 
 # You must implement an algorithm that runs in O(n) time and uses constant extra space.
 
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        digits = set(nums)
+        
+        for i in range(1, len(nums) + 2):
+            if i not in digits:
+                return i
+        return 0
