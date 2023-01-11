228ef5203c97aef0b5e5f10faf97e4cd64ee71a7
Red <crabmarket56@gmail.com>
2023-01-11 14:25:58 +0900

    Add initial solution

--- binary_search/problems/2089_find_target_indices_after_sorting_array.py
+++ binary_search/problems/2089_find_target_indices_after_sorting_array.py
@@ -4,3 +4,27 @@
 
 # Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
 
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        ans = []
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                j = k = mid
+                while j > 0 and nums[j - 1] == target:
+                    j -= 1
+                while k < len(nums) - 1 and nums[k + 1] == target:
+                    k += 1
+                for i in range(j, k + 1):
+                    ans.append(i)
+                return ans
+                
+        return ans
