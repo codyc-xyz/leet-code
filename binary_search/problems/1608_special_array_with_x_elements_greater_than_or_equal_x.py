a6fc92f80858b88224cc565a806c7c5b1ccff079
Red <crabmarket56@gmail.com>
2023-01-11 22:49:47 +0900

    Add initial solution

--- binary_search/problems/1608_special_array_with_x_elements_greater_than_or_equal_x.py
+++ binary_search/problems/1608_special_array_with_x_elements_greater_than_or_equal_x.py
@@ -4,3 +4,10 @@
 
 # Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
 
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums) + 1):
+            if len(nums) - bisect_left(nums, i) == i:
+                return i
+        return -1
