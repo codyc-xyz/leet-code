2b8026eb96980582901742b030a06a93085e3b23
Red <crabmarket56@gmail.com>
2023-01-11 15:57:16 +0900

    Add alternative solution

--- binary_search/problems/2389_longest_subsequence_with_limited_sum.py
+++ binary_search/problems/2389_longest_subsequence_with_limited_sum.py
@@ -20,3 +20,16 @@ class Solution:
                     ans.append(i + 1)
                 i += 1
         return ans
+
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        nums.sort()
+        
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        
+        ans = []
+        for q in queries:
+            idx = bisect_right(nums, q)
+            ans.append(idx)
+        return ans
