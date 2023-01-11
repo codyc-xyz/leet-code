60ce3c3b6353fc7981a12c908cd150fe4c5fc800
Red <crabmarket56@gmail.com>
2023-01-11 23:00:06 +0900

    Add initial solution

--- binary_search/problems/1539_kth_missing_positive_number.py
+++ binary_search/problems/1539_kth_missing_positive_number.py
@@ -2,3 +2,12 @@
 
 # Return the kth positive integer that is missing from this array.
 
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        nums = set(arr)
+        
+        for i in range(1, len(arr) + k + 1):
+            if i not in nums:
+                k -= 1
+                if k == 0:
+                    return i
