65501ccc8a34afb7e5ce52b953391a1d02a1b185
Red <crabmarket56@gmail.com>
2023-01-11 13:21:50 +0900

    Add initial solution

--- binary_search/problems/374_guess_higher_or_lower.py
+++ binary_search/problems/374_guess_higher_or_lower.py
@@ -10,3 +10,26 @@
 # 0: your guess is equal to the number I picked (i.e. num == pick).
 
 # Return the number that I picked.
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        
+        l, r = 1, n
+        
+        while l <= r:
+            mid = (l + r) // 2
+            res = guess(mid)
+            
+            if res == -1:
+                r = mid - 1
+            elif res == 1:
+                l = mid + 1
+            else:
+                return mid
