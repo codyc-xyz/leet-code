94b086c91b9544af6fc2f24ae26951ea26722d0a
Red <crabmarket56@gmail.com>
2023-01-11 14:16:13 +0900

    Add initial solution

--- binary_search/1351_count_negative_numbers_in_a_sorted_matrix.py
+++ binary_search/1351_count_negative_numbers_in_a_sorted_matrix.py
@@ -1,2 +1,10 @@
 # Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        neg = 0
+        for r in grid:
+            if r[-1] >= 0:
+                continue
+            neg += bisect_left(r[::-1], 0)
+        return neg
