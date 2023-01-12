9ff1ff559e1f90b1e6396cffcff9b45122c8979d
Red <crabmarket56@gmail.com>
2023-01-12 13:13:53 +0900

    Add initial solution

--- binary_search/problems/744_find_smallest_letter_greater_than_target.py
+++ binary_search/problems/744_find_smallest_letter_greater_than_target.py
@@ -2,3 +2,12 @@
 
 # Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        start = ord(target) + 1
+        end = ord('z') + 1
+        
+        for i in range(start, end):
+            if chr(i) in letters:
+                return chr(i)
+        return letters[0]
