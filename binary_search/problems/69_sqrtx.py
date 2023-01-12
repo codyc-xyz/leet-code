7ba79923f440117521e3da5897ebbd91d4bff4ad
Red <crabmarket56@gmail.com>
2023-01-12 13:36:10 +0900

    Add alternative solution

--- binary_search/problems/69_sqrtx.py
+++ binary_search/problems/69_sqrtx.py
@@ -12,3 +12,18 @@ class Solution:
             elif i * i > x:
                 return i - 1
         return 0
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if mid * mid > x:
+                r = mid - 1
+            elif mid * mid < x:
+                l = mid + 1
+            else:
+                return mid
+        return (l + r) // 2
