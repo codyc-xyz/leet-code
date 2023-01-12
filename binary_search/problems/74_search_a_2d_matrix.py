9cc38bf2bf430847fb98263d9cc1758308196c2f
Red <crabmarket56@gmail.com>
2023-01-12 13:59:19 +0900

    Add initial solution

--- binary_search/problems/74_search_a_2d_matrix.py
+++ binary_search/problems/74_search_a_2d_matrix.py
@@ -7,3 +7,39 @@
 
 # You must write a solution in O(log(m * n)) time complexity.
 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        l, r = 0, len(matrix) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if matrix[mid][0] > target:
+                r = mid - 1
+            elif matrix[mid][0] < target:
+                l = mid
+                if matrix[mid][len(matrix[mid]) - 1] > target:
+                    break
+                elif matrix[mid][len(matrix[mid]) - 1] == target:
+                    return True
+                l += 1
+            else:
+                return True
+            
+        row = l
+        if row < len(matrix):
+            l, r = 0, len(matrix[row]) - 1
+        else:
+            return False
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if matrix[row][mid] > target:
+                r = mid - 1
+            elif matrix[row][mid] < target:
+                l = mid + 1
+            else:
+                return True
+        return False
