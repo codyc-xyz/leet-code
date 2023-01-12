261dd7952eb4a8240dc42db9e61a398ab595185f
Red <crabmarket56@gmail.com>
2023-01-12 13:21:15 +0900

    Add alternative solution

--- binary_search/problems/744_find_smallest_letter_greater_than_target.py
+++ binary_search/problems/744_find_smallest_letter_greater_than_target.py
@@ -9,3 +9,11 @@ class Solution:
             if chr(i) in letter:
                 return chr(i)
         return letters[0]
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        idx = bisect_right(letters, target)
+        if idx < len(letters):
+            return letters[idx]
+        else:
+            return letters[0]
