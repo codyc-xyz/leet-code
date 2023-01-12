bdad967477349ca72016d2abc866714aeda7f60b
Red <crabmarket56@gmail.com>
2023-01-12 13:32:26 +0900

    Add initial solution

--- binary_search/problems/69_sqrtx.py
+++ binary_search/problems/69_sqrtx.py
@@ -3,3 +3,12 @@
 # You must not use any built-in exponent function or operator.
 # For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        for i in range(1, x + 1):
+            if i * i == x:
+                return i
+            elif i * i > x:
+                return i - 1
+        return 0
