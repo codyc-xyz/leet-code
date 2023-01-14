ad91a35dc7082ca380db9b6d76df319ca8af6cf0
Red <crabmarket56@gmail.com>
2023-01-14 14:11:45 +0900

    Refactor solution

--- binary_search/problems/34_find_first_and_last_position_of_element_in_sorted_array.py
+++ binary_search/problems/34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -6,36 +6,23 @@
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        n = set(nums)
-        if target not in n:
-            return [-1, -1]
-        
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = (l + r) // 2
-            
-            if nums[m] >= target:
-                r = m - 1
-            else:
-                l = m + 1
-        if r < 0:
-            left = 0
-        elif nums[r] != target:
-            left = r + 1
-        else:
-            left = r
-        
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = (l + r) // 2
-            
-            if nums[m] <= target:
-                l = m + 1
-            else:
-                r = m - 1
-        if nums[l] == target:
-            right = l
-        else:
-            right = l - 1
-        
+        left, right = self.binSearch(nums, target, True), self.binSearch(nums, target, False)
         return [left, right]
+    def binSearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
+        
+        while l <= r:
+            m = (l + r) // 2
+            
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                i = m
+                if leftBias:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return i
