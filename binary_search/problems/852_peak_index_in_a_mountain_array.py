a31a3b84b505c2f3a9774875bdb8192be6555f93
Red <crabmarket56@gmail.com>
2023-01-14 16:58:13 +0900

    Add initial solution

--- binary_search/problems/852_peak_index_in_a_mountain_array.py
+++ binary_search/problems/852_peak_index_in_a_mountain_array.py
@@ -9,3 +9,9 @@
 
 # You must solve it in O(log(arr.length)) time complexity.
 
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                return i
