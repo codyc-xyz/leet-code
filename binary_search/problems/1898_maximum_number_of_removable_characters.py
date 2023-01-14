d2df70d93c416d3ce213c342a5ee564e1768bae9
Red <crabmarket56@gmail.com>
2023-01-14 15:00:18 +0900

    Add initial solution

--- binary_search/problems/1898_maximum_number_of_removable_characters.py
+++ binary_search/problems/1898_maximum_number_of_removable_characters.py
@@ -7,3 +7,28 @@
 
 # A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
 
+class Solution:
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+        ans = 0
+        
+        def isSubseq(s, p, removed):
+            i = j = 0
+            while i < len(s) and j < len(p):
+                if i in removed or s[i] != p[j]:
+                    i += 1
+                    continue
+                i += 1
+                j += 1
+            return j == len(p)
+        
+        l, r = 0, len(removable) - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            removed = set(removable[:m + 1])
+            if isSubseq(s, p, removed):
+                ans = m + 1
+                l = m + 1
+            else:
+                r = m - 1
+        return ans
