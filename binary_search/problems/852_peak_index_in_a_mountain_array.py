205393137da72184d4fbdc0383ee9d5c936eb882
Red <crabmarket56@gmail.com>
2023-01-14 17:02:45 +0900

    Add alternative solution

--- binary_search/problems/852_peak_index_in_a_mountain_array.py
+++ binary_search/problems/852_peak_index_in_a_mountain_array.py
@@ -15,3 +15,16 @@ class Solution:
         for i in range(len(arr) - 1):
             if arr[i] > arr[i + 1]:
                 return i
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        l, r = 0, len(arr) - 1
+        
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] < arr[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l
