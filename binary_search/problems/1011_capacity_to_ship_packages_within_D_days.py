57a55fe993b032dc37357d77999ed03d10d51317
Red <crabmarket56@gmail.com>
2023-01-14 17:31:02 +0900

    Add initial solution

--- binary_search/problems/1011_capacity_to_ship_packages_within_D_days.py
+++ binary_search/problems/1011_capacity_to_ship_packages_within_D_days.py
@@ -5,3 +5,31 @@
 
 # Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
 
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+    
+        l, r = 0, max(weights) * len(weights)
+        ans = 0
+        while l <= r:
+            capacity = m = (l + r) // 2
+            time = 1
+            flag = True
+            for i, w in enumerate(weights):
+                if capacity > w:
+                    capacity -= w
+                elif capacity < w:
+                    if w > m:
+                        flag = False
+                        break
+                    capacity = m - w
+                    time += 1
+                else:
+                    if i < len(weights) - 1:
+                        capacity = m
+                        time += 1
+            if time <= days and flag == True:
+                ans = m
+                r = m - 1
+            else:
+                l = m + 1
+        return ans
