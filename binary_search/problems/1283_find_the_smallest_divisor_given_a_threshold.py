9da2bbd69ee2667d6e5ea2914a57978027d157e5
Red <crabmarket56@gmail.com>
2023-01-15 19:09:14 +0900

    Add initial solution

--- binary_search/problems/1283_find_the_smallest_divisor_given_a_threshold.py
+++ binary_search/problems/1283_find_the_smallest_divisor_given_a_threshold.py
@@ -5,3 +5,23 @@
 
 # The test cases are generated so that there will be an answer.
 
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        
+        def divideGreater(m):
+            res = 0
+            for n in nums:
+                res += math.ceil(n / m)            
+            return res <= threshold
+        
+        l, r = 1, sum(nums)
+        ans = 0
+        while l <= r:
+            m = (l + r) // 2
+            
+            if divideGreater(m):
+                ans = m
+                r = m - 1
+            else:
+                l = m + 1
+        return ans
