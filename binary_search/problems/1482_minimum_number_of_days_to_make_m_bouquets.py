f55f9cbc572bbbc05b0d9fb03855d2920d680397
Red <crabmarket56@gmail.com>
2023-01-16 00:03:49 +0900

    Add initial solution

--- binary_search/problems/1482_minimum_number_of_days_to_make_m_bouquets.py
+++ binary_search/problems/1482_minimum_number_of_days_to_make_m_bouquets.py
@@ -6,3 +6,31 @@
 
 # Return the minimum number of days you need to wait to be able to make m bouquets from the garden. If it is impossible to make m bouquets return -1.
 
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        
+        if len(bloomDay) < m * k:
+            return -1
+        
+        def makeBouquets(mid):
+            i = countK = countM = 0
+            while i < len(bloomDay):
+                if bloomDay[i] <= mid:
+                    countK += 1
+                else:
+                    countK = 0
+                if countK == k:
+                    countM += 1
+                    countK = 0
+                i += 1
+            return countM >= m
+                
+        l, r = 0, max(bloomDay)
+        while l <= r:
+            mid = (l + r) // 2
+            if makeBouquets(mid):
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
