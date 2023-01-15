c436b8262e1f0685b87df0675dbe09d90df87d07
Red <crabmarket56@gmail.com>
2023-01-15 15:52:30 +0900

    Add initial solution

--- binary_search/problems/540_single_element_in_a_sorted_array.py
+++ binary_search/problems/540_single_element_in_a_sorted_array.py
@@ -4,3 +4,21 @@
 
 # Your solution must run in O(log n) time and O(1) space.
 
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            
+            if m > 0 and nums[m] == nums[m - 1]:
+                if not (m - 1) % 2:
+                    l = m + 1
+                else:
+                    r = m - 2
+            elif m < len(nums) - 1 and nums[m] == nums[m + 1]:
+                if not (len(nums) - (m + 1) - 1) % 2:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                return nums[m]
