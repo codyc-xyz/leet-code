3f066e5a02e3afa19f9d7ea918077902cc03878d
Red <crabmarket56@gmail.com>
2023-01-15 11:53:44 +0900

    Add initial solution

--- binary_search/problems/2517_maximum_tastiness_of_candy_basket.py
+++ binary_search/problems/2517_maximum_tastiness_of_candy_basket.py
@@ -4,3 +4,25 @@
 
 # Return the maximum tastiness of a candy basket.
 
+class Solution:
+    def maximumTastiness(self, price: List[int], k: int) -> int:
+        price.sort()
+        def isValid(K, m):
+            prev = price[0]
+            for i in range(1, len(price)):
+                if price[i] - prev >= m:
+                    K -= 1
+                    prev = price[i]
+            return K <= 0
+        
+        l, r = 0, max(price) - min(price)
+        ans = 0
+        while l <= r:
+            m = (l + r) // 2
+            K = k - 1
+            if isValid(K, m):
+                l = m + 1
+                ans = m
+            else:
+                r = m - 1
+        return ans
