8c05b5740e14790d513a598671d21bbcf9e43ad9
Re <107221766+0xdacted@users.noreply.github.com>
2023-01-17 19:20:15 +0900

    Add initial solution

--- binary_search/problems/162_find_peak_element.py
+++ binary_search/problems/162_find_peak_element.py
@@ -7,3 +7,17 @@
 # In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
 
 # You must write an algorithm that runs in O(log n) time.
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) -1
+        
+        while l <= r:
+            m = (l + r) // 2
+            
+            if m > 0 and nums[m] < nums[m - 1]:
+                r = m - 1
+            elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
+                l = m + 1
+            else:
+                return m
