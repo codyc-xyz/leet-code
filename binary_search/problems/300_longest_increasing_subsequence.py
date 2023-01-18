84bc91b94914b1332cc4edd0d5a08a2a893a42c5
Red <crabmarket56@gmail.com>
2023-01-18 20:30:29 +0900

    Add initial solution

--- binary_search/problems/300_longest_increasing_subsequence.py
+++ binary_search/problems/300_longest_increasing_subsequence.py
@@ -1,2 +1,18 @@
 # Given an integer array nums, return the length of the longest strictly increasing subsequence.
 
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = []
+        for n in nums:
+            if not LIS or n > LIS[-1]:
+                LIS.append(n)
+            else:
+                l, r = 0, len(LIS) - 1
+                while l <= r:
+                    m = (l + r) // 2
+                    if LIS[m] < n:
+                        l = m + 1
+                    else:
+                        r = m - 1
+                LIS[l] = n
+        return len(LIS)
