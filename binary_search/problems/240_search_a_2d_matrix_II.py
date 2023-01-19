79db95beb169c2eca6feec4ef9303e2869be3d53
Red <crabmarket56@gmail.com>
2023-01-19 11:25:25 +0900

    Add initial solution

--- binary_search/problems/240_search_a_2d_matrix_II.py
+++ binary_search/problems/240_search_a_2d_matrix_II.py
@@ -2,3 +2,11 @@
 
 # Integers in each row are sorted in ascending from left to right.
 # Integers in each column are sorted in ascending from top to bottom.
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        for i in range(len(matrix)):
+            if bisect_right(matrix[i], target) != bisect_left(matrix[i], target):
+                return True
+        return False
