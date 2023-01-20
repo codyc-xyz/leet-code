92e164e3daa678e5c2aef25c7f0d541ac7e3bf9a
Red <crabmarket56@gmail.com>
2023-01-20 22:01:46 +0900

    Add initial solution

--- binary_search/problems/1300_sum_of_mutated_array_closest_to_target.py
+++ binary_search/problems/1300_sum_of_mutated_array_closest_to_target.py
@@ -5,3 +5,35 @@
 
 # Notice that the answer is not neccesarilly a number from arr.
 
+class Solution:
+    def findBestValue(self, arr: List[int], target: int) -> int:
+        arr.sort()
+        pSum = [arr[0]]
+        
+        for i in range(1, len(arr)):
+            pSum.append(pSum[-1] + arr[i])
+            
+        l, r = 0, max(arr)
+        res = float("inf")
+        while l <= r:
+            m = (l + r) // 2
+            
+            right = bisect_right(arr, m)
+            n = len(arr) - right
+            if right > 0:
+                curr = (pSum[right - 1] + (n * m)) 
+            else:
+                curr = (n * m) 
+            if n != 0:
+                diff = abs((curr - target) / n)
+            else:
+                diff = abs(curr - target)
+            print(diff, res)
+            if diff < res:
+                res = diff
+                ans = m
+            if curr < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return ans
