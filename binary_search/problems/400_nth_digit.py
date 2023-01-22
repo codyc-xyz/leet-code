3b2ebc0aa61572311ab6f28d9f17f4500f1c3267
Red <crabmarket56@gmail.com>
2023-01-22 11:35:30 +0900

    Add initial solution

--- binary_search/problems/400_nth_digit.py
+++ binary_search/problems/400_nth_digit.py
@@ -1,2 +1,9 @@
 # Given an integer n, return the nth digit of the infinite integer sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
 
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        count = 0
+        for i in range(1, n + 1):
+            if count + len(str(i)) >= n:
+                return int(str(i)[n - count - 1])
+            count += len(str(i))
