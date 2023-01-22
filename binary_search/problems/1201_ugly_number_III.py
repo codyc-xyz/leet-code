a3f5d78d08a46dd8d573c5a34e756b8ebfe355bf
Red <crabmarket56@gmail.com>
2023-01-22 20:25:31 +0900

    Add initial solution

--- binary_search/problems/1201_ugly_number_III.py
+++ binary_search/problems/1201_ugly_number_III.py
@@ -2,3 +2,26 @@
 
 # Given four integers n, a, b, and c, return the nth ugly number.
 
+class Solution:
+    def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
+        
+        def numUglies(m):
+            return (m // a + m // b + m // c) - (m // (ab) + m // (ac) + m // (bc)) + m // (abc)
+            
+        ab = a * b // math.gcd(a, b)
+        ac = a * c // math.gcd(a, c)
+        bc = b * c // math.gcd(b, c)
+        abc = a * bc // math.gcd(a, bc)
+        
+        l, r = 0, a * n  
+        
+        while l <= r:
+            m = (l + r) // 2
+            
+            res = numUglies(m)
+          
+            if res >= n:
+                r = m - 1
+            else:
+                l = m + 1
+        return l
