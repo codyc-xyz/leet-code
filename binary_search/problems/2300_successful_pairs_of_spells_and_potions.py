46fff0f22075bd96dfe4bd332d0866862afeb7ca
Red <crabmarket56@gmail.com>
2023-01-22 16:20:59 +0900

    Add initial solution

--- binary_search/problems/2300_successful_pairs_of_spells_and_potions.py
+++ binary_search/problems/2300_successful_pairs_of_spells_and_potions.py
@@ -4,3 +4,26 @@
 
 # Return an integer array pairs of length n where pairs[i] is the number of potions that will form a successful pair with the ith spell.
 
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        ans = []
+        n = len(potions)
+        
+        for s in spells:
+            l, r = 0, n
+            res = -1
+            while l <= r:
+                m = (l + r) // 2
+                if m >= n:
+                    break
+                if potions[m] * s >= success:
+                    r = m - 1
+                    res = m
+                else:
+                    l = m + 1
+            if res != -1:
+                ans.append(n - res)
+            else:
+                ans.append(0)
+        return ans
