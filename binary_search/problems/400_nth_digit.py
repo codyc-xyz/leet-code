e32f848d6f38ff998fa82d174197c0a7f01858bd
Red <crabmarket56@gmail.com>
2023-01-22 11:45:59 +0900

    Refactor solution

--- binary_search/problems/400_nth_digit.py
+++ binary_search/problems/400_nth_digit.py
@@ -7,3 +7,13 @@ class Solution:
             if count + len(str(i)) >= n:
                 return int(str(i)[n - count - 1])
             count += len(str(i))
+
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        base = digit = 1
+        while n > base * digit * 9:
+            n -= base * digit * 9
+            digit += 1
+            base *= 10
+        i, j = divmod(n - 1, digit)
+        return int(str(base + i)[j])
