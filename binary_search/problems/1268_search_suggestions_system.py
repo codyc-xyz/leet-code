89278714c78da41c621aa7df08763e72616eadd2
Red <crabmarket56@gmail.com>
2023-01-23 13:59:29 +0900

    Add initial solution

--- binary_search/problems/1268_search_suggestions_system.py
+++ binary_search/problems/1268_search_suggestions_system.py
@@ -4,3 +4,30 @@
 
 # Return a list of lists of the suggested products after each character of searchWord is typed.
 
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        ans = []
+        curr = ""
+        for s in searchWord:
+            curr += s
+            end = len(curr)
+            start = -1
+            l, r = 0, len(products) - 1
+            while l <= r:
+                m = (l + r) // 2
+                if products[m][:end] == curr:
+                    start = m
+                if products[m][:end] >= curr:
+                    r = m - 1
+                else:
+                    l = m + 1
+            
+            if start != -1:
+                j = start
+                while j < len(products) and products[j][:end] == curr and start + 3 > j:
+                    j += 1
+                ans.append(products[start:j])
+            else:
+                ans.append([])
+        return ans
