765e3a5992d869d27f0e80f0696462685cc05d1d
Red <crabmarket56@gmail.com>
2023-01-23 17:32:53 +0900

    Add initial solution

--- binary_search/problems/410_split_array_largest_sum.py
+++ binary_search/problems/410_split_array_largest_sum.py
@@ -4,3 +4,26 @@
 
 # A subarray is a contiguous part of the array.
 
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        
+        def splitSuccessful(m, K):
+            M = m
+            for n in nums:
+                if M - n < 0:
+                    K -= 1
+                    M = m 
+                M -= n
+            if M < 0:
+                K -= 1
+            return K >= 0
+        
+        l, r = max(nums), sum(nums)
+        while l <= r:
+            m = (l + r) // 2
+            K = k - 1
+            if splitSuccessful(m, K):
+                r = m - 1
+            else:
+                l = m + 1
+        return l
