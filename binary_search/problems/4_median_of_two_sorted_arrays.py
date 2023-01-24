d10c126b2f21558206f350e77cc4cd494e4c237e
Red <crabmarket56@gmail.com>
2023-01-24 09:48:37 +0900

    Add alternative solution

--- binary_search/problems/4_median_of_two_sorted_arrays.py
+++ binary_search/problems/4_median_of_two_sorted_arrays.py
@@ -25,3 +25,30 @@ class Solution:
             return arr[n // 2]
         else:
             return (arr[n // 2] + arr[n // 2 - 1]) / 2
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        if len(A) > len(B):
+            B, A = A, B
+            
+        total = len(A) + len(B)
+        half = total // 2
+        l, r = 0, len(A) - 1
+        while True:
+            m = (l + r) // 2
+            i = half - m - 2
+            aLeft = A[m] if m >= 0 else float("-inf")
+            aRight = A[m + 1] if (m + 1) < len(A) else float("inf")
+            bLeft = B[i] if i >= 0 else float("-inf")
+            bRight = B[i + 1] if (i + 1) < len(B) else float("inf")
+            
+            if aLeft <= bRight and bLeft <= aRight:
+                if total % 2:
+                    return min(aRight, bRight)
+                else:
+                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
+            elif aLeft > bRight:
+                r = m - 1
+            else:
+                l = m + 1
