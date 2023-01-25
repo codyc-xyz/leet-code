0b6866b3feab7d3dbf79fd3a5b3c0636fa61b3ef
Red <crabmarket56@gmail.com>
2023-01-25 12:26:48 +0900

    Add alternative solution

--- linked_list/problems/83_remove_duplicates_from_sorted_list.py
+++ linked_list/problems/83_remove_duplicates_from_sorted_list.py
@@ -17,3 +17,11 @@ class Solution:
             fast = fast.next
         return ans
             
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        while curr:
+            while curr.next and curr.next.val == curr.val:
+                curr.next = curr.next.next
+            curr = curr.next
+        return head
