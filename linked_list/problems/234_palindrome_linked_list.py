eb835e5dca1ce7eb7c4acba7f2ab7d0451f19d5c
Red <crabmarket56@gmail.com>
2023-01-25 10:19:53 +0900

    Add alternative solution

--- linked_list/problems/234_palindrome_linked_list.py
+++ linked_list/problems/234_palindrome_linked_list.py
@@ -23,3 +23,22 @@ class Solution:
                 return False
             head = head.next
         return True
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt  
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+        return True
