04cf9be2142570516bd4710c61a8a0bb8a143f76
Red <crabmarket56@gmail.com>
2023-01-25 17:51:06 +0900

    Add intitial solution

--- linked_list/problems/143_reorder_list.py
+++ linked_list/problems/143_reorder_list.py
@@ -6,4 +6,35 @@
 
 # You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        curr = slow.next
+        prev = slow.next = None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+        return head
