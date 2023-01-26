3cca6014051253ad76640bad06c4a125763fe615
Red <crabmarket56@gmail.com>
2023-01-26 18:02:03 +0900

    Add intiial solution

--- linked_list/problems/141_linked_list_cycle.py
+++ linked_list/problems/141_linked_list_cycle.py
@@ -4,3 +4,18 @@
 
 # Return true if there is a cycle in the linked list. Otherwise, return false.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
