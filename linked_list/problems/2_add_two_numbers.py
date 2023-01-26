89d3a743ebfa83208a1da1d796c88f0f18616973
Red <crabmarket56@gmail.com>
2023-01-26 17:58:44 +0900

    Add alternative solution

--- linked_list/problems/2_add_two_numbers.py
+++ linked_list/problems/2_add_two_numbers.py
@@ -26,3 +26,23 @@ class Solution:
             dummy.next = ListNode(int(c))
             dummy = dummy.next
         return head.next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:    
+        dummy = ans = ListNode()
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            val = val1 + val2 + carry
+            
+            carry = val // 10
+            val %= 10
+            dummy.next = ListNode(val)
+            dummy = dummy.next
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return ans.next
