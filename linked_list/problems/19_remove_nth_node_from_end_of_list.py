d040af1aac0392c51e61e6de1083430a2cedbff1
Red <crabmarket56@gmail.com>
2023-01-26 10:04:01 +0900

    Add initial solution

--- linked_list/problems/19_remove_nth_node_from_end_of_list.py
+++ linked_list/problems/19_remove_nth_node_from_end_of_list.py
@@ -1,2 +1,22 @@
 # Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = fast = head
+        while n > 0:
+            fast = fast.next
+            n -= 1
+            
+        if not fast:
+            return head.next
+            
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
