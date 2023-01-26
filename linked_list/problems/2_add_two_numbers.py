7deb377fe5cef46c3c124cfc14e41f0a8c7135c5
Red <crabmarket56@gmail.com>
2023-01-26 16:34:41 +0900

    Add initial solutions

--- linked_list/problems/2_add_two_numbers.py
+++ linked_list/problems/2_add_two_numbers.py
@@ -2,3 +2,27 @@
 
 # You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        curr1, curr2 = l1, l2
+        nums1, nums2 = "", ""
+        while curr1:
+            nums1 = str(curr1.val) + nums1
+            curr1 = curr1.next
+        
+        while curr2:
+            nums2 = str(curr2.val) + nums2
+            curr2 = curr2.next
+      
+        res = str(int(nums1) + int(nums2))
+        dummy = head = ListNode()
+        for c in res[::-1]:
+            dummy.next = ListNode(int(c))
+            dummy = dummy.next
+        return head.next
