d75f47444a40c51700b12e6e6bc290be7d49e1cd
Red <crabmarket56@gmail.com>
2023-01-27 11:54:11 +0900

    Add initial (brute force) solution

--- linked_list/problems/148_sort_list.py
+++ linked_list/problems/148_sort_list.py
@@ -1,2 +1,25 @@
 # Given the head of a linked list, return the list after sorting it in ascending order.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        arr = []
+        curr = head
+        
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
+        arr.sort()
+        
+        curr = head
+        i = 0
+        while curr:
+            curr.val = arr[i]
+            curr = curr.next
+            i += 1
+        return head
