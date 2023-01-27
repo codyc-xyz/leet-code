9a17f94a19472be0171183d7a3ef6cc77f7c23e6
Red <crabmarket56@gmail.com>
2023-01-27 13:50:50 +0900

    Add initial solution (brute force)

--- linked_list/problems/86_partition_list.py
+++ linked_list/problems/86_partition_list.py
@@ -2,3 +2,29 @@
 
 # You should preserve the original relative order of the nodes in each of the two partitions.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        
+        arr = []
+        curr = head
+        while curr:
+            if curr.val < x:
+                arr.append(curr.val)
+            curr = curr.next
+        curr = head
+        
+        while curr:
+            if curr.val >= x:
+                arr.append(curr.val)
+            curr = curr.next
+        dummy = ans = ListNode()
+        
+        for i in range(len(arr)):
+            dummy.next = ListNode(arr[i])
+            dummy = dummy.next
+        return ans.next
