a3f1b500f7e557b69eb0453f05981ca24d532a5f
Red <crabmarket56@gmail.com>
2023-01-28 19:14:11 +0900

    Add initial solution

--- linked_list/problems/328_odd_even_linked_list.py
+++ linked_list/problems/328_odd_even_linked_list.py
@@ -6,3 +6,28 @@
 
 # You must solve the problem in O(1) extra space complexity and O(n) time complexity.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        ans = odds = ListNode()
+        dummy = evens = ListNode()
+        curr = head
+        i = 1
+        while curr:
+            if i % 2:
+                odds.next = curr
+                odds = odds.next
+            else:
+                evens.next = curr
+                evens = evens.next
+            curr = curr.next
+            i += 1
+        
+        evens.next = None
+        odds.next = dummy.next
+        return ans.next
