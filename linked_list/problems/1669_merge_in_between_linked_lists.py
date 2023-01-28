8ba71726cb1afbdc2e5e7374ee4dc8488161aaa2
Red <crabmarket56@gmail.com>
2023-01-28 19:07:16 +0900

    Add initial solution

--- linked_list/problems/1669_merge_in_between_linked_lists.py
+++ linked_list/problems/1669_merge_in_between_linked_lists.py
@@ -4,3 +4,29 @@
 
 # The blue edges and nodes in the following figure indicate the result:
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        dummy = ListNode(next=list1)
+        i = 1
+        while i < a:
+            list1 = list1.next
+            i += 1
+        tail = list1.next
+        list1.next = list2
+        
+        while i <= b:
+            tail = tail.next
+            i += 1
+            
+        curr = list2
+        while curr and curr.next:
+            curr = curr.next
+        
+        curr.next = tail
+        return dummy.next
+        
