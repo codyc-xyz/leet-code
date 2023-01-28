b5034f20b84fa07ab183716e565f2448f374b488
Red <crabmarket56@gmail.com>
2023-01-28 21:10:47 +0900

    Add initial solution

--- linked_list/problems/817_linked_list_components.py
+++ linked_list/problems/817_linked_list_components.py
@@ -2,3 +2,22 @@
 
 # Return the number of connected components in nums where two values are connected if they appear consecutively in the linked list.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        
+        curr = head
+        ans = 0
+        while curr:
+            if curr.val in nums:
+                while curr and curr.val in nums:
+                    curr = curr.next
+                ans += 1
+            if curr:
+                curr = curr.next
+        return ans
+        
