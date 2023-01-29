321046fb3e615359e9f6e1dedad86193460075a3
Red <crabmarket56@gmail.com>
2023-01-29 13:07:50 +0900

    Add initial solution

--- linked_list/problems/2058_find_the_minimum_and_maximum_number_of_nodes_betweeen_critical_points.py
+++ linked_list/problems/2058_find_the_minimum_and_maximum_number_of_nodes_betweeen_critical_points.py
@@ -10,3 +10,31 @@
 # where minDistance is the minimum distance between any two distinct critical points and maxDistance is the maximum distance between any two distinct critical points. 
 # If there are fewer than two critical points, return [-1, -1].
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        
+        critPoints = []
+        prev, curr = head, head.next
+        i = 1
+        while curr and curr.next:
+            if curr.val > prev.val and curr.val > curr.next.val or curr.val < prev.val and curr.val < curr.next.val:
+                critPoints.append(i)
+            curr = curr.next
+            prev = prev.next
+            i += 1
+            
+        if len(critPoints) > 1:
+            maxima = max(critPoints) - min(critPoints)
+            minima = maxima
+            critPoints.sort()
+            for i in range(1, len(critPoints)):
+                minima = min(minima, critPoints[i] - critPoints[i - 1])
+        else:
+            maxima = -1
+            minima = -1
+        return [minima, maxima]
