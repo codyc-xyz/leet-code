af71dbd1c2ac62789aa6c87e4c1857cd8cde2de5
Red <crabmarket56@gmail.com>
2023-01-30 12:19:57 +0900

    Add intiial (brute force) solution

--- linked_list/problems/705_design_hashset.py
+++ linked_list/problems/705_design_hashset.py
@@ -5,3 +5,29 @@
 # void add(key) Inserts the value key into the HashSet.
 # bool contains(key) Returns whether the value key exists in the HashSet or not.
 # void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+class MyHashSet:
+
+    def __init__(self):
+        self.arr = []
+
+    def add(self, key: int) -> None:
+        if key not in self.arr:
+            self.arr.append(key)
+        
+    def remove(self, key: int) -> None:
+        if key in self.arr:
+            for i in range(len(self.arr)):
+                if self.arr[i] == key:
+                    self.arr.pop(i)
+                    break
+
+    def contains(self, key: int) -> bool:
+        return key in self.arr
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
