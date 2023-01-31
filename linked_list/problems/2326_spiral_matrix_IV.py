c96fb26400b792431e539152a5bc40d0e7ce8c6a
Red <crabmarket56@gmail.com>
2023-01-31 11:50:23 +0900

    Add initial solution

--- linked_list/problems/2326_spiral_matrix_IV.py
+++ linked_list/problems/2326_spiral_matrix_IV.py
@@ -6,3 +6,40 @@
 
 # Return the generated matrix.
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        
+        ans = [[-1 for i in range(n)] for j in range(m)]
+        curr = head
+        top,bottom,left,right = 0, m, 0, n
+        while left < right and top < bottom:
+            for i in range(left, right):
+                if not curr:
+                    break
+                ans[top][i] = curr.val
+                curr = curr.next
+            top += 1
+            for i in range(top, bottom):
+                if not curr:
+                    break
+                ans[i][right - 1] = curr.val
+                curr = curr.next
+            right -= 1
+            for i in range(right -1, left - 1, -1):
+                if not curr:
+                    break
+                ans[bottom - 1][i] = curr.val
+                curr = curr.next
+            bottom -= 1
+            for i in range(bottom - 1, top - 1, -1):
+                if not curr:
+                    break
+                ans[i][left] = curr.val
+                curr = curr.next
+            left += 1
+        return ans
