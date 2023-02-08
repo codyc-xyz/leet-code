5a4917d345e9c0297329266c5f01633f055200d1
Red <crabmarket56@gmail.com>
2023-02-08 09:24:34 +0900

    Add initial solution

--- linked_list/problems/146_LRU_cache.py
+++ linked_list/problems/146_LRU_cache.py
@@ -6,4 +6,49 @@
 # void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
 # The functions get and put must each run in O(1) average time complexity.
 
+class ListNode:
 
+    def __init__(self, key, val, next=None, prev=None):
+        self.val = val
+        self.key = key
+        self.next = next
+        self.prev = prev
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head, self.tail = ListNode(0, 0), ListNode(0, 0)
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev, nxt = self.tail.prev, self.tail
+        prev.next, nxt.prev = node, node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = ListNode(key, value)
+        self.insert(self.cache[key])
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
