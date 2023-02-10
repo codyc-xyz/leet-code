0d39623ce9617fee22990baf770c62521de58c3e
Red <crabmarket56@gmail.com>
2023-02-10 09:18:03 +0900

    Add alternative solution

--- binary_search/problems/34_find_first_and_last_position_of_element_in_sorted_array.py
+++ binary_search/problems/34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -26,3 +26,49 @@ class Solution:
                 else:
                     l = m + 1
         return i
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        if self.inNums(nums, target):
+            l = self.findL(nums, target)
+            r = self.findR(nums, target)
+            return [l, r]
+        
+        else:
+            return [-1, -1]
+
+    def findR(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return r
+
+    def findL(self, nums, target):
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m
+        return l
+
+    def inNums(self, nums, target):
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return True
+        return False
