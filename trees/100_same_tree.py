005c6057e9cfc5cfb504194e6de838b5993cca73
Red <crabmarket56@gmail.com>
2023-02-15 13:49:15 +0900

    Add alternative solution

--- trees/100_same_tree.py
+++ trees/100_same_tree.py
@@ -31,3 +31,13 @@ class Solution:
         traverse(p, True)
         traverse(q, False)
         return resP == resQ
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
