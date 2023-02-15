b50adc12593842ad74bf6cf8a870c4d4953b2484
Red <crabmarket56@gmail.com>
2023-02-15 16:49:17 +0900

    Add intiial solution

--- trees/problems/112_path_sum.py
+++ trees/problems/112_path_sum.py
@@ -2,3 +2,23 @@
 
 # A leaf is a node with no children.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        self.flag = False
+        currSum = 0
+        def dfs(root, currSum):
+            if not root:
+                return
+            currSum += root.val
+            dfs(root.left, currSum)
+            dfs(root.right, currSum)
+            if not root.left and not root.right and currSum == targetSum:
+                self.flag = True
+        dfs(root, currSum)
+        return self.flag 
