f1f89efb4daea762074893702502a35d8eaea562
Red <crabmarket56@gmail.com>
2023-02-15 13:23:41 +0900

    Add intiial solution

--- trees/problems/110_balanced_binary_tree.py
+++ trees/problems/110_balanced_binary_tree.py
@@ -1,1 +1,23 @@
 # Given a binary tree, determine if it is height-balanced
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.flag = True
+
+        def traverse(root):
+            if not root:
+                return 0
+            l = traverse(root.left)
+            r = traverse(root.right)
+            res = abs(l) - abs(r)
+            if res > 1 or res < -1:
+                self.flag = False
+            return max(l, r) + 1
+        traverse(root)
+        return self.flag
