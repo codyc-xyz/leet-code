06a557246eac20ce4cf385601839b35875dc7792
Red <crabmarket56@gmail.com>
2023-02-15 13:13:11 +0900

    Add initial solution

--- trees/problems/543_diameter_of_binary_tree.py
+++ trees/problems/543_diameter_of_binary_tree.py
@@ -4,3 +4,22 @@
 
 # The length of a path between two nodes is represented by the number of edges between them.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        
+        self.diameter = 0
+        def traverse(root):
+            if not root:
+                return 0
+            l = traverse(root.left)
+            r = traverse(root.right)
+            self.diameter = max(self.diameter, l + r)
+            return max(l, r) + 1
+        traverse(root)
+        return self.diameter
