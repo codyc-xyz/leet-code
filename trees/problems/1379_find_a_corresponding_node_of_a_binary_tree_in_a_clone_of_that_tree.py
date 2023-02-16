e6abca23b82964e87a7006e1e9f9b07abbdb869b
Red <crabmarket56@gmail.com>
2023-02-16 09:51:44 +0900

    Add initial solution

--- trees/problems/1379_find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
+++ trees/problems/1379_find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -6,3 +6,18 @@
 
 # Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        if not cloned:
+            return None
+        if cloned.val == target.val:
+            return cloned
+        return (self.getTargetCopy(original, cloned.left, target) or
+        self.getTargetCopy(original, cloned.right, target))
