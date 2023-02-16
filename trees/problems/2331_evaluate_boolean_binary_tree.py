871150027cd2c617bccd8807d103367f47aa6035
Red <crabmarket56@gmail.com>
2023-02-16 10:20:24 +0900

    Add intiial solution

--- trees/problems/2331_evaluate_boolean_binary_tree.py
+++ trees/problems/2331_evaluate_boolean_binary_tree.py
@@ -11,3 +11,20 @@
 # A full binary tree is a binary tree where each node has either 0 or 2 children.
 
 # A leaf node is a node that has zero children.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+
+        if not root.left:
+            return root.val == 1
+        b
+        if root.val == 2:
+            return (self.evaluateTree(root.left) or self.evaluateTree(root.right))
+        else:
+            return (self.evaluateTree(root.left) and self.evaluateTree(root.right))
