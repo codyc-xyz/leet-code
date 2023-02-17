34ee9b76975e28bceeb3c55490237567da85d3c9
Red <crabmarket56@gmail.com>
2023-02-17 10:42:05 +0900

    Add alternative solution

--- trees/problems/145_binary_tree_postorder_traversal.py
+++ trees/problems/145_binary_tree_postorder_traversal.py
@@ -17,3 +17,19 @@ class Solution:
             ans.append(root.val)
         postOrder(root)
         return ans        
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return None
+        ans = deque()
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            ans.appendleft(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return ans
