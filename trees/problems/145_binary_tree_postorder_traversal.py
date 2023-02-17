cf1664f708ebcc70b44f104342c74f1146c57503
Red <crabmarket56@gmail.com>
2023-02-17 10:36:32 +0900

    Add initial solution

--- trees/problems/145_binary_tree_postorder_traversal.py
+++ trees/problems/145_binary_tree_postorder_traversal.py
@@ -1,2 +1,19 @@
 # Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def postOrder(root):
+            if not root:
+                return 
+            postOrder(root.left)
+            postOrder(root.right)
+            ans.append(root.val)
+        postOrder(root)
+        return ans        
