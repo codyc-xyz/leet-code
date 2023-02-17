f6a6320548b359d12ff8694b1e428c56094745b3
Red <crabmarket56@gmail.com>
2023-02-17 10:08:28 +0900

    Add initial solution

--- trees/problems/965_univalued_binary_tree.py
+++ trees/problems/965_univalued_binary_tree.py
@@ -2,3 +2,21 @@
 
 # Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        
+        self.seen = set()
+        def dfs(node):
+            if not node:
+                return 
+            self.seen.add(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return len(self.seen) == 1
