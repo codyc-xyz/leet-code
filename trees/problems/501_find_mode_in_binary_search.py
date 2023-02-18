cef8189ce4fec35ad4ee218320d5fd9b4df1b47e
Red <crabmarket56@gmail.com>
2023-02-18 13:13:51 +0900

    Add initial solution

--- trees/problems/501_find_mode_in_binary_search.py
+++ trees/problems/501_find_mode_in_binary_search.py
@@ -7,3 +7,23 @@
 # The left subtree of a node contains only nodes with keys less than or equal to the node's key.
 # The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
 # Both the left and right subtrees must also be binary search trees.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        count = defaultdict(int)
+
+        def dfs(root):
+            if not root:
+                return
+            count[root.val] += 1
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        maxCount = max(count.values())
+        return [i for i, c in count.items() if c == maxCount]
