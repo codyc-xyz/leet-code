39b87c7455507418a6d9fdcd3646976aa540efca
Red <crabmarket56@gmail.com>
2023-02-19 11:14:57 +0900

    Add initial solution

--- trees/problems/1038_binary_search_tree_to_greater_sum_tree.py
+++ trees/problems/1038_binary_search_tree_to_greater_sum_tree.py
@@ -5,3 +5,22 @@
 # The left subtree of a node contains only nodes with keys less than the node's key.
 # The right subtree of a node contains only nodes with keys greater than the node's key.
 # Both the left and right subtrees must also be binary search trees.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.total = 0
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.right)
+            self.total += root.val
+            root.val = self.total
+            dfs(root.left)
+        dfs(root)
+        return root
