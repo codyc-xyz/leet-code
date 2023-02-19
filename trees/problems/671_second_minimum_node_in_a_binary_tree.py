263ad163fa42adad5e2e3fb8fb839d1aa20ade6f
Red <crabmarket56@gmail.com>
2023-02-19 09:41:29 +0900

    Add initial solution

--- trees/problems/671_second_minimum_node_in_a_binary_tree.py
+++ trees/problems/671_second_minimum_node_in_a_binary_tree.py
@@ -4,4 +4,22 @@
 # Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
 # If no such second minimum value exists, output -1 instead.
 
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        stack = [root]
+        second = float('inf')
+        while stack:
+            node = stack.pop()
+            if node.left:
+                if node.left.val != node.right.val:
+                    second = min(second,max(node.left.val, node.right.val))
+                
+                stack.append(node.left)
+                stack.append(node.right)
+        return second if second != float('inf') else -1
