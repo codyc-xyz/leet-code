dc791078bce4695405247d9fc4fa34e30300b5bc
Red <crabmarket56@gmail.com>
2023-02-19 09:23:51 +0900

    Add initial solution

--- trees/problems/111_minimum_depth_of_binary_tree.py
+++ trees/problems/111_minimum_depth_of_binary_tree.py
@@ -4,3 +4,24 @@
 
 # Note: A leaf is a node with no children.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        self.min = float('inf')
+
+        def dfs(node, depth):
+            if not node:
+                return
+            if not node.left and not node.right:
+                self.min = min(depth, self.min)
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        dfs(root, 1)
+        return self.min
