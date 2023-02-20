aed1b46529972d7b4ed12e33436935b55505520f
Red <crabmarket56@gmail.com>
2023-02-20 10:30:52 +0900

    Add alternative solution

--- trees/problems/701_insert_into_a_binary_search_tree.py
+++ trees/problems/701_insert_into_a_binary_search_tree.py
@@ -28,3 +28,15 @@ class Solution:
                 else:
                     curr.left = TreeNode(val)
                     return root
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        if not root:
+            return TreeNode(val)
+        
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
