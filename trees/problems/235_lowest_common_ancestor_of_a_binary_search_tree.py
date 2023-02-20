202fa3de9cfba6846a0199bd3de5cc98c1d2c901
Red <crabmarket56@gmail.com>
2023-02-20 10:02:15 +0900

    Add alternative solution

--- trees/problems/235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ trees/problems/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -20,3 +20,14 @@ class Solution:
                 stack.append(node.left)
             else:
                 stack.append(node.right)
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if p.val <= root.val <= q.val or q.val <= root.val <= p.val:
+                return root
+        elif p.val <= root.val and q.val <= root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
+            
