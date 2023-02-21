3e7255afd549fb559b09619d1ebc7714b2a91d5c
Red <crabmarket56@gmail.com>
2023-02-21 15:04:33 +0900

    Add initial solution

--- trees/problems/129_sum_root_to_leaf_numbers.py
+++ trees/problems/129_sum_root_to_leaf_numbers.py
@@ -6,3 +6,25 @@
 # Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
 
 # A leaf node is a node with no children.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        arr = []
+        self.sum = 0 
+        def dfs(root):
+            if not root:
+                return 
+            arr.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
+            if not root.left and not root.right:
+                self.sum += int(''.join(arr))
+            arr.pop()
+        dfs(root)
+        return self.sum
