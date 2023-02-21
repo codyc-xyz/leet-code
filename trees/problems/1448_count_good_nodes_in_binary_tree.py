718a4425cea8f8dd4a0fc9539e9d81f5e1519c82
Red <crabmarket56@gmail.com>
2023-02-21 09:10:31 +0900

    Add initial solution

--- trees/problems/1448_count_good_nodes_in_binary_tree.py
+++ trees/problems/1448_count_good_nodes_in_binary_tree.py
@@ -2,3 +2,23 @@
 
 # Return the number of good nodes in the binary tree.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        dq = deque([(root, root.val)])
+        ans = 0
+        while dq:
+            node, maxVal = dq.popleft()
+            if node.val >= maxVal:
+                ans += 1
+            if node.left:
+                dq.append((node.left, max(node.left.val, maxVal)))
+            if node.right:
+                dq.append((node.right, max(node.right.val, maxVal)))
+        return ans
