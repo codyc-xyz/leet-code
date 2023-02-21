95af82114198a989eecee66b73bd81717cf89cf4
Red <crabmarket56@gmail.com>
2023-02-21 09:15:13 +0900

    Add alternative solution

--- trees/problems/1448_count_good_nodes_in_binary_tree.py
+++ trees/problems/1448_count_good_nodes_in_binary_tree.py
@@ -22,3 +22,19 @@ class Solution:
             if node.right:
                 dq.append((node.right, max(node.right.val, maxVal)))
         return ans
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.ans = 0
+        
+        def dfs(root, maxVal):
+            if not root:
+                return
+            if root.val >= maxVal:
+                self.ans += 1
+                maxVal = root.val
+            dfs(root.left, maxVal)
+            dfs(root.right, maxVal)
+        dfs(root, root.val)
+        return self.ans
