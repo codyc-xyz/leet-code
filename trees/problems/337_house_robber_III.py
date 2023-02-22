899ddfb9eb474e81b4599545b989e0d4a68c88ae
Red <crabmarket56@gmail.com>
2023-02-22 09:21:58 +0900

    Add initial solution

--- trees/problems/337_house_robber_III.py
+++ trees/problems/337_house_robber_III.py
@@ -4,3 +4,24 @@
 # It will automatically contact the police if two directly-linked houses were broken into on the same night.
 
 # Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+               return [0, 0]
+
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withoutRoot = max(leftPair) + max(rightPair)
+            return [withRoot, withoutRoot]
+            
+        return max(dfs(root))
