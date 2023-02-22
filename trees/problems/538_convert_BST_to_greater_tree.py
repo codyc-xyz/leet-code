e5574d14c5e014bc21d19b15dcd81433ad9ec3c6
Red <crabmarket56@gmail.com>
2023-02-22 11:12:58 +0900

    Add initial solution

--- trees/problems/538_convert_BST_to_greater_tree.py
+++ trees/problems/538_convert_BST_to_greater_tree.py
@@ -6,3 +6,15 @@
 # The right subtree of a node contains only nodes with keys greater than the node's key.
 # Both the left and right subtrees must also be binary search trees.
  
+ class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        arr = []
+        def dfs(root):
+            if not root:
+                return 
+            dfs(root.right)
+            arr.append(root.val)
+            root.val += sum(arr) - root.val
+            dfs(root.left, currSum)
+            return root
+        return dfs(root, 0)
