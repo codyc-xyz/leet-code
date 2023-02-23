a85174551257b68908546fd495be7f0fe875b5b0
Red <crabmarket56@gmail.com>
2023-02-23 09:09:38 +0900

    Add initial solution

--- trees/problems/590_N-ary_tree_postorder_traversal.py
+++ trees/problems/590_N-ary_tree_postorder_traversal.py
@@ -2,3 +2,24 @@
 
 # Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        arr = deque()
+        dq = deque([root])
+        while dq:
+            node = dq.popleft()
+            arr.appendleft(node.val)
+            lenChildren = len(node.children) 
+            for i in range(lenChildren):
+                dq.appendleft(node.children[i])
+        return arr
