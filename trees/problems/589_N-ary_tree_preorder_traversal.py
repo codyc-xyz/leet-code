75e2e67ec29e0258bf1d1430c0a1a4203915be1f
Red <crabmarket56@gmail.com>
2023-02-23 09:22:36 +0900

    Add initial solution

--- trees/problems/589_N-ary_tree_preorder_traversal.py
+++ trees/problems/589_N-ary_tree_preorder_traversal.py
@@ -2,3 +2,22 @@
 
 # Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(root):
+            if not root:
+                return 
+            ans.append(root.val)
+            for i in range(len(root.children)):
+                dfs(root.children[i])
+        dfs(root)
+        return ans
