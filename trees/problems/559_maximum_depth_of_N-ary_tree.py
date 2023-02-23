76b24ac32708dd12a4fdd3af7dd1ad28fee25a72
Red <crabmarket56@gmail.com>
2023-02-23 09:43:09 +0900

    Add initial solution

--- trees/problems/559_maximum_depth_of_N-ary_tree.py
+++ trees/problems/559_maximum_depth_of_N-ary_tree.py
@@ -4,3 +4,25 @@
 
 # Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        level = 0
+        dq = deque([root])
+        while dq:
+            lenDq = len(dq)
+            for i in range(lenDq):
+                node = dq.popleft()
+                for c in node.children:
+                    dq.append(c)
+            level += 1
+        return level
