232a91f20db2dda5d38d767cd7faff9112fd35b0
Red <crabmarket56@gmail.com>
2023-02-23 11:12:21 +0900

    Add initial solution

--- trees/problems/1008_construct_binary_search_tree_from_preorder_traversal.py
+++ trees/problems/1008_construct_binary_search_tree_from_preorder_traversal.py
@@ -5,3 +5,26 @@
 # A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
 
 # A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        length = len(preorder)
+        root = TreeNode(preorder[0])
+        stack = [root]
+        for i in range(1, length):
+            last = None
+            node = TreeNode(preorder[i])
+            if preorder[i] < stack[-1].val:
+                stack[-1].left = node
+            while stack and preorder[i] > stack[-1].val:
+                last = stack.pop()
+            if last:
+                last.right = node
+            stack.append(node)
+        return root
