3a2be9f4cea28e7a29a9dc171cccb5ff465fae7c
Red <crabmarket56@gmail.com>
2023-02-24 12:39:24 +0900

    Add initial solution

--- trees/problems/1261_find_elements_in_contaminated_binary_tree.py
+++ trees/problems/1261_find_elements_in_contaminated_binary_tree.py
@@ -9,3 +9,34 @@
 
 # FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
 # bool find(int target) Returns true if the target value exists in the recovered binary tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.vals = set()
+        def dfs(root, prev, bias):
+            if not root:
+                return
+            if bias == True:
+                root.val = prev * 2 + 1
+            elif bias == False:
+                root.val = prev * 2 + 2
+            else:
+                root.val = 0
+            self.vals.add(root.val)
+            dfs(root.left, root.val, True)
+            dfs(root.right, root.val, False)
+        dfs(root, 0, None)
+
+
+    def find(self, target: int) -> bool:
+        if target in self.vals:
+            return True
+        else:
+            return False
