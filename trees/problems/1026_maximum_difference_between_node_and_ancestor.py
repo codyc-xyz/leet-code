29ef9b2e96602531c452a2b927ba256e2b8b5f8c
Red <crabmarket56@gmail.com>
2023-02-25 15:08:14 +0900

    Add initial solution

--- trees/problems/1026_maximum_difference_between_node_and_ancestor.py
+++ trees/problems/1026_maximum_difference_between_node_and_ancestor.py
@@ -2,3 +2,25 @@
 
 # A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        self.maxDiff = 0
+
+        def dfs(root, minVal, maxVal):
+            if not root:
+                return
+            
+            if root.val < minVal: minVal = root.val
+            if root.val > maxVal: maxVal = root.val
+            self.maxDiff = max(self.maxDiff, maxVal - minVal)
+            dfs(root.left, minVal, maxVal)
+            dfs(root.right, minVal, maxVal)
+
+        dfs(root, float('inf'), float('-inf'))
+        return self.maxDiff
