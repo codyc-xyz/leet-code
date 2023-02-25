caf316ab4c588b85c75bd0b5ba6e9bfbbfb2494b
Red <crabmarket56@gmail.com>
2023-02-25 15:36:41 +0900

    Add initial solution

--- trees/problems/814_binary_tree_pruning.py
+++ trees/problems/814_binary_tree_pruning.py
@@ -2,3 +2,18 @@
 
 # A subtree of a node node is node plus every node that is a descendant of node.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: 
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if (root.val == 0 and not root.left and not root.right):
+            return None
+        return root
