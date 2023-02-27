44642ed0764ccf34428f6cf1e39bfc965c30bf4d
Red <crabmarket56@gmail.com>
2023-02-27 11:37:51 +0900

    Add initial solution

--- trees/problems/998_maximum_binary_tree_II.py
+++ trees/problems/998_maximum_binary_tree_II.py
@@ -14,3 +14,16 @@
 # Suppose b is a copy of a with the value val appended to it. It is guaranteed that b has unique values.
 
 # Return Construct(b).
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoMaxTree(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root or val > root.val:
+            return TreeNode(val, root)
+        root.right = self.insertIntoMaxTree(root.right, val)
+        return root
