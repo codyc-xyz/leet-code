1be9feedd0f00cee8e91a7a257934f75eb1d49e4
Red <crabmarket56@gmail.com>
2023-02-27 09:30:50 +0900

    Add initial solution

--- trees/problems/429_N-ary_tree_level_order_traversal.py
+++ trees/problems/429_N-ary_tree_level_order_traversal.py
@@ -2,3 +2,27 @@
 
 # Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        dq = deque([root])
+        ans = []
+        while dq:
+            lenDq = len(dq)
+            curr = []
+            for i in range(lenDq):
+                node = dq.popleft()
+                curr.append(node.val)
+                for c in node.children:
+                    dq.append(c)
+            ans.append(curr)
+        return ans
