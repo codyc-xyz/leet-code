b4e8f6a68bff9574c9fe68c0dfeb366df5f22c1b
Red <crabmarket56@gmail.com>
2023-02-27 09:38:14 +0900

    Add initial solution

--- trees/problems/173_binary_search_tree_iterator.py
+++ trees/problems/173_binary_search_tree_iterator.py
@@ -6,3 +6,40 @@
 # Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
 
 # You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.vals = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            self.vals.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        self.idx = 0
+
+    def next(self) -> int:
+        curr = self.vals[self.idx]
+        self.idx += 1
+        return curr
+        
+
+    def hasNext(self) -> bool:
+        if self.idx < len(self.vals):
+            return True
+        return False
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
