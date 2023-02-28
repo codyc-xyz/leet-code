880843b4ea9f371b0bc5bfe7b4507277a6c3b868
Red <crabmarket56@gmail.com>
2023-02-28 10:34:18 +0900

    Initialize problem

---
+++ trees/problems/1600_throne_inheritance.py
@@ -0,0 +1,4 @@
+# A kingdom consists of a king, his children, his grandchildren, and so on. Every once in a while, someone in the family dies or a child is born.
+
+# The kingdom has a well-defined order of inheritance that consists of the king as the first member. 
+# Let's define the recursive function Successor(x, curOrder), which given a person x and the inheritance order so far, returns who should be the next person after x in the order of inheritance.
