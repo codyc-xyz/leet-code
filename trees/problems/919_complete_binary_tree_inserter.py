8ee50a19171074d1c43c8521122669e8f8fdc829
Red <crabmarket56@gmail.com>
2023-02-28 10:00:27 +0900

    Add initial solution

--- trees/problems/919_complete_binary_tree_inserter.py
+++ trees/problems/919_complete_binary_tree_inserter.py
@@ -7,3 +7,44 @@
 # CBTInserter(TreeNode root) Initializes the data structure with the root of the complete binary tree.
 # int insert(int v) Inserts a TreeNode into the tree with value Node.val == val so that the tree remains complete, and returns the value of the parent of the inserted TreeNode.
 # TreeNode get_root() Returns the root node of the tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class CBTInserter:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.ans = root
+        self.dq = deque([root])
+        while True:
+            flag = False
+            lenDq = len(self.dq)
+            for i in range(lenDq):
+                node = self.dq[0]
+                if node.left and node.right:
+                    self.dq.popleft()
+                    self.dq.append(node.left)
+                    self.dq.append(node.right)
+                else: 
+                    flag = True
+                    if node.left: self.dq.append(node.left)
+                    break
+            if flag == True:
+                break
+
+    def insert(self, val: int) -> int:
+        node = TreeNode(val)
+        parent = self.dq[0]
+        if not self.dq[0].left:
+            self.dq[0].left = node
+        else:
+            self.dq[0].right = node
+            self.dq.popleft()
+        self.dq.append(node)
+        return parent.val
+
+    def get_root(self) -> Optional[TreeNode]:
+        return self.ans
