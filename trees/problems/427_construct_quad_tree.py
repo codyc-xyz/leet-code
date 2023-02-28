16cef766dbbbc3a2258266d6d9edd8815eda0c64
Red <crabmarket56@gmail.com>
2023-02-28 09:19:08 +0900

    Add initial solution

--- trees/problems/427_construct_quad_tree.py
+++ trees/problems/427_construct_quad_tree.py
@@ -1,3 +1,36 @@
 # Given a n * n matrix grid of 0's and 1's only. We want to represent the grid with a Quad-Tree.
 
 # Return the root of the Quad-Tree representing the grid.
+
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+
+        def dfs(n, r, c):
+            allSame = True
+            for i in range(n):
+                for j in range(n):
+                    if grid[r][c] != grid[r + i][c + j]:
+                        allSame = False
+                        break
+            if allSame:
+                return Node(grid[r][c], True)
+            
+            n = n // 2
+            topLeft = dfs(n, r, c)
+            topRight = dfs(n, r, c + n)
+            bottomLeft = dfs(n, r + n, c)
+            bottomRight = dfs(n, r + n, c + n)
+            return Node(0, False, topLeft, topRight, bottomLeft, bottomRight)
+        return dfs(len(grid), 0, 0)
