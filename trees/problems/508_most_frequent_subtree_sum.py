e79c4e18edd04781d4e74c0d6cfca68052168cf0
Red <crabmarket56@gmail.com>
2023-02-28 10:20:13 +0900

    Add initial solution

--- trees/problems/508_most_frequent_subtree_sum.py
+++ trees/problems/508_most_frequent_subtree_sum.py
@@ -2,3 +2,28 @@
 
 # The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself).
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return
+        subSums = {}
+        def dfs(root, subSum):
+            if not root:
+                return 0
+            subSum = root.val + dfs(root.left, subSum) + dfs(root.right, subSum)
+            subSums[subSum] = subSums.get(subSum, 0) + 1
+            return subSum
+            
+        dfs(root, 0)
+        maxCount = max(subSums.values())
+        ans = []
+        for s in subSums:
+            if subSums[s] == maxCount:
+                ans.append(s)
+        return ans
