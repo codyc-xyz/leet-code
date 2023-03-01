ad8c928c10f74b54cf4436a8375af9c61e315162
Red <crabmarket56@gmail.com>
2023-03-01 09:49:46 +0900

    Add initial solution

--- trees/problems/107_binary_tree_level_order_traversal_II.py
+++ trees/problems/107_binary_tree_level_order_traversal_II.py
@@ -1,2 +1,25 @@
 # Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        levels = deque()
+        dq = deque([root])
+
+        while dq:
+            lenDq = len(dq)
+            level = []
+            for i in range(lenDq):
+                node = dq.popleft()
+                level.append(node.val)
+                if node.left: dq.append(node.left)
+                if node.right: dq.append(node.right)
+            levels.appendleft(level)
+        return levels
