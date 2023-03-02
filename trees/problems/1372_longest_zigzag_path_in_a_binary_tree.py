4a28cb612efe68845eec1900f6a4581778b84e32
Red <crabmarket56@gmail.com>
2023-03-02 11:28:56 +0900

    Add initial solution

--- trees/problems/1372_longest_zigzag_path_in_a_binary_tree.py
+++ trees/problems/1372_longest_zigzag_path_in_a_binary_tree.py
@@ -9,3 +9,23 @@
 # Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
 
 # Return the longest ZigZag path contained in that tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        def dfs(root):
+            if not root:
+                return 0, 0
+        
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.ans = max(self.ans, max(left[1], right[0]))
+            return left[1] + 1, right[0] + 1
+        dfs(root)
+        return self.ans
