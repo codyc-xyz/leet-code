441b1fd486ff77237c603838b54dd761e53bfaa2
Red <crabmarket56@gmail.com>
2023-03-02 09:34:47 +0900

    Add initial solution

--- trees/problems/222_count_complete_tree_nodes.py
+++ trees/problems/222_count_complete_tree_nodes.py
@@ -4,3 +4,20 @@
 
 # Design an algorithm that runs in less than O(n) time complexity.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        self.nodes = 0
+        def dfs(root):
+            if not root:
+                return
+            self.nodes += 1
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return self.nodes
