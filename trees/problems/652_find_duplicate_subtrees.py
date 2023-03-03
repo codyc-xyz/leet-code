7cb330ca045429f4d1e832a3a4d1c4d37d21c29d
Red <crabmarket56@gmail.com>
2023-03-03 10:19:38 +0900

    Add initial solution

--- trees/problems/652_find_duplicate_subtrees.py
+++ trees/problems/652_find_duplicate_subtrees.py
@@ -3,3 +3,30 @@
 # For each kind of duplicate subtrees, you only need to return the root node of any one of them.
 
 # Two trees are duplicate if they have the same structure with the same node values.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+
+        subTrees = set()
+        dupes = set()
+        dupeStr = set()
+
+        def dfs(root):
+            if not root:
+                return 'null'
+            string = '(' + str(root.val) + dfs(root.left) + dfs(root.right) + ')'
+            
+            if string in subTrees and string not in dupeStr:
+                dupes.add(root)
+                dupeStr.add(string)
+            else:
+                subTrees.add(string)
+            return string 
+        dfs(root)
+        return dupes
