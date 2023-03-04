662e7695857480340393494d7b3b37063f00ffeb
Red <crabmarket56@gmail.com>
2023-03-04 10:14:38 +0900

    Add initial solution

--- trees/problems/2368_reachable_nodes_with_restrictions.py
+++ trees/problems/2368_reachable_nodes_with_restrictions.py
@@ -6,3 +6,23 @@
 
 # Note that node 0 will not be a restricted node.
 
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        
+        nodes = defaultdict(list)
+        nodes[0].append(None)
+        for e1, e2 in edges:
+            if e1 not in restricted and e2 not in restricted:
+                nodes[e1].append(e2)
+                nodes[e2].append(e1)
+
+        visited = set()
+        def dfs(n):
+            if not nodes[n]:
+                return 
+            visited.add(n)
+            for c in nodes[n]:
+                if c not in visited:
+                    dfs(c)
+        dfs(0)
+        return len(visited)
