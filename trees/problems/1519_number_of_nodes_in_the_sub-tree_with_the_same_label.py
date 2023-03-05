51044b60d662a29b754267050915db6dce934d5e
Red <crabmarket56@gmail.com>
2023-03-05 12:49:46 +0900

    Add initial solution

--- trees/problems/1519_number_of_nodes_in_the_sub-tree_with_the_same_label.py
+++ trees/problems/1519_number_of_nodes_in_the_sub-tree_with_the_same_label.py
@@ -6,3 +6,24 @@
 # Return an array of size n where ans[i] is the number of nodes in the subtree of the ith node which have the same label as node i.
 
 # A subtree of a tree T is the tree consisting of a node in T and all of its descendant nodes.
+
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        adj = defaultdict(list)
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+        res = [0] * n
+
+        def dfs(node, prev):
+            count = Counter()
+            label = labels[node]
+            count[label] = 1
+
+            for child in adj[node]:
+                if child != prev:
+                    count += dfs(child, node)
+            res[node] = count[label]
+1            return count
+        dfs(0, -1)
+        return res
