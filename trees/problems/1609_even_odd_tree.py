85aa4ea59a6e95dc11c9d4d1e4ace3b12343256a
Red <crabmarket56@gmail.com>
2023-03-05 14:00:33 +0900

    Add initial solution

--- trees/problems/1609_even_odd_tree.py
+++ trees/problems/1609_even_odd_tree.py
@@ -5,3 +5,34 @@
 # For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
 # Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+
+        dq = deque([root])
+        depth = 0
+        while dq:
+            lenDq = len(dq)
+            prev = None
+            for i in range(lenDq):
+                node = dq.popleft()
+                if not depth % 2:
+                    if not node.val % 2:
+                        return False
+                    if prev and prev >= node.val:
+                        return False
+                else:
+                    if node.val % 2:
+                        return False
+                    if prev and prev <= node.val:
+                        return False
+                if node.left: dq.append(node.left)
+                if node.right: dq.append(node.right)
+                prev = node.val
+            depth += 1
+        return True
