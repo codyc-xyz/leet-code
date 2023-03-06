e2fb291f6bd23be8255dd279beb64c2cc8e00e2a
Red <crabmarket56@gmail.com>
2023-03-06 18:13:37 +0900

    Add initial solution

--- trees/problems/117_populating_next_right_pointers_in_each_node_II.py
+++ trees/problems/117_populating_next_right_pointers_in_each_node_II.py
@@ -4,3 +4,32 @@
 
 # Initially, all next pointers are set to NULL.
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        dq = deque([root])
+
+        while dq:
+            lenDq = len(dq)
+
+            for i in range(lenDq):
+                node = dq.popleft()
+                if i < lenDq - 1:
+                    node.next = dq[0]
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+
+        return root
