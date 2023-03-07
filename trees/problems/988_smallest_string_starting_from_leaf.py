6cc1775d4a0090bf646737081387acab52a01417
Red <crabmarket56@gmail.com>
2023-03-07 09:46:22 +0900

    Add initial solution

--- trees/problems/988_smallest_string_starting_from_leaf.py
+++ trees/problems/988_smallest_string_starting_from_leaf.py
@@ -6,3 +6,42 @@
 
 # For example, "ab" is lexicographically smaller than "aba".
 # A leaf of a node is a node that has no children.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+
+        self.ans = [99]
+
+        def dfs(root, path):
+            if not root:
+                return
+            path.append(root.val)
+            if not root.left and not root.right:
+                curr = path[::-1]
+                for i in range(len(self.ans)):
+                    if i < len(curr):
+                        if self.ans[i] > curr[i]:
+                            self.ans = curr
+                            break
+                        elif self.ans[i] == curr[i]:
+                            continue
+                        else:
+                            break
+                    else:
+                        self.ans = curr
+                        break
+                
+            dfs(root.left, path[:])
+            dfs(root.right, path[:])
+        dfs(root, [])
+        res = ""
+        for n in self.ans:
+            res += (chr(ord('a') + n))
+                
+        return res
