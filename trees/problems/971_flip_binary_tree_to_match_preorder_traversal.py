25d6a02fb7402f331371330c77079793c27a3a22
Red <crabmarket56@gmail.com>
2023-03-07 09:12:45 +0900

    Add initial solution

--- trees/problems/971_flip_binary_tree_to_match_preorder_traversal.py
+++ trees/problems/971_flip_binary_tree_to_match_preorder_traversal.py
@@ -3,3 +3,32 @@
 
 # Flip the smallest number of nodes so that the pre-order traversal of the tree matches voyage.
 # Return a list of the values of all flipped nodes. You may return the answer in any order. If it is impossible to flip the nodes in the tree to make the pre-order traversal match voyage, return the list [-1].
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipMatchVoyage(self, root: Optional[TreeNode], voyage: List[int]) -> List[int]:
+
+        self.i = 0
+        self.res = []
+        def dfs(root):
+            if not root:
+                return
+            if root.val != voyage[self.i]:
+                self.res = [-1]
+                return
+            self.i += 1
+
+            if root.left and root.left.val != voyage[self.i]:
+                self.res.append(root.val)
+                dfs(root.right)
+                dfs(root.left)
+            else:
+                dfs(root.left)
+                dfs(root.right)
+        dfs(root)
+        return self.res if -1 not in self.res else [-1]
