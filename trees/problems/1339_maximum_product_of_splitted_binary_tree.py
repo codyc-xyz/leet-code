881f41bb98fba60721559c328ce984976bba1d17
Red <crabmarket56@gmail.com>
2023-03-08 10:18:35 +0900

    Initialize problem

---
+++ trees/problems/1339_maximum_product_of_splitted_binary_tree.py
@@ -0,0 +1,5 @@
+# Given the root of a binary tree, split the binary tree into two subtrees by removing one edge such that the product of the sums of the subtrees is maximized.
+
+# Return the maximum product of the sums of the two subtrees. Since the answer may be too large, return it modulo 109 + 7.
+
+# Note that you need to maximize the answer before taking the mod and not after taking it.
