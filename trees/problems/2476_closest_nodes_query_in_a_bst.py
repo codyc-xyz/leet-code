ba62c16edd22fae162b0534369eb59eeb81be318
Red <crabmarket56@gmail.com>
2023-03-10 09:48:33 +0900

    Initialize problem

---
+++ trees/problems/2476_closest_nodes_query_in_a_bst.py
@@ -0,0 +1,7 @@
+# You are given the root of a binary search tree and an array queries of size n consisting of positive integers.
+
+# Find a 2D array answer of size n where answer[i] = [mini, maxi]:
+
+# mini is the largest value in the tree that is smaller than or equal to queries[i]. If a such value does not exist, add -1 instead.
+# maxi is the smallest value in the tree that is greater than or equal to queries[i]. If a such value does not exist, add -1 instead.
+# Return the array answer.
