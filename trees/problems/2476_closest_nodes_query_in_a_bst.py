ec3cd37f2f8453e2255620a154c26f5cb8de632b
Red <crabmarket56@gmail.com>
2023-03-10 09:48:49 +0900

    Add initial answer (TLE)

--- trees/problems/2476_closest_nodes_query_in_a_bst.py
+++ trees/problems/2476_closest_nodes_query_in_a_bst.py
@@ -5,3 +5,35 @@
 # mini is the largest value in the tree that is smaller than or equal to queries[i]. If a such value does not exist, add -1 instead.
 # maxi is the smallest value in the tree that is greater than or equal to queries[i]. If a such value does not exist, add -1 instead.
 # Return the array answer.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestNodes(self, root: Optional[TreeNode], queries: List[int]) -> List[List[int]]:
+
+        arr = []
+
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            arr.append(root.val)
+            dfs(root.right)
+        dfs(root)
+
+        ans = []
+        for q in queries:
+            l, r = 0, len(arr) - 1
+            while l < len(arr) - 1 and arr[l + 1] <= q:
+                l += 1
+            while r > 0 and arr[r - 1] >= q:
+                r -= 1
+            l = arr[l] if arr[l] <= q else -1
+            r = arr[r] if arr[r] >= q else -1
+            
+            ans.append([l, r])
+        return ans
