84b8961a8c1652bc6e991edbaf46387aad219406
Red <crabmarket56@gmail.com>
2023-03-12 10:57:25 +0900

    Add initial solution

--- backtracking/problems/90_subsets_II.py
+++ backtracking/problems/90_subsets_II.py
@@ -1,3 +1,19 @@
 # Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        def backtrack(i, path):
+            if i >= len(nums):
+                if path not in ans:
+                    ans.append(path)
+                return
+            path.append(nums[i])
+            backtrack(i + 1, path[:])
+            path.pop()
+            backtrack(i + 1, path[:])
+        backtrack(0, [])
+        return ans
