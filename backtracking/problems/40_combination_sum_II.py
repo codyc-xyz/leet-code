517113cc0ce8245c8f32fb861c5aab467c670d57
Red <crabmarket56@gmail.com>
2023-03-12 18:03:31 +0900

    Add initial solution (TLE)

--- backtracking/problems/40_combination_sum_II.py
+++ backtracking/problems/40_combination_sum_II.py
@@ -3,3 +3,25 @@
 # Each number in candidates may only be used once in the combination.
 
 # Note: The solution set must not contain duplicate combinations.
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans = []
+        def backtrack(i, currSum, path):
+            if i >= len(candidates) or currSum > target:
+                return
+            if currSum == target and path not in ans:
+                ans.append(path)
+                return
+            path.append(candidates[i])
+            if currSum + candidates[i] == target and path not in ans:
+                ans.append(path)
+                return
+            
+            backtrack(i + 1, currSum + candidates[i], path[:] )
+            path.pop()
+            backtrack(i + 1, currSum, path[:])
+        
+        backtrack(0, 0, [])
+        return ans
