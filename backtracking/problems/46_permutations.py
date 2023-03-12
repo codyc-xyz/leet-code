c2be52f3d01dde8d61a8ad814d5c1408643eb32d
Red <crabmarket56@gmail.com>
2023-03-12 10:32:32 +0900

    Add initial solution

--- backtracking/problems/46_permutations.py
+++ backtracking/problems/46_permutations.py
@@ -1,2 +1,17 @@
 # Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        ans = []
+        if len(nums) == 1:
+            return [nums[:]]
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for p in perms:
+                p.append(n)
+            ans.extend(perms)
+            nums.append(n)
+        return ans
