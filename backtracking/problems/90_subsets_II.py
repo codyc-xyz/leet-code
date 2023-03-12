21bd12fe9e41fdd32c409877bbc7d8b71c9cd001
Red <crabmarket56@gmail.com>
2023-03-12 11:03:52 +0900

    Refactor solution

--- backtracking/problems/90_subsets_II.py
+++ backtracking/problems/90_subsets_II.py
@@ -8,12 +8,13 @@ class Solution:
         ans = []
         def backtrack(i, path):
             if i >= len(nums):
-                if path not in ans:
-                    ans.append(path)
+                ans.append(path)
                 return
             path.append(nums[i])
             backtrack(i + 1, path[:])
             path.pop()
+            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                i += 1
             backtrack(i + 1, path[:])
         backtrack(0, [])
         return ans
