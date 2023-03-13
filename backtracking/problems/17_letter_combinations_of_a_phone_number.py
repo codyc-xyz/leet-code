35a643055c801dec17f781e24ab720136635584c
Red <crabmarket56@gmail.com>
2023-03-13 17:50:55 +0900

    Add alternative solution

--- backtracking/problems/17_letter_combinations_of_a_phone_number.py
+++ backtracking/problems/17_letter_combinations_of_a_phone_number.py
@@ -30,3 +30,31 @@ class Solution:
             
         backtrack(0, [])
         return ans
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        hm = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        
+        ans = []
+        def backtrack(i, path):
+            if i == len(digits):
+                ans.append(path)
+                return
+    
+            for letter in hm[digits[i]]:
+                backtrack(i + 1, path + letter)
+                
+        if digits: 
+            backtrack(0, "")
+        return ans
