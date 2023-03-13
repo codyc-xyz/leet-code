336462f20ecd04024c3a3d497bb33093e11320d3
Red <crabmarket56@gmail.com>
2023-03-13 18:15:42 +0900

    Add alternative solution

--- backtracking/problems/47_permutations_II.py
+++ backtracking/problems/47_permutations_II.py
@@ -19,3 +19,26 @@ class Solution:
                         seen.remove(j)
             backtrack([], set())
             return ans
+
+class Solution:
+        def permuteUnique(self, nums: List[int]) -> List[List[int]]:      
+            ans = []
+            path = []
+            count = collections.Counter(nums)
+            def backtrack(path):
+                if len(path) == len(nums) and path not in ans:
+                    ans.append(path)
+                    return
+
+                elif len(path) >= len(nums):
+                    return
+                    
+                for n in count:
+                    if count[n] > 0:
+                        count[n] -= 1
+                        path.append(n)
+                        backtrack(path[:])
+                        path.pop()
+                        count[n] += 1
+            backtrack([])
+            return ans
