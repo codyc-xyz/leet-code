03e817d655f2db491f16bd87170b28046469db97
Red <crabmarket56@gmail.com>
2023-03-13 18:04:11 +0900

    Add initial solution

--- backtracking/problems/47_permutations_II.py
+++ backtracking/problems/47_permutations_II.py
@@ -1,1 +1,21 @@
 # Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+class Solution:
+        def permuteUnique(self, nums: List[int]) -> List[List[int]]:      
+            ans = []
+            path = []
+            def backtrack(path, seen):
+                if len(path) == len(nums) and path not in ans:
+                    ans.append(path)
+                    return
+                elif len(path) == len(nums):
+                    return
+                for j, c in enumerate(nums):
+                    if j not in seen:
+                        path.append(c)
+                        seen.add(j)
+                        backtrack(path[:], seen)
+                        path.pop()
+                        seen.remove(j)
+            backtrack([], set())
+            return ans
