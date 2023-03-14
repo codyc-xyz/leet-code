758c0ecb97d1ba26402e7f00739dce432e4ca642
Red <crabmarket56@gmail.com>
2023-03-14 09:42:37 +0900

    Add initial solution

--- backtracking/problems/1980_find_unique_binary_string.py
+++ backtracking/problems/1980_find_unique_binary_string.py
@@ -1,2 +1,23 @@
 # Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
 
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+
+        self.ans = ""
+
+        def backtrack(curr):
+            if self.ans:
+                return
+            if len(curr) == len(nums):
+                res = "".join(curr)
+                if res not in nums:
+                    self.ans = res
+                return
+            curr.append('0')
+            backtrack(curr)
+            curr.pop()
+            curr.append('1')
+            backtrack(curr)
+            curr.pop()
+        backtrack([])
+        return self.ans
