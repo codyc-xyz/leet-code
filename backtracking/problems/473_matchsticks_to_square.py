afb18be8fe8c9ad04c95bc7fc42e622f8c7e975e
Red <crabmarket56@gmail.com>
2023-03-16 09:26:04 +0900

    Add initial solution (TLE)

--- backtracking/problems/473_matchsticks_to_square.py
+++ backtracking/problems/473_matchsticks_to_square.py
@@ -3,3 +3,38 @@
 
 # Return true if you can make this square and false otherwise.
 
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+
+        total = sum(matchsticks)
+        
+        if total % 4:
+            return False
+        self.target = total // 4
+        used = [False] * len(matchsticks)
+        buckets = [0] * 4
+        def backtrack(i, sides):
+            if sides == 4:
+                return True
+            
+            for j in range(i, len(matchsticks)):
+                if not used[j]:
+                    for x, b in enumerate(buckets):
+                        if b + matchsticks[j] == self.target:
+                            buckets[x] += matchsticks[j]
+                            used[j] = True
+                            sides += 1
+                            if backtrack(j + 1, sides):
+                                return True
+                            used[j] = False
+                            buckets[x] -= matchsticks[j]
+                            sides -= 1
+                        elif b + matchsticks[j] < self.target:
+                            buckets[x] += matchsticks[j]
+                            used[j] = True
+                            if backtrack(j + 1, sides):
+                                return True
+                            buckets[x] -= matchsticks[j]
+                            used[j] = False
+            return False
+        return backtrack(0, 0)
