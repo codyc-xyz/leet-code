9e085c85af799d3a5c6befa9b142cce78ca03162
Red <crabmarket56@gmail.com>
2023-03-16 10:16:49 +0900

    Add initial solution

--- backtracking/problems/797_all_paths_from_source_to_target.py
+++ backtracking/problems/797_all_paths_from_source_to_target.py
@@ -1,3 +1,18 @@
 # Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
 
 # The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        ans = []
+        def backtrack(i, path):
+            if i == len(graph) - 1:
+                ans.append(path)
+                return
+            for j in graph[i]:
+                path.append(j)
+                backtrack(j, path[:])
+                path.pop()
+        backtrack(0, [0])
+        return ans
