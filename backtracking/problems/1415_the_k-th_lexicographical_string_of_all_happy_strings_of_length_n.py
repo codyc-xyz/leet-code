4c7cbf9c5621fbf9069d16b67ee3d5178719af4d
Red <crabmarket56@gmail.com>
2023-03-17 09:46:26 +0900

    Add initial solution

--- backtracking/problems/1415_the_k-th_lexicographical_string_of_all_happy_strings_of_length_n.py
+++ backtracking/problems/1415_the_k-th_lexicographical_string_of_all_happy_strings_of_length_n.py
@@ -5,3 +5,28 @@
 # Given two integers n and k, consider a list of all happy strings of length n sorted in lexicographical order.
 
 # Return the kth string of this list or return an empty string if there are less than k happy strings of length n.
+
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+
+
+        happy = []
+
+        def backtrack(i, curr):
+            if len(happy) >= k:
+                return
+            if i == n:
+                happy.append(curr)
+                return
+            if not curr or curr[-1] != 'a':
+                backtrack(i + 1, curr + 'a')
+            if not curr or curr[-1] != 'b':
+                backtrack(i + 1, curr + 'b')
+            if not curr or curr[-1] != 'c':
+                backtrack(i + 1, curr + 'c')
+
+        backtrack(0, "")
+        if len(happy) >= k:
+            return happy[-1]
+        else:
+            return ""
