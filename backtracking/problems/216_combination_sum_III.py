f8ae01f1e7a4dbab17cf9b7d959bef02456bac68
Red <crabmarket56@gmail.com>
2023-03-17 09:56:17 +0900

    Add initial solution

--- backtracking/problems/216_combination_sum_III.py
+++ backtracking/problems/216_combination_sum_III.py
@@ -3,3 +3,21 @@
 # Only numbers 1 through 9 are used.
 # Each number is used at most once.
 # Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:   
+        ans = []
+
+        def backtrack(i, currSum, path):
+            if currSum > n or len(path) > k:
+                return
+            elif currSum == n and len(path) == k:
+                ans.append(path)
+                return
+            elif currSum == n or len(path) == k:
+                return
+            for j in range(i, 10):
+                backtrack(j + 1, currSum + j, path + [j])
+
+        backtrack(1, 0, [])
+        return ans
