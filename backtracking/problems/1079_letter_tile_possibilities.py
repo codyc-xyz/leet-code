2ec59d4ee289f6e175099786efce4ba1a9030c34
Red <crabmarket56@gmail.com>
2023-03-17 09:26:32 +0900

    Initialize problem

--- backtracking/problems/1079_letter_tile_possibilities.py
+++ backtracking/problems/1079_letter_tile_possibilities.py
@@ -4,6 +4,28 @@
 
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
+
+
+        self.ans = 0
+        seen = set()
+        count = collections.Counter(tiles)
+        def backtrack(i, curr):
+            if i == len(tiles):
+                if len(curr) > 0 and curr not in seen:
+                    self.ans += 1
+                    seen.add(curr)
+                return
+            for j in range(len(tiles)):
+                if count[tiles[j]] > 0:
+                    count[tiles[j]] -= 1
+                    backtrack(i + 1, curr + tiles[j])
+                    count[tiles[j]] += 1
+                    backtrack(i + 1, curr)
+        backtrack(0, "")
+        return self.ans
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
         seen = set()
         def backtrack(curr, arr):
             if curr:
