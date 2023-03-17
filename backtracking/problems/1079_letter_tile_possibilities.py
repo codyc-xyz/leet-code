27df918f13e92424e23d794b939df45a05f907b2
Red <crabmarket56@gmail.com>
2023-03-17 09:07:13 +0900

    Add initial solution (TLE)

--- backtracking/problems/1079_letter_tile_possibilities.py
+++ backtracking/problems/1079_letter_tile_possibilities.py
@@ -2,3 +2,24 @@
 
 # Return the number of possible non-empty sequences of letters you can make using the letters printed on those tiles.
 
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+
+
+        self.ans = 0
+        seen = set()
+        count = collections.Counter(tiles)
+        def backtrack(i, curr):
+            if i == len(tiles):
+                if len(curr) > 0 and curr not in seen:
+                    self.ans += 1
+                    seen.add(curr)
+                return
+            for j in range(len(tiles)):
+                if count[tiles[j]] > 0:
+                    count[tiles[j]] -= 1
+                    backtrack(i + 1, curr + tiles[j])
+                    count[tiles[j]] += 1
+                    backtrack(i + 1, curr)
+        backtrack(0, "")
+        return self.ans
