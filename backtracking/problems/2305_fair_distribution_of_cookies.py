3f2d228b8050c32ab17f3abd549bf8df0a824f8c
Red <crabmarket56@gmail.com>
2023-03-18 09:40:31 +0900

    Add initial solution (TLE)

--- backtracking/problems/2305_fair_distribution_of_cookies.py
+++ backtracking/problems/2305_fair_distribution_of_cookies.py
@@ -3,3 +3,26 @@
 # The unfairness of a distribution is defined as the maximum total cookies obtained by a single child in the distribution.
 
 # Return the minimum unfairness of all distributions.
+
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        cookies.sort(reverse=True)
+        self.ans = float('inf')
+        seen = [False] * len(cookies)
+        buckets = [0] * k
+        def backtrack(i):
+            if i >= len(seen):
+                if False not in seen:
+                    self.ans = min(self.ans, max(buckets))
+                return
+            for k in range(i, len(seen)):
+                if seen[k]:
+                    continue
+                for b in range(len(buckets)):
+                    buckets[b] += cookies[k]
+                    seen[k] = True
+                    backtrack(k + 1)
+                    buckets[b] -= cookies[k]
+                    seen[k] = False
+        backtrack(0)
+        return self.ans
