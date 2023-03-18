17d18cedcc19170641999ec897dcd68853a481c0
Red <crabmarket56@gmail.com>
2023-03-18 10:25:11 +0900

    Refactor solution

--- backtracking/problems/491_non-decreasing_subsequences.py
+++ backtracking/problems/491_non-decreasing_subsequences.py
@@ -1,14 +1,13 @@
 # Given an integer array nums, return all the different possible non-decreasing subsequences of the given array with at least two elements. You may return the answer in any order.
 
-
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
 
-        ans = []
+        ans = set()
 
         def backtrack(i, path):
-            if len(path) > 1 and path not in ans:
-                ans.append(path)
+            if len(path) > 1:
+                ans.add(tuple(path))
             if i >= len(nums):
                 return
             if not path or nums[i] >= path[-1]:
