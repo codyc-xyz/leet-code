defb848eb723faddb3d54fb7ef65f3be9a27d713
Red <crabmarket56@gmail.com>
2023-03-19 07:47:28 +0900

    Initialize problem

---
+++ backtracking/problems/967_numbers_with_same_consecutive_difference.py
@@ -0,0 +1,3 @@
+# Given two integers n and k, return an array of all the integers of length n where the difference between every two consecutive digits is k. You may return the answer in any order.
+
+# Note that the integers should not have leading zeros. Integers as 02 and 043 are not allowed.
