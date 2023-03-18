895bb11a2562c992fa14eba065ab17b29dc92a83
Red <crabmarket56@gmail.com>
2023-03-19 07:31:29 +0900

    Refactor solution

--- backtracking/problems/2178_maximum_split_of_positive_even_integers.py
+++ backtracking/problems/2178_maximum_split_of_positive_even_integers.py
@@ -4,18 +4,17 @@
 # Among them, (2 + 4 + 6) contains the maximum number of integers. Note that finalSum cannot be split into (2 + 2 + 4 + 4) as all the numbers should be unique.
 # Return a list of integers that represent a valid split containing a maximum number of integers. If no valid split exists for finalSum, return an empty list. You may return the integers in any order.
 
+
 class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
         if finalSum % 2:
             return []
         num = 2
-        currSum = 0
         ans = []
-
-        while currSum + num + num <= finalSum - 2:
-            currSum += num
+        while finalSum >= num:
+            finalSum -= num
             ans.append(num)
             num += 2
-        if currSum < finalSum:
-            ans.append(finalSum - currSum)
-        return ans               
+        if finalSum:
+            ans[-1] += finalSum
+        return ans
