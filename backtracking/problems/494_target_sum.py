3e504a5869c6e9eee4c91b7e7cc4642817b35b57
Red <crabmarket56@gmail.com>
2023-03-22 10:43:26 +0900

    Add initial solution (TLE)

--- backtracking/problems/494_target_sum.py
+++ backtracking/problems/494_target_sum.py
@@ -4,3 +4,18 @@
 
 # For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
 # Return the number of different expressions that you can build, which evaluates to target.
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        self.ans = 0
+        def backtrack(i, target):
+            if i == len(nums):
+                if target == 0:
+                    self.ans += 1
+                return
+
+            backtrack(i + 1, target - nums[i])
+            backtrack(i + 1, target + nums[i])
+        backtrack(0, target)
+        return self.ans
