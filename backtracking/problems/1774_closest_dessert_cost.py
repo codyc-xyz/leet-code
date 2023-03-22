121e5ad035cbe718777776b963b397628411e63a
Red <crabmarket56@gmail.com>
2023-03-22 10:04:43 +0900

    Add initial solution (TLE)

--- backtracking/problems/1774_closest_dessert_cost.py
+++ backtracking/problems/1774_closest_dessert_cost.py
@@ -11,3 +11,32 @@
 # You want to make a dessert with a total cost as close to target as possible.
 
 # Return the closest possible cost of the dessert to target. If there are multiple, return the lower one.
+
+
+class Solution:
+    def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
+
+        self.ans = self.closest = float('inf')
+        used = [0] * len(toppingCosts)
+        def backtrack(i, curr):
+            if abs(curr - target) > self.closest and curr > self.ans:
+                return
+        
+            if i == len(baseCosts):
+                if abs(curr - target) < self.closest:
+                    self.ans = curr
+                    self.closest = abs(curr - target)
+                elif abs(curr - target) == self.closest:
+                    self.ans = min(self.ans, curr)
+                return
+            
+            for j in range(len(toppingCosts)):
+                if used[j] < 2:
+                    used[j] += 1
+                    backtrack(i, curr + toppingCosts[j])
+                    used[j] -= 1
+                backtrack(i + 1, curr)
+
+        for b in baseCosts:
+            backtrack(0, b)
+        return self.ans
