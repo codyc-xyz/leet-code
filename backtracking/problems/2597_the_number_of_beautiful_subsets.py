c8966a2d5c810b110a4eb5daafed3b5d101d938d
Red <crabmarket56@gmail.com>
2023-03-25 09:32:10 +0900

    Add initial solution (TLE)

--- backtracking/problems/2597_the_number_of_beautiful_subsets.py
+++ backtracking/problems/2597_the_number_of_beautiful_subsets.py
@@ -6,3 +6,16 @@
 
 # A subset of nums is an array that can be obtained by deleting some (possibly none) elements from nums. Two subsets are different if and only if the chosen indices to delete are different.
 
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+
+        def backtrack(i, path):
+            if i == len(nums):
+                for j in path:
+                    if j - k in path:
+                        return 0
+                return 1 if path else 0
+
+            return backtrack(i + 1, path + [nums[i]]) + backtrack(i + 1, path)
+
+        return backtrack(0, [])
