96ed678d9b9f92be548b74b27cad4aff0fa25d63
Red <crabmarket56@gmail.com>
2023-03-26 12:45:13 +0900

    Add initial solution

--- heap/problems/703_kth_largest_element_in_a_stream.py
+++ heap/problems/703_kth_largest_element_in_a_stream.py
@@ -4,3 +4,23 @@
 
 # KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
 # int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = sorted(nums)
+        self.k = k
+        
+
+    def add(self, val: int) -> int:
+        i = 0
+        while i < len(self.heap) and val > self.heap[i]:
+            i += 1
+        self.heap.insert(i, val)
+        return self.heap[-self.k]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
