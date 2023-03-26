125083b3872cfea63838f425df393a5010a27d93
Red <crabmarket56@gmail.com>
2023-03-26 13:33:25 +0900

    Add heap solution

--- heap/problems/1046_last_stone_weight.py
+++ heap/problems/1046_last_stone_weight.py
@@ -25,3 +25,21 @@ class Solution:
             return stones[0]
         else:
             return 0
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        stones = [-s for s in stones]
+
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+
+            if second > first:
+                heapq.heappush(stones, first - second)
+        if stones:
+            return abs(stones[0])
+        return 0
