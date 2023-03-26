41cea8f57b20a4a8f56dd071ea4c794d8e178dee
Red <crabmarket56@gmail.com>
2023-03-26 13:00:56 +0900

    Refactor solution

--- heap/problems/703_kth_largest_element_in_a_stream.py
+++ heap/problems/703_kth_largest_element_in_a_stream.py
@@ -8,19 +8,19 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = sorted(nums)
-        self.k = k
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
         
-
     def add(self, val: int) -> int:
-        i = 0
-        while i < len(self.heap) and val > self.heap[i]:
-            i += 1
-        self.heap.insert(i, val)
-        return self.heap[-self.k]
+        heapq.heappush(self.minHeap, val)
+
+        if len(self.minHeap) > self.k:
+           heapq.heappop(self.minHeap)
         
-
-
+        return self.minHeap[0]
+        
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
