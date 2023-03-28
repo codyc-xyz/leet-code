0925898b902a19c8cf32dea205987c2512a4ef0f
Red <crabmarket56@gmail.com>
2023-03-28 09:31:31 +0900

    Add alternative solution

--- heap/problems/2357_make_array_zero_by_subtracting_equal_amounts.py
+++ heap/problems/2357_make_array_zero_by_subtracting_equal_amounts.py
@@ -19,3 +19,10 @@ class Solution:
                     if nums[i] >= curr:
                         nums[i] -= curr
         return ops
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+       
+        nums = set(nums)
+
+        return len(nums) - 1 if 0 in nums else len(nums)
