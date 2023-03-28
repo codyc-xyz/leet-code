e122835d9089cbc8ae76e9e71842ae517e56ab1a
Red <crabmarket56@gmail.com>
2023-03-28 09:15:50 +0900

    Add initial solution

--- heap/problems/1464_maximum_product_of_two_elements_in_an_array.py
+++ heap/problems/1464_maximum_product_of_two_elements_in_an_array.py
@@ -1,2 +1,8 @@
 # Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
 
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        maxHeap = [n * -1 for n in nums]
+        heapq.heapify(maxHeap)
+        return (heapq.heappop(maxHeap) + 1) * (heapq.heappop(maxHeap) + 1)
