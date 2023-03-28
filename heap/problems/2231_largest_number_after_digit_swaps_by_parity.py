d61aa3712fe1b6523559f8b0d1eaa6938f9eb082
Red <crabmarket56@gmail.com>
2023-03-28 10:17:05 +0900

    Add initial solution

--- heap/problems/2231_largest_number_after_digit_swaps_by_parity.py
+++ heap/problems/2231_largest_number_after_digit_swaps_by_parity.py
@@ -2,3 +2,24 @@
 
 # Return the largest possible value of num after any number of swaps.
 
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        nums = str(num)
+        odds = []
+        evens = []
+        for n in nums:
+            if int(n) % 2:
+                odds.append(int(n) * -1)
+            else:
+                evens.append(int(n) * -1)
+
+        heapq.heapify(odds)
+        heapq.heapify(evens)
+        ans = ""
+        for n in nums:
+            if int(n) % 2:
+                ans = ans + str(heapq.heappop(odds) * -1)
+            else:
+                ans = ans + str(heapq.heappop(evens) * -1)
+
+        return int(ans)
