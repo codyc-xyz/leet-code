102aa21e9e3b81c4f8b68459b5cd097778c609fb
Red <crabmarket56@gmail.com>
2023-03-28 10:31:40 +0900

    Add initial solution

--- heap/problems/506_relative_ranks.py
+++ heap/problems/506_relative_ranks.py
@@ -8,3 +8,28 @@
 # For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
 # Return an array answer of size n where answer[i] is the rank of the ith athlete.
 
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        scores = [s * -1 for s in score]
+
+        heapq.heapify(scores)
+        ans = [None] * len(score)
+        hm = {}
+        for i, s in enumerate(score):
+            hm[s * -1] = i
+            
+        i = 0
+        while scores:
+            curr = heapq.heappop(scores)
+            if i > 2:
+                ans[hm[curr]] = str(i + 1) 
+            elif i == 2:
+                ans[hm[curr]] = "Bronze Medal"
+            elif i == 1:
+                ans[hm[curr]] = "Silver Medal"
+            elif i == 0:
+                ans[hm[curr]] = "Gold Medal"
+            i += 1
+        return ans
+        
+        
