4c74e4278214ea1ec8a6d3b40193dd7278b763b4
Red <crabmarket56@gmail.com>
2023-03-29 11:52:16 +0900

    Add initial solution

--- heap/problems/1338_reduce_array_size_to_the_half.py
+++ heap/problems/1338_reduce_array_size_to_the_half.py
@@ -2,5 +2,18 @@
 
 # Return the minimum size of the set so that at least half of the integers of the array are removed.
 
- 
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
 
+        count = collections.Counter(arr)
+        
+        counts = []
+        for c in count.values():
+            counts.append(-c)
+        sumCount = len(arr)
+        heapq.heapify(counts)
+        ans = 0
+        while sumCount > len(arr) // 2:
+            sumCount += heapq.heappop(counts)
+            ans += 1
+        return ans
