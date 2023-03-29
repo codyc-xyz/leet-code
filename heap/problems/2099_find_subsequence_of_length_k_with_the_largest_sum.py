7729c476a3e9eb4284ad861eb01273ac6c07df95
Red <crabmarket56@gmail.com>
2023-03-29 10:03:37 +0900

    Add initial solution

--- heap/problems/2099_find_subsequence_of_length_k_with_the_largest_sum.py
+++ heap/problems/2099_find_subsequence_of_length_k_with_the_largest_sum.py
@@ -4,3 +4,25 @@
 
 # A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
 
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+    
+        K = k
+        res = []
+        i = 0
+        while K > 0:
+            res.append(nums[i])
+            i += 1
+            K -= 1
+
+        if i == len(nums):
+            return res
+        
+        while i < len(nums):
+            currMin = min(res)
+            if nums[i] > currMin:
+                res.pop(res.index(currMin))
+                res.append(nums[i])
+
+            i += 1
+        return res
