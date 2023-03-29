725b7ee33d99742340a15d5ebb632ce15a80e5de
Red <crabmarket56@gmail.com>
2023-03-29 10:54:10 +0900

    Add initial solution

--- heap/problems/2336_smallest_number_in_infinite_set.py
+++ heap/problems/2336_smallest_number_in_infinite_set.py
@@ -5,3 +5,24 @@
 # SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
 # int popSmallest() Removes and returns the smallest integer contained in the infinite set.
 # void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.heap = [i for i in range(1, 1001)]
+        heapq.heapify(self.heap)
+        
+
+    def popSmallest(self) -> int:
+        return heapq.heappop(self.heap)
+
+    def addBack(self, num: int) -> None:
+        if num not in self.heap:
+            heapq.heappush(self.heap, num)
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
