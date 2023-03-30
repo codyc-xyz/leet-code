b4616888b206c4fca3399b340af8fd6d7d100437
Red <crabmarket56@gmail.com>
2023-03-30 10:47:30 +0900

    Add initial solution

--- heap/problems/1405_longest_happy_string.py
+++ heap/problems/1405_longest_happy_string.py
@@ -9,3 +9,29 @@
 
 # A substring is a contiguous sequence of characters within a string.
 
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+
+        buckets = [[-a, 'a'], [-b, 'b'], [-c, 'c']]
+        heapq.heapify(buckets)
+        ans = ""
+        while buckets[0][0] < 0:
+            if len(ans) < 2 or (ans[-1] != buckets[0][1] or ans[-2] != buckets[0][1]) and buckets[0][0] < 0:
+                num, char = heapq.heappop(buckets)
+                ans += char
+                heapq.heappush(buckets, [num + 1, char])
+            elif len(ans) > 1 and ans[-1] == buckets[0][1] and ans[-2] == buckets[0][1]:
+                if buckets[1][0] < 0:
+                    buckets[1][0] += 1
+                    ans += buckets[1][1]
+                elif buckets[2][0] < 0:
+                    buckets[2][0] += 1
+                    ans += buckets[2][1]
+                else:
+                    break
+            else:
+                break
+
+        return ans
+
+
