2f7937b75372ca77c7efe5bc7c947ab73db030f7
Red <crabmarket56@gmail.com>
2023-03-30 10:22:58 +0900

    Add initial solution

--- heap/problems/912_sort_an_array.py
+++ heap/problems/912_sort_an_array.py
@@ -2,3 +2,13 @@
 
 # You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
 
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+       
+        heapq.heapify(nums)
+        ans = []
+
+        while nums:
+            ans.append(heapq.heappop(nums))
+
+        return ans
