76e20fe87e27a245466547b23553f0721c044ae0
Red <crabmarket56@gmail.com>
2023-03-30 09:14:16 +0900

    Add initial solution

--- heap/problems/1753_maximum_score_from_removing_stones.py
+++ heap/problems/1753_maximum_score_from_removing_stones.py
@@ -3,3 +3,18 @@
 
 # Given three integers a​​​​​, b,​​​​​ and c​​​​​, return the maximum score you can get.
 
+class Solution:
+    def maximumScore(self, a: int, b: int, c: int) -> int:
+
+        heap = [-a, -b, -c]
+        heapq.heapify(heap)
+        score = 0
+        while len(heap) > 1:
+            first = heapq.heappop(heap)
+            second = heapq.heappop(heap)
+            if first < 0 and second < 0:
+                heapq.heappush(heap, first + 1)
+                heapq.heappush(heap, second + 1)
+                score += 1
+                    
+        return score
