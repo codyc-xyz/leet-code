127db9dc17eb184f1f90126aee5bfdc5cae6d13d
Red <crabmarket56@gmail.com>
2023-03-30 09:21:58 +0900

    Add initial solution

--- heap/problems/1845_seat_reservation_manager.py
+++ heap/problems/1845_seat_reservation_manager.py
@@ -5,3 +5,22 @@
 # SeatManager(int n) Initializes a SeatManager object that will manage n seats numbered from 1 to n. All seats are initially available.
 # int reserve() Fetches the smallest-numbered unreserved seat, reserves it, and returns its number.
 # void unreserve(int seatNumber) Unreserves the seat with the given seatNumber.
+
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.heap = [i for i in range(1, n + 1)]
+        heapq.heapify(self.heap)
+
+    def reserve(self) -> int:
+        return heapq.heappop(self.heap)
+        
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.heap, seatNumber)
+        
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
