5772ae498540806f217c6c09481a8a557213a444
Red <crabmarket56@gmail.com>
2023-03-31 13:08:56 +0900

    Add initial solution

--- heap/problems/692_top_k_frequent_words.py
+++ heap/problems/692_top_k_frequent_words.py
@@ -2,3 +2,19 @@
 
 # Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
 
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = collections.Counter(words)
+        count = dict(sorted(count.items(), key = lambda item: item[1], reverse=True))
+        
+        ans = []
+        for c in count:
+            ans.append(c)
+
+            if len(ans) > 1 and count[ans[-2]] == count[ans[-1]]:
+                j = len(ans) - 1
+                while j > 0 and ans[j] < ans[j - 1] and count[ans[j]] == count[ans[j - 1]]:
+                    ans[j - 1], ans[j] = ans[j], ans[j - 1]
+                    j -= 1
+
+        return ans[:k]
