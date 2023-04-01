60af48a7fb7430caffe84301efc8976db837dcbb
Red <crabmarket56@gmail.com>
2023-04-02 08:31:28 +0900

    Add initial solution (TLE)

--- heap/problems/2424_longest_uploaded_prefix.py
+++ heap/problems/2424_longest_uploaded_prefix.py
@@ -7,3 +7,26 @@
 # LUPrefix(int n) Initializes the object for a stream of n videos.
 # void upload(int video) Uploads video to the server.
 # int longest() Returns the length of the longest uploaded prefix defined above.
+
+class LUPrefix:
+
+    def __init__(self, n: int):
+        self.pfix = [False] * n
+        self.maxV = 0
+    def upload(self, video: int) -> None:
+        self.pfix[video - 1] = True
+        self.maxV = max(self.maxV, video)
+
+        
+    def longest(self) -> int:
+        i = 0
+        while i < self.maxV and self.pfix[i]:
+            i += 1
+        return i
+        
+
+
+# Your LUPrefix object will be instantiated and called as such:
+# obj = LUPrefix(n)
+# obj.upload(video)
+# param_2 = obj.longest()
