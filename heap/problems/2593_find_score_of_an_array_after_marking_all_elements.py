1b81cd90d956b1fc2fa0ec573b4e412a78134ace
Red <crabmarket56@gmail.com>
2023-04-02 10:16:44 +0900

    Add initial solution

--- heap/problems/2593_find_score_of_an_array_after_marking_all_elements.py
+++ heap/problems/2593_find_score_of_an_array_after_marking_all_elements.py
@@ -8,3 +8,30 @@
 # Repeat until all the array elements are marked.
 # Return the score you get after applying the above algorithm.
 
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+
+        marked = set()
+        heap = defaultdict(list)
+        vals = []
+        for i, n in enumerate(nums):
+            heapq.heappush(heap[n], i)
+            heapq.heappush(vals, n)
+
+        score = 0
+        while vals:
+            curr = heapq.heappop(vals)
+            
+            while heap[curr] and heap[curr][0] in marked:
+                heapq.heappop(heap[curr])
+
+            if heap[curr]:
+                score += curr
+                idx = heapq.heappop(heap[curr])
+                marked.add(idx)
+                marked.add(idx + 1)
+                marked.add(idx - 1)
+
+        return score
+
+
