345fd57db8dbd1989ced1de5a5409eb8b944def9
Red <crabmarket56@gmail.com>
2023-04-03 14:23:50 +0900

    Add initial solution

--- heap/problems/743_network_delay_time.py
+++ heap/problems/743_network_delay_time.py
@@ -2,3 +2,24 @@
 
 # We will send a signal from a given node k. Return the minimum time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
 
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        adj = defaultdict(list)
+
+        for source, target, time in times:
+            heapq.heappush(adj[source], [target, time])
+
+        heap = [[0, k]]
+        seen = set()
+        while heap:
+            time, val = heapq.heappop(heap)
+            seen.add(val)
+            if len(seen) == n:
+                return time
+            
+            while adj[val]:
+                target, travel = heapq.heappop(adj[val])
+                heapq.heappush(heap, [time + travel, target])
+        return -1
+
