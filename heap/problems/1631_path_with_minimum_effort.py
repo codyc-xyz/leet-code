3c1e8fc4f911b71cbbed51c2db91d150753e2b50
Red <crabmarket56@gmail.com>
2023-04-03 14:55:37 +0900

    Add initial solution

--- heap/problems/1631_path_with_minimum_effort.py
+++ heap/problems/1631_path_with_minimum_effort.py
@@ -5,3 +5,30 @@
 
 # Return the minimum effort required to travel from the top-left cell to the bottom-right cell.
 
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows = len(heights)
+        cols = len(heights[0])
+
+        adj = defaultdict(list)
+
+        for i in range(rows):
+            for j in range(cols):
+                if i - 1 >= 0:
+                    heapq.heappush(adj[(i, j)], [abs(heights[i][j] - heights[i - 1][j]), (i - 1, j)])
+                if i + 1 < rows:
+                    heapq.heappush(adj[(i, j)], [abs(heights[i][j] - heights[i + 1][j]), (i + 1, j)])
+                if j - 1 >= 0:
+                    heapq.heappush(adj[(i, j)], [abs(heights[i][j] - heights[i][j - 1]), (i, j - 1)])
+                if j + 1 < cols:
+                    heapq.heappush(adj[(i, j)], [abs(heights[i][j] - heights[i][j + 1]), (i, j + 1)])
+
+        heap = [[0, (0, 0)]]
+        goal = (rows - 1, cols - 1)
+        while heap:
+            maxDiff, pos = heapq.heappop(heap)
+            if pos == goal:
+                return maxDiff
+            while adj[pos]:
+                maxDiff1, pos1 = heapq.heappop(adj[pos])
+                heapq.heappush(heap, [max(maxDiff, maxDiff1), pos1])
