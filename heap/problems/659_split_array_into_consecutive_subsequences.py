da7beb59c1f25d0ed73ff4def7821da83680c21c
Red <crabmarket56@gmail.com>
2023-04-04 09:23:44 +0900

    Add initial solution (Not TLE)

--- heap/problems/659_split_array_into_consecutive_subsequences.py
+++ heap/problems/659_split_array_into_consecutive_subsequences.py
@@ -9,3 +9,25 @@
 # A subsequence of an array is a new array that is formed from the original array by deleting some (can be none) of the elements without disturbing the relative positions of the remaining elements. 
 # (i.e., [1,3,5] is a subsequence of [1,2,3,4,5] while [1,3,2] is not).
 
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        
+        subseqs = [[]]
+        j = 0
+        for n in nums:
+            if not subseqs[j] or n == subseqs[j][-1] + 1:
+                subseqs[j].append(n)
+            else:
+                flag = False
+                for i in range(len(subseqs) -2, -1, -1):
+                    if n == subseqs[i][-1] +1:
+                        subseqs[i].append(n)
+                        flag = True
+                        break
+                if not flag:
+                    subseqs.append([n])
+                    j += 1
+        for s in subseqs:
+            if len(s) < 3:
+                return False
+        return True
