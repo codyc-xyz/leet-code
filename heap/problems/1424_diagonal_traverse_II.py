721c83012f94337b6452ad676b4ef3b50b71ac83
Red <crabmarket56@gmail.com>
2023-04-04 09:48:56 +0900

    Add initial solution

--- heap/problems/1424_diagonal_traverse_II.py
+++ heap/problems/1424_diagonal_traverse_II.py
@@ -1,2 +1,18 @@
 # Given a 2D integer array nums, return all elements of nums in diagonal order as shown in the below images.
 
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        rows = len(nums)
+        heap = defaultdict(list)
+        for i in range(rows):
+            for j in range(len(nums[i])):
+                heapq.heappush(heap[i + j], [j, nums[i][j]])
+
+        ans = []
+        for h in heap:
+            while heap[h]:
+                ans.append(heapq.heappop(heap[h])[1])
+         
+        return ans
+
+            
