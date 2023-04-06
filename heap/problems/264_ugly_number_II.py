baf71486600698299d4122a97f9bb5ade74bcd30
Red <crabmarket56@gmail.com>
2023-04-06 09:59:31 +0900

    Add alternative solution

--- heap/problems/264_ugly_number_II.py
+++ heap/problems/264_ugly_number_II.py
@@ -16,3 +16,17 @@ class Solution:
             ugly.append(min(ugly[two]*2, ugly[three]*3, ugly[five]*5))
         return ugly[-1]
 
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly = [1]
+        ans = []
+        seen = set()
+        while len(ans) < n:
+            curr = heapq.heappop(ugly)
+            for i in (2, 3, 5):
+                if i*curr not in seen:
+                    heapq.heappush(ugly, i*curr)
+                    seen.add(i*curr)
+            ans.append(curr)
+        return ans[-1]
