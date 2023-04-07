573f2c8cdbb6d4c184ce4078232326aadd8466ac
Red <crabmarket56@gmail.com>
2023-04-07 15:20:07 +0900

    Add initial solution

--- heap/problems/1054_distant_barcodes.py
+++ heap/problems/1054_distant_barcodes.py
@@ -2,3 +2,24 @@
 
 # Rearrange the barcodes so that no two adjacent barcodes are equal. You may return any answer, and it is guaranteed an answer exists.
 
+class Solution:
+    def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
+
+        count = collections.Counter(barcodes)
+        heap = []
+        for c in count:
+            heapq.heappush(heap, [-count[c], c])
+
+        ans = []
+        while heap:
+            cnt1, n1 = heapq.heappop(heap)
+            ans.append(n1)
+            if heap:
+                cnt2, n2 = heapq.heappop(heap)
+                ans.append(n2)
+                if cnt2 < -1:
+                    heapq.heappush(heap, [cnt2 + 1, n2])
+            if cnt1 < -1:
+                heapq.heappush(heap, [cnt1 + 1, n1])
+        return ans
+            
