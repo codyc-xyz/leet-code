83ece10e3c43323326f67b74fa316e6d04a76200
Red <crabmarket56@gmail.com>
2023-04-09 16:00:39 +0900

    Add initial solution

--- heap/problems/2530_maximal_score_after_applying_k_operations.py
+++ heap/problems/2530_maximal_score_after_applying_k_operations.py
@@ -8,3 +8,17 @@
 
 # The ceiling function ceil(val) is the least integer greater than or equal to val.
 
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+
+        heap = []
+        for n in nums:
+            heapq.heappush(heap, -n)
+           
+        ans = 0
+        while k > 0:
+            curr = heapq.heappop(heap)
+            ans += -curr
+            heapq.heappush(heap, -math.ceil(-curr / 3))
+            k -= 1
+        return ans
