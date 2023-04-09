52b306038381863ff68983ad80462ca74c86150b
Red <crabmarket56@gmail.com>
2023-04-09 11:34:32 +0900

    Add initial solutino (TLE)

--- heap/problems/2054_two_best_non-overlapping_events.py
+++ heap/problems/2054_two_best_non-overlapping_events.py
@@ -4,3 +4,40 @@
 
 # Note that the start time and end time is inclusive: that is, you cannot attend two events where one of them starts and the other ends at the same time. More specifically, if you attend an event with end time t, the next event must start at or after t + 1.
 
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        
+        events.sort(key=lambda x: x[0])
+        heaps = defaultdict(list)
+        for index, (start, end, value) in enumerate(events):
+            for h in heaps:
+                if start > heaps[h][0][0]:
+                    if len(heaps[h]) > 1:
+                        if value > heaps[h][-1][1]:
+                            heaps[h].pop()
+                        else:
+                            continue
+                    heapq.heappush(heaps[h], (end, value))
+            heapq.heappush(heaps[index], (end, value))
+
+        maxVal = 0
+        for h in heaps:
+            curr = 0
+            heap = []
+            for end, val in heaps[h]:
+                heapq.heappush(heap, val)
+                curr += val
+                if len(heap) > 2:
+                    curr -= heapq.heappop(heap)
+            maxVal = max(maxVal, curr)
+        
+        return maxVal
+
+
+
+               
+
+
+          
+    
+
