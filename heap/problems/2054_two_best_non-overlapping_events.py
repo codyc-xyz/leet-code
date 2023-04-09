09b91798e5ca84de0b09feb56009167c2805e798
Red <crabmarket56@gmail.com>
2023-04-09 11:59:29 +0900

    Refactor solution

--- heap/problems/2054_two_best_non-overlapping_events.py
+++ heap/problems/2054_two_best_non-overlapping_events.py
@@ -3,36 +3,20 @@
 # Return this maximum sum.
 
 # Note that the start time and end time is inclusive: that is, you cannot attend two events where one of them starts and the other ends at the same time. More specifically, if you attend an event with end time t, the next event must start at or after t + 1.
-
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         
-        events.sort(key=lambda x: x[0])
-        heaps = defaultdict(list)
-        for index, (start, end, value) in enumerate(events):
-            for h in heaps:
-                if start > heaps[h][0][0]:
-                    if len(heaps[h]) > 1:
-                        if value > heaps[h][-1][1]:
-                            heaps[h].pop()
-                        else:
-                            continue
-                    heapq.heappush(heaps[h], (end, value))
-            heapq.heappush(heaps[index], (end, value))
+        events.sort()
+        heap = []
+        bestSum = maxVal = 0
+        for s, e, v in events:
+            while heap and s > heap[0][0]:
+                maxVal = max(maxVal, heapq.heappop(heap)[1])
 
-        maxVal = 0
-        for h in heaps:
-            curr = 0
-            heap = []
-            for end, val in heaps[h]:
-                heapq.heappush(heap, val)
-                curr += val
-                if len(heap) > 2:
-                    curr -= heapq.heappop(heap)
-            maxVal = max(maxVal, curr)
-        
-        return maxVal
+            bestSum = max(bestSum, maxVal + v)
+            heapq.heappush(heap, [e, v])
 
+        return bestSum
 
 
                
