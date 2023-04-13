0cfb932b9bca0ae7e8aa84243408ded893aa6b73
Red <crabmarket56@gmail.com>
2023-04-13 10:39:48 +0900

    Add initial solution (TLE)

--- heap/problems/373_find_k_pairs_with_smallest_sums.py
+++ heap/problems/373_find_k_pairs_with_smallest_sums.py
@@ -4,3 +4,17 @@
 
 # Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
 
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        heap = []
+        i = j = 0
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                heapq.heappush(heap, ([nums1[i] + nums2[j]], [nums1[i], nums2[j]]))
+
+        ans = []
+        while heap and k > 0:
+            ans.append(heapq.heappop(heap)[1])
+            k -= 1
+
+        return ans
