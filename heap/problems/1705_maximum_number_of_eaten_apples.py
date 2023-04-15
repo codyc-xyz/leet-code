f72ec6c9eb69038b0d7187fd37474a7c84b96804
Red <crabmarket56@gmail.com>
2023-04-15 13:43:10 +0900

    Add intitial solution (TLE)

--- heap/problems/1705_maximum_number_of_eaten_apples.py
+++ heap/problems/1705_maximum_number_of_eaten_apples.py
@@ -4,3 +4,21 @@
 
 # Given two integer arrays days and apples of length n, return the maximum number of apples you can eat.
 
+class Solution:
+    def eatenApples(self, apples: List[int], days: List[int]) -> int:
+
+        heap = []
+        i = applesEaten = 0
+        while True:
+            if i < len(days):
+                for _ in range(apples[i]):
+                    heapq.heappush(heap, i + days[i])
+            while heap and i >= heap[0]:
+                heapq.heappop(heap)
+            if heap:
+                heapq.heappop(heap)
+                applesEaten += 1
+            elif not heap and i >= len(days):
+                break
+            i += 1
+        return applesEaten
