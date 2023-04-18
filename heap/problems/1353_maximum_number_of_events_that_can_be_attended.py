0079bddd373d73fb02da400c26a1b3e32889b7d3
Red <crabmarket56@gmail.com>
2023-04-18 16:34:07 +0900

    Add initial solution

--- heap/problems/1353_maximum_number_of_events_that_can_be_attended.py
+++ heap/problems/1353_maximum_number_of_events_that_can_be_attended.py
@@ -4,3 +4,33 @@
 
 # Return the maximum number of events you can attend.
 
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+
+        for i, e in enumerate(events):
+            e.append(i)
+        endFirstEvents = [[e, s, i] for s, e, i in events]
+        
+        heapq.heapify(events)
+        heapq.heapify(endFirstEvents)
+        seen = set()
+        i = 1
+        ans = 0
+        while endFirstEvents:
+            while (endFirstEvents and i > endFirstEvents[0][0]) or (endFirstEvents and endFirstEvents[0][2] in seen):
+                heapq.heappop(endFirstEvents)
+            if endFirstEvents and i >= endFirstEvents[0][1]:
+                _, _, j = heapq.heappop(endFirstEvents)
+                seen.add(j)
+                ans += 1
+            else:
+                while (events and events[0][2] in seen) or (events and i > events[0][1]):
+                    heapq.heappop(events)
+                if events and events[0][0] <= i:
+                    _, _, j = heapq.heappop(events)
+                    seen.add(j)
+                    ans += 1
+            i += 1
+        return ans
+
+
