1a758330021c8c08eb6d6d97b0a9662534e047ff
Red <crabmarket56@gmail.com>
2023-04-18 15:22:46 +0900

    Add initial solution

--- heap/problems/2349_design_a_number_container_system.py
+++ heap/problems/2349_design_a_number_container_system.py
@@ -6,3 +6,27 @@
 # NumberContainers() Initializes the number container system.
 # void change(int index, int number) Fills the container at index with the number. If there is already a number at that index, replace it.
 # int find(int number) Returns the smallest index for the given number, or -1 if there is no index that is filled by number in the system.
+
+class NumberContainers:
+
+    def __init__(self):
+        self.hm = {}
+        self.numberContainers = defaultdict(list)
+        
+    def change(self, index: int, number: int) -> None:
+        self.hm[index] = number
+        heapq.heappush(self.numberContainers[number], index)
+        
+    def find(self, number: int) -> int:
+        while self.numberContainers[number] and number != self.hm[self.numberContainers[number][0]]:
+            heapq.heappop(self.numberContainers[number])
+        if self.numberContainers[number]:
+            return self.numberContainers[number][0]
+        return -1
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
