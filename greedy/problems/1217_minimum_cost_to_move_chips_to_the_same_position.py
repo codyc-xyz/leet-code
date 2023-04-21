e4dc6cbdbfd67d1bc6b1b797af8897d1ee323cb5
Red <crabmarket56@gmail.com>
2023-04-21 16:12:17 +0900

    Add initial solution

--- greedy/problems/1217_minimum_cost_to_move_chips_to_the_same_position.py
+++ greedy/problems/1217_minimum_cost_to_move_chips_to_the_same_position.py
@@ -6,5 +6,14 @@
 # position[i] + 1 or position[i] - 1 with cost = 1.
 # Return the minimum cost needed to move all the chips to the same position.
 
- 
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
 
+        evens = odds = 0
+        for p in position:
+            if p % 2:
+                odds += 1
+            else:
+                evens += 1
+
+        return min(odds, evens)
