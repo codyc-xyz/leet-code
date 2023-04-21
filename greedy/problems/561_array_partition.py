f6169b1928683a1e2f11e61ef4439274548371a2
Red <crabmarket56@gmail.com>
2023-04-21 15:29:08 +0900

    Add initial solution

--- greedy/problems/561_array_partition.py
+++ greedy/problems/561_array_partition.py
@@ -1,2 +1,12 @@
 # Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
 
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        l, r = 0, 1
+        minMax = 0
+        while r < len(nums):
+            minMax += nums[l]
+            l += 2
+            r += 2
+        return minMax
