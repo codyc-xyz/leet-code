72b4cb1aed44a596d9cca7e8b4ff8f9919e4a8c9
Red <crabmarket56@gmail.com>
2023-04-21 15:44:57 +0900

    Add initial solution

--- greedy/problems/1974_minimum_time_to_type_word_using_special_typewriter.py
+++ greedy/problems/1974_minimum_time_to_type_word_using_special_typewriter.py
@@ -1,2 +1,14 @@
 # There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle with a pointer. A character can only be typed if the pointer is pointing to that character. The pointer is initially pointing to the character 'a'.
 
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+        seconds = 0
+        i = 0
+        prev = 'a'
+        while i < len(word):
+            curr = abs(ord(prev) - ord(word[i]))
+            seconds += min(curr, 26 - curr) + 1
+            prev = word[i]
+            i += 1
+        return seconds
+
