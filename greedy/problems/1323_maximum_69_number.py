5d0cf324433dfc7d8ea32bfa75d31d59bf045578
Red <crabmarket56@gmail.com>
2023-04-21 13:31:30 +0900

    Add initial solution

--- greedy/problems/1323_maximum_69_number.py
+++ greedy/problems/1323_maximum_69_number.py
@@ -2,3 +2,15 @@
 
 # Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
 
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        ans = ""
+        num = str(num)
+        flag = False
+        for i, n in enumerate(num):
+            if n == '6' and flag == False:
+               ans +=  '9'
+               flag = True
+            else:
+                ans += n
+        return int(ans)
