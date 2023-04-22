7ca991c821c9541093f641c7a296bd499bddbbad
Red <crabmarket56@gmail.com>
2023-04-22 13:38:19 +0900

    Add initial solution

--- greedy/problems/2144_minimum_cost_of_buying_candies_with_discount.py
+++ greedy/problems/2144_minimum_cost_of_buying_candies_with_discount.py
@@ -5,3 +5,18 @@
 # For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4.
 # Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
 
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        if len(cost) < 3:
+            return sum(cost)
+        cost.sort(reverse = True)
+        ans = cost[0] + cost[1]
+        
+        i = 3
+        for j in range(3, len(cost)):
+            i -= 1
+            if i == 0:
+                i = 3
+                continue
+            ans += cost[j]
+        return ans
