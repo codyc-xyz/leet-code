c6d068d6191bbcae888dbd9804702d710f3a2e1e
Red <crabmarket56@gmail.com>
2023-04-22 09:50:09 +0900

    Add initial solution

--- greedy/problems/2224_minimum_number_of_operations_to_convert_time.py
+++ greedy/problems/2224_minimum_number_of_operations_to_convert_time.py
@@ -6,3 +6,54 @@
 
 # Return the minimum number of operations needed to convert current to correct.
 
+class Solution:
+    def convertTime(self, current: str, correct: str) -> int:
+        ops = 0
+        currMin = corrMin = ops = 0
+        
+    
+        for i in range(len(current)):
+            if i == 0:
+                currMin += 600 * int(current[i])
+                corrMin += 600 * int(correct[i])
+            elif i == 1:
+                currMin += 60 * int(current[i])
+                corrMin += 60 * int(correct[i])
+            elif i == 2:
+                continue
+            elif i == 3:
+                currMin += 10 * int(current[i])
+                corrMin += 10 * int(correct[i])
+            elif i == 4:
+                currMin += int(current[i])
+                corrMin += int(correct[i])
+
+        while corrMin >= currMin + 600:
+            currMin += 600
+            ops += 10
+
+        while corrMin >= currMin + 60:
+            currMin += 60
+            ops += 1
+        
+        while corrMin >= currMin + 15:
+            currMin += 15
+            ops += 1
+        
+        while corrMin >= currMin + 5:
+            currMin += 5
+            ops += 1
+
+        while corrMin >= currMin + 1:
+            currMin += 1
+            ops += 1
+
+        return ops
+
+        
+
+
+            
+                
+
+               
