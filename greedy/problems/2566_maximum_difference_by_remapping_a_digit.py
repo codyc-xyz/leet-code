3fc613cf245c8aaf38bccc13985af28eb6fe26f7
Red <crabmarket56@gmail.com>
2023-04-22 14:00:56 +0900

    Add initial solution

--- greedy/problems/2566_maximum_difference_by_remapping_a_digit.py
+++ greedy/problems/2566_maximum_difference_by_remapping_a_digit.py
@@ -9,3 +9,41 @@
 # Danny can remap different digits for obtaining minimum and maximum values respectively.
 # The resulting number after remapping can contain leading zeroes.
  
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+
+        nums = list(str(num))
+        if nums[0] != '9':
+            maxPrev = nums[0]
+        else:
+            i = 1
+            while i < len(nums) and nums[i] == '9':
+                i += 1
+            if i == len(nums):
+                return num 
+            maxPrev = nums[i]
+        if nums[0] != '0':
+            minPrev = nums[0]
+        else:
+            i = 1
+            while i < len(nums) and nums[i] == '0':
+                i += 1
+            if i == len(nums):
+                return num 
+            minPrev = nums[i]
+        maxNum = ""
+        minNum = ""
+        for n in (nums):
+            if n == maxPrev or n == minPrev:
+                if n == maxPrev:
+                    maxNum += '9'
+                else:
+                    maxNum += n
+                if n == minPrev:
+                    minNum += '0'
+                else:
+                    minNum += n
+                continue
+            minNum += n
+            maxNum += n
+        return int(maxNum) - int(minNum)
