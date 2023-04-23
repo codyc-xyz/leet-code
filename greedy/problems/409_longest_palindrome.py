0206a21058c8b557ff64c3b33491db109a0c427e
Red <crabmarket56@gmail.com>
2023-04-23 11:29:33 +0900

    Add initial solution

--- greedy/problems/409_longest_palindrome.py
+++ greedy/problems/409_longest_palindrome.py
@@ -2,5 +2,20 @@
 
 # Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
 
- 
+ class Solution:
+    def longestPalindrome(self, s: str) -> int:
+
+        count = collections.Counter(s)
+        seenOdd = False
+        ans = 0
+        for c in count:
+            if count[c] % 2 and not seenOdd:
+                ans += count[c]
+                seenOdd = True
+            elif count[c] % 2:
+                ans += count[c] - 1
+            else:
+                ans += count[c]
+
+        return ans
 
