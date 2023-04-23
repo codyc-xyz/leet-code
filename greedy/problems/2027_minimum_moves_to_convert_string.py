d1fbb11f7241aa2b45245e27000b269ce44afbf5
Red <crabmarket56@gmail.com>
2023-04-23 11:53:27 +0900

    Add initial solution

--- greedy/problems/2027_minimum_moves_to_convert_string.py
+++ greedy/problems/2027_minimum_moves_to_convert_string.py
@@ -4,3 +4,26 @@
 
 # Return the minimum number of moves required so that all the characters of s are converted to 'O'.
 
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        heap = []
+
+        for i, c in enumerate(s):
+            if c == 'X':
+                heapq.heappush(heap, i)
+        
+        if heap:
+            init = heap[0]
+        else:
+            return 0
+        ans = 1
+        while heap:
+            curr = heapq.heappop(heap)
+            if curr <= init + 2:
+                continue
+            else:
+                ans += 1
+                init = curr
+        return ans
+
+        
