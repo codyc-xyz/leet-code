26561dc734784b32e84218cb2e2387c55eb87e7d
Red <crabmarket56@gmail.com>
2023-04-24 11:15:37 +0900

    Add initial solution

--- greedy/problems/1736_latest_time_by_replacing_hidden_digits.py
+++ greedy/problems/1736_latest_time_by_replacing_hidden_digits.py
@@ -4,3 +4,27 @@
 
 # Return the latest valid time you can get from time by replacing the hidden digits.
 
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        ans = ""
+
+        for i, c in enumerate(time):
+            if c == '?':
+                if i == 0:
+                    if time[i + 1] != '?' and int(time[i + 1]) >= 4:
+                        ans += '1'
+                    else:
+                        ans += '2'
+                elif i == 1:
+                    if ans == '2':
+                        ans += '3'
+                    else:
+                        ans += '9'
+                elif i == 3:
+                    ans += '5'
+                else:
+                    ans += '9'
+            else:
+                ans += c
+
+        return ans
