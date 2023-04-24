0fd9affebeaf89214997e0a501dc9105a674b4a8
Red <crabmarket56@gmail.com>
2023-04-24 11:07:28 +0900

    Add initial solution

--- greedy/problems/1013_partition_array_into_three_parts_with_equal_sum.py
+++ greedy/problems/1013_partition_array_into_three_parts_with_equal_sum.py
@@ -2,3 +2,16 @@
 
 # Formally, we can partition the array if we can find indexes i + 1 < j with (arr[0] + arr[1] + ... + arr[i] == arr[i + 1] + arr[i + 2] + ... + arr[j - 1] == arr[j] + arr[j + 1] + ... + arr[arr.length - 1])
 
+class Solution:
+    def canThreePartsEqualSum(self, arr: List[int]) -> bool:
+        targetSum = sum(arr) // 3 
+        partitions = currSum = j = 0
+        while j < len(arr):
+            currSum += arr[j]
+            if currSum == targetSum and (partitions < 2 or j == len(arr) - 1):
+                partitions += 1
+                currSum = 0
+            j += 1
+        if partitions == 3 and not currSum:
+            return True
+        return False
