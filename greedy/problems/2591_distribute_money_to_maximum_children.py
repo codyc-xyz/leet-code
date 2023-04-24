3c801b92311d32d3cf6f3d86a26cf955f9d5ac22
Red <crabmarket56@gmail.com>
2023-04-24 12:06:08 +0900

    Add initial solution

--- greedy/problems/2591_distribute_money_to_maximum_children.py
+++ greedy/problems/2591_distribute_money_to_maximum_children.py
@@ -7,3 +7,22 @@
 # Nobody receives 4 dollars.
 # Return the maximum number of children who may receive exactly 8 dollars if you distribute the money according to the aforementioned rules. If there is no way to distribute the money, return -1.
 
+class Solution:
+    def distMoney(self, money: int, children: int) -> int:
+        money -= children
+        if money < 0:
+            return -1
+        ans = i = 0
+        while money and i < children:
+            if money >= 7:
+                i += 1
+                if i == children and money > 7:
+                    break
+                ans += 1
+                money -= 7
+            elif money + 1 == 4 and ans and i == children - 1:
+                ans -= 1
+                break
+            else:
+                break
+        return ans
