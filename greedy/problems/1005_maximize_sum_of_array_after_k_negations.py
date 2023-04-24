826673bd3b52758f63b23203234cc78d8806fc2a
Red <crabmarket56@gmail.com>
2023-04-24 10:50:32 +0900

    Add initial solution

--- greedy/problems/1005_maximize_sum_of_array_after_k_negations.py
+++ greedy/problems/1005_maximize_sum_of_array_after_k_negations.py
@@ -5,3 +5,16 @@
 
 # Return the largest possible sum of the array after modifying it in this way.
 
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        heap = []
+
+        for n in nums:
+            heapq.heappush(heap, n)
+
+        while k > 0:
+            curr = heapq.heappop(heap)
+            heapq.heappush(heap, -curr)
+            k -= 1
+
+        return sum(heap)
