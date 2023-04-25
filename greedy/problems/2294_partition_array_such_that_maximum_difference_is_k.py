3688f33ec4288d11df4201f806beefd637fea7d0
Red <crabmarket56@gmail.com>
2023-04-25 10:47:33 +0900

    Add initial solution

--- greedy/problems/2294_partition_array_such_that_maximum_difference_is_k.py
+++ greedy/problems/2294_partition_array_such_that_maximum_difference_is_k.py
@@ -4,3 +4,15 @@
 
 # A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
 
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        i = passes = 0
+        while i < len(nums):
+            anchor = nums[i] + k
+            while i < len(nums) and nums[i] <= anchor:
+                i += 1
+            passes += 1
+
+        return passes
