240060c48b02e39a7d54e90ee4e6d07ee003d44c
Red <crabmarket56@gmail.com>
2023-04-26 14:05:52 +0900

    Add initial solution

--- greedy/problems/1433_check_if_a_string_can_break_another_string.py
+++ greedy/problems/1433_check_if_a_string_can_break_another_string.py
@@ -2,3 +2,29 @@
 
 # A string x can break string y (both of size n) if x[i] >= y[i] (in alphabetical order) for all i between 0 and n-1.
 
+class Solution:
+    def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+
+        s1 = sorted(s1)
+        s2 = sorted(s2)
+        if s1[0] > s2[0]:
+            for i in range(1, len(s1)):
+                if s2[i] > s1[i]:
+                    return False
+        elif s2[0] > s1[0]:
+            for i in range(1, len(s1)):
+                if s1[i] > s2[i]:
+                    return False
+        else:
+            flag = True
+            for i in range(1, len(s1)):
+                if s2[i] > s1[i]:
+                    flag = False
+                    break
+            if flag == True:
+                return True
+            for i in range(1, len(s1)):
+                if s1[i] > s2[i]:
+                    return False
+                    
+        return True
