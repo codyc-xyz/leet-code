4e1edd2f231a5e104b29ada2a7d66327c2c32e5f
Red <crabmarket56@gmail.com>
2023-04-27 07:20:15 +0900

    Add initial solution

--- greedy/problems/1029_two_city_scheduling.py
+++ greedy/problems/1029_two_city_scheduling.py
@@ -2,3 +2,30 @@
 
 # Return the minimum cost to fly every person to a city such that exactly n people arrive in each city.
 
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+
+        costHeap = [[-abs(costs[i][0] - costs[i][1]), costs[i][0], costs[i][1]] for i in range(len(costs))]
+        heapq.heapify(costHeap)
+
+        target = len(costs) // 2
+        a = 0
+        b = 0
+        ans = 0
+        while costHeap:
+            _, costA, costB = heapq.heappop(costHeap)
+            if costA < costB and a < target:
+                ans += costA
+                a += 1
+            elif costA > costB and b < target:
+                ans += costB
+                b += 1
+            else:
+                if a > b:
+                    ans += costB
+                    b += 1
+                else:
+                    ans += costA
+                    a += 1
+        return ans
+
