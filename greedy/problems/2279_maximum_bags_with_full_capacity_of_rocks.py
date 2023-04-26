2ee1c163d07faf9e5422e86d47c11ef8af5815b7
Red <crabmarket56@gmail.com>
2023-04-26 14:15:34 +0900

    Add initial solution

--- greedy/problems/2279_maximum_bags_with_full_capacity_of_rocks.py
+++ greedy/problems/2279_maximum_bags_with_full_capacity_of_rocks.py
@@ -2,3 +2,14 @@
 
 # Return the maximum number of bags that could have full capacity after placing the additional rocks in some bags.
 
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+
+        heap = [c - r for c, r in zip(capacity, rocks)]
+        heapq.heapify(heap)
+        ans = 0
+        while heap and additionalRocks - heap[0] >= 0:
+            additionalRocks -= heapq.heappop(heap)
+            ans += 1
+
+        return ans
