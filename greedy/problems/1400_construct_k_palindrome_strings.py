4f20196a3afa911abf83ba12a805cebfa360082c
Red <crabmarket56@gmail.com>
2023-04-27 21:26:44 +0900

    Add intiial solution

--- greedy/problems/1400_construct_k_palindrome_strings.py
+++ greedy/problems/1400_construct_k_palindrome_strings.py
@@ -1,2 +1,12 @@
 # Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
 
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        count = collections.Counter(s)
+        odds = 0
+        for c in count:
+            if count[c] % 2:
+                odds += 1
+        return odds <= k
