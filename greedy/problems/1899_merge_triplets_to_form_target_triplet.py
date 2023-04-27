04c2dbf293e885b86a2a974e5e4c8b8401ecb0a8
Red <crabmarket56@gmail.com>
2023-04-27 17:03:50 +0900

    Add initial solution

--- greedy/problems/1899_merge_triplets_to_form_target_triplet.py
+++ greedy/problems/1899_merge_triplets_to_form_target_triplet.py
@@ -6,3 +6,23 @@
 # For example, if triplets[i] = [2, 5, 3] and triplets[j] = [1, 7, 5], triplets[j] will be updated to [max(2, 1), max(5, 7), max(3, 5)] = [2, 7, 5].
 # Return true if it is possible to obtain the target triplet [x, y, z] as an element of triplets, or false otherwise.
 
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+
+        heap = []
+
+        for a, b, c in triplets:
+            if a == target[0] or b == target[1] or c == target[2]  :
+                heapq.heappush(heap, [a, b, c])
+
+        A = B = C = 0
+
+        while heap:
+            a, b, c = heapq.heappop(heap)
+            if a > target[0] or b > target[1] or c > target[2]:
+                continue
+            A, B, C = max(A, a), max(B, b), max(C, c)
+            if A == target[0] and B == target[1] and C == target[2]:
+                return True
+        
+        return False
