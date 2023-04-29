91744105f003fa7627db799e2e8d052d06ec414e
Red <crabmarket56@gmail.com>
2023-04-29 09:26:20 +0900

    Add initial solution

--- greedy/problems/1647_minimum_deletions_to_make_character_frequencies_unique.py
+++ greedy/problems/1647_minimum_deletions_to_make_character_frequencies_unique.py
@@ -4,3 +4,20 @@
 
 # The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
 
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        count = collections.Counter(s)
+        seen = set()
+        deletions = 0
+        for c in count.values():
+            if c not in seen:
+                seen.add(c)
+            else:
+                curr = c
+                while curr > 0 and curr in seen:
+                    curr -= 1
+                    deletions += 1
+                seen.add(curr)
+        return deletions
+
+
