5287726f3ca9bcd8b6fb48c5c920626dd36f71a6
Red <crabmarket56@gmail.com>
2023-04-30 12:54:35 +0900

    Add initial solution

--- greedy/problems/1296_divide_array_in_sets_of_K_consecutive_numbers.py
+++ greedy/problems/1296_divide_array_in_sets_of_K_consecutive_numbers.py
@@ -2,3 +2,27 @@
 
 # Return true if it is possible. Otherwise, return false.
 
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        count = collections.Counter(nums)
+        heap = []
+        for c in count:
+            heap.append([c, count[c]])
+        heapq.heapify(heap)
+        seen = set()
+        addBack = []
+        while heap:
+            N, countN = heapq.heappop(heap)
+            if not seen or N - 1 in seen:
+                seen.add(N)
+                if countN > 1:
+                    addBack.append([N, countN - 1])
+            else:
+                addBack.append([N, countN])
+            if len(seen) == k:
+                for num, countNum in addBack:
+                    heapq.heappush(heap, [num, countNum])
+                addBack = []
+                seen = set()
+
+        return False if addBack or seen else True
