bc79a85ae86b010aaa5101c23125e394e4d0dd0c
Red <crabmarket56@gmail.com>
2023-04-30 17:04:20 +0900

    Add initial solution

--- greedy/problems/1481_least_number_of_unique_integers_after_k_removals.py
+++ greedy/problems/1481_least_number_of_unique_integers_after_k_removals.py
@@ -1,2 +1,17 @@
 # Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
 
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        count = collections.Counter(arr)
+        heap = []
+
+        for c in count:
+            heapq.heappush(heap, [count[c], c])
+
+        while k > 0:
+            c, n = heapq.heappop(heap)
+            if c > 1:
+                heapq.heappush(heap, [c - 1, n])
+            k -= 1
+
+        return len(heap)
