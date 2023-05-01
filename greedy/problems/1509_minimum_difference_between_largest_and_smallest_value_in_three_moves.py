23c77d28cef03acf36945cd9bb4361cf74086919
Red <crabmarket56@gmail.com>
2023-05-01 14:04:02 +0900

    Add initial solution

--- greedy/problems/1509_minimum_difference_between_largest_and_smallest_value_in_three_moves.py
+++ greedy/problems/1509_minimum_difference_between_largest_and_smallest_value_in_three_moves.py
@@ -4,3 +4,14 @@
 
 # Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
 
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        
+        if len(nums) <= 4:
+            return 0
+        nums.sort()
+        numMins = nums[:-3]
+        numMaxs = nums[3:]
+        numMinMax1 = nums[1:] + nums[:-2]
+        numMinMax2 = nums[2:] + nums[:-1]
+        return min(numMins[-1] - numMins[0], numMaxs[-1] - numMaxs[0], numMinMax1[-1] - numMinMax1[0], numMinMax2[-1] - numMinMax2[0])
