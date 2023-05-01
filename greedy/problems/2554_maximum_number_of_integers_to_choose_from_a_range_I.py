aaa9cbf115fcc30f53df803f706ed74f81a23765
Red <crabmarket56@gmail.com>
2023-05-01 15:28:07 +0900

    Add initial solution

--- greedy/problems/2554_maximum_number_of_integers_to_choose_from_a_range_I.py
+++ greedy/problems/2554_maximum_number_of_integers_to_choose_from_a_range_I.py
@@ -6,3 +6,18 @@
 # The sum of the chosen integers should not exceed maxSum.
 # Return the maximum number of integers you can choose following the mentioned rules.
 
+class Solution:
+    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        banned = set(banned)
+        ans = currSum = 0
+        for i in range(1, n + 1):
+            if i in banned:
+                i += 1
+                continue
+            if currSum + i <= maxSum:
+                currSum += i
+                i += 1
+                ans += 1
+            else:
+                break
+        return ans
