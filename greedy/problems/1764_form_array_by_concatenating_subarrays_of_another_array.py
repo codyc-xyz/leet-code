ae8477a0135d9ef2622bc0303efdce3c19751e01
Red <crabmarket56@gmail.com>
2023-05-01 15:10:42 +0900

    Add initial solution

--- greedy/problems/1764_form_array_by_concatenating_subarrays_of_another_array.py
+++ greedy/problems/1764_form_array_by_concatenating_subarrays_of_another_array.py
@@ -6,3 +6,20 @@
 
 # Note that the subarrays are disjoint if and only if there is no index k such that nums[k] belongs to more than one subarray. A subarray is a contiguous sequence of elements within an array.
 
+class Solution:
+    def canChoose(self, groups: List[List[int]], nums: List[int]) -> bool:
+        k = i = j = 0
+        while i < len(nums):
+            if nums[i] == groups[j][k]:
+                I = i
+                while I < len(nums) and k < len(groups[j]) and nums[I] == groups[j][k]:
+                    I += 1
+                    k += 1
+                if k == len(groups[j]):
+                    j += 1
+                    i = I - 1
+                if j == len(groups):
+                    return True
+                k = 0
+            i += 1
+        return False
