e233aee55d9738684ac54a7389a5f18d9b1fb555
Red <crabmarket56@gmail.com>
2023-05-02 07:55:40 +0900

    Add initial solution

--- greedy/problems/870_advantage_shuffle.py
+++ greedy/problems/870_advantage_shuffle.py
@@ -2,5 +2,36 @@
 
 # Return any permutation of nums1 that maximizes its advantage with respect to nums2.
 
- 
+class Solution:
+    def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        num1 = sorted(nums1)
+        num2 = sorted(nums2)
+        hm = {}
+        idxs = defaultdict(list)
+        unused = []
+
+        for i, n in enumerate(nums2):
+            heapq.heappush(idxs[n], i)
+
+        i = j = 0
+        while i < len(nums1):
+            if num1[i] > num2[j]:
+                idx = heapq.heappop(idxs[num2[j]])
+                hm[idx] = num1[i]
+                j += 1
+            else:
+                unused.append(num1[i])
+            i += 1
+      
+        ans = [-1] * len(nums1)
+        for i, n in enumerate(nums2):
+            if i in hm:
+                ans[i] = hm[i]
+        k = 0
+        for i in range(len(ans)):
+            if ans[i] == -1:
+                ans[i] = unused[k]
+                k += 1
+        return ans
 
