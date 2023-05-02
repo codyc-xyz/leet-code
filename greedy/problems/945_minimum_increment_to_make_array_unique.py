61843a4bcaad4e2d43723a768dbdbfa5b2e55889
Red <crabmarket56@gmail.com>
2023-05-02 09:29:59 +0900

    Add initial solution

--- greedy/problems/945_minimum_increment_to_make_array_unique.py
+++ greedy/problems/945_minimum_increment_to_make_array_unique.py
@@ -4,3 +4,22 @@
 
 # The test cases are generated so that the answer fits in a 32-bit integer.
 
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+
+        count = collections.Counter(nums)
+        ans = 0
+        res = set()
+        arr = [[c, count[c]] for c in count]
+        arr.sort(reverse=True)
+        for c, count in arr:
+            for _ in range(count):
+                if c not in res:
+                    res.add(c)
+                else:
+                    curr = c
+                    while curr in res:
+                        curr += 1
+                        ans += 1
+                    res.add(curr)
+        return ans
