e6d545d297f1e1fc0f05c61bd3d6a01d0cb31b38
Red <crabmarket56@gmail.com>
2023-05-07 18:30:24 +0900

    Add initial solution

--- greedy/problems/1546_maximum_number_of_non-overlapping_subarrays_with_sum_equals_target.py
+++ greedy/problems/1546_maximum_number_of_non-overlapping_subarrays_with_sum_equals_target.py
@@ -1,2 +1,18 @@
 # Given an array nums and an integer target, return the maximum number of non-empty non-overlapping subarrays such that the sum of values in each subarray is equal to target.
 
+class Solution:
+    def maxNonOverlapping(self, nums: List[int], target: int) -> int:
+
+        pSum = {0: 0}
+        currSum = 0
+        ans = 0
+        for i, n in enumerate(nums):
+            currSum += n
+            if currSum - target in pSum:
+                ans += 1
+                pSum = {}
+            pSum[currSum] = i + 1
+        return ans
+                
+
+        
