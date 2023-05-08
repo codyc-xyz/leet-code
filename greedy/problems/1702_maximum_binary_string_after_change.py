722994e952707a063685fa979f08a176cb33276c
Red <crabmarket56@gmail.com>
2023-05-08 11:41:04 +0900

    Add initial solution

--- greedy/problems/1702_maximum_binary_string_after_change.py
+++ greedy/problems/1702_maximum_binary_string_after_change.py
@@ -6,3 +6,24 @@
 # For example, "00010" -> "00001"
 # Return the maximum binary string you can obtain after any number of operations. Binary string x is greater than binary string y if x's decimal representation is greater than y's decimal representation.
 
+class Solution:
+    def maximumBinaryString(self, binary: str) -> str:
+        if '0' not in binary:
+            return binary
+        idx = binary.index('0')
+        ans = ""
+        for i in range(idx):
+            ans += '1'
+
+        zeros = 0
+        for i in range(idx, len(binary)):
+            if binary[i] == '0':
+                zeros += 1
+
+        while zeros >= 2:
+            ans += '1'
+            zeros -= 1
+        ans += '0'
+        while len(ans) < len(binary):
+            ans += '1'
+        return ans
