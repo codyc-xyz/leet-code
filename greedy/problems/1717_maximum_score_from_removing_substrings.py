b7fa443556f9de0e8da6e47844a1da95c508f42e
Red <crabmarket56@gmail.com>
2023-05-09 13:33:41 +0900

    Add initial solution

--- greedy/problems/1717_maximum_score_from_removing_substrings.py
+++ greedy/problems/1717_maximum_score_from_removing_substrings.py
@@ -6,3 +6,45 @@
 # For example, when removing "ba" from "cabxbae" it becomes "cabxe".
 # Return the maximum points you can gain after applying the above operations on s.
 
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        score = 0
+        stack = []
+
+        if x > y:
+            for c in s:
+                stack.append(c)
+                while len(stack) >= 2:
+                    if stack[-2] == 'a' and stack[-1] == 'b':
+                        score += x
+                        stack.pop()
+                        stack.pop()
+                    else:
+                        break
+        else:
+            for c in s:
+                stack.append(c)
+                while len(stack) >= 2:
+                    if stack[-2] == 'b' and stack[-1] == 'a':
+                        score += y
+                        stack.pop()
+                        stack.pop()
+                    else:
+                        break
+        
+        stack2 = []
+        for c in stack:
+            stack2.append(c)
+            while len(stack2) >= 2:
+                if stack2[-2] == 'a' and stack2[-1] == 'b':
+                    stack2.pop()
+                    stack2.pop()
+                    score += x
+                elif stack2[-2] == 'b' and stack2[-1] == 'a':
+                    stack2.pop()
+                    stack2.pop()
+                    score += y
+                else:
+                    break
+        return score 
+
