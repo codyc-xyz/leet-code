a3f8c15a83590a794e23c56e0981bc39684de6e5
Red <crabmarket56@gmail.com>
2023-05-10 13:06:11 +0900

    Add initial solution (TLE)

--- greedy/problems/134_gas_station.py
+++ greedy/problems/134_gas_station.py
@@ -4,3 +4,35 @@
 
 # Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
 
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        gasCost = []
+        costSum = 0
+        for i, (g, c) in enumerate(zip(gas, cost)):
+            gasCost.append([g - c, i])
+            costSum += gasCost[-1][0]
+
+        if costSum < 0:
+            return -1
+    
+        for c, i in gasCost:
+            if c >= 0:
+                j = i + 1
+                rollingCost = c
+                while j != i:
+                    if j == len(gasCost):
+                        if i == 0:
+                            break
+                        j = 0
+                    rollingCost += gasCost[j][0]
+                    if rollingCost < 0:
+                        break
+                    j += 1
+                if rollingCost >= 0:
+                    return i
+        return -1
+
+
+        
+
+        
