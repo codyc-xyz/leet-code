4ab83f8773bc6ca13a9ce7c862c03c100e11bd45
Red <crabmarket56@gmail.com>
2023-05-11 10:36:36 +0900

    Add initial solution

--- greedy/problems/1864_minimum_number_of_swaps_to_make_the_binary_string_alternating.py
+++ greedy/problems/1864_minimum_number_of_swaps_to_make_the_binary_string_alternating.py
@@ -4,3 +4,59 @@
 
 # Any two characters may be swapped, even if they are not adjacent.
 
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        n = len(s)
+        count = collections.Counter(s)
+        ones = count['1']
+        zeros = count['0']
+        if n % 2 and abs(ones - zeros) > 1:
+            return -1
+        elif not n % 2 and ones - zeros != 0:
+            return -1
+
+        if ones == zeros:
+            ans1, ans2 = [], []
+
+            for i in range(n):
+                if i % 2:
+                    ans1.append('0')
+                    ans2.append('1')
+                else:
+                    ans1.append('1')
+                    ans2.append('0')
+            swaps1 = swaps2 = 0
+
+            for i in range(n):
+                if s[i] != ans1[i]:
+                    swaps1 += 1
+                else:
+                    swaps2 += 1
+            return math.ceil(min(swaps1, swaps2) / 2)
+        elif ones > zeros:
+            ans = []
+            for i in range(n):
+                if not i % 2:
+                    ans.append('1')
+                else:
+                    ans.append('0')
+            swaps = 0
+            for i in range(n):
+                if s[i] != ans[i]:
+                    swaps += 1
+            return math.ceil(swaps / 2)
+        else:
+            ans = []
+            for i in range(n):
+                if not i % 2:
+                    ans.append('0')
+                else:
+                    ans.append('1')
+            swaps = 0
+            for i in range(n):
+                if s[i] != ans[i]:
+                    swaps += 1
+            return math.ceil(swaps / 2)
+      
+      
+
