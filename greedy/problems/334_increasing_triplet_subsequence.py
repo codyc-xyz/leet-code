d54a3a7f2978fb5659c0d7bcd8af7849cdce79b4
Red <crabmarket56@gmail.com>
2023-05-12 09:39:12 +0900

    Add brute force solution (TLE)

--- greedy/problems/334_increasing_triplet_subsequence.py
+++ greedy/problems/334_increasing_triplet_subsequence.py
@@ -1,2 +1,18 @@
 # Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
 
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        
+        arrDict = defaultdict(list)
+
+        for i, n in enumerate(nums):
+            for a in arrDict:
+                if n > arrDict[a][-1]:
+                    arrDict[a].append(n)
+                    if len(arrDict[a]) == 3:
+                        return True
+                elif n < arrDict[a][-1] and n > min(arrDict[a]):
+                    arrDict[a].pop()
+                    arrDict[a].append(n)
+            arrDict[i].append(n)
+        return False
