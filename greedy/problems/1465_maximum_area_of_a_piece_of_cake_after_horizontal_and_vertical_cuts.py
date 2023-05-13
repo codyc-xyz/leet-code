e377a9937de6110716a93317f1d0cdb12eef50c1
Red <crabmarket56@gmail.com>
2023-05-13 14:06:34 +0900

    Add initial solution

--- greedy/problems/1465_maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.py
+++ greedy/problems/1465_maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.py
@@ -4,3 +4,20 @@
 # verticalCuts[j] is the distance from the left of the rectangular cake to the jth vertical cut.
 # Return the maximum area of a piece of cake after you cut at each horizontal and vertical position provided in the arrays horizontalCuts and verticalCuts. Since the answer can be a large number, return this modulo 109 + 7.
 
+class Solution:
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        MOD = 10**9 + 7
+
+        horizontalCuts.sort(reverse=True)
+        verticalCuts.sort(reverse=True)
+        maxH = maxV = 0
+        prevH, prevV = h, w
+        for hc in horizontalCuts:
+            maxH = max(maxH, prevH - hc)
+            prevH = hc
+        maxH = max(maxH, prevH)
+        for vc in verticalCuts:
+            maxV = max(maxV, prevV - vc)
+            prevV = vc
+        maxV = max(maxV, prevV)
+        return (maxH * maxV) % MOD
