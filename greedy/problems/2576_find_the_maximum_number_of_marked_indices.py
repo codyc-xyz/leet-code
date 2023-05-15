b8f056f7e1c825ba3051e5a2b74265880e9a2241
Red <crabmarket56@gmail.com>
2023-05-15 11:33:00 +0900

    Add initial solution

--- greedy/problems/2576_find_the_maximum_number_of_marked_indices.py
+++ greedy/problems/2576_find_the_maximum_number_of_marked_indices.py
@@ -5,5 +5,18 @@
 # Pick two different unmarked indices i and j such that 2 * nums[i] <= nums[j], then mark i and j.
 # Return the maximum possible number of marked indices in nums using the above operation any number of times.
 
- 
-
+class Solution:
+    def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
+        nums.sort()
+        l, r = 0, math.ceil(len(nums) / 2)
+        marked = set()
+        while r < len(nums):
+            while r < len(nums) and r in marked:
+                r += 1
+            while l < r and l in marked:
+                l += 1
+            if nums[l] * 2 <= nums[r]:
+                marked.add(l)
+                marked.add(r)
+            r += 1
+        return len(marked)
