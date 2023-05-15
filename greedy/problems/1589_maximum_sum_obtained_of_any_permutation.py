694ca971d539eb84f41930ac25a4e71468717bd3
Red <crabmarket56@gmail.com>
2023-05-15 11:57:03 +0900

    Add initial solution (TLE)

--- greedy/problems/1589_maximum_sum_obtained_of_any_permutation.py
+++ greedy/problems/1589_maximum_sum_obtained_of_any_permutation.py
@@ -4,3 +4,20 @@
 
 # Since the answer may be too large, return it modulo 109 + 7.
 
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        arr = [0 for _ in range(len(nums))]
+        mod = 10**9+7
+        for s, e in requests:
+            for i in range(s, e + 1):
+                arr[i] -= 1
+
+        heapq.heapify(arr)
+        nums.sort()
+        i = len(nums) - 1
+        ans = 0
+        while arr and arr[0] < 0:
+            ans += nums[i] * -arr[0]
+            heapq.heappop(arr)
+            i -= 1
+        return ans % mod
