01368e6041a96bba0cb820afddde5789f23473f0
Red <crabmarket56@gmail.com>
2023-05-17 05:29:02 +0900

    Add initial solution

--- greedy/problems/955_delete_columns_to_make_sorted_II.py
+++ greedy/problems/955_delete_columns_to_make_sorted_II.py
@@ -6,5 +6,23 @@
 
 # Suppose we chose a set of deletion indices answer such that after deletions, the final array has its elements in lexicographic order (i.e., strs[0] <= strs[1] <= strs[2] <= ... <= strs[n - 1]). Return the minimum possible value of answer.length.
 
- 
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        ans = 0
+        compare = [True] * len(strs)
+
+        for i in range(len(strs[0])):
+            compareCols = compare[:]
+            for j in range(1, len(strs)):
+                if compareCols[j]:
+                    if strs[j][i] < strs[j - 1][i]:
+                        ans += 1
+                        compare = compareCols
+                        break
+                    elif strs[j][i] > strs[j - 1][i]:
+                        compare[j] = False
+            if True not in compare:
+                return ans
+
+        return ans
 
