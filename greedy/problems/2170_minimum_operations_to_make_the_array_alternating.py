c13e3cca4228d7081e12f53a08290714a6a374f6
Red <crabmarket56@gmail.com>
2023-05-18 15:19:52 +0900

    Add initial solution

--- greedy/problems/2170_minimum_operations_to_make_the_array_alternating.py
+++ greedy/problems/2170_minimum_operations_to_make_the_array_alternating.py
@@ -8,3 +8,61 @@
 
 # Return the minimum number of operations required to make the array alternating.
 
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+
+        countOdd = {}
+        countEven = {}
+
+        for i, n in enumerate(nums):
+            if i % 2:
+                if n in countOdd:
+                    countOdd[n] += 1
+                else:
+                    countOdd[n] = 1
+            else:
+                if n in countEven:
+                    countEven[n] += 1
+                else:
+                    countEven[n] = 1
+
+        maxOdd = maxCountOdd = secondOdd = secondCountOdd = 0
+        for c in countOdd:
+            if countOdd[c] > maxCountOdd:
+                secondOdd = maxOdd
+                maxOdd = c
+                secondCountOdd = maxCountOdd
+                maxCountOdd = countOdd[c]
+            elif countOdd[c] > secondCountOdd:
+                secondOdd = c
+                secondCountOdd = countOdd[c]
+    
+        maxEven = maxCountEven = secondEven = secondCountEven = 0
+        for c in countEven:
+            if countEven[c] > maxCountEven:
+                secondEven = maxEven
+                maxEven = c
+                secondCountEven = maxCountEven
+                maxCountEven = countEven[c]
+            elif countEven[c] > secondCountEven:
+                secondEven = c
+                secondCountEven = countEven[c]
+
+        if maxOdd == maxEven:
+            if secondCountOdd or secondCountEven:
+                if secondCountOdd and secondCountEven:
+                    if maxCountOdd - secondCountOdd > maxCountEven - secondCountEven:
+                        maxEven = secondEven
+                        maxCountEven = secondCountEven
+                    else:
+                        maxOdd = secondOdd
+                        maxCountOdd = secondCountOdd
+                elif secondCountOdd:
+                    maxCountOdd = secondCountOdd
+                    maxOdd = secondOdd
+                elif secondCountEven:
+                    maxCountEven = secondCountEven
+                    maxEven = secondEven
+            else:
+                return len(nums) // 2
+        return len(nums) - (maxCountOdd + maxCountEven)
