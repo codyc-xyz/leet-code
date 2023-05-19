98b3b84ec52df7f55026012591bd2c3c50fa0dfc
Red <crabmarket56@gmail.com>
2023-05-19 11:14:02 +0900

    Add initial solution (MLE)

--- greedy/problems/2271_maximum_white_tiles_covered_by_a_carpet.py
+++ greedy/problems/2271_maximum_white_tiles_covered_by_a_carpet.py
@@ -4,3 +4,24 @@
 
 # Return the maximum number of white tiles that can be covered by the carpet.
 
+class Solution:
+    def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
+        tiles.sort()
+        pSum = [0 for _ in range(tiles[-1][1] + 1)]
+        for l, r in tiles:
+            for i in range(l, r + 1):
+                pSum[i] = 1
+        currSum = 0
+        carpetLen = min(carpetLen, len(pSum))
+        for i in range(carpetLen):
+            currSum += pSum[i]
+        maxSum = currSum
+        j = 0
+        for i in range(carpetLen, len(pSum)):
+            currSum -= pSum[j]
+            currSum += pSum[i]
+            maxSum = max(maxSum, currSum)
+            j += 1
+        
+        return maxSum
+            
