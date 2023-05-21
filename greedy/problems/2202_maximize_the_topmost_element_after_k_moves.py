0641422c5873691a5d24c88890116a8561550056
Red <crabmarket56@gmail.com>
2023-05-21 16:38:19 +0900

    Add initial solution

--- greedy/problems/2202_maximize_the_topmost_element_after_k_moves.py
+++ greedy/problems/2202_maximize_the_topmost_element_after_k_moves.py
@@ -8,3 +8,14 @@
 
 # Return the maximum value of the topmost element of the pile possible after exactly k moves. In case it is not possible to obtain a non-empty pile after k moves, return -1.
 
+class Solution:
+    def maximumTop(self, nums: List[int], k: int) -> int:
+
+        if len(nums) == 1 and k % 2:
+            return -1
+        ans = max(nums[:k + 1])
+        if nums.index(ans) != k - 1:
+            return ans
+        else: 
+            nums[k - 1] = -1
+            return max(nums[:k + 1])
