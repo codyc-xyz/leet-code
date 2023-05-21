e12499c124077b40a25185b893a09bdce610d06d
Red <crabmarket56@gmail.com>
2023-05-21 11:04:23 +0900

    Add initial solution

--- greedy/problems/2384_largest_palindromic_number.py
+++ greedy/problems/2384_largest_palindromic_number.py
@@ -6,3 +6,34 @@
 
 # You do not need to use all the digits of num, but you must use at least one digit.
 # The digits can be reordered.
+
+class Solution:
+    def largestPalindromic(self, num: str) -> str:
+        count = collections.Counter()
+        biggestOdd = float('-inf')
+        for c in num:
+            count[c] += 1
+
+        heap = []
+        for c in count:
+            if count[c] % 2:
+                biggestOdd = max(biggestOdd, int(c))
+        if biggestOdd != float('-inf'):
+            ans = str(biggestOdd)
+            count[ans] -= 1
+        else:
+            ans = ""
+        for c in count:
+            if not count[c] % 2 and count[c] > 0:
+                heapq.heappush(heap, [int(c), count[c]])
+            elif count[c] % 2 and count[c] > 2:
+                heapq.heappush(heap, [int(c), count[c] - 1])
+
+        while heap:
+            n, count = heapq.heappop(heap)
+            if n == 0 and not heap:
+                break
+            n = str(n)
+            ans = (n * (count // 2)) + ans + (n * (count // 2))
+        return ans if ans else "0"
+
