68027355d6d6b4bc305ba893d34f269183e48984
Red <crabmarket56@gmail.com>
2023-05-22 13:51:12 +0900

    Add initial solution

--- intervals/problems/435_non-overlapping_intervals.py
+++ intervals/problems/435_non-overlapping_intervals.py
@@ -1,2 +1,19 @@
 # Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
 
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+
+        intervals.sort()
+        skipped = ans = j = 0
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < intervals[j][1]:
+                ans += 1
+                if intervals[i][1] < intervals[j][1]:
+                    j = i
+                    skipped = 0
+                else:
+                    skipped += 1
+                continue
+            j += 1 + skipped
+            skipped = 0
+        return ans
