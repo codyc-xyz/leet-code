5087942558ed8b5403eae1516f62a6be771a2ad2
Red <crabmarket56@gmail.com>
2023-05-23 07:30:33 +0900

    Add initial solution

--- graph/problems/1791_find_center_of_star_graph.py
+++ graph/problems/1791_find_center_of_star_graph.py
@@ -2,3 +2,16 @@
 
 # You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
 
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        count = collections.Counter()
+        for e1, e2 in edges:
+            count[e1] += 1
+            count[e2] += 1
+
+        maxCount, ans = 0, None
+        for c in count:
+            if count[c] > maxCount:
+                maxCount = count[c]
+                ans = c
+        return ans
