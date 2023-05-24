0ebab66a23de71c51b1b655a730cd9d12d542dc8
Red <crabmarket56@gmail.com>
2023-05-25 07:15:47 +0900

    Add initial solution

--- graph/problems/133_clone_graph.py
+++ graph/problems/133_clone_graph.py
@@ -4,3 +4,31 @@
 
 # Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+
+        hm = {}
+        if not node:
+            return node
+        curr = collections.deque()
+        curr.append(node)
+        while curr:
+            currNode = curr.popleft()
+            if currNode not in hm:
+                hm[currNode] = Node(currNode.val)
+            for n in currNode.neighbors:
+                if n not in hm:
+                    hm[n] = Node(n.val)
+                    curr.append(n)
+                hm[currNode].neighbors.append(hm[n])
+        return hm[node]
+
+
