f4b316e2db497a8b5cb0cf57571d6865b0158d3e
Red <crabmarket56@gmail.com>
2023-05-24 14:13:23 +0900

    Add initial solution

--- graph/problems/200_number_of_islands.py
+++ graph/problems/200_number_of_islands.py
@@ -2,3 +2,23 @@
 
 # An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def dfs(row, col):
+            seen.add((row, col))
+            for l, r in ((row-1, col), (row+1, col), (row, col-1), (row, col+1)):
+                if (l, r) not in seen and l >=0 and l < len(grid) and r >= 0 and r < len(grid[0]) and grid[l][r] == '1':
+                    dfs(l, r)
+
+            return 1
+            
+
+
+        ans = 0
+        seen = set()
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1' and (row, col) not in seen:
+                    ans += dfs(row, col)
+        return ans
