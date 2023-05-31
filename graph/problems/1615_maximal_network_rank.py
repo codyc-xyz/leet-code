e7c666c4d7a208074437389b0ac5a2779067dcbf
Red <crabmarket56@gmail.com>
2023-05-31 09:51:30 +0900

    Add initial solution

--- graph/problems/1615_maximal_network_rank.py
+++ graph/problems/1615_maximal_network_rank.py
@@ -6,3 +6,41 @@
 
 # Given the integer n and the array roads, return the maximal network rank of the entire infrastructure.
 
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        if not roads:
+            return 0
+        adj = defaultdict(list)
+        cities = []
+        for r1, r2 in roads:
+            adj[r1].append(r2)
+            adj[r2].append(r1)
+            if r1 not in cities:
+                cities.append(r1)
+            if r2 not in cities:
+                cities.append(r2)
+
+        
+        def networkRank(city1, city2):
+            res = 0
+            for c1 in adj[city1]:
+                if (city1, c1) not in seen and (c1, city1) not in seen:
+                    res += 1
+                    seen.add((city1, c1))
+
+            for c2 in adj[city2]:
+                if (city2, c2) not in seen and (c2, city2) not in seen:
+                    res += 1
+                    seen.add((city2, c2))
+                
+            return res
+            
+        ans = 0
+        for i in range(len(cities)):
+            for j in range(i, len(cities)):
+                seen = set()
+                ans = max(ans, networkRank(cities[i], cities[j]))
+        return ans
+
+
+
