438e3dce85ea0eec37bc22e90ff9f711745e10d5
Red <crabmarket56@gmail.com>
2023-06-01 14:02:15 +0900

    Add initial solution

--- greedy/problems/547_number_of_provinces.py
+++ greedy/problems/547_number_of_provinces.py
@@ -6,3 +6,33 @@
 
 # Return the total number of provinces.
 
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        ROWS = len(isConnected)
+        COLS = len(isConnected[0])
+        adj = defaultdict(list)
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if isConnected[i][j] and j + 1 not in adj[i + 1]:
+                    adj[i + 1].append(j + 1)
+                    adj[j + 1].append(i + 1)
+                    
+
+        def dfs(curr):
+            if curr in seen:
+                return 0
+            seen.add(curr)
+            for n in adj[curr]:
+                if n not in seen:
+                    dfs(n)
+            return 1
+
+
+        seen = set()
+        ans = 0
+        for i in range(1, ROWS + 1): 
+            ans += dfs(i) 
+            if len(seen) == ROWS:
+                break
+        return ans              
