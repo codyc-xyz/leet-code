726360c247dccd7f115c6038ffd01f8d4c5b3ec9
Red <crabmarket56@gmail.com>
2023-06-01 13:46:29 +0900

    Add initial solution (TLE)

--- graph/problems/851_loud_and_rich.py
+++ graph/problems/851_loud_and_rich.py
@@ -4,3 +4,29 @@
 
 # Return an integer array answer where answer[x] = y if y is the least quiet person (that is, the person y with the smallest value of quiet[y]) among all people who definitely have equal to or more money than the person x.
 
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+
+        richerThan = defaultdict(list)
+        for richerP, poorerP in richer:
+            richerThan[poorerP].append(richerP)
+
+        def dfs(p):
+            for richerP in richerThan[p]:
+                if quiet[richerP] < self.minLoud:
+                    self.minLoud = quiet[richerP]
+                    self.loudestP = richerP
+                dfs(richerP)
+
+        ans = []
+
+        for i in range(len(quiet)):
+            self.minLoud = quiet[i]
+            self.loudestP = i
+            dfs(i)
+            ans.append(self.loudestP)
+        return ans
+
+
+
+
