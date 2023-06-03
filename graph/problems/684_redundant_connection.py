f607b9b216240b9bd2402825fb93790c54629fb7
Red <crabmarket56@gmail.com>
2023-06-03 13:56:58 +0900

    Refactor alternative solution

--- graph/problems/684_redundant_connection.py
+++ graph/problems/684_redundant_connection.py
@@ -28,7 +28,6 @@ class Solution:
         N = len(edges)
         par = [i for i in range(N + 1)]
         rank = [1 for i in range(N + 1)]
-        self.ans = 0
         def find(n):
             res = n
             while res != par[res]:
@@ -39,19 +38,18 @@ class Solution:
             p1, p2 = find(n1), find(n2)
 
             if p1 == p2:
-                self.ans = [n1, n2]
+                return False
             if rank[p1] > rank[p2]:
                 par[p2] = p1
                 rank[p1] += 1
             else:
                 par[p1] = p2
                 rank[p2] += 1
+            return True
       
 
         for e1, e2 in edges:
-           union(e1, e2)
-           if self.ans:
-               return self.ans
-
-
+           if not union(e1, e2):
+               return [e1, e2]
+           
             
