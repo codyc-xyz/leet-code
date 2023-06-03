ebaec053226019fd788d87b3454d5f91b8de0cd6
Red <crabmarket56@gmail.com>
2023-06-03 13:54:21 +0900

    Add alternative solution

--- graph/problems/684_redundant_connection.py
+++ graph/problems/684_redundant_connection.py
@@ -22,3 +22,36 @@ class Solution:
                 return [e1, e2]
             adj[e1].append(e2)
             adj[e2].append(e1)
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        N = len(edges)
+        par = [i for i in range(N + 1)]
+        rank = [1 for i in range(N + 1)]
+        self.ans = 0
+        def find(n):
+            res = n
+            while res != par[res]:
+                res = par[res]
+            return res
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                self.ans = [n1, n2]
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += 1
+            else:
+                par[p1] = p2
+                rank[p2] += 1
+      
+
+        for e1, e2 in edges:
+           union(e1, e2)
+           if self.ans:
+               return self.ans
+
+
+            
