033c889a1e8ff48af94efb591b4dceced691afa2
Red <crabmarket56@gmail.com>
2023-06-04 09:33:14 +0900

    Add initial solution

--- graph/problems/1319_number_of_operations_to_make_network_connected.py
+++ graph/problems/1319_number_of_operations_to_make_network_connected.py
@@ -4,3 +4,32 @@
 
 # Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
 
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        N = len(connections)
+
+        if N < n - 1:
+            return -1
+
+        adj = defaultdict(list)
+
+        for n1, n2 in connections:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        def dfs(n):
+            if n in seen:
+                return 0
+            seen.add(n)
+            for nei in adj[n]:
+                if nei not in seen:
+                    dfs(nei)
+            return 1
+
+        seen = set()
+        ans = 0
+        for i in range(n):
+            if i not in seen:
+                ans += dfs(i)
+
+        return ans - 1
