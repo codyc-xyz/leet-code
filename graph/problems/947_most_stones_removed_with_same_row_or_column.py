bc8a3d1ce80edf8fc51fe24f049069af44b62459
Red <crabmarket56@gmail.com>
2023-06-07 09:44:56 +0900

    Add initial solution

--- graph/problems/947_most_stones_removed_with_same_row_or_column.py
+++ graph/problems/947_most_stones_removed_with_same_row_or_column.py
@@ -4,5 +4,34 @@
 
 # Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
 
- 
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
 
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+
+        for r, c in stones:
+            rows[r].append(c)
+            cols[c].append(r)
+            
+        def dfs(r, c):
+            if (r, c) in seen:
+                return 
+            seen.add((r, c))
+            for col in rows[r]:
+                if (r, col) in seen:
+                    continue
+                dfs(r, col)
+            for row in cols[c]:
+                if (row, c) in seen:
+                    continue
+                dfs(row, c)
+
+        seen = set()
+        components = 0
+        for r, c in stones:
+            if (r, c) not in seen:
+                dfs(r, c)
+                components += 1
+
+        return len(seen) - components
