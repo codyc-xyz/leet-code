6e7ac8a1678b04b5b4f7f50286a5e8c1c0c8a78a
Red <crabmarket56@gmail.com>
2023-06-08 11:34:39 +0900

    Add initial solution

--- graph/problems/399_evaluate_division.py
+++ graph/problems/399_evaluate_division.py
@@ -6,3 +6,38 @@
 
 # Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
 
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+
+        adj = defaultdict(list)
+        vals = set()
+        for i, (e1, e2) in enumerate(equations):
+            adj[e1].append((e2, values[i]))
+            adj[e2].append((e1, 1 / values[i]))
+            vals.add(e1)
+            vals.add(e2)
+
+        def dfs(curr, target, path):
+            if self.res:
+                return
+            if curr == target and curr in vals and target in vals:
+                self.res = 1
+                for n in path:
+                    self.res *= n
+                return
+            seen.add(curr)
+            for c, val in adj[curr]:
+                if c not in seen:
+                    dfs(c, target, path + [val])
+
+        ans = []
+        for curr, target in queries:
+            self.res = None
+            seen = set()
+            dfs(curr, target, [])
+            if self.res != None:
+                ans.append(self.res)
+            else:
+                ans.append(-1)
+
+        return ans
