3c752fcab796105a51cd93ece56ee4b49b029dd4
Red <crabmarket56@gmail.com>
2023-06-10 13:30:59 +0900

    Add initial solution

--- graph/problems/802_find_eventual_safe_states.py
+++ graph/problems/802_find_eventual_safe_states.py
@@ -4,3 +4,23 @@
 
 # Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
 
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+
+        seen = [0 for _ in range(len(graph))]
+        def dfs(curr):
+            if seen[curr] != 0:
+                return seen[curr] == 1
+            seen[curr] = -1
+            for n in graph[curr]:
+                if not dfs(n):
+                    return False
+            seen[curr] = 1
+            return True
+               
+        ans = []
+        for i in range(len(graph)):
+            if dfs(i):
+                ans.append(i)
+
+        return ans
