9727a1f3600827b7682fe3278a97bd6e555b163a
Red <crabmarket56@gmail.com>
2023-06-14 06:44:07 +0900

    Add initial solution

--- graph/2039_the_time_when_the_network_becomes_idle.py
+++ graph/2039_the_time_when_the_network_becomes_idle.py
@@ -12,5 +12,38 @@
 
 # Return the earliest second starting from which the network becomes idle.
 
- 
+class Solution:
+    def networkBecomesIdle(self, edges: List[List[int]], patience: List[int]) -> int:
 
+        n = len(patience)
+        adj = defaultdict(list)
+
+        for e1, e2 in edges:
+            adj[e1].append(e2)
+            adj[e2].append(e1)
+
+        q = deque()
+        res = [float('inf') for _ in range(n)]
+        q.append((0, 0))
+        seen = set()
+        seen.add(0)
+        while q:
+            curr, hops = q.popleft()
+            res[curr] = hops * 2
+            for n in adj[curr]:
+                if n not in seen:
+                    q.append((n, hops + 1))
+                    seen.add(n)
+
+        ans = max(res)
+        for i, r in enumerate(res):
+            if i == 0:
+                continue
+            diff = r
+            if r % patience[i] == 0:
+                diff -= patience[i]
+            else:
+                diff -= (r % patience[i])
+            ans = max(ans, r + diff)
+        return ans + 1
+              
