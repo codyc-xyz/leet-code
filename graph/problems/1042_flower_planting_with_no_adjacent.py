c57ac5a06a4beeb590bc210fb3be74ec5223c156
Red <crabmarket56@gmail.com>
2023-06-15 08:48:25 +0900

    Add initial solution

--- graph/problems/1042_flower_planting_with_no_adjacent.py
+++ graph/problems/1042_flower_planting_with_no_adjacent.py
@@ -6,3 +6,32 @@
 
 # Return any such a choice as an array answer, where answer[i] is the type of flower planted in the (i+1)th garden. The flower types are denoted 1, 2, 3, or 4. It is guaranteed an answer exists.
 
+class Solution:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+
+        adj = defaultdict(list)
+
+        for n1, n2 in paths:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        hm = {}
+        def dfs(curr, val):
+            length = len(adj[curr])
+            for nei in adj[curr]:
+                if nei in hm and hm[nei] == val:
+                    break
+                length -= 1
+            return length == 0
+
+        for i in range(1, n + 1):
+            for j in range(1, 5):
+                if dfs(i, j):
+                    hm[i] = j
+                    break
+
+        ans = [None for _ in range(n)]
+
+        for n in hm:
+            ans[n - 1] = hm[n]
+        return ans
