cb8837f803cf64e00254c1fe34a083956f445f5d
Red <crabmarket56@gmail.com>
2023-06-25 16:05:25 +0900

    Add initial solution

--- graph/problems/2316_count_unreachable_pairs_of_nodes_in_an_undirected_graph.py
+++ graph/problems/2316_count_unreachable_pairs_of_nodes_in_an_undirected_graph.py
@@ -2,3 +2,34 @@
 
 # Return the number of pairs of different nodes that are unreachable from each other.
 
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+
+        adj = defaultdict(list)
+       
+
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        seen = set()
+        sizes = []
+        def dfs(curr):
+            size = 1
+            seen.add(curr)
+
+            for n in adj[curr]:
+                if n not in seen:
+                    size += dfs(n)
+            return size
+
+        for i in range(n):
+            if i not in seen:
+                sizes.append(dfs(i))
+
+        remaining = sum(sizes)
+        ans = 0
+        for i in range(len(sizes)):
+            remaining -= sizes[i]
+            ans += sizes[i] * remaining
+        return ans
