75b8013676bcfd4a3f2111025e4c87b5a8e912e5
Red <crabmarket56@gmail.com>
2023-07-01 16:30:49 +0900

    Add initial solution

--- graph/problems/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.py
+++ graph/problems/1334_find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance.py
@@ -4,3 +4,34 @@
 
 # Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
 
+class Solution:
+    def findTheCity(self, N: int, edges: List[List[int]], distanceThreshold: int) -> int:
+
+        adj = defaultdict(list)
+
+        for n1, n2, weight in edges:
+            adj[n1].append((weight, n2))
+            adj[n2].append((weight, n1))
+
+        numReachable = [0 for _ in range(N)]
+        for i in range(N):
+            heap = [(0, i)]
+            seen = set()
+            seen.add(i)
+            while heap:
+                currWeight, curr = heapq.heappop(heap)
+                if curr not in seen:
+                    numReachable[i] += 1
+                    seen.add(curr)
+                for w, n in adj[curr]:
+                    if currWeight + w <= distanceThreshold and n not in seen:
+                        heapq.heappush(heap, (currWeight + w, n))
+                        
+        minNum = float('inf')
+        ans = -1
+        for i in range(N - 1, -1, -1):
+            if numReachable[i] < minNum:
+                minNum = numReachable[i]
+                ans = i
+
+        return ans   
