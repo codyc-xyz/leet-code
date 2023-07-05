c681000f65fdc0701755f3ae2014b4373f4d4593
Red <crabmarket56@gmail.com>
2023-07-05 15:44:11 +0900

    Add initial solution (TLE)

--- graph/problems/2662_Minimum_Cost_of_a_Path_With_Special_Roads.py
+++ graph/problems/2662_Minimum_Cost_of_a_Path_With_Special_Roads.py
@@ -6,3 +6,19 @@
 
 # Return the minimum cost required to go from (startX, startY) to (targetX, targetY).
 
+class Solution:
+    def minimumCost(self, start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
+
+
+        q = deque([(start[0], start[1], 0)])
+        ans = float('inf')
+        while q:
+            currX, currY, currCost = q.popleft()
+            for startX, startY, endX, endY, cost in specialRoads:
+                if cost + abs(currX - startX) + abs(currY - startY) >= abs(endX - currX) + abs(endY - currY) or abs(target[0] - currX) + abs(target[1] - currY) <= abs(target[0] - endX) + abs(target[1] - endY):
+                    continue
+                q.append([endX, endY, currCost + cost + abs(currX - startX) + abs(currY - startY)])
+            ans = min(ans, currCost + abs(target[0] - currX) + abs(target[1] - currY))
+        return ans
+
+            
