a7070d21175fe3c096a8eec9a0937a26cea31f89
Red <crabmarket56@gmail.com>
2023-07-06 17:04:58 +0900

    Add initial solution

--- dynamic_programming/problems/746_min_cost_climbing_stairs.py
+++ dynamic_programming/problems/746_min_cost_climbing_stairs.py
@@ -3,3 +3,14 @@
 # You can either start from the step with index 0, or the step with index 1.
 
 # Return the minimum cost to reach the top of the floor.
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i -1], dp[i-2]) + cost[i]
+
+        return min(dp[-1], dp[-2])
