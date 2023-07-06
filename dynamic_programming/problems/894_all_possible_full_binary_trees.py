bc83aa9601c220c823dcbe421aa78c6a99dfd3ff
Red <crabmarket56@gmail.com>
2023-07-07 07:47:11 +0900

    Add initial solution

--- dynamic_programming/problems/894_all_possible_full_binary_trees.py
+++ dynamic_programming/problems/894_all_possible_full_binary_trees.py
@@ -4,3 +4,30 @@
 
 # A full binary tree is a binary tree where each node has exactly 0 or 2 children.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if not n % 2:
+            return []
+
+        memo = {0: [], 1: [TreeNode(0)]}
+        def dfs(n):
+            res = []
+            if n not in memo:
+                for l in range(n):
+                    r = n - 1 - l
+                    leftTrees, rightTrees = dfs(l), dfs(r)
+                    for t1 in leftTrees:
+                        for t2 in rightTrees:
+                            res.append(TreeNode(0, t1, t2))
+                memo[n] = res
+            else:
+                res.extend(memo[n])
+            return res
+        
+        return dfs(n)
