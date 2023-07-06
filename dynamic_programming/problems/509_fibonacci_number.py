e723461255a3cfbd875687a15bddba1b4617b8f2
Red <crabmarket56@gmail.com>
2023-07-06 16:33:14 +0900

    Add initial solution

--- dynamic_programming/problems/509_fibonacci_number.py
+++ dynamic_programming/problems/509_fibonacci_number.py
@@ -3,3 +3,16 @@
 # F(n) = F(n - 1) + F(n - 2), for n > 1.
 # Given n, calculate F(n).
 
+class Solution:
+    def fib(self, n: int) -> int:
+
+        dp = [0] * (n + 1)
+        if n == 0:
+            return 0
+            
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[-1]
