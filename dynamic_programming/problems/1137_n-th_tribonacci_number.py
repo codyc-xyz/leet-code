ddd0d49e8d836154c287a9a09a210d5884b12527
Red <crabmarket56@gmail.com>
2023-07-06 17:43:35 +0900

    Add initial solution

--- dynamic_programming/problems/1137_n-th_tribonacci_number.py
+++ dynamic_programming/problems/1137_n-th_tribonacci_number.py
@@ -4,3 +4,16 @@
 
 # Given n, return the value of Tn.
 
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        dp[1], dp[2] = 1, 1
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+
+        return dp[-1]
