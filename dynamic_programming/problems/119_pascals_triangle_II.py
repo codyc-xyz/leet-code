eb1de0e8d0f5b29991b41da5e5655313598e92b1
Red <crabmarket56@gmail.com>
2023-07-06 17:54:19 +0900

    Add initial solution

--- dynamic_programming/problems/119_pascals_triangle_II.py
+++ dynamic_programming/problems/119_pascals_triangle_II.py
@@ -2,3 +2,14 @@
 
 # In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        rows = []
+        for i in range(rowIndex + 1):
+            rows.append([1] * (i + 1))
+
+        for i in range(2, rowIndex + 1):
+            for j in range(1, len(rows[i]) - 1):
+                rows[i][j] = rows[i - 1][j - 1] + rows[i - 1][j]
+        
+        return rows[-1]
