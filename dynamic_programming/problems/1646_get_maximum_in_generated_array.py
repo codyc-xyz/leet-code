0c143b1a404896bd2a71b0a5f7fa6d8dd0f2fc38
Red <crabmarket56@gmail.com>
2023-07-07 07:34:52 +0900

    Add initial solution

--- dynamic_programming/problems/1646_get_maximum_in_generated_array.py
+++ dynamic_programming/problems/1646_get_maximum_in_generated_array.py
@@ -6,3 +6,20 @@
 # nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
 # Return the maximum integer in the array nums​​​.
 
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+
+        dp = [0] * (n + 1)
+
+        if n == 0:
+            return 0
+        
+        dp[1] = 1
+
+        for i in range(n + 1):
+            if 2 <= 2 * i <= n:
+                dp[i * 2] = dp[i]
+            if 2 <= 2 * i + 1 <= n:
+                dp[2 * i + 1] = dp[i] + dp[i + 1]
+ 
+        return max(dp)
