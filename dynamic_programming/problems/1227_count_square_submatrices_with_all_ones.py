1e73e6529a8d733d0db379d967a69f4e50bce222
Red <crabmarket56@gmail.com>
2023-07-07 13:28:02 +0900

    Add initial solution

--- dynamic_programming/problems/1227_count_square_submatrices_with_all_ones.py
+++ dynamic_programming/problems/1227_count_square_submatrices_with_all_ones.py
@@ -1,2 +1,19 @@
 # Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
 
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        count = 0
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        for col in range(COLS):
+            count += matrix[0][col]
+
+        for row in range(1, ROWS):
+            count += matrix[row][0]
+
+        for i in range(1, ROWS):
+            for j in range(1, COLS):
+                if matrix[i][j] == 1 and matrix[i-1][j] > 0 and matrix[i][j - 1] > 0 and matrix[i - 1][j- 1] > 0:
+                    matrix[i][j] += min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1])
+                count += matrix[i][j]
+        return count
