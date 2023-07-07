6e976f089b7e7ef7bd498f403f66ff8054631379
Red <crabmarket56@gmail.com>
2023-07-07 09:09:19 +0900

    Add initial solution

--- dynamic_programming/problems/1641_count_sorted_vowel_strings.py
+++ dynamic_programming/problems/1641_count_sorted_vowel_strings.py
@@ -2,3 +2,16 @@
 
 # A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
 
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+
+        def recurse(n, k):
+            if n == 0:
+                return 1
+            total = 0
+            for i in range(k, 5):
+                total += recurse(n - 1, i)
+            return total
+
+        return recurse(n, 0)
+        
