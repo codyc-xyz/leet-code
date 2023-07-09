211a651f412f993b6b956a0d92f50b2b784547bd
Red <crabmarket56@gmail.com>
2023-07-10 06:08:12 +0900

    Add initial solution

--- dynamic_programming/problems/1043_partition_array_for_maximum_sum.py
+++ dynamic_programming/problems/1043_partition_array_for_maximum_sum.py
@@ -2,3 +2,16 @@
 
 # Return the largest sum of the given array after partitioning. Test cases are generated so that the answer fits in a 32-bit integer.
 
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        N = len(arr) 
+        dp = [0] * (N + 1)
+
+        for i in range(1, N + 1):
+            currMax = 0
+            for j in range(1, min(i, k) + 1):
+                currMax = max(currMax, arr[i - j])
+                dp[i] = max(dp[i], dp[i - j] + currMax * j)
+        return dp[N]
+                
+        
