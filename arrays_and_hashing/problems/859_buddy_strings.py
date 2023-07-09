18c23d0c4d0adf28f6a0258d73c3b5dfc11fea1b
Red <crabmarket56@gmail.com>
2023-07-10 06:43:33 +0900

    Add initial solution

--- arrays_and_hashing/problems/859_buddy_strings.py
+++ arrays_and_hashing/problems/859_buddy_strings.py
@@ -3,3 +3,18 @@
 # Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
 
 # For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        countS = collections.Counter(s)
+        if s == goal:
+            return max(countS.values()) > 1
+
+        countG = collections.Counter(goal)
+        if countS == countG:
+            diffs = 0
+            for i, j in zip(s, goal):
+                if i != j:
+                    diffs += 1
+            return diffs == 2
+        return False
