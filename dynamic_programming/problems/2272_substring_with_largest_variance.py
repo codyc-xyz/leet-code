f844c018f89d7a66dd5934819f352602dff028e5
Red <crabmarket56@gmail.com>
2023-07-10 06:26:22 +0900

    Add initial solution

--- dynamic_programming/problems/2272_substring_with_largest_variance.py
+++ dynamic_programming/problems/2272_substring_with_largest_variance.py
@@ -4,3 +4,24 @@
 
 # A substring is a contiguous sequence of characters within a string.
 
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        maxSum = 0
+        dp = [0] * 26
+        start = ord('a')
+        for i in range(26):
+            for j in range(26):
+                if i == j:
+                    continue  
+                for n in range(len(s)):
+                    seen = False
+                    currSum = 0
+                    for k in range(n, len(s)):
+                        if s[k] == chr(start + i):
+                            currSum += 1
+                        elif s[k] == chr(start + j):
+                            currSum -= 1
+                            seen = True
+                        if seen:
+                            maxSum = max(maxSum, currSum)
+        return maxSum
