0c83751eb36dbd15ccd4a16f9e458195e852b819
Red <crabmarket56@gmail.com>
2023-07-10 14:11:06 +0900

    Add initial solution

--- two-pointers/problems/9_palindrome_number.py
+++ two-pointers/problems/9_palindrome_number.py
@@ -1,1 +1,14 @@
 # Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        X = str(x)
+        l, r = 0, len(X) - 1
+
+        while l < r:
+            if X[l] != X[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
