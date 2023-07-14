3d6037fc9f000a7586d51d195386ba5da5d8544d
Red <crabmarket56@gmail.com>
2023-07-14 09:11:40 +0900

    Add initial solution

--- dynamic_programming/problems/1218_longest_arithmetic_subsequence_of_given_difference.py
+++ dynamic_programming/problems/1218_longest_arithmetic_subsequence_of_given_difference.py
@@ -1,3 +1,21 @@
 # Given an integer array arr and an integer difference, return the length of the longest subsequence in arr which is an arithmetic sequence such that the difference between adjacent elements in the subsequence equals difference.
 
 # A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+
+        if difference == 0:
+            count = collections.Counter(arr)
+            return max(count.values())
+
+        hm = defaultdict(list)
+
+        for n in arr:
+            if n not in hm:
+                hm[n] = 1
+            curr = n - difference
+            if curr in hm:
+                hm[n] = hm[curr] + 1
+
+        return max(hm.values())
