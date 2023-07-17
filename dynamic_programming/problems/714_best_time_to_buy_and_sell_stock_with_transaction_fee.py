02e910555520abcbdae2fa6c2b6d4cc4dd9b61b5
Red <crabmarket56@gmail.com>
2023-07-17 14:14:46 +0900

    Add initial solution

--- dynamic_programming/problems/714_best_time_to_buy_and_sell_stock_with_transaction_fee.py
+++ dynamic_programming/problems/714_best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -6,3 +6,38 @@
 
 # You may not engage in multiple transactions simultaneously(i.e., you must sell the stock before you buy again).
 # The transaction fee is only charged once for each stock purchase and sale.
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        N = len(prices)
+        buy_has_cache = [False] * N
+        buy_cache = [None] * N
+
+        sell_has_cache = [False] * N
+        sell_cache = [None] * N
+
+        def buy(day):
+            if day == N:
+                return 0
+            if buy_has_cache[day]:
+                return buy_cache[day]
+            bought = sell(day + 1) - prices[day] - fee
+            not_bought = buy(day + 1)
+            best = max(bought, not_bought)
+            buy_has_cache[day] = True
+            buy_cache[day] = best
+            return best
+
+        def sell(day):
+            if day == N:
+                return 0
+            if sell_has_cache[day]:
+                return sell_cache[day]
+
+            sold = buy(day + 1) + prices[day]
+            not_sold = sell(day + 1)
+            best = max(sold, not_sold)
+            sell_has_cache[day] = True
+            sell_cache[day] = best
+            return best
+        return buy(0)
