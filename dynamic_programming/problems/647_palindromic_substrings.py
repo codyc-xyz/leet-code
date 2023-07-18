08d3be3642ca93547330b3266a049fd966e85ddf
Red <crabmarket56@gmail.com>
2023-07-18 13:01:22 +0900

    Add initial solution

--- dynamic_programming/problems/647_palindromic_substrings.py
+++ dynamic_programming/problems/647_palindromic_substrings.py
@@ -3,3 +3,15 @@
 # A string is a palindrome when it reads the same backward as forward.
 
 # A substring is a contiguous sequence of characters within the string.
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        N = len(s)
+        ans = 0
+
+        for i in range(N):
+            for j in range(i, N):
+                curr = s[i:j+1]
+                if curr == curr[::-1]:
+                    ans += 1
+        return ans
