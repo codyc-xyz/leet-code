031489644b9e7a8adf383394c1fbcd9b9720ccfa
Red <crabmarket56@gmail.com>
2023-07-18 13:12:19 +0900

    Add alternative solution

--- dynamic_programming/problems/647_palindromic_substrings.py
+++ dynamic_programming/problems/647_palindromic_substrings.py
@@ -15,3 +15,23 @@ class Solution:
                 if curr == curr[::-1]:
                     ans += 1
         return ans
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        N = len(s)
+        ans = 0
+
+        for i in range(N):
+            l = r = i
+            while l >= 0 and r < N and s[l] == s[r]:
+                ans += 1
+                l -= 1
+                r += 1
+            l = i
+            r = i + 1
+            while l >= 0 and r < N and s[l] == s[r]:
+                ans += 1
+                l -= 1
+                r += 1
+        return ans
