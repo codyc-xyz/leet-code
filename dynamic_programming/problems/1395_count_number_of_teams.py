ee27502a4d63d84a934caea10afab3204eab8179
Red <crabmarket56@gmail.com>
2023-07-19 10:38:51 +0900

    Add alternative solution (Not TLE)

--- dynamic_programming/problems/1395_count_number_of_teams.py
+++ dynamic_programming/problems/1395_count_number_of_teams.py
@@ -27,3 +27,30 @@ class Solution:
             return res
 
         return backtrack(0, 0, float('inf'), True) + backtrack(0, 0, 0, False)
+
+
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        N = len(rating)
+        greaterR = [0 for _ in range(N)]
+        lessR = [0 for _ in range(N)]
+        greaterL = [0 for _ in range(N)]
+        lessL = [0 for _ in range(N)]
+
+        for i in range(N - 1):
+            for j in range(i+1, N):
+                if rating[j] > rating[i]:
+                    greaterR[i] += 1
+                else:
+                    lessR[i] += 1
+
+        for i in range(1, N):
+            for j in range(i - 1, -1, -1):
+                if rating[j] > rating[i]:
+                    greaterL[i] += 1
+                else:
+                    lessL[i] += 1
+        ans = 0
+        for i in range(N):
+            ans += lessL[i] * greaterR[i] + greaterL[i] * lessR[i]
+        return ans
