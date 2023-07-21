304e290e154444e9ef7a48973eac68e2fb119337
Red <crabmarket56@gmail.com>
2023-07-21 09:45:39 +0900

    Add initial brute force solution (TLE)

--- dynamic_programming/problems/673_number_of_longest_increasing_subsequence.py
+++ dynamic_programming/problems/673_number_of_longest_increasing_subsequence.py
@@ -1,3 +1,19 @@
 # Given an integer array nums, return the number of longest increasing subsequences.
 
 # Notice that the sequence has to be strictly increasing.
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        lenIncreasing = defaultdict(int)
+
+        def backtrack(i, prev, length):
+            if i == N:
+                lenIncreasing[length] += 1
+                return
+            if nums[i] > prev:
+                backtrack(i + 1, nums[i], length + 1)
+            backtrack(i + 1, prev, length)
+
+        backtrack(0, float('-inf'), 0)
+        return lenIncreasing[max(lenIncreasing)]
