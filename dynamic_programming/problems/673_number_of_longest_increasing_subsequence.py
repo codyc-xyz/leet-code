152782b7862354e78f6edb981bf2581abaf3865d
Red <crabmarket56@gmail.com>
2023-07-21 10:22:00 +0900

    Add dp solution (Not TLE)

--- dynamic_programming/problems/673_number_of_longest_increasing_subsequence.py
+++ dynamic_programming/problems/673_number_of_longest_increasing_subsequence.py
@@ -17,3 +17,31 @@ class Solution:
 
         backtrack(0, float('-inf'), 0)
         return lenIncreasing[max(lenIncreasing)]
+
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        lenLIS = [0] * N
+        count = [0] * N
+        lenIncreasing, res = 0, 0
+
+        for i in range(N - 1, -1, -1):
+            maxLen, maxCnt = 1, 1
+            for j in range(i + 1, N):
+                if nums[j] > nums[i]:
+                    jLen, jCount = lenLIS[j], count[j]
+                    if jLen + 1 > maxLen:
+                        maxLen, maxCnt = jLen + 1, jCount
+                    elif jLen + 1 == maxLen:
+                        maxCnt += jCount
+            lenLIS[i] = maxLen
+            count[i] = maxCnt
+
+        totMax = max(lenLIS)
+        ans = 0
+        for i in range(N):
+            if lenLIS[i] == totMax:
+                ans += count[i]
+
+        return ans
