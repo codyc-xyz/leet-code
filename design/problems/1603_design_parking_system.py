75f9e0c4675c85cf8efe5642eb561b0362b713a6
Red <crabmarket56@gmail.com>
2023-07-30 12:58:39 +0900

    Add initial solution

--- design/problems/1603_design_parking_system.py
+++ design/problems/1603_design_parking_system.py
@@ -4,3 +4,19 @@
 
 # ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class . The number of slots for each parking space are given as part of the constructor.
 # bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.hm = {1: big, 2: medium, 3: small}
+
+    def addCar(self, carType: int) -> bool:
+        if self.hm[carType] > 0:
+            self.hm[carType] -= 1
+            return True
+        return False
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
