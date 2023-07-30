7e416602c0ed4ef5c76c753ebf84b0d3f57432f0
Red <crabmarket56@gmail.com>
2023-07-30 11:23:28 +0900

    Add initial solution

--- heap/problems/2333_minimum_sum_of_squared_difference.py
+++ heap/problems/2333_minimum_sum_of_squared_difference.py
@@ -7,3 +7,52 @@
 # Return the minimum sum of squared difference after modifying array nums1 at most k1 times and modifying array nums2 at most k2 times.
 
 # Note: You are allowed to modify the array elements to become negative integers.
+
+class Solution:
+    def minSumSquareDiff(self, nums1: List[int], nums2: List[int], k1: int, k2: int) -> int:
+
+        N = len(nums1)
+        remaining = k1 + k2
+        diffs = [abs(nums1[i] - nums2[i]) for i in range(N)]
+        if remaining >= sum(diffs):
+            return 0
+        diffs.sort(reverse=True)
+        for i, d in enumerate(diffs):
+            if i < N - 1:
+                currDiff = abs(diffs[i + 1] - diffs[i])
+                if currDiff * (i + 1) <= remaining:
+                    for j in range(i + 1):
+                        if currDiff > remaining:
+                            break
+                        diffs[j] -= currDiff
+                        remaining -= currDiff
+                else:
+                    currDiff = max(remaining // (i + 1), 1)
+                    for j in range(i + 1):
+                        if not remaining:
+                            break
+                        diffs[j] -= currDiff
+                        remaining -= currDiff
+                    if remaining:
+                        for j in range(i + 1):
+                            if not remaining:
+                                break
+                            diffs[j] -= 1
+                            remaining -= 1
+            else:
+                currDiff = max(remaining // (i + 1), 1)
+                for j in range(i + 1):
+                    if not remaining or currDiff > remaining:
+                        break
+                    diffs[j] -= currDiff
+                    remaining -= currDiff
+                if remaining:
+                    for j in range(i + 1):
+                        if not remaining:
+                            break
+                        diffs[j] -= 1
+                        remaining -= 1
+
+            if not remaining:
+                break
+        return sum(d**2 for d in diffs)
