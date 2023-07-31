49ff8ddfc8d2acc3e61792493e84cb1e948d1179
Red <crabmarket56@gmail.com>
2023-07-31 10:14:57 +0900

    Add initial solution

--- arrays_and_hashing/problems/1913_maximum_product_difference_between_two_pairs.py
+++ arrays_and_hashing/problems/1913_maximum_product_difference_between_two_pairs.py
@@ -4,3 +4,23 @@
 # Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs(nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
 
 # Return the maximum such product difference.
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+
+        maxVal = scndMaxVal = float('-inf')
+        minVal = scndMinVal = float('inf')
+        for n in nums:
+            if n > maxVal:
+                scndMaxVal = maxVal
+                maxVal = n
+            elif n > scndMaxVal:
+                scndMaxVal = n
+
+            if n < minVal:
+                scndMinVal = minVal
+                minVal = n
+            elif n < scndMinVal:
+                scndMinVal = n
+
+        return (maxVal * scndMaxVal) - (minVal * scndMinVal)
