884093f7442f6e6815c72576d54a07aa6f9f25c4
Red <crabmarket56@gmail.com>
2023-07-31 11:11:55 +0900

    Add initial solution

--- math/problems/13_roman_integer.py
+++ math/problems/13_roman_integer.py
@@ -16,3 +16,22 @@
 # X can be placed before L(50) and C(100) to make 40 and 90.
 # C can be placed before D(500) and M(1000) to make 400 and 900.
 # Given a roman numeral, convert it to an integer.
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        add = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        subtract = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        i = ans = 0
+
+        while i < len(s):
+            if i < len(s) - 1:
+                posNext = s[i] + s[i+1]
+                if posNext in subtract:
+                    ans += subtract[posNext]
+                    i += 2
+                    continue
+            ans += add[s[i]]
+            i += 1
+
+        return ans
