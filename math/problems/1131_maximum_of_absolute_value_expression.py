27490982121daaf317b0aced7b1eff68204043d6
Red <crabmarket56@gmail.com>
2023-07-31 10:36:59 +0900

    Add initial solution (TLE)

--- math/problems/1131_maximum_of_absolute_value_expression.py
+++ math/problems/1131_maximum_of_absolute_value_expression.py
@@ -3,3 +3,12 @@
 # |arr1[i] - arr1[j] | + | arr2[i] - arr2[j] | + | i - j|
 
 # where the maximum is taken over all 0 <= i, j < arr1.length.
+
+class Solution:
+    def maxAbsValExpr(self, arr1: List[int], arr2: List[int]) -> int:
+        ans = float('-inf')
+        for i in range(len(arr1)):
+            for j in range(len(arr2)):
+                ans = max(ans, abs(arr1[i] - arr1[j]) +
+                          abs(arr2[i] - arr2[j] + abs(i - j)))
+        return ans
