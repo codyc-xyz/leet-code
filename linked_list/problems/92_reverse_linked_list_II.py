d9a178ff20e77cc84a1495961779ffc88c990872
Red <crabmarket56@gmail.com>
2023-09-12 07:08:45 +0800

    Add daily solution

--- linked_list/problems/92_reverse_linked_list_II.py
+++ linked_list/problems/92_reverse_linked_list_II.py
@@ -24,3 +24,43 @@ class Solution:
         prevL.next = prev
         
         return dummy.next
+    
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None, prev=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+
+        curr = head
+        prev = None
+
+        for i in range(left - 1):
+            prev = curr
+            curr = curr.next
+
+        con, tail = prev, curr
+        remainder = right - left
+
+        while remainder >= 0:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+            remainder -= 1
+
+        if con:
+            con.next = prev
+        # If 'con' is None, it means the reversed segment starts from the head. Update the head.
+        else:
+            head = prev
+
+        # 'tail' is the end of the reversed segment. Link it to 'curr', which is the node after the reversed segment
+        tail.next = curr
+
+        # Return the modified list
+        return head
