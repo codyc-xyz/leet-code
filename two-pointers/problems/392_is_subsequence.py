39bedc04cb33593bef8c4d17b540a2d758e9c3fd
Red <crabmarket56@gmail.com>
2023-09-22 12:12:30 -0400

    Add daily solution

--- two-pointers/problems/392_is_subsequence.py
+++ two-pointers/problems/392_is_subsequence.py
@@ -17,3 +17,19 @@ class Solution:
             if len(s) - a > (len(t) - i - 1):
                 break
         return False
+    
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        if len(s) > len(t):
+            return False
+
+        i = 0
+        for c in t:
+            if i == len(s):
+                return True
+            if c == s[i]:
+                i += 1
+
+        return i == len(s)
