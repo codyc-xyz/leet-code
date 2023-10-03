d83b348923e3742f5f0a7965b9dd3d78ddac67c0
Red <crabmarket56@gmail.com>
2023-10-03 15:44:49 -0400

    Add initial solution

--- arrays_and_hashing/1512_number_of_good_pairs.py
+++ arrays_and_hashing/1512_number_of_good_pairs.py
@@ -1,3 +1,13 @@
 # Given an array of integers nums, return the number of good pairs.
 
 # A pair(i, j) is called good if nums[i] == nums[j] and i < j.
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == nums[i]:
+                    ans += 1
+
+        return ans
