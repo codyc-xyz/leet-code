a2add56e3b2e48ef10aed080d4bdd4b408e04c10
Red <crabmarket56@gmail.com>
2023-10-05 16:54:11 -0400

    Add initial solution

--- arrays_and_hashing/problems/229_majority_element.py
+++ arrays_and_hashing/problems/229_majority_element.py
@@ -1,2 +1,16 @@
 # Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        lenNums = len(nums)
+        target = lenNums / 3
+        count = collections.Counter(nums)
+
+        ans = []
+
+        for c in count:
+            if count[c] > target:
+                ans.append(c)
+
+        return ans
