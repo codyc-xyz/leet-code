b7971750bef2edb8d193c35797e47f52eaf56c8d
Red <crabmarket56@gmail.com>
2023-10-15 13:54:32 -0400

    Add initial solution (Daily LC)

--- dynamic_programming/problems/1269_number_of_ways_to_stay_in_the_same_place_after_some_steps.py
+++ dynamic_programming/problems/1269_number_of_ways_to_stay_in_the_same_place_after_some_steps.py
@@ -2,3 +2,29 @@
 
 # Given two integers steps and arrLen, return the number of ways such that your pointer is still at index 0 after exactly steps steps. Since the answer may be too large, return it modulo 109 + 7.
 
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+
+        mod = 10**9+7
+
+        dp = defaultdict(int)
+        dp[0, 0] = 1
+
+        def dfs(pos, steps):
+            if dp[pos, steps]:
+                return dp[pos, steps]
+            if steps == 0 or steps < pos:
+                dp[pos, steps] = 0
+                return 0
+            waysToZero = 0
+
+            waysToZero += dfs(pos, steps - 1)
+            if pos < arrLen - 1:
+                waysToZero += dfs(pos + 1, steps - 1)
+            if pos > 0:
+                waysToZero += dfs(pos - 1, steps - 1)
+            dp[pos, steps] = waysToZero
+
+            return waysToZero
+        ans = dfs(0, steps) % mod
+        return ans
