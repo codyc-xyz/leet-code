b7ede8f83d523b0e2c115dff36c7ac992b613a01
Red <crabmarket56@gmail.com>
2023-10-17 17:14:46 -0400

    Add initial solution

--- dynamic_programming/problems/343_integer_break.py
+++ dynamic_programming/problems/343_integer_break.py
@@ -1,3 +1,12 @@
 # Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
 
 # Return the maximum product you can get.
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3:
+            return n - 1
+        nums = [0, 1, 2, 3]
+        for i in range(4, n+1):
+            nums.append(max(nums[i - 3] * 3, nums[i - 2] * 2))
+        return nums[-1]
