7f8541b532b40744c3bfe50c66662371bf5b1da2
Red <crabmarket56@gmail.com>
2023-10-24 10:45:19 -0400

    Add daily LC solution

--- trees/problems/515_find_largest_value_in_each_row.py
+++ trees/problems/515_find_largest_value_in_each_row.py
@@ -24,3 +24,24 @@ class Solution:
                 maxVal = max(maxVal, node.val)
             ans.append(maxVal)
         return ans
+    
+
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+
+        dq = deque()
+        if root:
+            dq.append(root)
+        ans = []
+        while dq:
+            lenDq = len(dq)
+            currMax = float('-inf')
+            for _ in range(lenDq):
+                node = dq.popleft()
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+                currMax = max(currMax, node.val)
+            ans.append(currMax)
+        return ans
