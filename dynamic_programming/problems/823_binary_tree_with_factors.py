3d82fb1cb34fb23d74d952a9db9133fd9725339f
Red <crabmarket56@gmail.com>
2023-10-26 06:15:09 -0400

    Add initial solution

--- dynamic_programming/problems/823_binary_tree_with_factors.py
+++ dynamic_programming/problems/823_binary_tree_with_factors.py
@@ -4,3 +4,27 @@
 
 # Return the number of binary trees we can make. The answer may be too large so return the answer modulo 109 + 7.
 
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        MOD = 10**9+7
+        arr.sort()
+        nums = set(n for n in arr)
+        numTrees = {x: 1 for x in arr}
+
+        def buildTree(n):
+            for c in arr:
+                if c > n**0.5:
+                    break
+                curr = n // c
+                if n % c == 0 and curr in nums:
+                    if curr == c:
+                        numTrees[n] += numTrees[c] * numTrees[c]
+                    else:
+                        numTrees[n] += numTrees[c] * numTrees[curr] * 2
+                    numTrees[n] %= MOD
+            return numTrees[n]
+        ans = 0
+        for n in arr:
+            ans += buildTree(n)
+
+        return ans % MOD
