59fa274d1d9d8b23e4bc77e3bb3c2bf5f6a557ed
Red <crabmarket56@gmail.com>
2024-01-13 16:16:24 -0500

    add alternative solution

--- arrays_and_hashing/problems/1_two_sum.py
+++ arrays_and_hashing/problems/1_two_sum.py
@@ -14,3 +14,21 @@ class Solution:
                 return [hm[t], i]
             else:
                 hm[n] = i
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, n in enumerate(nums):
+            nums[i] = [n, i]
+
+        nums.sort()
+        l, r = 0, len(nums) - 1
+
+        while nums[l][0] + nums[r][0] != target:
+            curr = nums[l][0] + nums[r][0]
+            if curr > target:
+                r -= 1
+            else:
+                l += 1
+
+        return [nums[l][1], nums[r][1]]
