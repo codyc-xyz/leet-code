ac86b21b91b25cabdc053dceabc720747ef92667
Red <crabmarket56@gmail.com>
2024-01-13 16:20:05 -0500

    Add initial solution

--- two-pointers/problems/1704_determine_if_string_halves_are_alike.py
+++ two-pointers/problems/1704_determine_if_string_halves_are_alike.py
@@ -3,3 +3,19 @@
 # Two strings are alike if they have the same number of vowels('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
 
 # Return true if a and b are alike. Otherwise, return false.
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+
+        l, r = 0, len(s) // 2
+        lVowels = rVowels = 0
+        while r < len(s):
+            if s[l] in vowels:
+                lVowels += 1
+            if s[r] in vowels:
+                rVowels += 1
+            l += 1
+            r += 1
+
+        return lVowels == rVowels
