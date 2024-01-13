4243f7f368462e33218676f2c54fdb9ccc1d1ed7
Red <crabmarket56@gmail.com>
2024-01-13 16:18:32 -0500

    Add alternative solution

--- trees/problems/2385_amount_of_time_for_binary_tree_to_be_infected.py
+++ trees/problems/2385_amount_of_time_for_binary_tree_to_be_infected.py
@@ -47,3 +47,42 @@ class Solution:
             
         dfs(nodes[start], 0)
         return self.count 
+    
+
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        parent = {}
+        nodes = {}
+        seen = set()
+
+        def dfs(root, prev):
+            if not root:
+                return
+            parent[root] = prev
+            nodes[root.val] = root
+            seen.add(root)
+            dfs(root.left, root)
+            dfs(root.right, root)
+        dfs(root, None)
+
+        visited = set()
+        self.count = 0
+        self.maxDepth = 0
+        self.flag = False
+
+        def dfs(curr, depth):
+            if not curr or self.flag == True:
+                return
+            self.maxDepth = max(depth, self.maxDepth)
+            visited.add(curr)
+            if len(visited) == len(seen):
+                self.count = self.maxDepth
+                self.flag = True
+                return
+
+            for n in (curr.left, curr.right, parent[curr]):
+                if n and n not in visited:
+                    dfs(n, depth + 1)
+
+        dfs(nodes[start], 0)
+        return self.count
