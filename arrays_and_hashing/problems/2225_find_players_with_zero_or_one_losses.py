93f8a789f78d0a04ba6d8b1aef2fb5e7a2e6cf77
Red <crabmarket56@gmail.com>
2024-01-14 20:50:41 -0500

    Add initial solution

--- arrays_and_hashing/problems/2225_find_players_with_zero_or_one_losses.py
+++ arrays_and_hashing/problems/2225_find_players_with_zero_or_one_losses.py
@@ -10,3 +10,25 @@
 
 # You should only consider the players that have played at least one match.
 # The testcases will be generated such that no two matches will have the same outcome.
+
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        losses = {}
+        seen = set()
+
+        for w, l in matches:
+            if l in losses:
+                losses[l] += 1
+            else:
+                losses[l] = 1
+            seen.add(w)
+            seen.add(l)
+
+        ans = [[], []]
+        for p in seen:
+            if p not in losses:
+                ans[0].append(p)
+            elif losses[p] == 1:
+                ans[1].append(p)
+
+        return [sorted(ans[0]), sorted(ans[1])]
