436a7d0b6ed0a08ddf5250e960f65482ab9de75b
Red <crabmarket56@gmail.com>
2024-01-16 10:35:54 -0500

    Add alternate solution (Daily LC)

--- arrays_and_hashing/problems/380_insert_delete_getrandom_O1.py
+++ arrays_and_hashing/problems/380_insert_delete_getrandom_O1.py
@@ -32,3 +32,27 @@ class RandomizedSet:
         j = len(self.set)
         i = random.randrange(0, j)
         return list(self.set)[i]
+    
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.randomSet = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.randomSet:
+            return False
+        else:
+            self.randomSet.add(val)
+            return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.randomSet:
+            self.randomSet.remove(val)
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        listSet = list(self.randomSet)
+        return random.choice(listSet)
