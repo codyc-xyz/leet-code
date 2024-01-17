8017f6dffec947371db7d0ee5977f0b467f99788
Red <crabmarket56@gmail.com>
2024-01-17 10:48:41 -0500

    Add initial solution (Daily LC)

--- arrays_and_hashing/problems/1207_unique_number_of_occurences.py
+++ arrays_and_hashing/problems/1207_unique_number_of_occurences.py
@@ -1,3 +1,11 @@
 # Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
 
-
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        count = collections.Counter(arr)
+        seen = set()
+        for c in count:
+            if count[c] in seen:
+                return False
+            seen.add(count[c])
+        return True
