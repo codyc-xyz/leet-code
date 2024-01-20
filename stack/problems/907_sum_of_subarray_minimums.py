4f330317ee4dc3a3d506f3f3ea3655373380b883
Red <crabmarket56@gmail.com>
2024-01-20 13:46:23 -0500

    Fix daily LC

--- stack/problems/907_sum_of_subarray_minimums.py
+++ stack/problems/907_sum_of_subarray_minimums.py
@@ -48,3 +48,25 @@ class Solution:
             if i < len(arr):
                 stack.append([i, arr[i]])
         return arrSums%mod
+    
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        ans = 0
+        N = len(arr)
+        MOD = 10**9+7
+        stack = []
+        for i in range(len(arr)):
+            while stack and stack[-1][0] > arr[i]:
+                val, idx = stack.pop()
+                left = idx - (stack[-1][1] if stack else -1)
+                right = i - idx
+                ans += val * left * right
+            stack.append([arr[i], i])
+
+        while stack:
+            val, idx = stack.pop()
+            left = idx - (stack[-1][1] if stack else -1)
+            right = N - idx
+            ans += val * left * right
+        return ans % MOD
