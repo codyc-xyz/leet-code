72102282626e4869e3164a9c6ec1f5adc2e16977
Red <crabmarket56@gmail.com>
2024-01-23 16:25:48 -0500

    Add init solution

--- database/problems/627_swap_salary.sql
+++ database/problems/627_swap_salary.sql
@@ -17,3 +17,4 @@ Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 
 
 Note that you must write a single update statement, do not write any select statement for this problem.
 
+UPDATE Salary SET sex = IF(sex='f', 'm','f');
