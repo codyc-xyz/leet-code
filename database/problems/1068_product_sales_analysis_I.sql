3fa205cb12262173403382147ba4a1400b7318fd
Red <crabmarket56@gmail.com>
2024-01-24 12:20:58 -0500

    Add initial solution

--- database/problems/1068_product_sales_analysis_I.sql
+++ database/problems/1068_product_sales_analysis_I.sql
@@ -31,3 +31,4 @@ Write a solution to report the product_name, year, and price for each sale_id in
 
 Return the resulting table in any order.
 
+SELECT product_name, year, price FROM Sales LEFT JOIN Product ON Sales.product_id = Product.product_id;
