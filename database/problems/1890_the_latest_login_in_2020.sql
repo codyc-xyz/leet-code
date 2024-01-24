26f0b2b41b45639d3b695c1dfe06f9689a7bcd31
Red <crabmarket56@gmail.com>
2024-01-24 12:38:11 -0500

    Add initial solution

--- database/problems/1890_the_latest_login_in_2020.sql
+++ database/problems/1890_the_latest_login_in_2020.sql
@@ -14,3 +14,4 @@ Write a solution to report the latest login for all users in the year 2020. Do n
 
 Return the result table in any order.
 
+SELECT user_id, MAX(time_stamp) AS last_stamp FROM Logins WHERE time_stamp >= '2020-01-01 00:00:00' AND time_stamp <= '2020-12-31 23:59:59' GROUP BY user_id;
