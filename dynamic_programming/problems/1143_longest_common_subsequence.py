9dbafd8270c09bd696f81893ce93c707ee7a9e3c
Red <crabmarket56@gmail.com>
2024-01-25 10:59:50 -0500

    Add initial solution

--- dynamic_programming/problems/1143_longest_common_subsequence.py
+++ dynamic_programming/problems/1143_longest_common_subsequence.py
@@ -5,3 +5,17 @@
 # For example, "ace" is a subsequence of "abcde".
 # A common subsequence of two strings is a subsequence that is common to both strings.
 
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        N = len(text1)
+        M = len(text2)
+        dp = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
+
+        for i in range(1, N + 1):
+            for j in range(1, M + 1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[N][M]
