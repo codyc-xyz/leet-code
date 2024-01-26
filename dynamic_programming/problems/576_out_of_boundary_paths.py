ed6146d32ce585629c51559e78bbe880a5f4fabc
Red <crabmarket56@gmail.com>
2024-01-26 16:49:22 -0500

    Add daily LC (Not TLE)

--- dynamic_programming/problems/576_out_of_boundary_paths.py
+++ dynamic_programming/problems/576_out_of_boundary_paths.py
@@ -21,3 +21,25 @@ class Solution:
         self.seen = set()
         recurse(startRow, startColumn, maxMove)
         return self.ans % MOD
+
+class Solution:
+    def findPaths(self, M: int, N: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        MOD = 10**9 + 7
+
+        def recurse(m, n, moves):
+            if moves < 0:
+                return 0
+            if (m,n,moves) in self.seen and self.seen[(m,n,moves)] >= 0:
+                return self.seen[(m,n,moves)]
+            if m < 0 or m == M or n < 0 or n == N:
+                return 1
+            mL = recurse(m + 1, n, moves - 1)
+            mR = recurse(m - 1, n, moves - 1)
+            nL = recurse(m, n + 1, moves - 1)
+            nR = recurse(m, n - 1, moves - 1)
+            self.seen[(m,n,moves)] = mL + mR + nL + nR
+            return self.seen[(m,n,moves)]
+
+        self.seen = {}
+        recurse(startRow, startColumn, maxMove)
+        return (self.seen[startRow,startColumn,maxMove]) % MOD
