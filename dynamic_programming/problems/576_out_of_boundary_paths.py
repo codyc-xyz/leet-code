2326a59efd0ef88f1c451cd8fda0c890f1176a3b
Red <crabmarket56@gmail.com>
2024-01-26 16:37:01 -0500

    Add initial solution (TLE)

--- dynamic_programming/problems/576_out_of_boundary_paths.py
+++ dynamic_programming/problems/576_out_of_boundary_paths.py
@@ -2,3 +2,22 @@
 
 # Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
 
+class Solution:
+    def findPaths(self, M: int, N: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        MOD = 10**9 + 7
+        self.ans = 0
+
+        def recurse(m, n, moves):
+            if moves < 0:
+                return
+            if m < 0 or m == M or n < 0 or n == N:
+                self.ans += 1
+                return
+            recurse(m + 1, n, moves - 1)
+            recurse(m - 1, n, moves - 1)
+            recurse(m, n + 1, moves - 1)
+            recurse(m, n - 1, moves - 1)
+
+        self.seen = set()
+        recurse(startRow, startColumn, maxMove)
+        return self.ans % MOD
