0785661c2a94a14ce07220c0919d966ffa5bdddd
Red <crabmarket56@gmail.com>
2024-01-27 11:21:41 -0500

    Add init solution

--- database/problems/183_customers_who_never_order.sql
+++ database/problems/183_customers_who_never_order.sql
@@ -27,3 +27,8 @@ Write a solution to find all customers who never order anything.
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT name AS Customers
+FROM Customers 
+WHERE id NOT IN (SELECT customerID FROM Orders); 
