37d68f7a0b3ad4b1278e0e0813f74d344e07503c
Red <crabmarket56@gmail.com>
2024-01-26 21:07:27 -0500

    Add init solution

--- database/problems/1148_article_views_I.sql
+++ database/problems/1148_article_views_I.sql
@@ -16,3 +16,7 @@ Note that equal author_id and viewer_id indicate the same person.
 Write a solution to find all the authors that viewed at least one of their own articles.
 
 Return the result table sorted by id in ascending order
+
+# Write your MySQL query statement below
+
+SELECT DISTINCT(author_id) AS id FROM Views WHERE author_id = viewer_id ORDER BY id ASC;
