c41fcd55ed8443b2002a0f4bfe6986f7fb2f8e6c
Red <crabmarket56@gmail.com>
2024-01-27 11:08:40 -0500

    Add init solution

--- database/problems/584_find_customer_referee.sql
+++ database/problems/584_find_customer_referee.sql
@@ -15,3 +15,6 @@ Find the names of the customer that are not referred by the customer with id = 2
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT name FROM customer WHERE referee_id != 2 OR referee_id IS NULL;
