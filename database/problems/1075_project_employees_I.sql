6e4b4548fe73e294fc5022686cd7fe4139c40089
Red <crabmarket56@gmail.com>
2024-01-27 16:40:55 -0500

    Add init solution

--- database/problems/1075_project_employees_I.sql
+++ database/problems/1075_project_employees_I.sql
@@ -20,9 +20,17 @@ Table: Employee
 | name             | varchar |
 | experience_years | int     |
 +------------------+---------+
-employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+employee_id is the primary key of this table. It''s guaranteed that experience_years is not NULL.
 Each row of this table contains information about one employee.
  
 
 Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
 
+# Write your MySQL query statement below
+
+SELECT project_id,
+ROUND(SUM(experience_years) / COUNT(project_id), 2) AS average_years
+FROM Project
+LEFT JOIN Employee ON Project.employee_id = Employee.employee_id
+WHERE Project.employee_id = Employee.employee_id AND Employee.experience_years IS NOT NULL
+GROUP BY project_id;
