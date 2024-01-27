a2d3871431a764d94adcb65ed4bbcee6fe978a48
Red <crabmarket56@gmail.com>
2024-01-26 21:15:37 -0500

    Add initial solution

--- database/problems/577_employee_bonus.sql
+++ database/problems/577_employee_bonus.sql
@@ -29,3 +29,6 @@ Write a solution to report the name and bonus amount of each employee with a bon
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT name, bonus FROM Employee LEFT JOIN Bonus ON Employee.empId = Bonus.empId WHERE Bonus.bonus < 1000 OR Bonus.bonus IS NULL;
