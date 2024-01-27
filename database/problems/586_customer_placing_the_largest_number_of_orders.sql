dffbf85e07f273e27535b462edb5417a754e88c5
Red <crabmarket56@gmail.com>
2024-01-27 16:10:27 -0500

    Add init solution

--- database/problems/586_customer_placing_the_largest_number_of_orders.sql
+++ database/problems/586_customer_placing_the_largest_number_of_orders.sql
@@ -14,3 +14,9 @@ Write a solution to find the customer_number for the customer who has placed the
 
 The test cases are generated so that exactly one customer will have placed more orders than any other customer.
 
+# Write your MySQL query statement below
+
+SELECT customer_number FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1;
