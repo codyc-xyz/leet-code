3e8d476d12140daa2d6bba1bcf1d88571ca833e0
Red <crabmarket56@gmail.com>
2024-01-26 21:03:18 -0500

    Add init solution

--- database/problems/511_game_play_analysis_I.sql
+++ database/problems/511_game_play_analysis_I.sql
@@ -17,3 +17,6 @@ Write a solution to find the first login date for each player.
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id;
