07a7cbc8f01ca06d4f3ae3490ae60fe273cd3303
Red <crabmarket56@gmail.com>
2024-01-27 16:22:48 -0500

    Add init solution

--- database/problems/1789_primary_department_for_each_employee.sql
+++ database/problems/1789_primary_department_for_each_employee.sql
@@ -19,3 +19,14 @@ Write a solution to report all the employees with their primary department. For 
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT employee_id, department_id 
+FROM Employee
+WHERE primary_flag = "Y" OR
+employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id 
+    HAVING COUNT(employee_id) < 2
+    );
