f7b5ae8550f88cb04e5fa3cd8603521917c779c6
Red <crabmarket56@gmail.com>
2024-01-26 21:40:30 -0500

    Add init solution

--- database/problems/610_triangle_judgement.sql
+++ database/problems/610_triangle_judgement.sql
@@ -15,3 +15,10 @@ Report for every three line segments whether they can form a triangle.
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+SELECT x, y, z,
+CASE 
+WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes' 
+ELSE 'No' 
+END AS triangle 
+FROM Triangle;
