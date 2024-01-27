dcf45416c13f146d43a840e1196b10e054073188
Red <crabmarket56@gmail.com>
2024-01-26 21:28:54 -0500

    Add init solution

--- database/problems/1965_employees_with_missing_information.sql
+++ database/problems/1965_employees_with_missing_information.sql
@@ -28,3 +28,9 @@ The employee's name is missing, or
 The employee's salary is missing.
 Return the result table ordered by employee_id in ascending order.
 
+# Write your MySQL query statement below
+
+SELECT Employees.employee_id FROM Employees LEFT JOIN Salaries ON Employees.employee_id = Salaries.employee_id WHERE Salaries.salary IS NULL
+UNION
+SELECT Salaries.employee_id FROM Employees RIGHT JOIN Salaries ON Employees.employee_id = Salaries.employee_id WHERE Employees.name IS NULL
+ORDER BY employee_id;
