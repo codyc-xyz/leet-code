c6beaeed8a660089b629809f1cd4a76a4e1b1f47
Red <crabmarket56@gmail.com>
2024-01-28 17:35:26 -0500

    Add init solution

--- database/problems/608_tree_node.sql
+++ database/problems/608_tree_node.sql
@@ -20,3 +20,14 @@ Write a solution to report the type of each node in the tree.
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT id, 
+    CASE 
+        WHEN p_id IS NULL THEN "Root"
+        WHEN id NOT IN (
+            SELECT p_id FROM Tree WHERE p_id IS NOT NULL
+        ) THEN "Leaf"
+        ELSE "Inner"
+    END AS type
+FROM Tree;
