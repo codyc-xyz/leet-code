104ee1edf230d1b71720d3e9d2834ac25e84e432
Red <crabmarket56@gmail.com>
2024-01-28 13:21:32 -0500

    Add init solution

--- database/problems/1141_user_activity_for_the_past_30_days_I.sql
+++ database/problems/1141_user_activity_for_the_past_30_days_I.sql
@@ -18,3 +18,9 @@ Write a solution to find the daily active user count for a period of 30 days end
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-26'
+GROUP BY activity_date;
