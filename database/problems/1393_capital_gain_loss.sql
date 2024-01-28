45dd33bc0eb7b28327732cb958e067940e943a1b
Red <crabmarket56@gmail.com>
2024-01-28 16:18:30 -0500

    Add init solution

--- database/problems/1393_capital_gain_loss.sql
+++ database/problems/1393_capital_gain_loss.sql
@@ -20,3 +20,17 @@ The Capital gain/loss of a stock is the total gain or loss after buying and sell
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT 
+    stock_name, 
+    SUM(
+        CASE 
+            WHEN operation = 'Sell' THEN price 
+            ELSE -price 
+        END
+    ) AS capital_gain_loss
+FROM 
+    Stocks
+GROUP BY 
+    stock_name;
