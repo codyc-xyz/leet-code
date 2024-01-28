6235e6553c9b0d21301c457691d50a6f3ea906b3
Red <crabmarket56@gmail.com>
2024-01-28 17:44:37 -0500

    Add init solution

--- database/problems/626_exchange_seats.sql
+++ database/problems/626_exchange_seats.sql
@@ -15,3 +15,15 @@ Write a solution to swap the seat id of every two consecutive students. If the n
 
 Return the result table ordered by id in ascending order.
 
+# Write your MySQL query statement below
+
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id != (
+            SELECT COUNT(*) FROM Seat) 
+            THEN id + 1 
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+        END AS id, student
+FROM Seat
+ORDER BY id;
