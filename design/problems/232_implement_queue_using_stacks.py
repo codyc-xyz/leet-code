2d730a1704c353613891933c5efd36eea8dfb062
Red <crabmarket56@gmail.com>
2024-01-28 20:32:37 -0500

    Add init solution (LC Daily)

--- design/problems/232_implement_queue_using_stacks.py
+++ design/problems/232_implement_queue_using_stacks.py
@@ -10,3 +10,39 @@
 
 # You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
 # Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+        
+
+    def push(self, x: int) -> None:
+        while self.s1:
+            self.s2.append(self.s1.pop())
+
+        self.s1.append(x)
+        
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
+    def pop(self) -> int:
+        return self.s1.pop()
+        
+        
+    def peek(self) -> int:
+        return self.s1[-1]
+        
+
+    def empty(self) -> bool:
+        return not self.s1
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
