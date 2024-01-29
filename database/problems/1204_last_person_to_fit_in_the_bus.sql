8f8b805ae98f49852d0c145bca30596f23196e14
Red <crabmarket56@gmail.com>
2024-01-28 20:22:40 -0500

    Add init solution

--- database/problems/1204_last_person_to_fit_in_the_bus.sql
+++ database/problems/1204_last_person_to_fit_in_the_bus.sql
@@ -19,3 +19,11 @@ There is a queue of people waiting to board a bus. However, the bus has a weight
 
 Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
 
+# Write your MySQL query statement below
+
+SELECT person_name FROM (
+    SELECT person_name, SUM(weight) OVER (ORDER BY turn) AS total_weight FROM Queue
+) AS sub
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
