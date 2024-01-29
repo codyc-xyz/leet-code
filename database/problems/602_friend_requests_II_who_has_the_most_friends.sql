8d5b09ae1505f90c12a7bf73c40001753942593d
Red <crabmarket56@gmail.com>
2024-01-29 18:45:51 -0500

    Add init solution

--- database/problems/602_friend_requests_II_who_has_the_most_friends.sql
+++ database/problems/602_friend_requests_II_who_has_the_most_friends.sql
@@ -15,3 +15,32 @@ Write a solution to find the people who have the most friends and the most frien
 
 The test cases are generated so that only one person has the most friends.
 
+# Write your MySQL query statement below
+
+WITH MaxRequests AS (
+    SELECT requester_id, COUNT(requester_id) AS num_requests
+    FROM RequestAccepted
+    GROUP BY requester_id
+),
+
+MaxAccepts AS (
+    SELECT accepter_id, COUNT(accepter_id) AS num_accepts
+    FROM RequestAccepted
+    GROUP BY accepter_id
+)
+
+SELECT COALESCE(MaxRequests.requester_id, MaxAccepts.accepter_id) AS id, 
+(COALESCE(num_requests, 0) + COALESCE(num_accepts, 0)) AS num
+FROM MaxRequests
+LEFT JOIN MaxAccepts ON MaxRequests.requester_id = MaxAccepts.accepter_id
+
+UNION
+
+SELECT COALESCE(MaxRequests.requester_id, MaxAccepts.accepter_id) AS id, 
+(COALESCE(num_requests, 0) + COALESCE(num_accepts, 0)) AS num
+FROM MaxRequests
+RIGHT JOIN MaxAccepts ON MaxRequests.requester_id = MaxAccepts.accepter_id
+
+GROUP BY MaxRequests.requester_id, MaxAccepts.accepter_id
+ORDER BY num DESC
+LIMIT 1;
