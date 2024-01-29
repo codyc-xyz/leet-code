135e251c01adb0bbc8b00de4f131dc632377b100
Red <crabmarket56@gmail.com>
2024-01-29 09:22:55 -0500

    Add init solution

--- database/problems/178_rank_scores.sql
+++ database/problems/178_rank_scores.sql
@@ -17,3 +17,7 @@ If there is a tie between two scores, both should have the same ranking.
 After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
 Return the result table ordered by score in descending order.
 
+# Write your MySQL query statement below
+
+SELECT score, DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank'
+FROM Scores;
