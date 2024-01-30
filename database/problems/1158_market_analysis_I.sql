7ff5468082a54e9dc935ad60b484ca4af041abc7
Red <crabmarket56@gmail.com>
2024-01-30 10:30:12 -0500

    Add init solution

--- database/problems/1158_market_analysis_I.sql
+++ database/problems/1158_market_analysis_I.sql
@@ -42,3 +42,20 @@ Write a solution to find for each user, the join date and the number of orders t
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+WITH UserDetails AS (
+    SELECT user_id, join_date
+    FROM Users
+),
+
+UserOrders AS (
+    SELECT order_date, buyer_id, COUNT(buyer_id) AS raw_orders_in_2019 
+    FROM Orders
+    WHERE SUBSTR(order_date, 1, 4) = "2019"
+    GROUP BY buyer_id
+)
+
+SELECT user_id AS buyer_id, join_date, COALESCE(raw_orders_in_2019, 0) AS orders_in_2019
+FROM UserDetails
+LEFT JOIN UserOrders ON UserDetails.user_id = UserOrders.buyer_id;
