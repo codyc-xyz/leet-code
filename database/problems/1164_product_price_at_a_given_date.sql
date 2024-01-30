7bfed078703a0985b53093fe6a13a50f057af480
Red <crabmarket56@gmail.com>
2024-01-30 11:32:59 -0500

    Add init solution

--- database/problems/1164_product_price_at_a_given_date.sql
+++ database/problems/1164_product_price_at_a_given_date.sql
@@ -15,3 +15,21 @@ Write a solution to find the prices of all products on 2019-08-16. Assume the pr
 
 Return the result table in any order.
 
+WITH MaxDate AS (
+    SELECT product_id, MAX(change_date) AS max_change_date
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+
+SELECT Products.product_id, 
+CASE 
+    WHEN Products.product_id NOT IN (SELECT MaxDate.product_id FROM MaxDate)
+    THEN 10
+    ELSE Products.new_price
+    END AS price
+FROM Products
+LEFT JOIN MaxDate ON Products.product_id = MaxDate.product_id 
+WHERE Products.change_date = MaxDate.max_change_date OR MaxDate.product_id IS NULL
+GROUP BY product_id
+ORDER BY product_id, new_price;
