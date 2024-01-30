aebd61088538731e1183003e2c41216e263e52a0
Red <crabmarket56@gmail.com>
2024-01-30 16:48:21 -0500

    Add init solution

--- database/problems/1174_immediate_food_delivery_II.sql
+++ database/problems/1174_immediate_food_delivery_II.sql
@@ -19,3 +19,28 @@ The first order of a customer is the order with the earliest order date that the
 
 Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
 
+# Write your MySQL query statement below
+
+WITH FirstOrder AS (
+    SELECT customer_id, MIN(order_date) AS first_order FROM Delivery
+    GROUP BY customer_id
+),
+
+CountImmediate AS (
+    SELECT FirstOrder.customer_id, first_order, customer_pref_delivery_date, 
+    SUM(
+        CASE WHEN first_order = customer_pref_delivery_date THEN 1 ELSE 0 END
+    )  AS num_immediate
+    FROM Delivery 
+    RIGHT JOIN FirstOrder
+    ON Delivery.customer_id = FirstOrder.customer_id AND order_date = first_order
+),
+
+NumCustomers AS (
+    SELECT COUNT(DISTINCT customer_id) AS num_customers FROM Delivery
+)
+
+SELECT ROUND(
+    (SELECT num_immediate FROM CountImmediate) /
+    (SELECT num_customers FROM NumCustomers) * 100, 2
+) AS immediate_percentage;
