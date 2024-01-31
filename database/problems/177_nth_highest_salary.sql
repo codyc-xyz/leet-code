c255438f166f4804bf4e585a9661a0ddf7c4f9ac
Red <crabmarket56@gmail.com>
2024-01-31 14:21:44 -0500

    Init problem

---
+++ database/problems/177_nth_highest_salary.sql
@@ -0,0 +1,14 @@
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the nth highest salary from the Employee table. If there is no nth highest salary, return null.
+
