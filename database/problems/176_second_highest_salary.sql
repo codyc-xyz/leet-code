c8e50e43fd6202f9fadc01dfc3a8cdf8f1926c46
Red <crabmarket56@gmail.com>
2024-01-31 14:12:09 -0500

    Add init solution

--- database/problems/176_second_highest_salary.sql
+++ database/problems/176_second_highest_salary.sql
@@ -12,3 +12,16 @@ Each row of this table contains information about the salary of an employee.
 
 Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
 
+# Write your MySQL query statement below
+
+WITH MaxSalary AS (
+    SELECT MAX(salary) AS max_salary FROM Employee
+)
+
+SELECT 
+    (CASE WHEN COUNT(salary) > 0 THEN MAX(salary) ELSE NULL END) AS SecondHighestSalary 
+FROM Employee
+LEFT JOIN MaxSalary ON Employee.salary != max_salary
+WHERE salary < max_salary
+ORDER BY salary DESC
+LIMIT 1;
