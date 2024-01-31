04236fd30596d8517a7b732322b08e6ad8c3f034
Red <crabmarket56@gmail.com>
2024-01-30 21:29:20 -0500

    Add init solution

--- database/problems/180_consecutive_numbers.sql
+++ database/problems/180_consecutive_numbers.sql
@@ -14,3 +14,11 @@ Find all numbers that appear at least three times consecutively.
 
 Return the result table in any order.
 
+# Write your MySQL query statement below
+
+SELECT L1.num AS ConsecutiveNums
+FROM Logs L1
+LEFT JOIN Logs L2 ON L2.id = L1.id + 1 AND L1.num = L2.num
+LEFT JOIN Logs L3 ON L3.id = L2.id + 1 AND L2.num = L3.num
+WHERE L1.num = L3.num
+GROUP BY ConsecutiveNums;
