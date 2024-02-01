b7e5b920341aff6531876308c7231a44441c5967
Red <crabmarket56@gmail.com>
2024-02-01 12:52:09 -0500

    Add init solution

--- simulation/problems/1806_minimum_number_of_operations_to_reinitialize_a_permutation.py
+++ simulation/problems/1806_minimum_number_of_operations_to_reinitialize_a_permutation.py
@@ -8,3 +8,26 @@
 
 # Return the minimum non-zero number of operations you need to perform on perm to return the permutation to its initial value.
 
+class Solution:
+    def reinitializePermutation(self, n: int) -> int:
+        
+        perm = [i for i in range(n)]
+        arr = []
+        for i, p in enumerate(perm):
+            if not p % 2:
+                arr.append(perm[i//2])
+            else:
+                arr.append(perm[n//2 + (i-1) // 2])
+        
+        ans = 1
+        while arr != perm:
+            prev_arr = [0] * n
+            for i in range(n):
+                if not i % 2:
+                    prev_arr[i] = arr[i//2]
+                else:
+                    prev_arr[i] = arr[n//2 + (i-1) // 2]
+            ans += 1
+            arr = prev_arr
+
+        return ans
