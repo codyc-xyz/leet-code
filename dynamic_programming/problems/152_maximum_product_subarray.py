eb93610bc44109172badbffba15ed2767e79e0d6
Red <crabmarket56@gmail.com>
2024-02-01 13:16:18 -0500

    Add init solution (TLE)

--- dynamic_programming/problems/152_maximum_product_subarray.py
+++ dynamic_programming/problems/152_maximum_product_subarray.py
@@ -1,3 +1,16 @@
 # Given an integer array nums, find a subarray that has the largest product, and return the product.
 # The test cases are generated so that the answer will fit in a 32-bit integer.
 
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans = float('-inf')
+        dp = []
+        i = 0
+        for i, n1 in enumerate(nums):
+            dp.append([n1])
+            ans = max(ans, dp[i][-1])
+            for j in range(i+1, len(nums)):
+                dp[i].append(dp[i][-1]*nums[j])
+                ans = max(ans, dp[i][-1])
+            i += 1
+        return ans
