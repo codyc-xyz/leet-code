bc31308fce8bfa7210699266e207f3ae8b7044cd
Red <crabmarket56@gmail.com>
2024-02-01 18:13:56 -0500

    Add init solution

--- pandas/problems/2887_fill_missing_data.py
+++ pandas/problems/2887_fill_missing_data.py
@@ -8,3 +8,10 @@
 # +-------------+--------+
 # Write a solution to fill in the missing value as 0 in the quantity column.
 
+import pandas as pd
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+
+    products['quantity'] = products['quantity'].fillna(0)
+    return products
+    
