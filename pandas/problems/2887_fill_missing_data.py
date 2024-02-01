8b693dc7c29d1150b06a0b87ec664344de32ccee
Red <crabmarket56@gmail.com>
2024-02-01 18:13:46 -0500

    Init problem

---
+++ pandas/problems/2887_fill_missing_data.py
@@ -0,0 +1,10 @@
+# DataFrame products
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | name        | object |
+# | quantity    | int    |
+# | price       | int    |
+# +-------------+--------+
+# Write a solution to fill in the missing value as 0 in the quantity column.
+
