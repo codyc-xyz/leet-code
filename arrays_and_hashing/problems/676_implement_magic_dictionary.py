6e419ff2ebcdaff141d96be935c125ffb293b813
Red <crabmarket56@gmail.com>
2024-02-01 11:58:59 -0500

    Add init solution

--- arrays_and_hashing/problems/676_implement_magic_dictionary.py
+++ arrays_and_hashing/problems/676_implement_magic_dictionary.py
@@ -5,3 +5,37 @@
 # MagicDictionary() Initializes the object.
 # void buildDict(String[] dictionary) Sets the data structure with an array of distinct strings dictionary.
 # bool search(String searchWord) Returns true if you can change exactly one character in searchWord to match any string in the data structure, otherwise returns false.
+
+class MagicDictionary:
+
+    def __init__(self):
+        self.magicDict = []
+        
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        self.magicDict = dictionary
+
+    def search(self, searchWord: str) -> bool:
+        for d in self.magicDict:
+            diff = 0
+            i = 0
+            if abs(len(searchWord) - len(d)) > 0:
+                continue
+            for c in d:
+                if c != searchWord[i]:
+                    diff += 1
+                    if diff > 1:
+                        break
+                i += 1
+            if diff == 1:
+                return True
+        return False
+
+
+        
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
