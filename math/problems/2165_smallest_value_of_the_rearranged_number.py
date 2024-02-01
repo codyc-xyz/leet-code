19f06109e6e4630aa7c73e63957dd84ca19794e5
Red <crabmarket56@gmail.com>
2024-02-01 12:11:23 -0500

    Add init solution

--- math/problems/2165_smallest_value_of_the_rearranged_number.py
+++ math/problems/2165_smallest_value_of_the_rearranged_number.py
@@ -4,3 +4,29 @@
 
 # Note that the sign of the number does not change after rearranging the digits.
 
+class Solution:
+    def smallestNumber(self, num: int) -> int:
+        numArr = []
+        neg = False
+        for n in str(num):
+            if n == '-':
+                neg = True
+                continue
+            numArr.append(n)
+    
+        if not neg:
+            numArr.sort()
+            if numArr[0] == '0':
+                i = 0
+                while i < len(numArr) and numArr[i] == '0':
+                    i += 1
+                if i == len(numArr):
+                    return 0
+                numArr[0], numArr[i] = numArr[i], numArr[0]
+        else:
+            numArr.sort(reverse=True)
+
+        ans = '' if not neg else '-'
+        for n in numArr:
+            ans += n
+        return int(ans)
