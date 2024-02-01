5e59c0da4f4e97824e40c9b3266605149dc7bdcd
Red <crabmarket56@gmail.com>
2024-02-01 08:49:35 -0500

    Add init solution

--- greedy/problems/2966_divide_arrays_into_arrays_with_max_difference.py
+++ greedy/problems/2966_divide_arrays_into_arrays_with_max_difference.py
@@ -6,3 +6,21 @@
 # The difference between any two elements in one array is less than or equal to k.
 # Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
 
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        ans = [[]]
+        i = 0
+        prev = None
+        for n in nums:
+            if len(ans[i]) > 2:
+                ans.append([])
+                i += 1
+                prev = None
+            if prev and n > prev + k:
+                return []
+            ans[i].append(n)
+            if len(ans[i]) == 1:
+                prev = n
+
+        return ans
