3078497711c9e63e86cfb7b8cc06c63311c62972
Red <crabmarket56@gmail.com>
2024-02-01 11:02:40 -0500

    Add init solution (TLE)

--- dynamic_programming/problems/2684_maximum_number_of_moves_in_a_grid.py
+++ dynamic_programming/problems/2684_maximum_number_of_moves_in_a_grid.py
@@ -5,3 +5,19 @@
 # From a cell (row, col), you can move to any of the cells: (row - 1, col + 1), (row, col + 1) and (row + 1, col + 1) such that the value of the cell you move to, should be strictly bigger than the value of the current cell.
 # Return the maximum number of moves that you can perform.
 
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dirs = {(-1, 1), (0, 1), (1, 1)}
+        self.ans = 0
+        def dfs(x, y, moves):
+            for dX, dY in dirs:
+                currX = dX + x
+                currY = dY + y
+                if currX  >= 0 and currX < ROWS and currY >= 0 and currY < COLS and grid[x][y] < grid[currX][currY]:
+                    dfs(currX, currY, moves + 1)
+                self.ans = max(self.ans, moves)
+
+        for i in range(ROWS):
+            dfs(i, 0, 0)
+        return self.ans
