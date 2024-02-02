58e38b76b6865f79766085d65a49e49754159a1f
Red <crabmarket56@gmail.com>
2024-02-01 20:20:11 -0500

    Add init solution

--- enumeration/problems/1291_sequential_digits.py
+++ enumeration/problems/1291_sequential_digits.py
@@ -2,3 +2,19 @@
 
 # Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
 
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        
+        n1 = len(str(low))
+        n2 = len(str(high))
+        ans = []
+        for i in range(1, 10):
+            res = ""
+            for j in range(i, 10):
+                res += str(j)
+                curr = int(res)
+                if curr >= low and curr <= high:
+                    ans.append(curr)
+                if int(res) > high:
+                    break
+        return sorted(ans)
