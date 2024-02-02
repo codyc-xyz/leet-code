26583a68df18f1863710bf74162ebb1924e1d4aa
Red <crabmarket56@gmail.com>
2024-02-02 11:58:10 -0500

    Add init solution

--- dynamic_programming/problems/53_maximum_subarray.py
+++ dynamic_programming/problems/53_maximum_subarray.py
@@ -1,2 +1,15 @@
 # Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        dp = [0 for _ in range(len(nums))]
+        res = 0
+        for i, n in enumerate(nums):
+            if i > 0:
+                res = max(res + n, n)
+            else:
+                res = n
+            dp[i] = res
+        return max(dp)
+
