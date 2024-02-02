5fcea316e6347811167d88758e3f44b8c9341c4d
Red <crabmarket56@gmail.com>
2024-02-02 11:40:06 -0500

    Add init solution

--- arrays_and_hashing/problems/1309_decrypt_string_from_alphabet_to_integer_mapping.py
+++ arrays_and_hashing/problems/1309_decrypt_string_from_alphabet_to_integer_mapping.py
@@ -6,3 +6,47 @@
 
 # The test cases are generated so that a unique mapping will always exist.
 
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        hm = {
+            '1': 'a',
+            '2': 'b',
+            '3': 'c',
+            '4': 'd',
+            '5': 'e',
+            '6': 'f',
+            '7': 'g',
+            '8': 'h',
+            '9': 'i',
+            '10#': 'j',
+            '11#': 'k',
+            '12#': 'l',
+            '13#': 'm',
+            '14#': 'n',
+            '15#': 'o',
+            '16#': 'p',
+            '17#': 'q',
+            '18#': 'r',
+            '19#': 's',
+            '20#': 't',
+            '21#': 'u',
+            '22#': 'v',
+            '23#': 'w',
+            '24#': 'x',
+            '25#': 'y',
+            '26#': 'z'        
+            }
+
+        i = 0
+        N = len(s)
+        ans = ""
+        while i < N:
+            if i + 2 < N:
+                if s[i+2] == '#':
+                    ans += hm[s[i:i+3]]
+                    i += 3
+                    continue
+            ans += hm[s[i]]
+            i += 1
+        return ans
+
