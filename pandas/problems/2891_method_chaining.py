cbe6e5c5058018e7a35da8374f61cab470ce7a18
Red <crabmarket56@gmail.com>
2024-02-02 12:25:30 -0500

    init problem

---
+++ pandas/problems/2891_method_chaining.py
@@ -0,0 +1,13 @@
+# DataFrame animals
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | name        | object |
+# | species     | object |
+# | age         | int    |
+# | weight      | int    |
+# +-------------+--------+
+# Write a solution to list the names of animals that weigh strictly more than 100 kilograms.
+
+# Return the animals sorted by weight in descending order.
+
