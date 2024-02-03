dcea07efa4b4bbd552c9fe72da351cdbe6a2e8e8
Red <crabmarket56@gmail.com>
2024-02-03 10:49:43 -0500

    Add init solution

--- string/problems/repeated_substring_pattern.py
+++ string/problems/repeated_substring_pattern.py
@@ -1,2 +1,11 @@
 # Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
 
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        N = len(s)
+        for i in range(1, N // 2 + 1):
+            if N % i:
+                continue
+            if s[:i] * (N // i) == s:
+                return True
+        return False
