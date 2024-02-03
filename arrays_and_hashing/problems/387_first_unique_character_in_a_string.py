4d0dae0d6e475de683f3a606adefc0886e80f6ed
Red <crabmarket56@gmail.com>
2024-02-03 11:23:40 -0500

    Add init solution

--- arrays_and_hashing/problems/387_first_unique_character_in_a_string.py
+++ arrays_and_hashing/problems/387_first_unique_character_in_a_string.py
@@ -1,2 +1,10 @@
 # Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        count = collections.Counter(s)
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i
+        return -1
