db1c476ed45cea26a9926c9b105535ccb4141df9
Red <crabmarket56@gmail.com>
2024-02-04 15:36:02 -0500

    Add init solution

--- design/problems/2043_simple_bank_system.py
+++ design/problems/2043_simple_bank_system.py
@@ -10,3 +10,35 @@
 # boolean transfer(int account1, int account2, long money) Transfers money dollars from the account numbered account1 to the account numbered account2. Return true if the transaction was successful, false otherwise.
 # boolean deposit(int account, long money) Deposit money dollars into the account numbered account. Return true if the transaction was successful, false otherwise.
 # boolean withdraw(int account, long money) Withdraw money dollars from the account numbered account. Return true if the transaction was successful, false otherwise.
+
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 <= len(self.balance) and account2 <= len(self.balance) and self.balance[account1-1] >= money:
+            self.balance[account1-1] -= money
+            self.balance[account2-1] += money
+            return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account <= len(self.balance):
+            self.balance[account-1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account <= len(self.balance) and self.balance[account-1] >= money:
+            self.balance[account-1] -= money
+            return True
+        return False
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
