e6e3a693b090d5e168e2d4881b317d9330d6a82c
Red <crabmarket56@gmail.com>
2024-02-04 18:39:54 -0500

    Add init solution

--- math/problems/1513_number_of_substrings_with_only_1s.py
+++ math/problems/1513_number_of_substrings_with_only_1s.py
@@ -1,2 +1,13 @@
 # Given a binary string s, return the number of substrings with all characters 1's. Since the answer may be too large, return it modulo 109 + 7.
 
+class Solution:
+    def numSub(self, s: str) -> int:
+        MOD = 10**9+7
+        N = len(s)
+        consecOnes = [0]
+        for c in s:
+            if c == '1':
+                consecOnes.append(consecOnes[-1] + 1)
+            else:
+                consecOnes.append(0)
+        return sum(consecOnes) % MOD
