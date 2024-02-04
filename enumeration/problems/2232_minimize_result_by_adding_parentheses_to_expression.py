d775e117b6e3cdb061a58ddfc128908a652a1fd2
Red <crabmarket56@gmail.com>
2024-02-04 12:54:40 -0500

    Add init solution

--- enumeration/problems/2232_minimize_result_by_adding_parentheses_to_expression.py
+++ enumeration/problems/2232_minimize_result_by_adding_parentheses_to_expression.py
@@ -6,5 +6,22 @@
 
 # The input has been generated such that the original value of expression, and the value of expression after adding any pair of parentheses that meets the requirements fits within a signed 32-bit integer.
 
- 
+class Solution:
+    def minimizeResult(self, expression: str) -> str:
+        left, right = expression.split('+')
+        ans = "(" + expression + ')'
+        minRes = int(left) + int(right)
+        if len(expression) == 3:
+            return ans
 
+        for i in range(len(left)):
+            for j in range(1, len(right) + 1):
+                multi1 = left[:i]
+                add1 = left[i:]
+                add2 = right[:j]
+                multi2 = right[j:]
+                res = (int(multi1) if multi1 else 1) * ((int(add1) if add1 else 0) + (int(add2) if add2 else 0)) * (int(multi2) if multi2 else 1)
+                if res < minRes:
+                    ans = multi1 + '(' + add1 + '+' + add2 + ')' + multi2
+                    minRes = res
+        return ans
