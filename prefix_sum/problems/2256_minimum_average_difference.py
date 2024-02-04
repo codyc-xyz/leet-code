d35d6e34a8734af766a3f8cf527718d546bdb561
Red <crabmarket56@gmail.com>
2024-02-04 18:54:12 -0500

    Add init solution

--- prefix_sum/problems/2256_minimum_average_difference.py
+++ prefix_sum/problems/2256_minimum_average_difference.py
@@ -9,3 +9,27 @@
 # The absolute difference of two numbers is the absolute value of their difference.
 # The average of n elements is the sum of the n elements divided (integer division) by n.
 # The average of 0 elements is considered to be 0.
+
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        N = len(nums)
+        minAvgDiff = float('inf')
+        pSum = [0]
+        ans = 0
+        for n in nums:
+            pSum.append(pSum[-1] + n)
+
+        for i in range(1, N + 1):
+            l = int(pSum[i] / i)
+            if N-i == 0:
+                r = 0
+            else:
+                r = int((pSum[-1] - pSum[i]) / (N - i))
+            curr = abs(l-r)
+            if curr < minAvgDiff:
+                ans = i - 1
+                minAvgDiff = curr
+
+      
+        return ans
+
