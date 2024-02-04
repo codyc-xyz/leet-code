b16f8295d9a07ab6a5cab16e9c768631eddb4a6b
Red <crabmarket56@gmail.com>
2024-02-04 17:48:50 -0500

    Add init solution

--- matrix/problems/885_spiral_matrix_III.py
+++ matrix/problems/885_spiral_matrix_III.py
@@ -4,3 +4,30 @@
 
 # Return an array of coordinates representing the positions of the grid in the order you visited them.
 
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        
+        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
+        self.totMoves = 1
+        self.prev = 0
+        ans = []
+        def dfs(x,y,numMoves, i):
+            if len(ans) == rows * cols:
+                return
+            if x > -1 and y > -1 and x < rows and y < cols:
+                ans.append([x,y])
+            if numMoves == 0:
+                i += 1
+                if i - self.prev == 2 or self.prev - i == 2:
+                    self.prev = i
+                    self.totMoves += 1
+                if i == 4:
+                    i = 0
+                numMoves = self.totMoves
+
+            currX,currY = dirs[i]
+            dfs(x+currX,y+currY,numMoves - 1,i)
+        dfs(rStart,cStart,1,0)
+        return ans
+
+            
