56cc67deedec3b96461aa3c73b303822565ac825
Red <crabmarket56@gmail.com>
2024-02-04 14:42:11 -0500

    Add init solution

--- simulation/problems/1409_queries_on_a_permutation_with_key.py
+++ simulation/problems/1409_queries_on_a_permutation_with_key.py
@@ -4,3 +4,17 @@
 # For the current i, find the position of queries[i] in the permutation P (indexing from 0) and then move this at the beginning of the permutation P. Notice that the position of queries[i] in P is the result for queries[i].
 # Return an array containing the result for the given queries.
 
+class Solution:
+    def processQueries(self, queries: List[int], m: int) -> List[int]:
+        permutations = deque(i for i in range(1, m + 1))
+        ans = []
+        for q in queries:
+            idx = permutations.index(q)
+            ans.append(idx)
+            permutations.rotate(-idx)
+            curr = permutations.popleft()
+            permutations.rotate(idx)
+            permutations.appendleft(curr)
+
+        return ans
+        
