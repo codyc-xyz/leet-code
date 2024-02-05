ae3f1a3c06ffe96375d42c30db0200519a79ea4d
Red <crabmarket56@gmail.com>
2024-02-05 08:44:33 -0500

    Init problem

---
+++ arrays_and_hashing/problems/565_array_nesting.py
@@ -0,0 +1,9 @@
+# You are given an integer array nums of length n where nums is a permutation of the numbers in the range [0, n - 1].
+
+# You should build a set s[k] = {nums[k], nums[nums[k]], nums[nums[nums[k]]], ... } subjected to the following rule:
+
+# The first element in s[k] starts with the selection of the element nums[k] of index = k.
+# The next element in s[k] should be nums[nums[k]], and then nums[nums[nums[k]]], and so on.
+# We stop adding right before a duplicate element occurs in s[k].
+# Return the longest length of a set s[k].
+
