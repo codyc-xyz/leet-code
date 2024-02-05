9f97cb61601a99efa3b318c2705b4f63e08bae09
Red <crabmarket56@gmail.com>
2024-02-04 20:05:30 -0500

    Add init solution

--- greedy/problems/2811_check_if_it_is_possible_to_split_array.py
+++ greedy/problems/2811_check_if_it_is_possible_to_split_array.py
@@ -6,3 +6,11 @@
 # The sum of elements of the subarray is greater than or equal to m.
 # Return true if you can split the given array into n arrays, otherwise return false.
 
+class Solution:
+    def canSplitArray(self, nums: List[int], m: int) -> bool:
+        if len(nums) <= 2:
+            return True
+        for i in range(len(nums) - 1):
+            if nums[i] + nums[i+1] >= m:
+                return True 
+        return False
