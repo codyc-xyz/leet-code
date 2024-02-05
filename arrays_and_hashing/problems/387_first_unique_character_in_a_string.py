ce4978b643d6020eb638a553e26b1c108904c4a2
Red <crabmarket56@gmail.com>
2024-02-05 08:38:45 -0500

    Add daily LC

--- arrays_and_hashing/problems/387_first_unique_character_in_a_string.py
+++ arrays_and_hashing/problems/387_first_unique_character_in_a_string.py
@@ -8,3 +8,12 @@ class Solution:
             if count[c] == 1:
                 return i
         return -1
+    
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = collections.Counter(s)
+
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i
+        return -1
