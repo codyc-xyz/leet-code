13cdb1a40e3d2c683ee1ca2f87000fa7e8a9e2b4
Red <crabmarket56@gmail.com>
2024-02-05 11:33:45 -0500

    Add init solution

--- sliding-window/problems/shortest_and_lexicographically_smallest_beautiful_string.py
+++ sliding-window/problems/shortest_and_lexicographically_smallest_beautiful_string.py
@@ -10,4 +10,24 @@
 
 # For example, "abcd" is lexicographically larger than "abcc" because the first position they differ is at the fourth character, and d is greater than c.
  
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        ans = ""
+        length = float('inf')
+        count = windowStart = 0
 
+        for windowEnd in range(len(s)):
+            if s[windowEnd] == '1':
+                count += 1
+                if count == k:
+                    while windowStart <= windowEnd and count == k:
+                        if s[windowStart] == '1':
+                            count -= 1
+                        windowStart += 1
+                    curr = s[windowStart-1:windowEnd+1]
+                    if windowEnd - windowStart < length:
+                        ans = curr
+                        length = windowEnd - windowStart
+                    elif windowEnd - windowStart == length:
+                        ans = min(ans, curr)
+        return ans
