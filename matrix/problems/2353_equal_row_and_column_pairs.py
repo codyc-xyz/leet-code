b71a0f7f8e847167c6b92266c76c99c05314dfcf
Red <crabmarket56@gmail.com>
2024-02-04 19:35:18 -0500

    Add init solution

--- matrix/problems/2353_equal_row_and_column_pairs.py
+++ matrix/problems/2353_equal_row_and_column_pairs.py
@@ -2,3 +2,21 @@
 
 # A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
 
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        cols = [[] for _ in range(COLS)]
+        rows = [[] for _ in range(ROWS)]
+    
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                rows[i].append(grid[i][j])
+                cols[j].append(grid[i][j])
+
+        ans = 0
+        for r in rows:
+            for c in cols:
+                if r == c:
+                    ans += 1
+        return ans
