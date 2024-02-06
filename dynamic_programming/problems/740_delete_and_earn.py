706cf712fa126ef84489150fc00d8fbf866975fc
Red <crabmarket56@gmail.com>
2024-02-06 10:11:15 -0500

    Add init solution

--- dynamic_programming/problems/740_delete_and_earn.py
+++ dynamic_programming/problems/740_delete_and_earn.py
@@ -3,5 +3,16 @@
 # Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
 # Return the maximum number of points you can earn by applying the above operation some number of times.
 
- 
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        
+        count = collections.Counter(nums)
+        N = max(nums)
+        dp = [0 for _ in range(N+1)]
+        dp[1] = count.get(1, 0)
+        for n in range(2, N+1):
+            dp[n] = max(dp[n-1], dp[n-2] + (n * count[n]))
+
+        return dp[N]
+
 
