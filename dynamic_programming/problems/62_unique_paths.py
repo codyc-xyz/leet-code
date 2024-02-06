537992b32b962891ce7ab89260bdfd4ff94b6d7e
Red <crabmarket56@gmail.com>
2024-02-06 14:41:53 -0500

    Add explicit cache solution

--- dynamic_programming/problems/62_unique_paths.py
+++ dynamic_programming/problems/62_unique_paths.py
@@ -19,4 +19,19 @@ class Solution:
         return dfs(0,0)
         
 
-        
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        cache = {}
+        def dfs(i, j):
+            if i < 0 or j < 0 or i == m or j == n:
+                return 0
+            if (i, j) in cache:
+                return cache[(i,j)]
+            if i == m - 1 and j == n - 1:
+                return 1
+            r = dfs(i, j+1)
+            l = dfs(i+1, j)
+            cache[(i,j)] = l + r
+            return cache[(i,j)]
+        return dfs(0,0)
