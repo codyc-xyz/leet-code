9db69ac27dbf8b5cf25a4c6052296f23f098f300
Red <crabmarket56@gmail.com>
2024-02-06 11:11:11 -0500

    Add init solution

--- arrays_and_hashing/problems/2942_find_words_containing_character.py
+++ arrays_and_hashing/problems/2942_find_words_containing_character.py
@@ -4,3 +4,10 @@
 
 # Note that the returned array may be in any order.
 
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        for i, w in enumerate(words):
+            if x in w:
+                ans.append(i)
+        return ans
