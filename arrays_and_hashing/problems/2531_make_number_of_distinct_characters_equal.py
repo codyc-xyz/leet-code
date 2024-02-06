d394a8651166ca16de2659bf2a67a1c4f2406785
Red <crabmarket56@gmail.com>
2024-02-06 17:18:07 -0500

    Add init solution

--- arrays_and_hashing/problems/2531_make_number_of_distinct_characters_equal.py
+++ arrays_and_hashing/problems/2531_make_number_of_distinct_characters_equal.py
@@ -4,3 +4,35 @@
 
 # Return true if it is possible to get the number of distinct characters in word1 and word2 to be equal with exactly one move. Return false otherwise.
 
+class Solution:
+    def isItPossible(self, word1: str, word2: str) -> bool:
+        
+        count1 = [0 for _ in range(26)]
+        count2 = [0 for _ in range(26)]
+
+        for c1 in word1:
+            count1[ord(c1) - ord('a')] += 1
+
+        for c2 in word2:
+            count2[ord(c2) - ord('a')] += 1
+
+        for i in range(26):
+            for j in range(26):
+                if count1[i] and count2[j]:
+                    count1[i] -= 1
+                    count2[i] += 1
+                    count2[j] -= 1
+                    count1[j] += 1
+                    res1=res2=0
+                    for k in range(26):
+                        if count1[k]:
+                            res1+=1
+                        if count2[k]:
+                            res2+=1
+                    if res1==res2:
+                        return True
+                    count1[i] += 1
+                    count1[j] -= 1
+                    count2[j] += 1
+                    count2[i] -= 1
+        return False
