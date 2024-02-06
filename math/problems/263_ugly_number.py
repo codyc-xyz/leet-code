1c4b33b440c96a00ee456f35a44de9c0f194a45b
Red <crabmarket56@gmail.com>
2024-02-06 11:30:41 -0500

    Add init solution

--- math/problems/263_ugly_number.py
+++ math/problems/263_ugly_number.py
@@ -2,3 +2,15 @@
 
 # Given an integer n, return true if n is an ugly number.
 
+class Solution:
+    def isUgly(self, n: int) -> bool:
+
+        if n <= 0:
+            return False
+        while not n % 2:
+            n //= 2
+        while not n % 3:
+            n //= 3
+        while not n % 5:
+            n //= 5
+        return n == 1
