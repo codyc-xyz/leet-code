ed33fe8b1776a7c595bae77945d59ac04a526f7d
Red <crabmarket56@gmail.com>
2024-02-06 20:20:09 -0500

    Init problem

---
+++ backtracking/problems/2708_maximum_strength_of_group.py
@@ -0,0 +1,4 @@
+# You are given a 0-indexed integer array nums representing the score of students in an exam. The teacher would like to form one non-empty group of students with maximal strength, where the strength of a group of students of indices i0, i1, i2, ... , ik is defined as nums[i0] * nums[i1] * nums[i2] * ... * nums[ik​].
+
+# Return the maximum strength of a group the teacher can create.
+
