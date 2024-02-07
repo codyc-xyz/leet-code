d338897d3160178d2361c001854b5dffd582dd32
Red <crabmarket56@gmail.com>
2024-02-07 11:15:23 -0500

    Add init solution

--- union_find/problems/1061_lexicographically_smallest_equivalent_string.py
+++ union_find/problems/1061_lexicographically_smallest_equivalent_string.py
@@ -12,3 +12,33 @@
 
 # Return the lexicographically smallest equivalent string of baseStr by using the equivalency information from s1 and s2.
 
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        
+        equiv = defaultdict(set)
+        smallest = defaultdict(str)
+        for c1, c2 in zip(s1, s2):
+            if c1 == c2:
+                continue
+            equiv[c1].add(c2)
+            equiv[c2].add(c1)
+
+        def setMinChar(curr, minCh):
+            if curr in smallest:
+                return
+            smallest[curr] = minCh
+            for eq in equiv[curr]:
+                setMinChar(eq, minCh)
+
+        for c in sorted(equiv.keys()):
+            if c in smallest:
+                continue
+            setMinChar(c, c)
+        return ''.join([smallest[c] if c in smallest else c for c in baseStr])
+            
+        
+
+        
+                    
+
+
