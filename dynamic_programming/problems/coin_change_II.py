e515d41ede0130129abe2b489c6227257be66122
Red <crabmarket56@gmail.com>
2024-02-07 09:23:36 -0500

    Add init solution

--- dynamic_programming/problems/coin_change_II.py
+++ dynamic_programming/problems/coin_change_II.py
@@ -6,3 +6,16 @@
 
 # The answer is guaranteed to fit into a signed 32-bit integer.
 
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        @cache
+        def dfs(i, rem):
+            if rem == 0:
+                return 1
+            if i == len(coins) or rem < 0:
+                return 0
+            res = 0
+            for j in range(rem // coins[i] + 1):
+                res += dfs(i+1, rem - coins[i] * j)
+            return res
+        return dfs(0, amount)
