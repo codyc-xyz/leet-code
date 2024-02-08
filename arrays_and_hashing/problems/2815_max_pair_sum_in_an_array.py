6ea1280096d56713dbdbc6bafd08ae1c94b819d0
Red <crabmarket56@gmail.com>
2024-02-08 14:20:10 -0500

    Add init solution

--- arrays_and_hashing/problems/2815_max_pair_sum_in_an_array.py
+++ arrays_and_hashing/problems/2815_max_pair_sum_in_an_array.py
@@ -2,3 +2,20 @@
 
 # Return the maximum sum or -1 if no such pair exists.
 
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        
+        maxDigit = defaultdict(list)
+
+        for n in nums:
+            N = str(n)
+            maxNum = max(int(c) for c in N)
+            maxDigit[maxNum].append(n)
+
+        for k in maxDigit:
+            maxDigit[k].sort(reverse=True)
+        ans = -1
+        for k in maxDigit:
+            if len(maxDigit[k]) > 1:
+                ans = max(ans, maxDigit[k][0] + maxDigit[k][1])
+        return ans
