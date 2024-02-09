4432cca8c37330019bb9e6d6d309c4a230645717
Red <crabmarket56@gmail.com>
2024-02-09 10:42:29 -0500

    Add alternate solution (Not TLE)

--- dynamic_programming/problems/368_largest_divisible_subset.py
+++ dynamic_programming/problems/368_largest_divisible_subset.py
@@ -27,3 +27,21 @@ class Solution:
             backtrack(i+1, path)
         backtrack(0, [])
         return self.ans
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        N = len(nums)
+        cache = {}
+        def dfs(i, prev):
+            if i == N:
+                return []
+            if (i, prev) in cache:
+                return cache[(i, prev)]
+            res = dfs(i+1, prev)
+            if not nums[i] % prev:
+                tmp = [nums[i]] + dfs(i+1, nums[i])
+                res = tmp if len(tmp) > len(res) else res
+            cache[(i, prev)] = res
+            return cache[(i, prev)]
+        return dfs(0, 1)
