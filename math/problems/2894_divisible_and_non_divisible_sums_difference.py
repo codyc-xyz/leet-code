1b421c34387b0288482fbdc844a64bc2f1126b78
Red <crabmarket56@gmail.com>
2024-02-09 11:14:01 -0500

    Add init solution

--- math/problems/2894_divisible_and_non_divisible_sums_difference.py
+++ math/problems/2894_divisible_and_non_divisible_sums_difference.py
@@ -6,5 +6,14 @@
 # num2: The sum of all integers in the range [1, n] that are divisible by m.
 # Return the integer num1 - num2.
 
- 
-
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        
+        num1 = num2= 0
+        for i in range(1, n + 1):
+            if i % m:
+                num1 += i
+            else:
+                num2 += i
+        return num1-num2
+        
