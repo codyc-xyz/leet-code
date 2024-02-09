afa748dac596fefc32e4927d5723dc89570fac95
Red <crabmarket56@gmail.com>
2024-02-09 11:09:39 -0500

    Add init solution

--- arrays_and_hashing/1122_relative_sort_array.py
+++ arrays_and_hashing/1122_relative_sort_array.py
@@ -2,3 +2,18 @@
 
 # Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
 
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr1.sort()
+        count1 = collections.Counter(arr1)
+        set2 = set(arr2)
+        ans = []
+        for n in arr2:
+            ans += [n] * count1[n]
+
+        seen = set()
+        for n in arr1:
+            if n not in set2 and n not in seen:
+                ans += [n] * count1[n]
+                seen.add(n)
+        return ans
