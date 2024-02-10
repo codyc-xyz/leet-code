1732d74f8e502dc14b0e78971b5060a005cb747f
Red <crabmarket56@gmail.com>
2024-02-10 10:42:57 -0500

    Add init solution

--- math/problems/415_add_strings.py
+++ math/problems/415_add_strings.py
@@ -2,3 +2,23 @@
 
 # You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
 
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+        l1, l2 = len(num1) - 1, len(num2) - 1
+        x, carry = 0, 0
+        ans = deque()
+
+        while l1 >= 0 or l2 >= 0 or carry > 0:
+            digit1 = int(num1[l1]) if l1 >= 0 else 0
+            digit2 = int(num2[l2]) if l2 >= 0 else 0
+
+            x = digit1 + digit2 + carry
+            carry = x // 10
+            x %= 10
+            ans.appendleft(str(x))
+            if l1 >= 0:
+                l1 -= 1
+            if l2 >= 0:
+                l2 -= 1
+        return ''.join(ans)
