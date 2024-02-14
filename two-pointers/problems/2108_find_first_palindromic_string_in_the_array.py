aa8ca4e65b85fbef23efa4b0077ea7a95563681e
Red <crabmarket56@gmail.com>
2024-02-15 03:17:57 +0700

    Add daily LC

--- two-pointers/problems/2108_find_first_palindromic_string_in_the_array.py
+++ two-pointers/problems/2108_find_first_palindromic_string_in_the_array.py
@@ -22,3 +22,17 @@ class Solution:
                 palindromic += word
                 break
         return palindromic
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        
+        for w in words:
+            l,r=0,len(w) - 1
+            while w[l] == w[r] and l < r:
+                l +=1
+                r -=1
+            if l >= r:
+                return w
+        return ""
+
+        
