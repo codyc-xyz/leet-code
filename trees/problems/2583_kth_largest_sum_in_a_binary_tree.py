b2b9b890e5d465b13ac5b6cfe76bee98ea8592ec
Red <crabmarket56@gmail.com>
2024-02-15 03:45:21 +0700

    Add init solution

--- trees/problems/2583_kth_largest_sum_in_a_binary_tree.py
+++ trees/problems/2583_kth_largest_sum_in_a_binary_tree.py
@@ -6,3 +6,26 @@
 
 # Note that two nodes are on the same level if they have the same distance from the root.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        q = deque(([root]))
+        levels = []
+        while q:
+            levSum = 0
+            lenQ = len(q)
+            while lenQ:
+                curr = q.popleft()
+                levSum += curr.val
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+                lenQ -= 1
+            levels.append(levSum)
+        return sorted(levels)[len(levels)-k] if len(levels) >= k else -1
