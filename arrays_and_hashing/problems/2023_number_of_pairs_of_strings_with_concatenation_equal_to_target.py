69cc488611109b2a8f289cd84bfef450c49619dc
Red <crabmarket56@gmail.com>
2024-02-15 12:56:49 +0700

    Add init solution

--- arrays_and_hashing/problems/2023_number_of_pairs_of_strings_with_concatenation_equal_to_target.py
+++ arrays_and_hashing/problems/2023_number_of_pairs_of_strings_with_concatenation_equal_to_target.py
@@ -1,2 +1,10 @@
 # Given an array of digit strings nums and a digit string target, return the number of pairs of indices (i, j) (where i != j) such that the concatenation of nums[i] + nums[j] equals target.
 
+class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target:
+                    ans += 1
+        return ans
