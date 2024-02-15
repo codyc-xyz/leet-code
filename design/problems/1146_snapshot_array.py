047b747fd75a129bb96b4835fca0448464f024b2
Red <crabmarket56@gmail.com>
2024-02-15 09:34:00 +0700

    Add alternate solution (Not TLE)

--- design/problems/1146_snapshot_array.py
+++ design/problems/1146_snapshot_array.py
@@ -31,3 +31,33 @@ class SnapshotArray:
 # obj.set(index,val)
 # param_2 = obj.snap()
 # param_3 = obj.get(index,snap_id)
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snapArr = [[[-1,0]] for _ in range(length)]
+        self.snapId = 0
+        
+
+    def set(self, index: int, val: int) -> None:
+        if self.snapArr[index][-1][0] == self.snapId:
+            self.snapArr[index][-1][1] = val
+        else:
+            self.snapArr[index].append([self.snapId, val])
+        
+
+    def snap(self) -> int:
+        self.snapId += 1
+        return self.snapId - 1
+        
+
+    def get(self, index: int, snap_id: int) -> int:
+        snapshots = self.snapArr[index]
+        l, r = 0, len(snapshots) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if snapshots[m][0] <= snap_id:
+                l = m + 1
+            else:
+                r = m - 1
+        return snapshots[r][1]
