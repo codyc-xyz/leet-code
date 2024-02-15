b68ae4a89b134d8c79bdac3e3234b16f6b1b8a54
Red <crabmarket56@gmail.com>
2024-02-15 12:10:25 +0700

    Add init solution

--- dynamic_programming/problems/2369_check_if_there_is_a_valid_partition_for_the_array.py
+++ dynamic_programming/problems/2369_check_if_there_is_a_valid_partition_for_the_array.py
@@ -7,3 +7,18 @@
 # The subarray consists of exactly 3 consecutive increasing elements, that is, the difference between adjacent elements is 1. For example, the subarray [3,4,5] is good, but the subarray [1,3,5] is not.
 # Return true if the array has at least one valid partition. Otherwise, return false.
 
+class Solution:
+    def validPartition(self, nums: List[int]) -> bool:
+        N = len(nums)
+        @cache
+        def dfs(i):
+            if i == N:
+                return True
+            if i+1<N and nums[i] == nums[i+1] and dfs(i+2):
+                return True
+            if i+2<N and nums[i] == nums[i+1] and nums[i] == nums[i+2] and dfs(i+3):
+                return True
+            if i+2<N and nums[i] == nums[i+1] - 1 and nums[i+1] == nums[i+2] -1 and dfs(i+3):
+                return True
+            return False
+        return dfs(0)
