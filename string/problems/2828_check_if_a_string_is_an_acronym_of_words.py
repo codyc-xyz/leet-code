9c48e68b0e88b2dcaa9100701a8338001df06b97
Red <crabmarket56@gmail.com>
2024-02-16 15:11:58 +0700

    Add init solution

--- string/problems/2828_check_if_a_string_is_an_acronym_of_words.py
+++ string/problems/2828_check_if_a_string_is_an_acronym_of_words.py
@@ -4,3 +4,11 @@
 
 # Return true if s is an acronym of words, and false otherwise.
 
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        res = ""
+        for w in words:
+            res += w[0]
+
+        return res == s
+
