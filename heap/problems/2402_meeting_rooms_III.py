ab57b490be754cca71e8d6bf0281faffc53ec401
Red <crabmarket56@gmail.com>
2024-02-18 17:28:52 +0700

    Add init solution

--- heap/problems/2402_meeting_rooms_III.py
+++ heap/problems/2402_meeting_rooms_III.py
@@ -11,3 +11,28 @@
 
 # A half-closed interval [a, b) is the interval between a and b including a and not including b.
 
+class Solution:
+    def mostBooked(self, N: int, meetings: List[List[int]]) -> int:
+        meetings.sort(key=lambda x: x[0])
+        countMeetings = defaultdict(int)
+        avail_rooms = [i for i in range(N)]
+        heapq.heapify(avail_rooms)
+        heap = []
+        for start, end in meetings:
+            while heap and heap[0][0] <= start:
+                avail_time, n = heapq.heappop(heap)
+                heapq.heappush(avail_rooms, n)
+            if avail_rooms:
+                n = heapq.heappop(avail_rooms)
+                heapq.heappush(heap, [end,n])
+                countMeetings[n] += 1
+            else:
+                avail_time, n = heapq.heappop(heap)
+                heapq.heappush(heap, [avail_time + end - start, n])
+                countMeetings[n] += 1
+        maxMeetings = max(countMeetings.values())
+        for i in range(N):
+            if countMeetings[i] == maxMeetings:
+                return i
+
+            
