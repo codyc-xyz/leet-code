1046e3c67b53df869166a609682cc10fb37d0aad
Red <crabmarket56@gmail.com>
2024-02-19 09:34:16 +0700

    Add init daily LC solution

--- math/problems/231_power_of_two.py
+++ math/problems/231_power_of_two.py
@@ -2,5 +2,11 @@
 
 # An integer n is a power of two, if there exists an integer x such that n == 2x.
 
- 
-
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        i = 0
+        while 2**i <= n:
+            if 2**i == n:
+                return True
+            i += 1
+        return False
