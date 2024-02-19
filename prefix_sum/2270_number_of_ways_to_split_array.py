7d0dac00deb7abac7ce949d12f2daff51966618c
Red <crabmarket56@gmail.com>
2024-02-19 09:35:08 +0700

    Add init solution

--- prefix_sum/2270_number_of_ways_to_split_array.py
+++ prefix_sum/2270_number_of_ways_to_split_array.py
@@ -6,3 +6,14 @@
 # There is at least one element to the right of i. That is, 0 <= i < n - 1.
 # Return the number of valid splits in nums.
 
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        pSum = [0]
+        ans = 0
+        for n in nums:
+            pSum.append(pSum[-1] + n)
+
+        for i in range(1, len(pSum) - 1):
+            if pSum[i] >= pSum[-1] - pSum[i]:
+                ans += 1
+        return ans
