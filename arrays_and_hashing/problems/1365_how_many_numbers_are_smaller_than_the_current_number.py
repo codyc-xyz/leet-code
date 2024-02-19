e2acece3b23b510aa6e7dcfda309ec0337eee61d
Red <crabmarket56@gmail.com>
2024-02-19 10:51:42 +0700

    Add alternate solution

--- arrays_and_hashing/problems/1365_how_many_numbers_are_smaller_than_the_current_number.py
+++ arrays_and_hashing/problems/1365_how_many_numbers_are_smaller_than_the_current_number.py
@@ -10,3 +10,20 @@ class Solution:
                 if nums[j] < nums[i]:
                     ans[i] += 1
         return ans
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        N = sorted(nums)
+        ans = [0 for _ in range(len(nums))]
+        hm = {}
+        prev = None
+        for i in range(len(N)):
+            if N[i] == prev:
+                continue
+            else:
+                hm[N[i]] = i
+                prev = N[i]
+
+        for i, n in enumerate(nums):
+            ans[i] = hm[n]
+        return ans
