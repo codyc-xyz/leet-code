d5ab9e501373e80c889ceb03e784077a84605e71
Red <crabmarket56@gmail.com>
2024-02-19 09:41:13 +0700

    Add init solution

--- dynamic_programming/problems/416_partition_equal_subset_sum.py
+++ dynamic_programming/problems/416_partition_equal_subset_sum.py
@@ -1,2 +1,19 @@
 # Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
 
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        total = sum(nums)
+        if total % 2:
+            return False
+        @cache
+        def dfs(i, curr):
+            if i == len(nums) or curr > total / 2:
+                return False
+            elif curr == total // 2:
+                return True
+            if dfs(i+1,curr):
+                return True
+            if dfs(i+1, curr+nums[i]):
+                return True
+        return dfs(0, 0)
