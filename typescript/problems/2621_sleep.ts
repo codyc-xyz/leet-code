b7bd8c1b3f2782b21492dce0c3691d5bfbfd2ef4
Red <crabmarket56@gmail.com>
2024-02-20 12:44:47 +0700

    Add init solution

--- typescript/problems/2621_sleep.ts
+++ typescript/problems/2621_sleep.ts
@@ -1,2 +1,12 @@
 // Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 
+async function sleep(millis: number): Promise<void> {
+    await new Promise(resolve => setTimeout(resolve, millis));
+   
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
