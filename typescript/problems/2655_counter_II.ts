7c3ca3f334a5a88bae4cbc8a6722fa049de26d9a
Red <crabmarket56@gmail.com>
2024-02-20 13:38:49 +0700

    Add init solution

--- typescript/problems/2655_counter_II.ts
+++ typescript/problems/2655_counter_II.ts
@@ -7,3 +7,34 @@
 // reset() sets the current value to init and then returns it.
  
 
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    const first: number = init;
+    let curr: number = init;
+    return {
+        increment: function() {
+            curr++
+            return curr
+        },
+        decrement: function() {
+            curr--
+            return curr
+        },
+        reset: function() {
+            curr = first
+            return curr
+        }
+    }    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
