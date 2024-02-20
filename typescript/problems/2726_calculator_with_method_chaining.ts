f4d5853f6a7143d383cec0d9cb40de7e98ac7370
Red <crabmarket56@gmail.com>
2024-02-20 15:00:56 +0700

    Add init solution

--- typescript/problems/2726_calculator_with_method_chaining.ts
+++ typescript/problems/2726_calculator_with_method_chaining.ts
@@ -10,3 +10,44 @@
 // getResult - This method returns the result.
 // Solutions within 10-5 of the actual result are considered correct.
 
+class Calculator {
+    private result: number
+    constructor(value: number) {
+        this.result = value;
+    }
+    
+    add(value: number): Calculator {
+        this.result += value
+        return this
+    }
+    
+    subtract(value: number): Calculator {
+        this.result -= value
+        return this
+    }
+    
+    multiply(value: number): Calculator {
+        this.result *= value
+        return this
+        
+    }
+    
+    divide(value: number): Calculator {
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed")
+        }
+        this.result /= value
+        return this
+        
+    }
+    
+    power(value: number): Calculator {
+        this.result**=value
+        return this
+        
+    }
+    
+    getResult(): number {
+        return this.result
+    }
+}
