4e876edc3a09ed30ebfef417975706d2dce5b6d9
Red <crabmarket56@gmail.com>
2024-02-20 13:54:34 +0700

    Add init solution

--- typescript/problems/2666_allow_one_function_call.ts
+++ typescript/problems/2666_allow_one_function_call.ts
@@ -2,3 +2,28 @@
 
 // The first time the returned function is called, it should return the same result as fn.
 // Every subsequent time it is called, it should return undefined.
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let counter: number = 0;
+    return function (...args) {
+        if (counter === 0) {
+            counter++
+            return fn(...args);
+        }
+        else {
+            return undefined;
+        };
+        
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
