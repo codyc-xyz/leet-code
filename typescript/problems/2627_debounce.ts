0cca60aff2784390750e2603298f290610b4e343
Red <crabmarket56@gmail.com>
2024-02-20 15:55:55 +0700

    Add init solution

--- typescript/problems/2627_debounce.ts
+++ typescript/problems/2627_debounce.ts
@@ -4,3 +4,22 @@
 
 // For example, let's say t = 50ms, and the function was called at 30ms, 60ms, and 100ms. The first 2 function calls would be cancelled, and the 3rd function call would be executed at 150ms. If instead t = 35ms, The 1st call would be cancelled, the 2nd would be executed at 95ms, and the 3rd would be executed at 135ms.
 
+type F = (...args: number[]) => void
+
+function debounce(fn: F, t: number): F {
+    let timer: ReturnType <typeof setTimeout>
+
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn(...args)
+        }, t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
