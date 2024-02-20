281d098fbe776a4d5db5b7596142f27e3a769bd2
Red <crabmarket56@gmail.com>
2024-02-20 13:13:58 +0700

    Init problem

--- typescript/problems/2634_filter_elements_from_array.ts
+++ typescript/problems/2634_filter_elements_from_array.ts
@@ -11,7 +11,7 @@
 type Fn = (n: number, i: number) => any
 
 function filter(arr: number[], fn: Fn): number[] {
-    let ans = [];
+    let ans: number[] = [];
 
     for (let i = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
