5882e293d1f1afdad78e3c6ca9fdf27b1c746888
Red <crabmarket56@gmail.com>
2024-02-20 11:58:16 +0700

    Add init solution

--- arrays_and_hashing/problems/268_missing_number.py
+++ arrays_and_hashing/problems/268_missing_number.py
@@ -1,2 +1,10 @@
 # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        N = set(nums)
+        for i in range(len(nums) + 1):
+            if i not in N:
+                return i
+        
