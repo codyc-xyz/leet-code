205c96db6968fa5b2abd3f5c38177ce33c95a932
Red <crabmarket56@gmail.com>
2024-02-20 13:30:32 +0700

    Add init solution

--- typescript/problems/2648_generate_fibonacci_sequence.ts
+++ typescript/problems/2648_generate_fibonacci_sequence.ts
@@ -4,3 +4,23 @@
 
 // The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
 
+function* fibGenerator(): Generator<number, any, number> {
+    let fib: number[] = []
+    yield 0;
+    fib.push(0);
+    yield 1;
+    fib.push(1);
+    while(true) {
+        const curr = fib[fib.length-1] + fib[fib.length-2];
+        yield curr
+        fib.push(curr);
+    }
+};
+
+
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
