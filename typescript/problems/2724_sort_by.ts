5d6f56f678816a0d8d1cd8d6bafb2b8dddcc9276
Red <crabmarket56@gmail.com>
2024-02-20 14:45:58 +0700

    Add init solution

--- typescript/problems/2724_sort_by.ts
+++ typescript/problems/2724_sort_by.ts
@@ -2,3 +2,11 @@
 
 // You may assume that fn will never duplicate numbers for a given array.
 
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (value: JSONValue) => number
+
+function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
+        arr.sort((a, b) => fn(a) - fn(b))
+        return arr
+
+};
