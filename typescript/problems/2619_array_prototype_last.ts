bcf18b3e2d336b892906a5dc78a71dd06f2b5d89
Red <crabmarket56@gmail.com>
2024-02-20 12:49:30 +0700

    add init solution

--- typescript/problems/2619_array_prototype_last.ts
+++ typescript/problems/2619_array_prototype_last.ts
@@ -2,3 +2,26 @@
 
 // You may assume the array is the output of JSON.parse.
 
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function() {
+    const len = this.length;
+    if (len > 0) {
+        return this[len-1];
+    }
+    else {
+        return -1;
+    }
+    
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
