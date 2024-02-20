507ce778036e25c724d110a7bdadd3eaac3a118a
Red <crabmarket56@gmail.com>
2024-02-20 14:14:26 +0700

    Add init solution

--- typescript/problems/2695_array_wrapper.ts
+++ typescript/problems/2695_array_wrapper.ts
@@ -4,3 +4,27 @@
 // When the String() function is called on the instance, it will return a comma separated string surrounded by brackets. For example, [1,2,3].
  
 
+class ArrayWrapper {
+    private nums: number[];
+    constructor(nums: number[]) {
+        this.nums = nums;
+    }
+    
+    valueOf(): number {
+        return this.nums.reduce((acc, curr) => acc + curr, 0);
+        
+    }
+    
+    toString(): string {
+        const str = this.nums.toString();
+        return `[${str}]`; 
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
