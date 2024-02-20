07e507a922b596f7bfa1cac1758340bed7d71dc6
Red <crabmarket56@gmail.com>
2024-02-20 14:24:18 +0700

    Add init solution

--- typescript/problems/2704_to_be_or_not_to_be.ts
+++ typescript/problems/2704_to_be_or_not_to_be.ts
@@ -2,3 +2,36 @@
 
 // toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
 // notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    const val1: any = val;
+    return {
+        toBe: function(val2: any): boolean {
+            if (val1 === val2) {
+                return true
+            }
+            else {
+                throw new Error ("Not Equal")
+            }
+        },
+        notToBe: function(val2: any): boolean {
+            if (val1 !== val2) {
+                return true
+            }
+            else {
+                throw new Error("Equal")
+            }
+        }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
