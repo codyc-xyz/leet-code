bfb484af61dd1486162cc9e7f41dd87e83fb8188
Red <crabmarket56@gmail.com>
2024-02-20 13:19:17 +0700

    Add init solution

--- typescript/problems/2626_array_reduce_transformation.ts
+++ typescript/problems/2626_array_reduce_transformation.ts
@@ -6,3 +6,14 @@
 
 // Please solve it without using the built-in Array.reduce method.
 
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    
+    let ans: number = init;
+
+    for (let i = 0; i < nums.length; i++) {
+        ans = fn(ans, nums[i]);
+    }
+    return ans
+};
