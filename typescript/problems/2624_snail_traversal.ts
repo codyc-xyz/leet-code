a7d2e2185f92305b63be15b0709e0218e293fa1c
Red <crabmarket56@gmail.com>
2024-02-21 10:03:01 +0700

    Add init solution

--- typescript/problems/2624_snail_traversal.ts
+++ typescript/problems/2624_snail_traversal.ts
@@ -2,3 +2,40 @@
 
 // Snail traversal order starts at the top left cell with the first value of the current array. It then moves through the entire first column from top to bottom, followed by moving to the next column on the right and traversing it from bottom to top. This pattern continues, alternating the direction of traversal with each column, until the entire current array is covered. For example, when given the input array [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15] with rowsCount = 5 and colsCount = 4, the desired output matrix is shown below. Note that iterating the matrix following the arrows corresponds to the order of numbers in the original array.
 
+declare global {
+    interface Array<T> {
+        snail(rowsCount: number, colsCount: number): number[][];
+    }
+}
+
+Array.prototype.snail = function(rowsCount: number, colsCount: number): number[][] {
+    
+    if (rowsCount * colsCount != this.length) {
+        return []
+    };
+    
+    let ans: number[][] = Array(rowsCount).fill(null).map(() => Array(colsCount).fill(null));
+
+    let bias: boolean = false;
+    let prev: number = 0;
+
+    for (let i = 0; i < colsCount; i++) {
+        bias = !bias;
+        let curr = this.slice(prev, prev+rowsCount);
+        prev+=rowsCount
+        if (!bias) {
+            curr = curr.reverse();
+        }
+        let idx: number = 0
+        for (let j = 0; j < rowsCount; j++) {
+            ans[j][i] = curr[idx];
+            idx += 1
+        }
+    }
+    return ans
+}
+
+/**
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */
