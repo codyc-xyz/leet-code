2d48eb03c80916ec18eba4abbacdb0acc5864af4
Red <crabmarket56@gmail.com>
2024-02-21 14:22:26 +0700

    Add init solution (TLE)

--- typescript/problems/2722_join_two_arrays_by_id.ts
+++ typescript/problems/2722_join_two_arrays_by_id.ts
@@ -6,3 +6,23 @@
 
 // If a key only exists in one object, that single key-value pair should be included in the object.
 // If a key is included in both objects, the value in the object from arr2 should override the value from arr1.
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type ArrayType = { "id": number } & Record<string, JSONValue>;
+
+function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
+
+    let ans: ArrayType[] = [...arr1];
+
+    for (let i = 0; i < arr2.length; i++) {
+        let curr: ArrayType = arr2[i];
+        let idx = ans.findIndex(item => item.id === curr.id);
+        if (idx === -1) {
+            ans.push(curr);
+        }
+        else {
+            ans[idx] = {...ans[idx], ...curr}
+        }
+    }
+    return ans.sort((a, b) => a.id - b.id)
+};
