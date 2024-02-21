336da88e47dc1e0d50bc4e03a6cef71010073baa
Red <crabmarket56@gmail.com>
2024-02-21 11:22:59 +0700

    Init problem

--- typescript/problems/2649_nested_array_generator.ts
+++ typescript/problems/2649_nested_array_generator.ts
@@ -4,3 +4,26 @@
 
 // inorder traversal iterates over each array from left to right, yielding any integers it encounters or applying inorder traversal to any arrays it encounters.
 
+type MultidimensionalArray = (MultidimensionalArray | number)[]
+
+function* inorderTraversal(arr: MultidimensionalArray): Generator<number, void, unknown> {
+    if (arr.length === 0) {
+        return 
+    }
+
+    for (const item of arr) {
+        if (Array.isArray(item)) {
+            yield* inorderTraversal(item)
+        }
+        else if (item !== undefined) {
+            yield item
+        }
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
