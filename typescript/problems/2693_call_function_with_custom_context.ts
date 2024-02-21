603488c22e3160304d79652d14d8fd4ac063e052
Red <crabmarket56@gmail.com>
2024-02-21 11:23:08 +0700

    Add init solution

--- typescript/problems/2693_call_function_with_custom_context.ts
+++ typescript/problems/2693_call_function_with_custom_context.ts
@@ -12,3 +12,19 @@
 
 // Please solve this without using the built-in Function.call method.
 
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+declare global { 
+    interface Function {
+        callPolyfill(context: Record<string, JSONValue>, ...args: JSONValue[]): JSONValue;
+    }
+}
+
+Function.prototype.callPolyfill = function(context, ...args): JSONValue {
+    return this.apply(context ,args);
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
