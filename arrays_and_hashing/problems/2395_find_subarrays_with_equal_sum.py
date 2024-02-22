f6f75b94b62ca039228a1b72118fed17d5d856d8
Red <crabmarket56@gmail.com>
2024-02-22 10:45:51 +0700

    Add init solution

--- arrays_and_hashing/problems/2395_find_subarrays_with_equal_sum.py
+++ arrays_and_hashing/problems/2395_find_subarrays_with_equal_sum.py
@@ -4,3 +4,16 @@
 
 # A subarray is a contiguous non-empty sequence of elements within an array.
 
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+
+        hm = defaultdict(int)
+
+        for i in range(len(nums) - 1):
+            curr = nums[i] + nums[i+1]
+            if curr in hm:
+                return True
+            else:
+                hm[curr] += 1
+        return False
+        
