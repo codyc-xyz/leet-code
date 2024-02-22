588030fe6ad1aab12c5ca2851361a2dab3f895a4
Red <crabmarket56@gmail.com>
2024-02-22 11:06:17 +0700

    Add init solution

--- arrays_and_hashing/problems/1790_check_if_one_string_swap_can_make_strings_equal.py
+++ arrays_and_hashing/problems/1790_check_if_one_string_swap_can_make_strings_equal.py
@@ -2,5 +2,17 @@
 
 # Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
 
- 
-
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        seen = []
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                if len(seen) > 1:
+                    return False
+                else: 
+                    seen.append(i)
+        if len(seen) == 1:
+            return False
+        elif len(seen) == 0:
+            return True
+        return s1[seen[0]] == s2[seen[1]] and s1[seen[1]] == s2[seen[0]]
