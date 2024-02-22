d9f47a533e8a1ed92021c87ac2572fca08f97773
Red <crabmarket56@gmail.com>
2024-02-22 10:32:55 +0700

    Add init solution

--- typescript/problems/2433_remove_letter_to_equalize_frequency.ts
+++ typescript/problems/2433_remove_letter_to_equalize_frequency.ts
@@ -6,3 +6,25 @@
 
 // The frequency of a letter x is the number of times it occurs in the string.
 // You must remove exactly one letter and cannot choose to do nothing.
+
+function equalFrequency(word: string): boolean {
+
+    let res: number[] = new Array(26).fill(0);
+    for (let i = 0; i < word.length; i++) {
+        let idx: number = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        res[idx] += 1
+    }
+
+    for (let i = 0; i < res.length; i++) {
+        if (res[i] > 0) {
+            res[i] -= 1
+            let set = new Set(res)
+            set.delete(0)
+            if (set.size === 1) {
+                return true
+            }
+            res[i] += 1
+        }
+    }
+    return false
+}
