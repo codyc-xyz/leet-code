8b174bd455e500ffd63aa280f75adc28786236a2
Red <crabmarket56@gmail.com>
2024-02-22 08:27:50 +0700

    Add init solution

--- math/problems/1342_number_of_steps_to_reduce_a_number_to_zero.py
+++ math/problems/1342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -2,3 +2,14 @@
 
 # In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        ans = 0
+        while num:
+            if num % 2:
+                num -= 1
+            else:
+                num /= 2
+            ans += 1
+        return ans
+                
