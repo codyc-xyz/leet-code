784dd183eb343ce4ba6d37678bb1f3e510dad27e
Red <crabmarket56@gmail.com>
2024-02-22 11:02:57 +0700

    Add init solution

--- typescript/problems/1790_check_if_one_string_swap_can_make_strings_equal.ts
+++ typescript/problems/1790_check_if_one_string_swap_can_make_strings_equal.ts
@@ -2,3 +2,20 @@
 
 // Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
 
+function areAlmostEqual(s1: string, s2: string): boolean {
+
+    let diffIdx: number[] = []
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] != s2[i]) {
+            diffIdx.push(i)
+        }
+    }
+    if (diffIdx.length === 0) {
+        return true
+    }
+    else if (diffIdx.length === 2 && s1[diffIdx[0]] === s2[diffIdx[1]] && s1[diffIdx[1]] === s2[diffIdx[0]]) {
+        return true
+    }
+    return false
+};
