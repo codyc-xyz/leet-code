490cf70f5fc98163182b3f132f36166347eff3ec
Red <crabmarket56@gmail.com>
2024-02-25 14:33:09 +0700

    Add init solution

--- typescript/problems/2293_min_max_game.ts
+++ typescript/problems/2293_min_max_game.ts
@@ -9,3 +9,19 @@
 // Repeat the entire process starting from step 1.
 // Return the last number that remains in nums after applying the algorithm.
 
+function minMaxGame(nums: number[]): number {
+    
+    while (nums.length > 1) {
+        let newNums: number[] = [];
+        for (let i = 0; i < nums.length / 2; i++) {
+            if (i % 2 === 0) {
+                newNums.push(Math.min(nums[2*i], nums[2*i+1]))
+            }
+            else {
+                newNums.push(Math.max(nums[2*i], nums[2*i+1]))
+            }
+        }
+        nums = newNums
+    }
+    return nums[0]
+};
