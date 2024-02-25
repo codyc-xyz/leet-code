2e80a0c63000055e360ff418814e3b95975eaaad
Red <crabmarket56@gmail.com>
2024-02-25 14:28:57 +0700

    Add init solution

--- simulation/problems/2283_min_max_game.py
+++ simulation/problems/2283_min_max_game.py
@@ -9,3 +9,16 @@
 # Repeat the entire process starting from step 1.
 # Return the last number that remains in nums after applying the algorithm.
 
+class Solution:
+    def minMaxGame(self, nums: List[int]) -> int:
+        N = len(nums)
+        while N > 1:
+            newNums = []
+            for i in range(len(nums) // 2):
+                if not i % 2:
+                    newNums.append(min(nums[2*i], nums[2*i+1]))
+                else:
+                    newNums.append(max(nums[2*i], nums[2*i+1]))
+            nums = newNums
+            N = len(nums)
+        return nums[0]
