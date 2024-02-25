e2280a497774e68f817f6e80f0bc64a7d92d7312
Red <crabmarket56@gmail.com>
2024-02-25 14:54:58 +0700

    Add init solution

--- arrays_and_hashing/problems/1640_check_array_formation_through_concatenation.py
+++ arrays_and_hashing/problems/1640_check_array_formation_through_concatenation.py
@@ -2,5 +2,23 @@
 
 # Return true if it is possible to form the array arr from pieces. Otherwise, return false.
 
- 
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        
+        complete = [False for _ in range(len(arr))]
+        
+        i = 0
+        while i < len(pieces):
+            if len(pieces[i]) > 1:
+                for j in range(len(arr) - len(pieces[i]) + 1):
+                    for k in range(j+len(pieces[i]) - 1, len(arr) + 1):
+                        if arr[j:k] == pieces[i]:
+                            for q in range(j, k):
+                                complete[q] = True
 
+            else:
+                if pieces[i][0] in arr:
+                    idx = arr.index(pieces[i][0])
+                    complete[idx] = True
+            i += 1
+        return all(complete) == True
