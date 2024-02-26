8efbe6e7ea75d6e51bfe9ffbe69a29452e1187fa
Red <crabmarket56@gmail.com>
2024-02-26 14:36:42 +0700

    Add init solution

--- simulation/problems/2138_divide_a_string_into_groups_of_size_k.py
+++ simulation/problems/2138_divide_a_string_into_groups_of_size_k.py
@@ -6,3 +6,13 @@
 
 # Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
 
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        
+        ans = []
+
+        for i in range(0, len(s), k):
+            ans.append(s[i:i+k])
+        while len(ans[-1]) < k:
+            ans[-1] += fill
+        return ans
