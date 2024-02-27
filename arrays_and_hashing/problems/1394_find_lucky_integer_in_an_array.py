6c14d83cccdb5d86b0fc65ba57fe65c41251fcc0
Red <crabmarket56@gmail.com>
2024-02-27 12:40:47 +0700

    Add init solution

--- arrays_and_hashing/problems/1394_find_lucky_integer_in_an_array.py
+++ arrays_and_hashing/problems/1394_find_lucky_integer_in_an_array.py
@@ -2,3 +2,14 @@
 
 # Return the largest lucky integer in the array. If there is no lucky integer return -1.
 
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+
+        count = collections.Counter(arr)
+        ans = []
+        for c in sorted(count, reverse=True):
+            if c == count[c]:
+                return c
+        return -1
+        
+        
