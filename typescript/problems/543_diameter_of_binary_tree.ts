7f9ffb3ac6008ca7e32453d731027c02cb8867c3
Red <crabmarket56@gmail.com>
2024-02-27 12:30:43 +0700

    Add init solution

--- typescript/problems/543_diameter_of_binary_tree.ts
+++ typescript/problems/543_diameter_of_binary_tree.ts
@@ -4,3 +4,32 @@
 
 // The length of a path between two nodes is represented by the number of edges between them.
 
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let ans: number = 0;
+    function dfs(n: TreeNode): number {
+        if (n === null) {
+            return 0
+        }
+        const l = dfs(n.left);
+        const r = dfs(n.right);
+        ans = Math.max(ans, l+r);
+        return Math.max(l, r) + 1
+    }
+    dfs(root)
+    return ans
+    
+};
