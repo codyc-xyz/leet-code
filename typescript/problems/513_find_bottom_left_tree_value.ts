12bdde9a616a325079520e58456ffba22692db92
Red <crabmarket56@gmail.com>
2024-02-28 11:10:16 +0700

    Add init solution

--- typescript/problems/513_find_bottom_left_tree_value.ts
+++ typescript/problems/513_find_bottom_left_tree_value.ts
@@ -1,2 +1,33 @@
 // Given the root of a binary tree, return the leftmost value in the last row of the tree.
 
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function findBottomLeftValue(root: TreeNode | null): number {
+    
+    let dq: TreeNode[] = [root];
+    let ans: number;
+    while (dq.length > 0) {
+        let curr: TreeNode = dq.shift();
+        ans = curr.val
+        if (curr.right !== null) {
+            dq.push(curr.right)
+        }
+        if (curr.left !== null) {
+            dq.push(curr.left)
+        }
+    }
+    return ans
+
+};
