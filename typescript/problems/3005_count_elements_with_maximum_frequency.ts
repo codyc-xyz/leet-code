82d5d6ed0420022245a8f2a9da681652df0d72e0
Red <crabmarket56@gmail.com>
2024-02-29 10:36:37 +0700

    Add init solution

--- typescript/problems/3005_count_elements_with_maximum_frequency.ts
+++ typescript/problems/3005_count_elements_with_maximum_frequency.ts
@@ -5,3 +5,23 @@ Return the total frequencies of elements in nums such that those elements all ha
 
 The frequency of an element is the number of occurrences of that element in the array.
  */
+
+function maxFrequencyElements(nums: number[]): number {
+    
+    let count: Record<number, number> = {};
+    let maxCount: number = 0;
+    let ans: number = 0;
+    for (const n of nums) {
+        if (count[n] === undefined) {
+            count[n] = 0
+        }
+        count[n] += 1
+        maxCount = Math.max(maxCount, count[n])
+    }
+    for (const val of Object.values(count)) {
+        if (val === maxCount) {
+            ans += maxCount
+        }
+    }
+    return ans
+};
