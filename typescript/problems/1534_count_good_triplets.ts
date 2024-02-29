912823538a3f6239fc3ee89a517a580afdb20961
Red <crabmarket56@gmail.com>
2024-02-29 10:53:31 +0700

    Add init solution

--- typescript/problems/1534_count_good_triplets.ts
+++ typescript/problems/1534_count_good_triplets.ts
@@ -11,3 +11,18 @@ Where |x| denotes the absolute value of x.
 
 Return the number of good triplets.
  */
+
+function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
+    let ans: number = 0;
+    for (let i: number = 0; i < arr.length - 2; i++) {
+        for (let j: number = i+1; j < arr.length - 1; j++) {
+            for (let k: number = j+1; k < arr.length; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                    ans++
+                }
+            }
+        }
+    }
+    return ans
+
+};
