d4b5893b60bcbed8fed0b3832cd5e95f222f0aa5
Red <crabmarket56@gmail.com>
2024-02-29 10:57:26 +0700

    Add innit solution

--- typescript/problems/1295_find_numbers_with_even_number_of_digits.ts
+++ typescript/problems/1295_find_numbers_with_even_number_of_digits.ts
@@ -1,3 +1,13 @@
 /**
   Given an array nums of integers, return how many of them contain an even number of digits.
  */
+
+function findNumbers(nums: number[]): number {
+    let ans: number = 0;
+    for (const n of nums) {
+        if (String(n).length % 2 === 0) {
+            ans++
+        }
+    }
+    return ans
+};
