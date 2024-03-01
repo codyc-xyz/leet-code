7525bac5dbce350064214356fcc7b23946a30eb0
Red <crabmarket56@gmail.com>
2024-03-01 18:22:36 +0700

    Add init solution

--- typescript/problems/2864_maximum_odd_binary_number.ts
+++ typescript/problems/2864_maximum_odd_binary_number.ts
@@ -8,3 +8,29 @@ Return a string representing the maximum odd binary number that can be created f
 Note that the resulting string can have leading zeros.
 
  */
+
+function maximumOddBinaryNumber(s: string): string {
+    let count: Record<string, number> = {};
+    let ans: string = "";
+    for (const c of s) {
+        if (c in count) {
+            count[c] += 1
+        }
+        else {
+            count[c] = 1;
+        }
+    }
+    while (count['1'] > 1) {
+        ans += '1'
+        count['1'] -= 1
+    }
+    if ('0' in count) {
+        while (count['0'] > 0) {
+            ans += '0'
+            count['0'] -= 1
+        }
+
+    }
+    return ans + '1'
+
+};
