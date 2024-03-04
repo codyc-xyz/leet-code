c5a7ec49a57abe81baf1b1da7ef63a83d1b5fff5
Red <crabmarket56@gmail.com>
2024-03-04 14:19:20 +0700

    Add alternate solution

--- typescript/problems/977_squares_of_a_sorted_array.ts
+++ typescript/problems/977_squares_of_a_sorted_array.ts
@@ -11,3 +11,22 @@ function sortedSquares(nums: number[]): number[] {
     ans.sort((a, b) => a - b);
     return ans
 };
+
+function sortedSquares(nums: number[]): number[] {
+    let ans: number[] = [];
+    let l: number = 0;
+    let r: number = nums.length - 1;
+
+    while (l <= r) {
+        if (Math.abs(nums[l]) > Math.abs(nums[r])) {
+            ans.unshift(nums[l]**2)
+            l++
+        }
+        else {
+            ans.unshift(nums[r]**2)
+            r--
+        }
+    }
+    return ans
+
+}
