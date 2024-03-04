6cef589712bb66ef1bc1bc64d19efacaa9118884
Red <crabmarket56@gmail.com>
2024-03-04 14:11:36 +0700

    Add init solution

--- arrays_and_hashing/problems/977_squares_of_a_sorted_array.py
+++ arrays_and_hashing/problems/977_squares_of_a_sorted_array.py
@@ -1,3 +1,11 @@
 '''
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 '''
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for n in nums:
+            ans.append(n**2)
+        return sorted(ans)
