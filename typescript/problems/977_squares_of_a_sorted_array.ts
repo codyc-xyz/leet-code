01abbbc4df5e51f0c7a8cf7229e1828f67acfd55
Red <crabmarket56@gmail.com>
2024-03-04 14:13:31 +0700

    Add init solution

--- typescript/problems/977_squares_of_a_sorted_array.ts
+++ typescript/problems/977_squares_of_a_sorted_array.ts
@@ -1,3 +1,13 @@
 /**
  Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
  */
+
+function sortedSquares(nums: number[]): number[] {
+    let ans: number[] = [];
+
+    for (const n of nums) {
+        ans.push(n**2)
+    }
+    ans.sort((a, b) => a - b);
+    return ans
+};
