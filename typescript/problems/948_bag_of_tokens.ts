8baa4f387c22cd58f96728efe556dfeca71fcfc1
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 11:40:07 +0700

    Add init solution

--- typescript/problems/948_bag_of_tokens.ts
+++ typescript/problems/948_bag_of_tokens.ts
@@ -7,3 +7,28 @@ Face-up: If your current power is at least tokens[i], you may play tokeni, losin
 Face-down: If your current score is at least 1, you may play tokeni, gaining tokens[i] power and losing 1 score.
 Return the maximum possible score you can achieve after playing any number of tokens.
  */
+
+
+function bagOfTokensScore(tokens: number[], power: number): number {
+    let l: number = 0;
+    let r: number = tokens.length - 1;
+    let score: number = 0;
+    tokens.sort((a, b) => a - b);
+
+    while (l <= r) {
+        if (tokens[l] <= power) {
+            power -= tokens[l]
+            l++
+            score++;
+        }
+        else if (r - l > 1 && score > 0) {
+            power += tokens[r]
+            r--
+            score--
+        }
+        else {
+            break
+        }
+    }
+    return score
+};
