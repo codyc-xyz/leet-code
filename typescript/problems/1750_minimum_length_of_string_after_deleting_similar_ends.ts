9cae08566de8c7b2688341235ccb070e317e0245
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 11:48:47 +0700

    Add init solution

--- typescript/problems/1750_minimum_length_of_string_after_deleting_similar_ends.ts
+++ typescript/problems/1750_minimum_length_of_string_after_deleting_similar_ends.ts
@@ -8,3 +8,27 @@ The characters from the prefix and suffix must be the same.
 Delete both the prefix and the suffix.
 Return the minimum length of s after performing the above operation any number of times (possibly zero times).
  */
+
+
+function minimumLength(s: string): number {
+    let l: number = 0;
+    let r: number = s.length - 1;
+    let ans: number = s.length;
+
+    while (l < r) {
+        if (s[l] === s[r]) {
+            while (s[l] === s[r]) {
+                l += 1
+                ans -= 1
+            }
+            while (l - 1 < r && s[r] === s[l-1]) {
+                r -= 1
+                ans -= 1
+            }
+        }
+        else {
+            break
+        }
+    }
+    return ans
+};
