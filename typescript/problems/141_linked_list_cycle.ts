b36d334a0f1df3ccc2cc2199112fe347e528d8ab
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 12:27:02 +0700

    Add init solution

--- typescript/problems/141_linked_list_cycle.ts
+++ typescript/problems/141_linked_list_cycle.ts
@@ -5,3 +5,29 @@ There is a cycle in a linked list if there is some node in the list that can be 
 
 Return true if there is a cycle in the linked list. Otherwise, return false.
  */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    let curr1: ListNode | null = head
+    let curr2: ListNode | null = head
+
+    while (curr2 && curr2.next) {
+        curr1 = curr1.next
+        curr2 = curr2.next.next
+        if (curr1 === curr2) {
+            return true
+        }
+    }
+    return false
+};
