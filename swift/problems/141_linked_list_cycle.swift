a0faa6ae324f0c8a9f00b9f0b8bc5b9de87c2501
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 12:31:42 +0700

    Add init solution

--- swift/problems/141_linked_list_cycle.swift
+++ swift/problems/141_linked_list_cycle.swift
@@ -5,3 +5,31 @@ There is a cycle in a linked list if there is some node in the list that can be 
 
 Return true if there is a cycle in the linked list. Otherwise, return false.
 */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var curr1 = head
+        var curr2 = head
+
+        while curr2 != nil && curr2?.next != nil {
+            curr1 = curr1?.next
+            curr2 = curr2?.next?.next
+            if curr1 === curr2 {
+                return true
+            }
+        }
+        return false
+    }
+}
