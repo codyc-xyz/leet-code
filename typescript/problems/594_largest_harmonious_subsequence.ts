c43450e8e377ca5bbc10ba0f941df358ab9eeaef
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 16:13:28 +0700

    Add init solution

--- typescript/problems/594_largest_harmonious_subsequence.ts
+++ typescript/problems/594_largest_harmonious_subsequence.ts
@@ -6,3 +6,27 @@ Given an integer array nums, return the length of its longest harmonious subsequ
 A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
 */
 
+function findLHS(nums: number[]): number {
+    let ans: number = 0
+    let count: Record<number, number> = {}
+
+    for (const n of nums) {
+        if (count[n] === undefined) {
+            count[n] = 0
+        }
+        count[n]++
+    }
+    const keys: string[] = Object.keys(count)
+    for (const key of keys) {
+        const curr = count[key]
+        const minusOne = String(Number(key) - 1)
+        const plusOne = String(Number(key) + 1)
+        if (count[minusOne] !== undefined) {
+            ans = Math.max(ans, curr + count[minusOne])
+        }
+        if (count[plusOne] !== undefined) {
+            ans = Math.max(ans, curr + count[plusOne])
+        }
+    }
+    return ans
+};
