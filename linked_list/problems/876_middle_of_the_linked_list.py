35c0a331262be51941dab4ca6391bd872cd9b92b
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 09:39:28 +0700

    Add init solution

--- linked_list/problems/876_middle_of_the_linked_list.py
+++ linked_list/problems/876_middle_of_the_linked_list.py
@@ -3,3 +3,19 @@ Given the head of a singly linked list, return the middle node of the linked lis
 
 If there are two middle nodes, return the second middle node.
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        i = 0
+        res = []
+        while head:
+            res.append(head)
+            i+=1
+            head = head.next
+        return res[i//2]
