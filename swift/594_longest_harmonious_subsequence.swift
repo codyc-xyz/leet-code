c9d0a63c187ed019828b7e0f85e9900e49d6aa84
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 09:36:33 +0700

    Add init solution

--- swift/594_longest_harmonious_subsequence.swift
+++ swift/594_longest_harmonious_subsequence.swift
@@ -5,3 +5,24 @@ Given an integer array nums, return the length of its longest harmonious subsequ
 
 A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
 **/
+
+class Solution {
+    func findLHS(_ nums: [Int]) -> Int {
+        var ans: Int = 0
+        var count = [Int: Int]()
+
+        for n in nums {
+            count[n, default: 0]+=1
+        }
+    
+        for (num, cnt) in count {
+            if let nextCount = count[num+1] {
+                ans = max(ans, cnt + nextCount)
+            }
+            if let prevCount = count[num-1] {
+                ans = max(ans, cnt + prevCount)
+            }
+        }
+        return ans
+    }
+}
