ff2a0127efc9f2738f6986a3d44a7b0f4ee87f38
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 09:40:20 +0700

    Add alternate solution

--- linked_list/problems/876_middle_of_the_linked_list.py
+++ linked_list/problems/876_middle_of_the_linked_list.py
@@ -19,3 +19,13 @@ class Solution:
             i+=1
             head = head.next
         return res[i//2]
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
