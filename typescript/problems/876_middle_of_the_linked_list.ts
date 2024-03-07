866e629a026e94c3d045a3e597b622074cfde5d8
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 09:42:45 +0700

    Add init solution

--- typescript/problems/876_middle_of_the_linked_list.ts
+++ typescript/problems/876_middle_of_the_linked_list.ts
@@ -3,3 +3,28 @@ Given the head of a singly linked list, return the middle node of the linked lis
 
 If there are two middle nodes, return the second middle node.
  */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+
+    let fast: ListNode = head
+    let slow: ListNode = head
+
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+    }
+    return slow
+    
+};
