804e1c2059b944fcba33de4edf46c18e09adf27b
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 13:05:28 +0700

    Add init solution

--- swift/problems/2651_calculate_delayed_arrival_time.swift
+++ swift/problems/2651_calculate_delayed_arrival_time.swift
@@ -5,3 +5,13 @@ Return the time when the train will arrive at the station.
 
 Note that the time in this problem is in 24-hours format.
 **/
+
+class Solution {
+    func findDelayedArrivalTime(_ arrivalTime: Int, _ delayedTime: Int) -> Int {
+        let res: Int = arrivalTime + delayedTime
+        if res >= 24 {
+            return res - 24
+        }
+        return res
+    }
+}
