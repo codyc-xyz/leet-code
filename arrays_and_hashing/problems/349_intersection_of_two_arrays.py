6fccd34117f4ae5871327b41a911aa1bac5c1c07
codyc-xyz <crabmarket56@gmail.com>
2024-03-11 16:50:59 +0700

    Add init solution

--- arrays_and_hashing/problems/349_intersection_of_two_arrays.py
+++ arrays_and_hashing/problems/349_intersection_of_two_arrays.py
@@ -1,1 +1,13 @@
 # Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        N1 = set(nums1)
+        N2 = set(nums2)
+        ans = []
+        for n in N1:
+            if n in N2:
+                ans.append(n)
+        return ans
+        
+        
