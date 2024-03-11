b211ca12bb597181e4653e4174bedb332dcd6a5c
codyc-xyz <crabmarket56@gmail.com>
2024-03-11 16:54:20 +0700

    Add init solution

--- typescript/problems/349_intersection_of_two_arrays.ts
+++ typescript/problems/349_intersection_of_two_arrays.ts
@@ -1,3 +1,15 @@
 /**
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
  */
+
+function intersection(nums1: number[], nums2: number[]): number[] {
+    const N1: Set<number> = new Set(nums1);
+    const N2: Set<number> = new Set(nums2);
+    let ans: number[] = [];
+    for (const n of N1) {
+        if (N2.has(n)) {
+            ans.push(n)
+        }
+    }
+    return ans
+};
