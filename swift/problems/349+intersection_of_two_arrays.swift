c3353fb8cb9969b2953c04e6c213c9fdb83296ea
codyc-xyz <crabmarket56@gmail.com>
2024-03-11 17:02:28 +0700

    add init solution

--- swift/problems/349+intersection_of_two_arrays.swift
+++ swift/problems/349+intersection_of_two_arrays.swift
@@ -1,3 +1,18 @@
 /**
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 **/
+
+class Solution {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        let N1: Set<Int> = Set(nums1)
+        let N2: Set<Int> = Set(nums2)
+        var ans: [Int] = []
+
+        for n in N1 {
+            if N2.contains(n) {
+                ans.append(n)
+            }
+        }
+        return ans
+    }
+}
