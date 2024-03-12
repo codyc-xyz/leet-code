9c96af8d7c4406c4dcd9c084a3f87f5539c7c9ae
codyc-xyz <crabmarket56@gmail.com>
2024-03-12 10:40:30 +0700

    Add init solution

--- typescript/problems/1171_remove_zero_sum_consecutive_nodes_from_linked_list.ts
+++ typescript/problems/1171_remove_zero_sum_consecutive_nodes_from_linked_list.ts
@@ -4,5 +4,40 @@ Given the head of a linked list, we repeatedly delete consecutive sequences of n
 After doing so, return the head of the final linked list.  You may return any such answer.
 
 (Note that in the examples below, all sequences are serializations of ListNode objects.)
-
  */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeZeroSumSublists(head: ListNode | null): ListNode | null {
+
+    let curr: ListNode | null = new ListNode(0, head);
+    let dummy: ListNode | null = curr;
+    let hm: Record<number, ListNode> = {}
+    let currSum: number = 0;
+    while (curr != null) {
+        currSum += curr.val
+        hm[currSum] = curr
+        curr = curr.next
+    }
+
+    curr = dummy
+    currSum = 0
+
+    while (curr != null) {
+        currSum += curr.val
+        curr.next = hm[currSum].next
+        curr = curr.next
+    }
+    return dummy.next
+    
+};
