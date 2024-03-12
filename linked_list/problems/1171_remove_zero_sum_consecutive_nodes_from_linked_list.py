2bf6b7f4a59ec37817433c6083471fb82d2220b8
codyc-xyz <crabmarket56@gmail.com>
2024-03-12 10:32:36 +0700

    Add init solution

--- linked_list/problems/1171_remove_zero_sum_consecutive_nodes_from_linked_list.py
+++ linked_list/problems/1171_remove_zero_sum_consecutive_nodes_from_linked_list.py
@@ -3,3 +3,27 @@ Given the head of a linked list, we repeatedly delete consecutive sequences of n
 
 After doing so, return the head of the final linked list.  You may return any such answer.
 '''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        hm = {}
+        currSum = 0
+        curr = dummy = ListNode(next=head)
+
+        while curr:
+            currSum += curr.val
+            hm[currSum] = curr
+            curr = curr.next
+
+        currSum = 0
+        curr = dummy
+        while curr:
+            currSum += curr.val
+            curr.next = hm[currSum].next
+            curr = curr.next
+        return dummy.next
