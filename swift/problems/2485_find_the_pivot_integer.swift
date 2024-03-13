4a14a50a7ff240e8858b96c0f40768dd40d75145
codyc-xyz <crabmarket56@gmail.com>
2024-03-13 14:19:21 +0700

    Add init solution

--- swift/problems/2485_find_the_pivot_integer.swift
+++ swift/problems/2485_find_the_pivot_integer.swift
@@ -4,3 +4,20 @@ Given a positive integer n, find the pivot integer x such that:
 The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
 Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
 **/
+
+class Solution {
+    func pivotInteger(_ n: Int) -> Int {
+        var pSum: [Int] = [0]
+
+        for i in 1...n {
+            pSum.append(pSum.last! + i)
+        }
+
+        for i in 1...pSum.count-1 {
+            if pSum[i] == pSum.last! - pSum[i-1] {
+                return i
+            }
+        }
+        return -1
+    }
+}
