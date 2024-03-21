d90ce6a010ff186bd2e37a1fdf386879b594a098
codyc-xyz <crabmarket56@gmail.com>
2024-03-21 10:41:02 +0700

    Add init solution

--- typescript/problems/206_reverse_linked_list.ts
+++ typescript/problems/206_reverse_linked_list.ts
@@ -2,3 +2,28 @@
 Given the head of a singly linked list, reverse the list, and return the reversed list.
  */
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+
+    let prev: ListNode | null = null
+
+    while (head !== null) {
+        let nxt: ListNode | null = head.next;
+        head.next = prev
+        prev = head
+        head = nxt
+    }
+    return prev
+    
+};
