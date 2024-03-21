c5a7fb3b3ce907781212df1e33d68db8a2dff935
codyc-xyz <crabmarket56@gmail.com>
2024-03-21 10:44:27 +0700

    Add init solution

--- swift/problems/206_reverse_linked_list.swift
+++ swift/problems/206_reverse_linked_list.swift
@@ -1,3 +1,27 @@
 /**
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 **/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode? = nil
+        var curr: ListNode? = head
+        while curr != nil {
+            var nxt: ListNode? = curr?.next
+            curr?.next = prev
+            prev = curr
+            curr = nxt
+        }
+        return prev
+    }
+}
