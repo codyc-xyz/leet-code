e731f2f6849c238d91256e9bb1c4ee615fe70e3b
codyc-xyz <crabmarket56@gmail.com>
2024-03-25 13:11:48 +0700

    Add init solution

--- arrays_and_hashing/problems/442_find_all_duplicates_in_an_array.py
+++ arrays_and_hashing/problems/442_find_all_duplicates_in_an_array.py
@@ -3,3 +3,13 @@ Given an integer array nums of length n where all the integers of nums are in th
 
 You must write an algorithm that runs in O(n) time and uses only constant extra space.
 '''
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        seen = {}
+        ans = []
+        for n in nums:
+            if n in seen:
+                ans.append(n)
+            seen[n] = True
+        return ans
