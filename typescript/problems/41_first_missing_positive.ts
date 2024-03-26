72123b33001aa00ebf465b8744f96a4c20d3a317
codyc-xyz <crabmarket56@gmail.com>
2024-03-26 09:47:00 +0700

    Add init solution

--- typescript/problems/41_first_missing_positive.ts
+++ typescript/problems/41_first_missing_positive.ts
@@ -3,3 +3,25 @@ Given an unsorted integer array nums. Return the smallest positive integer that 
 
 You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
  */
+
+function firstMissingPositive(nums: number[]): number {
+    nums.sort((a, b) => a - b)
+
+    let j: number = 1;
+    let prev: number = -1;
+
+    for (const n of nums) {
+        if (n <= 0 || n === prev) {
+            continue
+        }
+        if (n != j) {
+            return j
+        }
+        j += 1
+        prev = n
+    }
+    if (nums[nums.length - 1] === j) {
+        return j + 1
+    }
+    return j
+};
